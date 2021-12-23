8efb2fe116ddf52d4e690cfe777d5f9622c3a7b6
Jamie <ijamespine@me.com>
2021-12-23 00:25:35 -0800

    upgrade swift-rs

--- src-tauri/swift-lib/Package.swift
+++ src-tauri/swift-lib/Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "SwiftRs", url: "https://github.com/Brendonovich/swift-rs", from: "0.2.2")
+        .package(name: "SwiftRs", url: "https://github.com/Brendonovich/swift-rs", from: "0.2.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
