fd745a4d36fbd866b46166ffa83817d8c16baeab
Jamie <ijamespine@me.com>
2021-12-24 04:24:12 -0800

    smashing errors

--- packages/core/lib/main.rs
+++ packages/core/lib/main.rs
@@ -1,3 +1,30 @@
+use once_cell::sync::OnceCell;
+use serde::{Deserialize, Serialize};
+
+mod crypto;
+mod db;
+mod file;
+mod util;
+
+// static configuration
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AppConfig {
+    pub data_dir: std::path::PathBuf,
+    pub primary_db: std::path::PathBuf,
+    pub file_type_thumb_dir: std::path::PathBuf,
+}
+pub static CONFIG: OnceCell<AppConfig> = OnceCell::new();
+
+pub fn configure(data_dir: std::path::PathBuf) {
+    let config = AppConfig {
+        data_dir: data_dir.clone(),
+        primary_db: data_dir.clone().join("primary.db3"),
+        file_type_thumb_dir: data_dir.clone().join("file_icons"),
+    };
+    CONFIG.set(config).unwrap();
+}
+
 fn main() {
+    // hello!
     println!("Hello, world!");
 }
