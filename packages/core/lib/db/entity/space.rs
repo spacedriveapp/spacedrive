836178ca45c2f374b7ea13967c60577d5c8d170c
Jamie <ijamespine@me.com>
2021-12-24 22:51:13 -0800

    better relations

--- packages/core/lib/db/entity/space.rs
+++ packages/core/lib/db/entity/space.rs
@@ -8,17 +8,24 @@ use serde::{Deserialize, Serialize};
 #[sea_orm(table_name = "spaces")]
 // -------------------------------------
 pub struct Model {
-  #[sea_orm(primary_key)]
-  pub id: u32,
-  pub name: String,
-  pub calculated_size_in_bytes: Option<String>,
-  pub calculated_file_count: Option<u32>,
-  pub library_id: String,
-  pub date_created: Option<NaiveDateTime>,
-  pub date_modified: Option<NaiveDateTime>,
+    #[sea_orm(primary_key)]
+    pub id: u32,
+    pub name: String,
+    pub calculated_size_in_bytes: Option<String>,
+    pub calculated_file_count: Option<u32>,
+    pub library_id: String,
+    pub date_created: Option<NaiveDateTime>,
+    pub date_modified: Option<NaiveDateTime>,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::library::Entity",
+        from = "Column::LibraryId",
+        to = "super::library::Column::Id"
+    )]
+    Library,
+}
 
 impl ActiveModelBehavior for ActiveModel {}
