da002c479d0cc4a294e68e2ccb97bba2d0f49990
Brendan Allan <brendonovich@outlook.com>
2022-03-30 18:04:58 +0800

    feat: migrate from yarn to pnpm

--- packages/core/bindings/JobReport.ts
+++ packages/core/bindings/JobReport.ts
@@ -1,3 +1,3 @@
 import type { JobStatus } from "./JobStatus";
 
-export interface JobReport { id: string, date_created: string, date_modified: string, status: JobStatus, task_count: bigint, completed_task_count: bigint, message: string, seconds_elapsed: string, }
+export interface JobReport { id: string, date_created: string, date_modified: string, status: JobStatus, task_count: number, completed_task_count: number, message: string, seconds_elapsed: string, }
