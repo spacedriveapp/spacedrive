abc72a18b013ad691221393c834afeefdea0fcbd
Jamie Pine <32987599+jamiepine@users.noreply.github.com>
2022-03-31 22:37:18 -0700

    fix first time library creation

--- core/src/db/mod.rs
+++ core/src/db/mod.rs
@@ -9,22 +9,18 @@ pub enum DatabaseError {
 	MissingConnection,
 	#[error("Unable find current_library in the client config")]
 	MalformedConfig,
-	#[error("Unable to initialise the Prisma client")]
+	#[error("Unable to initialize the Prisma client")]
 	ClientError(#[from] prisma::NewClientError),
 }
 
 pub async fn create_connection() -> Result<PrismaClient, DatabaseError> {
 	let config = state::client::get();
 
-	let current_library = config
-		.libraries
-		.iter()
-		.find(|l| l.library_uuid == config.current_library_uuid)
-		.ok_or(DatabaseError::MalformedConfig)?;
+	let current_library = config.get_current_library();
 
 	let path = current_library.library_path.clone();
-	// TODO: Error handling when brendan adds it to prisma-client-rust
 
 	let client = prisma::new_client_with_url(&format!("file:{}", &path)).await?;
+
 	Ok(client)
 }
