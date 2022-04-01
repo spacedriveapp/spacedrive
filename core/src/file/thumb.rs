6ef869fe69e67f48805aec30283c1e3dfaba8879
Jamie Pine <32987599+jamiepine@users.noreply.github.com>
2022-04-01 07:28:09 -0700

    fixed indexer time + added thumbnail generation

--- core/src/file/thumb.rs
+++ core/src/file/thumb.rs
@@ -1,5 +1,7 @@
+use crate::job::jobs::JobReportUpdate;
 use crate::prisma::FilePathData;
 use crate::state::client;
+use crate::sys;
 use crate::{
 	job::{jobs::Job, worker::WorkerContext},
 	prisma::FilePath,
@@ -8,6 +10,7 @@ use crate::{
 use anyhow::Result;
 use image::*;
 use prisma_client_rust::or;
+use std::fs;
 use std::path::Path;
 use webp::*;
 
@@ -16,26 +19,58 @@ pub struct ThumbnailJob {
 	pub location_id: i32,
 }
 
-static THUMBNAIL_SIZE_FACTOR: f32 = 1.0;
+static THUMBNAIL_SIZE_FACTOR: f32 = 0.2;
 static THUMBNAIL_QUALITY: f32 = 30.0;
 static CACHE_DIR_NAME: &str = "thumbnails";
 
 #[async_trait::async_trait]
 impl Job for ThumbnailJob {
 	async fn run(&self, ctx: WorkerContext) -> Result<()> {
+		let config = client::get();
 		let core_ctx = ctx.core_ctx.clone();
+
+		let location = sys::locations::get_location(&core_ctx, self.location_id).await?;
+
+		fs::create_dir_all(
+			Path::new(&config.data_path)
+				.join(CACHE_DIR_NAME)
+				.join(format!("{}", self.location_id)),
+		)
+		.unwrap();
+
+		let root_path = location.path.unwrap();
+
 		let image_files = get_images(&core_ctx, self.location_id).await?;
+
+		let location_id = location.id.clone();
 
 		println!("Found {:?} files", image_files.len());
 
-		for image_file in image_files {
-			generate_thumbnail(
-				&image_file.materialized_path,
-				&image_file.extension.unwrap(),
-				self.location_id,
-			)
-			.unwrap();
-		}
+		tokio::task::spawn_blocking(move || {
+			ctx.progress(vec![
+				JobReportUpdate::TaskCount(image_files.len()),
+				JobReportUpdate::Message(format!(
+					"Preparing to process {} files",
+					image_files.len()
+				)),
+			]);
+
+			for (i, image_file) in image_files.iter().enumerate() {
+				ctx.progress(vec![JobReportUpdate::Message(format!(
+					"Processing {}",
+					image_file.materialized_path.clone()
+				))]);
+				let path = format!("{}{}", root_path, image_file.materialized_path);
+				let checksum = image_file.temp_checksum.as_ref().unwrap();
+
+				generate_thumbnail(&path, checksum, location_id).unwrap();
+
+				ctx.progress(vec![JobReportUpdate::CompletedTaskCount(i + 1)]);
+			}
+		})
+		.await
+		.unwrap();
+
 		Ok(())
 	}
 }
@@ -67,6 +102,9 @@ pub fn generate_thumbnail(
 		.join(format!("{}", location_id))
 		.join(file_hash)
 		.with_extension("webp");
+
+	println!("Writing to {}", output_path.display());
+
 	std::fs::write(&output_path, &*webp).unwrap();
 
 	Ok(())
--- core/src/util/mod.rs
+++ core/src/util/mod.rs
@@ -1,1 +1,1 @@
-pub mod time;
+
