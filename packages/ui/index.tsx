c27c59da538677019686ed327fd97598be999549
Jamie Pine <32987599+jamiepine@users.noreply.github.com>
2022-03-31 20:18:25 -0700

    fix a bunch of dependency issues & remove web

--- packages/ui/index.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from "react";
-
-export * from "./primitive";
-// export * from "./screens";
---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+
+export * from './primitive';
+// export * from "./screens";
