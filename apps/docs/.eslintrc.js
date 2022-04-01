19d5036cce360c81ff2854d7fb6aad32d604a787
Jamie Pine <32987599+jamiepine@users.noreply.github.com>
2022-03-31 17:08:19 -0700

    remove docs app

--- apps/docs/.eslintrc.js
+++
@@ -1,1 +0,0 @@
-module.exports = require("@calcom/config/eslint-preset");
--- apps/docs/next.config.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.js',
-  unstable_staticImage: true
-});
-
-module.exports = withNextra();
