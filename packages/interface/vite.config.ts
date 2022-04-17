70de8ab1b1e69381da2cf733af33d27d5a721438
Jamie Pine <ijamespine@me.com>
2022-04-17 11:59:28 -0700

    god knows

--- packages/interface/vite.config.ts
+++ packages/interface/vite.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src', 'index.ts'),
-      formats: ['es', 'cjs'],
+      formats: ['es'],
       fileName: (ext) => `index.${ext}.js`
     },
     outDir: path.resolve(__dirname, 'dist'),
