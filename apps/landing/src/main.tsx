8938584a70806be08b225580199ca588660028c1
Jamie Pine <ijamespine@me.com>
2022-04-24 11:24:54 -0700

    working static landing

--- apps/landing/src/main.tsx
+++ apps/landing/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import '@sd/ui/style';
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import './style.scss';
+import '@sd/ui/style';
+import routes from '~react-pages';
+
+// eslint-disable-next-line no-console
+console.log(routes);
+
+function App() {
+  return <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>;
+}
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
--- apps/landing/vite.config.ts
+++ apps/landing/vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // @ts-expect-error
 import svg from 'vite-plugin-svgr';
-import ssr from 'vite-plugin-ssr/plugin';
+import pages from 'vite-plugin-pages';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   // @ts-ignore
-  plugins: [react(), svg({ svgrOptions: { icon: true } }), ssr()],
+  plugins: [react(), svg({ svgrOptions: { icon: true } }), pages({ dirs: 'src/pages' })],
   server: {
     port: 8003
   },
