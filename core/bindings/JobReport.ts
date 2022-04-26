5bf08e0199965655081c0e810873bc3512f96cf6
Jamie Pine <ijamespine@me.com>
2022-04-25 22:45:48 -0700

    test deploy, commit core ts bindings

---
+++ core/bindings/JobReport.ts
@@ -0,0 +1,3 @@
+import type { JobStatus } from "./JobStatus";
+
+export interface JobReport { id: string, date_created: string, date_modified: string, status: JobStatus, task_count: number, completed_task_count: number, message: string, seconds_elapsed: string, }
---
+++ core/bindings/LocationResource.ts
@@ -0,0 +1,2 @@
+
+export interface LocationResource { id: number, name: string | null, path: string | null, total_capacity: number | null, available_capacity: number | null, is_removable: boolean | null, is_online: boolean, date_created: string, }
