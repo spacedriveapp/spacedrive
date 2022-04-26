073bded0dc4fcde88ce96ec5d6faf6bc0a668820
Jamie Pine <ijamespine@me.com>
2022-04-25 18:01:00 -0700

    visual tweaks to new ui

--- packages/interface/src/components/device/Device.tsx
+++ packages/interface/src/components/device/Device.tsx
@@ -10,7 +10,7 @@ import React, { useState } from 'react';
 import FileItem from '../file/FileItem';
 import { Button } from '@sd/ui';
 import ProgressBar from '../primitive/ProgressBar';
-import { CogIcon } from '@heroicons/react/solid';
+import { CogIcon, LockClosedIcon } from '@heroicons/react/solid';
 import { KeyIcon } from '@heroicons/react/outline';
 import LoadingIcons, { Rings } from 'react-loading-icons';
 
@@ -38,25 +38,26 @@ export function Device(props: DeviceProps) {
         {props.type === 'desktop' && <Desktop weight="fill" size={20} className="mr-2" />}
         <h3 className="font-semibold text-md">{props.name}</h3>
         <div className="flex flex-row space-x-1.5 mt-0.5">
-          <span className="font-semibold  h-[19px] ml-3 py-0.5 px-1.5 text-[10px] rounded-md text-gray-400 bg-gray-550">
-            Primary
-          </span>
-          <span className="font-semibold  h-[19px] py-0.5 px-1.5 text-[10px]  rounded-md text-gray-400 bg-gray-550">
-            {props.size}
+          <span className="font-semibold flex flex-row h-[19px] -mt-0.5 ml-3 py-0.5 px-1.5 text-[10px] rounded  bg-gray-500 text-gray-400">
+            <LockClosedIcon className="w-3 h-3 mr-1 -ml-0.5 m-[1px]" />
+            P2P
           </span>
         </div>
+        <span className="font-semibold  py-0.5 px-1.5 text-sm ml-2  text-gray-400 ">
+          {props.size}
+        </span>
         <div className="flex flex-grow" />
         {props.runningJob && (
-          <div className="flex flex-row ml-5 bg-opacity-50 border border-gray-500 rounded bg-gray-550">
+          <div className="flex flex-row ml-5 border rounded-md bg-opacity-10 bg-gray-750 border-gray-550 ">
             <Rings
               stroke="#2599FF"
               strokeOpacity={4}
               strokeWidth={10}
               speed={0.5}
-              className="ml-0.5 mt-0.5 -mr-1 w-7 h-7"
+              className="ml-0.5 mt-[2px] -mr-1 w-7 h-7"
             />
-            <div className="flex flex-col p-1.5">
-              <span className="mb-[3px] -mt-0.5 truncate text-gray-450 text-tiny">
+            <div className="flex flex-col p-2">
+              <span className="mb-[2px] -mt-1 truncate text-gray-450 text-tiny">
                 {props.runningJob.task}...
               </span>
               <ProgressBar value={props.runningJob?.amount} total={100} />
