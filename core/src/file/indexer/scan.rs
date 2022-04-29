a63e684019775dcee9585b6a8f84ec2169e98227
Koding Dev <kodingdev@pm.me>
2022-04-29 22:11:52 +1000

    Fixed windows scanning + improved progress UI

--- core/src/file/indexer/scan.rs
+++ core/src/file/indexer/scan.rs
@@ -83,8 +83,16 @@ pub async fn scan_path(
         .unwrap_or("");
       let parent_dir_id = dirs.get(&*parent_path);
 
+      let str = match path.as_os_str().to_str() {
+        Some(str) => str,
+        None => {
+          println!("Error reading file {}", &path.display());
+          continue;
+        }
+      };
+
       on_progress(vec![
-        ScanProgress::Message(format!("Found: {:?}", &path)),
+        ScanProgress::Message(format!("{}", str)),
         ScanProgress::ChunkCount(paths.len() / 100),
       ]);
 
