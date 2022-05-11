1b8ae5d084208291cd91f2f4b4511e9e4d3ef688
maxichrome <maxichrome@users.noreply.github.com>
2022-05-11 01:59:03 -0500

    use native window controls on macOS

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -4,6 +4,9 @@ use std::env::consts;
 use sdcore::{ClientCommand, ClientQuery, Core, CoreController, CoreEvent, CoreResponse};
 use tauri::api::path;
 use tauri::Manager;
+use cocoa::appkit::{NSWindow, NSWindowStyleMask};
+use cocoa::base::nil;
+use tauri::{Runtime, Window};
 mod menu;
 
 #[tauri::command(async)]
@@ -34,6 +37,55 @@ async fn client_command_transport(
   }
 }
 
+pub trait WindowExt {
+  #[cfg(target_os = "macos")]
+  fn set_transparent_titlebar(&self, transparent: bool);
+}
+
+impl<R: Runtime> WindowExt for Window<R> {
+  #[cfg(target_os = "macos")]
+  fn set_transparent_titlebar(&self, transparent: bool) {
+    use cocoa::{appkit::{NSWindowTitleVisibility, NSToolbar, NSApplication}, foundation::NSString};
+
+    unsafe {
+      let id = self.ns_window().unwrap() as cocoa::base::id;
+
+      let mut style_mask = id.styleMask();
+      style_mask.set(
+        NSWindowStyleMask::NSFullSizeContentViewWindowMask | NSWindowStyleMask::NSUnifiedTitleAndToolbarWindowMask,
+        transparent,
+      );
+      id.setStyleMask_(style_mask);
+
+      // TODO: figure out if this is how to correctly hide the toolbar in full screen
+      // and if so, figure out why tf it panics:
+
+      // let mut presentation_options = id.presentationOptions_();
+      // presentation_options.set(
+      //   NSApplicationPresentationOptions::NSApplicationPresentationAutoHideToolbar,
+      //   transparent,
+      // );
+      // id.setPresentationOptions_(presentation_options);
+
+      let toolbar = NSToolbar::alloc(nil).initWithIdentifier_(NSString::alloc(nil).init_str("wat"));
+      toolbar.setShowsBaselineSeparator_(false);
+      id.setToolbar_(toolbar);
+
+      id.setTitleVisibility_(if transparent {
+        NSWindowTitleVisibility::NSWindowTitleHidden
+      } else {
+        NSWindowTitleVisibility::NSWindowTitleVisible
+      });
+
+      id.setTitlebarAppearsTransparent_(if transparent {
+        cocoa::base::YES
+      } else {
+        cocoa::base::NO
+      });
+    }
+  }
+}
+
 #[tokio::main]
 async fn main() {
   let data_dir = path::data_dir().unwrap_or(std::path::PathBuf::from("./"));
@@ -60,15 +112,16 @@ async fn main() {
           window_shadows::set_shadow(&window, true).unwrap_or(());
 
           if consts::OS == "windows" {
-              window.set_decorations(true);
-              println!("Hello World!");
+            let _ = window.set_decorations(true);
           }
-
-          window.start_dragging().unwrap_or(());
         });
       }
 
-      
+      #[cfg(target_os = "macos")]
+      {
+        let win = app.get_window("main").unwrap();
+        win.set_transparent_titlebar(true);
+      }
 
       // core event transport
       tokio::spawn(async move {
