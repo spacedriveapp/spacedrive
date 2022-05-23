7b9892a489afd3d8b634cd1b2951556ce3844f25
maxichrome <maxichrome@users.noreply.github.com>
2022-05-23 04:40:50 -0500

    Merge branch 'main' into jamie-unsorted-changes

--- packages/interface/src/components/file/FileThumb.tsx
+++ packages/interface/src/components/file/FileThumb.tsx
@@ -2,37 +2,38 @@ import { useBridgeQuery } from '@sd/client';
 import { FilePath } from '@sd/core';
 import clsx from 'clsx';
 import React, { useContext } from 'react';
+
 import { AppPropsContext } from '../../App';
 import icons from '../../assets/icons';
 import { ReactComponent as Folder } from '../../assets/svg/folder.svg';
 
 export default function FileThumb(props: {
-  file: FilePath;
-  locationId: number;
-  hasThumbnailOverride: boolean;
-  className?: string;
+	file: FilePath;
+	locationId: number;
+	hasThumbnailOverride: boolean;
+	className?: string;
 }) {
-  const appPropsContext = useContext(AppPropsContext);
-  const { data: client } = useBridgeQuery('ClientGetState');
+	const appPropsContext = useContext(AppPropsContext);
+	const { data: client } = useBridgeQuery('ClientGetState');
 
-  if (props.file.is_dir) {
-    return <Folder className="max-w-[170px]" />;
-  }
+	if (props.file.is_dir) {
+		return <Folder className="max-w-[170px]" />;
+	}
 
-  if (client?.data_path && (props.file.has_local_thumbnail || props.hasThumbnailOverride)) {
-    return (
-      <img
-        className="pointer-events-none z-90"
-        src={appPropsContext?.convertFileSrc(
-          `${client.data_path}/thumbnails/${props.locationId}/${props.file.temp_cas_id}.webp`
-        )}
-      />
-    );
-  }
+	if (client?.data_path && (props.file.has_local_thumbnail || props.hasThumbnailOverride)) {
+		return (
+			<img
+				className="pointer-events-none z-90"
+				src={appPropsContext?.convertFileSrc(
+					`${client.data_path}/thumbnails/${props.locationId}/${props.file.temp_cas_id}.webp`
+				)}
+			/>
+		);
+	}
 
-  if (icons[props.file.extension as keyof typeof icons]) {
-    let Icon = icons[props.file.extension as keyof typeof icons];
-    return <Icon className={clsx('max-w-[170px] w-full h-full', props.className)} />;
-  }
-  return <div></div>;
+	if (icons[props.file.extension as keyof typeof icons]) {
+		let Icon = icons[props.file.extension as keyof typeof icons];
+		return <Icon className={clsx('max-w-[170px] w-full h-full', props.className)} />;
+	}
+	return <div></div>;
 }
--- packages/interface/src/screens/settings/GeneralSettings.tsx
+++ packages/interface/src/screens/settings/GeneralSettings.tsx
@@ -1,28 +1,28 @@
-import { InputContainer } from '../../components/primitive/InputContainer';
-import { Input } from '../../components/primitive';
+import { useBridgeCommand, useBridgeQuery } from '@sd/client';
 import { Button } from '@sd/ui';
 import React, { useState } from 'react';
 
+import { Input } from '../../components/primitive';
+import { InputContainer } from '../../components/primitive/InputContainer';
 import Listbox from '../../components/primitive/Listbox';
 import Slider from '../../components/primitive/Slider';
-import { useBridgeCommand, useBridgeQuery } from '@sd/client';
 
 export default function GeneralSettings() {
-  const { data: volumes } = useBridgeQuery('SysGetVolumes');
+	const { data: volumes } = useBridgeQuery('SysGetVolumes');
 
-  return (
-    <div className="flex flex-col flex-grow max-w-4xl space-y-4">
-      <div className="mt-3 mb-3">
-        <h1 className="text-2xl font-bold">General Settings</h1>
-        <p className="mt-1 text-sm text-gray-400">Basic settings related to this client</p>
-        {/* <hr className="mt-4 border-gray-550" /> */}
-      </div>
-      <p className="px-5 py-3 mb-3 text-sm text-gray-400 rounded-md bg-gray-50 dark:text-gray-400 dark:bg-gray-600">
-        <b>Note: </b>This is a pre-alpha build of Spacedrive, many features are yet to be
-        functional.
-      </p>
+	return (
+		<div className="flex flex-col flex-grow max-w-4xl space-y-4">
+			<div className="mt-3 mb-3">
+				<h1 className="text-2xl font-bold">General Settings</h1>
+				<p className="mt-1 text-sm text-gray-400">Basic settings related to this client</p>
+				{/* <hr className="mt-4 border-gray-550" /> */}
+			</div>
+			<p className="px-5 py-3 mb-3 text-sm text-gray-400 rounded-md bg-gray-50 dark:text-gray-400 dark:bg-gray-600">
+				<b>Note: </b>This is a pre-alpha build of Spacedrive, many features are yet to be
+				functional.
+			</p>
 
-      {/* <InputContainer
+			{/* <InputContainer
         title="Test scan directory"
         description="This will create a job to scan the directory you specify to the database."
       >
@@ -47,23 +47,23 @@ export default function GeneralSettings() {
         </div>
       </InputContainer> */}
 
-      <InputContainer title="Volumes" description="A list of volumes running on this device.">
-        <div className="flex flex-row space-x-2">
-          <div className="flex flex-grow">
-            <Listbox
-              options={
-                volumes?.map((volume) => ({
-                  key: volume.name,
-                  option: volume.name,
-                  description: volume.mount_point
-                })) ?? []
-              }
-            />
-          </div>
-        </div>
-      </InputContainer>
+			<InputContainer title="Volumes" description="A list of volumes running on this device.">
+				<div className="flex flex-row space-x-2">
+					<div className="flex flex-grow">
+						<Listbox
+							options={
+								volumes?.map((volume) => ({
+									key: volume.name,
+									option: volume.name,
+									description: volume.mount_point
+								})) ?? []
+							}
+						/>
+					</div>
+				</div>
+			</InputContainer>
 
-      {/* <div className="">{JSON.stringify({ config })}</div> */}
-    </div>
-  );
+			{/* <div className="">{JSON.stringify({ config })}</div> */}
+		</div>
+	);
 }
