f2d83cd590ffd74de3454ca1cebfcec3352d9f8b
Benjamin Akar <benjaminakar2001@gmail.com>
2022-05-24 21:17:52 +0200

    fix typo

--- apps/landing/src/components/NewBanner.tsx
+++ apps/landing/src/components/NewBanner.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function NewBanner() {
 	return (
 		<div className="text-xs sm:text-base fade-in-whats-new px-5 py-1.5 bg-opacity-50 cursor-pointer mb-5 flex flex-row bg-gray-800 hover:bg-gray-750 border border-gray-600 hover:border-gray-550 rounded-full transition">
-			<span className="truncate text-gray-350">Spacedrive raises $1.9M lead by OSS Capital</span>
+			<span className="truncate text-gray-350">Spacedrive raises $1.9M led by OSS Capital</span>
 			<div className="w-[1px] mx-4 h-22 bg-gray-500" />
 			<span className="flex-shrink-0 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-blue-600">
 				Read post →
