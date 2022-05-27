b9b25e7270a6ca60f13e43d071608fc698307e30
maxichrome <maxichrome@users.noreply.github.com>
2022-05-27 02:20:14 -0500

    add some demo stories

--- packages/ui/src/Button.stories.tsx
+++ packages/ui/src/Button.stories.tsx
@@ -4,15 +4,33 @@ import React from 'react';
 import { Button } from './Button';
 
 export default {
-	title: 'Example/Button',
+	title: 'UI/Button',
 	component: Button,
-	argTypes: {}
+	argTypes: {},
+	parameters: {
+		backgrounds: {
+			default: 'dark'
+		}
+	},
+	args: {
+		children: 'Button'
+	}
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
+export const Default = Template.bind({});
+Default.args = {
+	variant: 'default'
+};
+
 export const Primary = Template.bind({});
 Primary.args = {
+	variant: 'primary'
+};
+
+export const PrimarySmall = Template.bind({});
+PrimarySmall.args = {
 	variant: 'primary',
-	children: 'Button'
+	size: 'sm'
 };
