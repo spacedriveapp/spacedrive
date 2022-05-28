d55c9a28159b61616c23e5a93103f04b8e0262e2
maxichrome <maxichrome@users.noreply.github.com>
2022-05-28 00:24:45 -0500

    improve context menu hitboxes

--- packages/ui/src/ContextMenu.tsx
+++ packages/ui/src/ContextMenu.tsx
@@ -25,7 +25,7 @@ export const ContextMenu: React.FC<ContextMenuProps> = (props) => {
 		<div
 			role="menu"
 			className={clsx(
-				'shadow-box shadow-gray-300 dark:shadow-gray-700 flex flex-col select-none cursor-default bg-gray-50 text-gray-800 border-gray-200 dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500 text-left text-sm font-semibold rounded p-1.5 gap-1.5 border-2',
+				'shadow-box shadow-gray-300 dark:shadow-gray-700 flex flex-col select-none cursor-default bg-gray-50 text-gray-800 border-gray-200 dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500 text-left text-sm font-semibold rounded gap-1.5 border-2 py-1.5',
 				className
 			)}
 			{...rest}
@@ -41,7 +41,7 @@ export const ContextMenu: React.FC<ContextMenuProps> = (props) => {
 							<span className="text-xs ml-2 mt-1 uppercase text-gray-400">{sec.heading}</span>
 						)}
 
-						<ul>
+						<ul className="flex flex-col gap-[2px]">
 							{sec.items.map(({ icon: ItemIcon = Question, ...item }) => (
 								<li key={item.label} className="flex">
 									<button
@@ -49,16 +49,15 @@ export const ContextMenu: React.FC<ContextMenuProps> = (props) => {
 											font: 'inherit',
 											textAlign: 'inherit'
 										}}
-										className={clsx(
-											'flex flex-row gap-1.5 items-center cursor-default rounded-sm flex-1 px-1.5 py-1 focus-visible:bg-gray-150 hover:bg-gray-150 dark:focus-visible:bg-gray-500 dark:hover:bg-gray-500',
-											{
-												'text-red-600 dark:text-red-400': item.danger
-											}
-										)}
+										className={clsx('group cursor-default flex-1 px-1.5 py-0 group-first:pt-1.5', {
+											'text-red-600 dark:text-red-400': item.danger
+										})}
 										onClick={item.onClick}
 									>
-										{<ItemIcon size={18} />}
-										<span className="leading-snug">{item.label}</span>
+										<div className="px-1.5 py-1 group-focus-visible:bg-gray-150 group-hover:bg-gray-150 dark:group-focus-visible:bg-gray-500 dark:group-hover:bg-gray-500 flex flex-row gap-1.5 items-center rounded-sm">
+											{<ItemIcon size={18} />}
+											<span className="leading-snug">{item.label}</span>
+										</div>
 									</button>
 								</li>
 							))}
