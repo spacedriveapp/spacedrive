97b8558aa1c798f25df9edacac53840c9622da0d
xPolar <50601857+xPolar@users.noreply.github.com>
2022-05-30 01:14:28 -0700

    Store isExperimental in local storage. (#196)
    * store isExperimental in local storage
    
    * Repo visualizer: updated diagram
    
    * Repo visualizer: updated diagram
    
    * Repo visualizer: updated diagram
    
    Co-authored-by: maxichrome <33473181+maxichrome@users.noreply.github.com>
    Co-authored-by: repo-visualizer <repo-visualizer@users.noreply.github.com>
    Co-authored-by: Jamie Pine <32987599+jamiepine@users.noreply.github.com>

--- packages/interface/src/components/device/Stores.tsx
+++ packages/interface/src/components/device/Stores.tsx
@@ -1,5 +1,19 @@
 import create from 'zustand';
 
-export const useStore = create((set) => ({
-	experimental: false
+const getLocalStorage = (key: string) => JSON.parse(window.localStorage.getItem(key) || '{}');
+const setLocalStorage = (key: string, value: any) =>
+	window.localStorage.setItem(key, JSON.stringify(value));
+
+type NodeState = {
+	isExperimental: boolean;
+	setIsExperimental: (experimental: boolean) => void;
+};
+
+export const useNodeStore = create<NodeState>((set) => ({
+	isExperimental: getLocalStorage('isExperimental') || false,
+	setIsExperimental: (experimental: boolean) =>
+		set((state) => {
+			setLocalStorage('isExperimental', experimental);
+			return { ...state, isExperimental: experimental };
+		})
 }));
