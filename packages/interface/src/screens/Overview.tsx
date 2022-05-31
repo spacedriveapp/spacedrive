0988c479ea0a3e3fa8e4ebf1f4a0a658ffe038a4
Polar <polar@polar.blue>
2022-05-30 21:11:12 -0700

    push changes for haden to take over.

--- packages/interface/src/screens/Overview.tsx
+++ packages/interface/src/screens/Overview.tsx
@@ -3,8 +3,9 @@ import { useBridgeQuery } from '@sd/client';
 import { Statistics } from '@sd/core';
 import { Button, Input } from '@sd/ui';
 import byteSize from 'byte-size';
+import type { ByteSizeResult } from 'byte-size';
 import clsx from 'clsx';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useCountUp } from 'react-countup';
 import create from 'zustand';
 
@@ -14,47 +15,88 @@ import Dialog from '../components/layout/Dialog';
 
 interface StatItemProps {
 	name: string;
-	value?: string;
-	unit?: string;
+	value: string;
+	statistics_key: keyof Statistics;
 }
 
+const StatItemNames: Record<string, string> = {
+	total_bytes_capacity: 'Total capacity',
+	preview_media_bytes: 'Preview media',
+	library_db_size: 'Index size',
+	total_bytes_free: 'Free space'
+};
+
 type OverviewState = {
-	hasOverviewStatsRan: boolean;
-	setOverviewStatsRan: (ran: boolean) => void;
+	overviewStats: Record<keyof Statistics, { long: string; short: number }>;
+	setOverviewStatsItem: (
+		statName: keyof Statistics,
+		statAmount: number | string,
+		type: 'long' | 'short'
+	) => void;
+	setOverviewStats: (stats: Record<keyof Statistics, { long: string; short: number }>) => void;
 };
 
 export const useOverviewState = create<OverviewState>((set) => ({
-	hasOverviewStatsRan: false,
-	setOverviewStatsRan: (ran: boolean) =>
+	overviewStats: {} as Record<keyof Statistics, { long: string; short: number }>,
+	setOverviewStatsItem: (statName, statAmount, type) =>
 		set((state) => ({
 			...state,
-			hasOverviewStatsRan: ran
-		}))
+			overviewStats: {
+				...state.overviewStats,
+				[statName]: { ...state.overviewStats[statName], [type]: statAmount }
+			}
+		})),
+	setOverviewStats(stats) {
+		set((state) => ({
+			...state,
+			overviewStats: stats
+		}));
+	}
 }));
 
-const StatItem: React.FC<StatItemProps> = (props) => {
+const StatItem: React.FC<StatItemProps> = ({ name, statistics_key, value }) => {
 	const countUp = React.useRef(null);
-	const hiddenCountUp = React.useRef(null);
 	const appPropsContext = useContext(AppPropsContext);
-	let size = byteSize(Number(props.value) || 0);
 
-	let amount = parseFloat(size.value);
+	const [size, setSize] = React.useState<ByteSizeResult | null>(null);
 
-	const { hasOverviewStatsRan, setOverviewStatsRan } = useOverviewState();
+	let amount = size ? +size.value : 0;
 
-	const { update } = useCountUp({
-		ref: hasOverviewStatsRan ? hiddenCountUp : countUp,
-		end: amount,
+	const { overviewStats, setOverviewStatsItem } = useOverviewState();
+	const shouldAnimate = amount !== 0 && overviewStats[statistics_key].short !== amount;
+
+	useEffect(() => {
+		setSize(byteSize(+value));
+	}, [value]);
+
+	useEffect(() => {
+		console.log({
+			name,
+			stat: overviewStats[statistics_key],
+			amount,
+			shouldAnimate,
+			amountsEqual: overviewStats[statistics_key].short === amount
+		});
+	}, [amount, overviewStats]);
+
+	const { update: countTo } = useCountUp({
+		ref: countUp,
+		start: 0,
+		end: 0,
 		delay: 0.1,
 		decimals: 1,
 		duration: appPropsContext?.demoMode ? 1 : 0.5,
-		useEasing: true,
-		onEnd: () => {
-			setOverviewStatsRan(true);
-		}
+		useEasing: true
 	});
 
-	useEffect(() => update(amount), [amount]);
+	useEffect(() => {
+		if (shouldAnimate) {
+			console.log('updating to', amount);
+			countTo(amount);
+		}
+
+		setOverviewStatsItem(statistics_key, amount, 'short');
+	}, [amount]);
 
 	return (
 		<div
@@ -63,40 +105,64 @@ const StatItem: React.FC<StatItemProps> = (props) => {
 				!amount && 'hidden'
 			)}
 		>
-			<span className="text-sm text-gray-400">{props.name}</span>
+			<span className="text-sm text-gray-400">{name}</span>
 			<span className="text-2xl font-bold">
-				<span className="hidden" ref={hiddenCountUp} />
-				{hasOverviewStatsRan ? <span>{size.value}</span> : <span ref={countUp} />}
-				<span className="ml-1 text-[16px] text-gray-400">{size.unit}</span>
+				{/* <span className="hidden" aria-hidden="true" ref={hiddenCountUp} /> */}
+				<span ref={countUp} />
+				<span className="ml-1 text-[16px] text-gray-400">{size?.unit}</span>
 			</span>
+			{JSON.stringify(shouldAnimate)}
 		</div>
 	);
 };
 
-export const OverviewScreen: React.FC<{}> = (props) => {
+export const OverviewScreen = () => {
 	const { data: libraryStatistics } = useBridgeQuery('GetLibraryStatistics');
 	const { data: clientState } = useBridgeQuery('NodeGetState');
 
-	const [stats, setStats] = useState<Statistics>(libraryStatistics || ({} as Statistics));
+	const { overviewStats, setOverviewStats, setOverviewStatsItem } = useOverviewState();
 
 	// get app props context
 	const appPropsContext = useContext(AppPropsContext);
 
 	useEffect(() => {
 		if (appPropsContext?.demoMode == true && !libraryStatistics?.library_db_size) {
-			setStats({
-				total_bytes_capacity: '8093333345230',
-				preview_media_bytes: '2304387532',
-				library_db_size: '83345230',
-				total_file_count: 20342345,
-				total_bytes_free: '89734502034',
-				total_bytes_used: '8093333345230',
-				total_unique_bytes: '9347397'
-			});
+			if (!Object.entries(overviewStats).length)
+				setOverviewStats({
+					total_bytes_capacity: { long: '8093333345230', short: 0 },
+					preview_media_bytes: { long: '2304387532', short: 0 },
+					library_db_size: { long: '83345230', short: 0 },
+					total_file_count: { long: '20342345', short: 0 },
+					total_bytes_free: { long: '89734502034', short: 0 },
+					total_bytes_used: { long: '8093333345230', short: 0 },
+					total_unique_bytes: { long: '9347397', short: 0 }
+				});
 		} else {
-			setStats(libraryStatistics as Statistics);
+			const newStatistics = {
+				total_bytes_capacity: { long: '', short: 0 },
+				preview_media_bytes: { long: '', short: 0 },
+				library_db_size: { long: '', short: 0 },
+				total_file_count: { long: '', short: 0 },
+				total_bytes_free: { long: '', short: 0 },
+				total_bytes_used: { long: '', short: 0 },
+				total_unique_bytes: { long: '', short: 0 }
+			};
+
+			Object.entries(libraryStatistics as Statistics).map(([key, value]) => {
+				newStatistics[key as keyof Statistics] = { long: value as string, short: 0 };
+			});
+
+			setOverviewStats(newStatistics);
 		}
 	}, [appPropsContext, libraryStatistics]);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setOverviewStatsItem('total_bytes_capacity', '4093333345230', 'long');
+		}, 10000);
+	}, [overviewStats]);
+
+	const validStatItems = Object.keys(StatItemNames);
 
 	return (
 		<div className="flex flex-col w-full h-screen overflow-x-hidden custom-scroll page-scroll">
@@ -107,33 +173,18 @@ export const OverviewScreen: React.FC<{}> = (props) => {
 				<div className="flex w-full">
 					{/* STAT CONTAINER */}
 					<div className="flex pb-4 overflow-hidden">
-						<StatItem
-							name="Total capacity"
-							value={stats?.total_bytes_capacity}
-							unit={stats?.total_bytes_capacity}
-						/>
-						<StatItem
-							name="Index size"
-							value={stats?.library_db_size}
-							unit={stats?.library_db_size}
-						/>
-						<StatItem
-							name="Preview media"
-							value={stats?.preview_media_bytes}
-							unit={stats?.preview_media_bytes}
-						/>
-						<StatItem
-							name="Free space"
-							value={stats?.total_bytes_free}
-							unit={stats?.total_bytes_free}
-						/>
-						<StatItem name="Total at-risk" value={'0'} unit={stats?.preview_media_bytes} />
-						{/* <StatItem
-              name="Total at-risk"
-              value={'0'}
-              unit={stats?.preview_media_bytes}
-            />
-            <StatItem name="Total backed up" value={'0'} unit={''} /> */}
+						{Object.entries(overviewStats).map(([key, value]) => {
+							if (!validStatItems.includes(key)) return <></>;
+
+							return (
+								<StatItem
+									key={key}
+									name={StatItemNames[key]}
+									value={value.long}
+									statistics_key={key as keyof Statistics}
+								/>
+							);
+						})}
 					</div>
 					<div className="flex-grow" />
 					<div className="space-x-2">
