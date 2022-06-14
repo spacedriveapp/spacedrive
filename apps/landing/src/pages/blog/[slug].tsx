87c8014a40b33c3413f5992364dbd445efdc4d2f
Jamie Pine <ijamespine@me.com>
2022-06-13 17:49:54 -0700

    fix types

--- apps/landing/src/pages/blog/[slug].tsx
+++ apps/landing/src/pages/blog/[slug].tsx
@@ -1,3 +1,4 @@
+import { PostOrPage, Tag } from '@tryghost/content-api';
 import Prism from 'prismjs';
 import 'prismjs/components/prism-rust';
 import React, { useEffect, useState } from 'react';
@@ -6,7 +7,7 @@ import '../../atom-one.css';
 import { getPost } from './posts';
 
 function MarkdownPage() {
-	const [post, setPost] = useState([]);
+	const [post, setPost] = useState<PostOrPage | null>(null);
 
 	useEffect(() => {
 		const get = async () => {
@@ -17,21 +18,22 @@ function MarkdownPage() {
 		Prism.highlightAll();
 	}, []);
 	return (
-		<div className="container max-w-4xl p-4 mt-32 mb-20 m-auto prose lg:prose-xs dark:prose-invert">
+		<div className="container max-w-4xl p-4 m-auto mt-32 mb-20 prose lg:prose-xs dark:prose-invert">
 			<figure>
+				{/* @ts-expect-error */}
 				<figcaption dangerouslySetInnerHTML={{ __html: post.feature_image_caption }}></figcaption>
-				<img src={post.feature_image} alt="" className="rounded-xl" />
+				<img src={post?.feature_image as string} alt="" className="rounded-xl" />
 			</figure>
-			<section className="flex flex-wrap gap-4 -mx-8 p-8 rounded-xl">
+			<section className="flex flex-wrap gap-4 p-8 -mx-8 rounded-xl">
 				<div className="flex-grow">
-					<h1 className="m-0">{post.title}</h1>
+					<h1 className="m-0">{post?.title}</h1>
 					<p className="m-0">
-						by {post.primary_author?.name} &middot;{' '}
-						{new Date(post.published_at ?? '').toLocaleDateString()}
+						by {post?.primary_author?.name} &middot;{' '}
+						{new Date(post?.published_at ?? '').toLocaleDateString()}
 					</p>
 				</div>
 				<div className="flex flex-wrap gap-2">
-					{post.tags?.map((tag: Tag) => {
+					{post?.tags?.map((tag: Tag) => {
 						return (
 							<span
 								className={`px-2 py-1 rounded-full text-sm h-8`}
@@ -47,6 +49,7 @@ function MarkdownPage() {
 					})}
 				</div>
 			</section>
+			{/* @ts-expect-error */}
 			<article id="content" dangerouslySetInnerHTML={{ __html: post.html }}></article>
 		</div>
 	);
