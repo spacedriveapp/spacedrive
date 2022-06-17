52fe17cb89b8482bc7f2221278719eedcaa1ddca
maxichrome <maxichrome@users.noreply.github.com>
2022-06-17 06:06:45 -0500

    only use swift_rs on macOS in build script missed this in #239, silly me

--- apps/desktop/src-tauri/build.rs
+++ apps/desktop/src-tauri/build.rs
@@ -1,8 +1,11 @@
-use swift_rs::build::{link_swift, link_swift_package};
-
 fn main() {
-	link_swift();
-	link_swift_package("sd-desktop-macos", "./native/macos/");
+	#[cfg(target_os = "macos")]
+	{
+		use swift_rs::build::{link_swift, link_swift_package};
+
+		link_swift();
+		link_swift_package("sd-desktop-macos", "./native/macos/");
+	}
 
 	tauri_build::build();
 }
