9ab8b26e920f899d4bbc1e502d03bc84f52f440b
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2022-06-22 18:30:05 +0000

    updated scripts

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo npm install -y -g npm@latest pnpm
 sudo apt install -y cargo pnpm
+# this is the distro specific part
 DEBIAN_TAURI_DEPS="libwebkit2gtk-4.0-dev build-essential curl wget libssl-dev libgtk-3-dev libappindicator3-dev librsvg2-dev" # Tauri dependencies
 DEBIAN_FFMPEG_DEPS="libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavresample-dev libavutil-dev libswscale-dev libswresample-dev ffmpeg" # FFMPEG dependencies
 DEBIAN_BINDGEN_DEPS="pkg-config clang" # Bindgen dependencies - it's used by a dependency of Spacedrive
-
 sudo apt-get -y update
 sudo apt-get -y install ${SPACEDRIVE_CUSTOM_APT_FLAGS:-} $DEBIAN_TAURI_DEPS $DEBIAN_FFMPEG_DEPS $DEBIAN_BINDGEN_DEPS
 
