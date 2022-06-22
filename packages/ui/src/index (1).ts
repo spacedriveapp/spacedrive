d9d66750b281f0e8ade9360f5585d72a16eaf5bb
muhsinkutay <33558081+muhsinkutay@users.noreply.github.com>
2022-06-22 07:33:08 +0300

    Moved UI components.

--- packages/ui/src/index (1).ts
+++
@@ -1,6 +0,0 @@
-import App from './App';
-import { AppProps, Platform } from './AppPropsContext';
-
-export type { AppProps, Platform };
-
-export default App;
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,10 @@
+import App from './App';
+import { AppProps, Platform } from './AppPropsContext';
+
+export type { AppProps, Platform };
+
+export default App;
+
 export * from './Button';
 export * from './Dropdown';
 export * from './ContextMenu';
