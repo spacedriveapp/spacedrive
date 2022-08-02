7c32e0c24f649e7f7019bb5d920f27d837dece97
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2022-08-03 00:54:35 +0300

    Show folder icons on overview screen

--- apps/mobile/src/components/device/Device.tsx
+++ apps/mobile/src/components/device/Device.tsx
@@ -1,9 +1,11 @@
+import { FilePath } from '@sd/core';
 import { Cloud, Desktop, DeviceMobileCamera, Laptop } from 'phosphor-react-native';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import { LockClosedIcon } from 'react-native-heroicons/solid';
 
 import tw from '../../lib/tailwind';
+import FileItem from '../file/FileItem';
 
 export interface DeviceProps {
 	name: string;
@@ -12,45 +14,109 @@ export interface DeviceProps {
 	locations: Array<{ name: string; folder?: boolean; format?: string; icon?: string }>;
 }
 
-// const placeholderFileItems: FilePath[] = [
-// 	{
-// 		is_dir: true,
-// 		date_created: '2020-01-01T00:00:00.000Z',
-// 		date_indexed: '2020-01-01T00:00:00.000Z',
-// 		date_modified: '2020-01-01T00:00:00.000Z',
-// 		extension: '',
-// 		file_id: 5,
-// 		id: 5,
-// 		location_id: 1,
-// 		materialized_path: '',
-// 		name: 'Hello World',
-// 		parent_id: 0,
-// 		file: {
-// 			id: 1,
-// 			cas_id: '',
-// 			ipfs_id: '',
-// 			has_thumbnail: false,
-// 			favorite: false,
-// 			has_thumbstrip: false,
-// 			has_video_preview: false,
-// 			hidden: false,
-// 			important: false,
-// 			integrity_checksum: '',
-// 			kind: 'Image',
-// 			note: '',
-// 			paths: [],
-// 			size_in_bytes: '555',
-// 			date_created: '',
-// 			date_indexed: '',
-// 			date_modified: ''
-// 		}
-// 	}
-// ];
+const placeholderFileItems: FilePath[] = [
+	{
+		is_dir: true,
+		date_created: '2020-01-01T00:00:00.000Z',
+		date_indexed: '2020-01-01T00:00:00.000Z',
+		date_modified: '2020-01-01T00:00:00.000Z',
+		extension: '',
+		file_id: 1,
+		id: 1,
+		location_id: 1,
+		materialized_path: '',
+		name: 'Documents',
+		parent_id: 0,
+		file: {
+			id: 1,
+			cas_id: '',
+			ipfs_id: '',
+			has_thumbnail: false,
+			favorite: false,
+			has_thumbstrip: false,
+			has_video_preview: false,
+			hidden: false,
+			important: false,
+			integrity_checksum: '',
+			kind: 'Image',
+			note: '',
+			paths: [],
+			size_in_bytes: '555',
+			date_created: '',
+			date_indexed: '',
+			date_modified: ''
+		}
+	},
+	{
+		is_dir: true,
+		date_created: '2020-01-01T00:00:00.000Z',
+		date_indexed: '2020-01-01T00:00:00.000Z',
+		date_modified: '2020-01-01T00:00:00.000Z',
+		extension: '',
+		file_id: 2,
+		id: 2,
+		location_id: 2,
+		materialized_path: '',
+		name: 'Movies',
+		parent_id: 0,
+		file: {
+			id: 2,
+			cas_id: '',
+			ipfs_id: '',
+			has_thumbnail: false,
+			favorite: false,
+			has_thumbstrip: false,
+			has_video_preview: false,
+			hidden: false,
+			important: false,
+			integrity_checksum: '',
+			kind: 'Image',
+			note: '',
+			paths: [],
+			size_in_bytes: '555',
+			date_created: '',
+			date_indexed: '',
+			date_modified: ''
+		}
+	},
+	{
+		is_dir: true,
+		date_created: '2020-01-01T00:00:00.000Z',
+		date_indexed: '2020-01-01T00:00:00.000Z',
+		date_modified: '2020-01-01T00:00:00.000Z',
+		extension: '',
+		file_id: 3,
+		id: 3,
+		location_id: 3,
+		materialized_path: '',
+		name: 'Minecraft',
+		parent_id: 0,
+		file: {
+			id: 3,
+			cas_id: '',
+			ipfs_id: '',
+			has_thumbnail: false,
+			favorite: false,
+			has_thumbstrip: false,
+			has_video_preview: false,
+			hidden: false,
+			important: false,
+			integrity_checksum: '',
+			kind: 'Image',
+			note: '',
+			paths: [],
+			size_in_bytes: '555',
+			date_created: '',
+			date_indexed: '',
+			date_modified: ''
+		}
+	}
+];
 
 const Device = ({ name, locations, size, type }: DeviceProps) => {
 	return (
 		<View style={tw`bg-gray-600 border rounded-md border-gray-550 mt-4`}>
-			<View style={tw`flex flex-row items-center justify-between px-4 pt-3 pb-2`}>
+			<View style={tw`flex flex-row items-center px-4 pt-3 pb-2`}>
 				<View style={tw`flex flex-row items-center`}>
 					{type === 'phone' && (
 						<DeviceMobileCamera color="white" weight="fill" size={18} style={tw`mr-2`} />
@@ -68,8 +134,14 @@ const Device = ({ name, locations, size, type }: DeviceProps) => {
 				{/* Size */}
 				<Text style={tw`font-semibold text-sm ml-2 text-gray-400`}>{size}</Text>
 			</View>
-			{/* Locations/Files */}
-			<View style={tw`mt-4 p-4`} />
+			{/* Locations/Files TODO: Maybe use FlashList? */}
+			<FlatList
+				data={placeholderFileItems}
+				renderItem={({ item }) => <FileItem file={item} />}
+				keyExtractor={(item) => item.id.toString()}
+				horizontal
+				contentContainerStyle={tw`mt-4 ml-2`}
+			/>
 		</View>
 	);
 };
--- apps/mobile/src/components/file/FileItem.tsx
+++ apps/mobile/src/components/file/FileItem.tsx
@@ -2,18 +2,39 @@ import { FilePath } from '@sd/core';
 import React from 'react';
 import { Text, View } from 'react-native';
 
+import tw from '../../lib/tailwind';
+import FolderIcon from '../icons/Folder';
+
 type FileItemProps = {
 	file?: FilePath | null;
 };
 
+// TODO: Menu for file actions (File details, Share etc.)
+// TODO: Sheet Modal for file details
+
 const FileItem = ({ file }: FileItemProps) => {
-	// If DIR show folder icon
-	// else if file has thumbnail show thumbnail
-	// else show file with ext. icon
-	// else show default icon
 	return (
-		<View>
-			<Text>FileItem</Text>
+		<View style={tw`w-24 h-24`}>
+			<View style={tw`items-center`}>
+				{/* Folder Icons/Thumbnail etc. */}
+				<>
+					{file?.is_dir ? (
+						<FolderIcon size={48} />
+					) : file?.file?.has_thumbnail ? (
+						<>{/* TODO */}</>
+					) : (
+						<View>
+							<Text>wat</Text>
+						</View>
+					)}
+				</>
+				{/* Name */}
+				<View style={tw`px-1.5 py-[1px] mt-1`}>
+					<Text numberOfLines={1} style={tw`text-gray-300 text-center text-xs font-medium`}>
+						{file?.name}
+					</Text>
+				</View>
+			</View>
 		</View>
 	);
 };
