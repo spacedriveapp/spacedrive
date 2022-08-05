e62e06eee4086127c47cb67e784fc976e1f2c081
Brendan Allan <brendonovich@outlook.com>
2022-08-05 19:00:46 +0800

    fix pnpm & expo & monorepo

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,7 +1,7 @@
-module.exports = function (api) {
-	api.cache(true);
-	return {
-		presets: ['babel-preset-expo'],
-		plugins: ['react-native-reanimated/plugin']
-	};
+module.exports = function(api) {
+  api.cache(true);
+  return {
+	plugins: ['react-native-reanimated/plugin'],
+    presets: ['babel-preset-expo'],
+  };
 };
--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import { DefaultTheme, NavigationContainer, Theme } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect } from 'react';
@@ -11,7 +12,7 @@ import tw from './lib/tailwind';
 import RootNavigator from './navigation';
 import OnboardingNavigator from './navigation/OnboardingNavigator';
 import { useOnboardingStore } from './stores/useOnboardingStore';
-
+//
 const NavigatorTheme: Theme = {
 	...DefaultTheme,
 	colors: {
