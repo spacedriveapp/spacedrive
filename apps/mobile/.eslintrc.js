ab432d4dd00f408d2ecedfbfa0ceafd8943d34a2
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2022-08-11 12:22:58 +0300

    Fix types - Mobile

--- apps/mobile/.eslintrc.js
+++ apps/mobile/.eslintrc.js
@@ -1,8 +1,42 @@
 module.exports = {
-	...require('@sd/config/eslint-react-native.js'),
-	parserOptions: {
-		tsconfigRootDir: __dirname,
-		project: './tsconfig.json'
+	env: {
+		'react-native/react-native': true
 	},
-	ignorePatterns: ['**/*.js', '**/*.json', 'android', 'ios', '.expo']
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 12,
+		sourceType: 'module'
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	plugins: ['react', 'react-native'],
+	rules: {
+		'react/display-name': 'off',
+		'react/prop-types': 'off',
+		'react/no-unescaped-entities': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-var-requires': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'no-control-regex': 'off',
+		'no-mixed-spaces-and-tabs': ['warn', 'smart-tabs']
+	},
+	ignorePatterns: ['**/*.js', '**/*.json', 'node_modules', 'android', 'ios', '.expo'],
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	}
 };
