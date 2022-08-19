d10cf558bd0775c2add7619d765baff00a5d9858
Brendan Allan <brendonovich@outlook.com>
2022-08-20 02:18:52 +0800

    use ghost api v5 on landing

--- apps/landing/src/pages/blog/post.page.server.ts
+++ apps/landing/src/pages/blog/post.page.server.ts
@@ -3,22 +3,22 @@ import { PageContextBuiltIn } from 'vite-plugin-ssr';
 import { getPost, getPosts } from './api';
 
 export async function onBeforeRender(pageContext: PageContextBuiltIn) {
-	const post = await getPost(pageContext.routeParams['slug']);
+  const post = await getPost(pageContext.routeParams['slug']);
 
-	return {
-		pageContext: {
-			pageProps: {
-				post
-			}
-		}
-	};
+  return {
+    pageContext: {
+      pageProps: {
+        post
+      }
+    }
+  };
 }
 
 export async function prerender() {
-	const posts = await getPosts();
+  const posts = await getPosts();
 
-	return posts.map((post) => ({
-		url: `/blog/${post.slug}`,
-		pageContext: { pageProps: { post } }
-	}));
+  return posts.map((post) => ({
+    url: `/blog/${post.slug}`,
+    pageContext: { pageProps: { post } }
+  }));
 }
