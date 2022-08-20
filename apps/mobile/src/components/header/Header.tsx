335ecedb3ef1b73b7a3cae4ad9440616ad742d9e
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2022-08-20 12:40:07 +0300

    Search header

--- apps/mobile/src/components/header/Header.tsx
+++ apps/mobile/src/components/header/Header.tsx
@@ -18,20 +18,20 @@ const Header = () => {
 
 	return (
 		<View style={tw.style('mx-4 bg-gray-550 rounded-md', { marginTop: top + 20 })}>
-			<View style={tw`flex flex-row items-center h-11`}>
+			<View style={tw`flex flex-row items-center h-10`}>
 				<Pressable style={tw`px-3 h-full justify-center`} onPress={() => navigation.openDrawer()}>
 					<MotiView
 						animate={{ rotate: isDrawerOpen ? '90deg' : '0deg' }}
 						transition={{ type: 'timing' }}
 					>
-						<List size={20} color={'white'} />
+						<List size={20} color={tw.color('gray-300')} weight="fill" />
 					</MotiView>
 				</Pressable>
 				<Pressable
 					style={tw`flex-1 h-full justify-center`}
 					onPress={() => navigation.navigate('Search')}
 				>
-					<Text style={tw`text-gray-300 font-semibold`}>Search</Text>
+					<Text style={tw`text-gray-300 font-semibold text-sm`}>Search</Text>
 				</Pressable>
 			</View>
 		</View>
--- apps/mobile/src/screens/modals/Search.tsx
+++ apps/mobile/src/screens/modals/Search.tsx
@@ -1,29 +1,57 @@
-import React from 'react';
-import { Pressable, Text, View } from 'react-native';
-import { ChevronLeftIcon } from 'react-native-heroicons/outline';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { MagnifyingGlass } from 'phosphor-react-native';
+import React, { useState } from 'react';
+import { ActivityIndicator, Pressable, Text, TextInput, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
+import { Button } from '../../components/base/Button';
 import tw from '../../lib/tailwind';
 import { RootStackScreenProps } from '../../navigation';
 
 const SearchScreen = ({ navigation }: RootStackScreenProps<'Search'>) => {
+	const { top } = useSafeAreaInsets();
+
+	const [loading, setLoading] = useState(false);
+
 	return (
-		<SafeAreaView edges={['top']} style={tw`flex-1 pt-4`}>
+		<View style={tw.style('flex-1', { marginTop: top + 20 })}>
 			{/* Header */}
-			<View style={tw`flex flex-row items-center px-4`}>
-				{/* Back Button */}
-				<Pressable onPress={() => navigation.goBack()}>
-					<ChevronLeftIcon color={tw.color('primary-500')} width={25} height={25} />
-				</Pressable>
+			<View style={tw`flex flex-row items-center mx-4`}>
 				{/* Search Input */}
-				<View>
-					<Text style={tw`text-white`}>Search</Text>
+				<View style={tw`flex-1 bg-gray-550 rounded-md h-10 mr-3`}>
+					<View style={tw`flex flex-row h-full items-center px-3`}>
+						<View style={tw`mr-3`}>
+							{loading ? (
+								<ActivityIndicator size={'small'} color={'white'} />
+							) : (
+								<MagnifyingGlass size={20} weight="regular" color={'white'} />
+							)}
+						</View>
+						<TextInput
+							placeholder={'Search'}
+							clearButtonMode="never" // can't change the color??
+							underlineColorAndroid="transparent"
+							placeholderTextColor={tw.color('gray-300')}
+							style={tw`text-white flex-1 text-sm`}
+							textContentType={'none'}
+							autoFocus
+							autoCapitalize="none"
+							autoCorrect={false}
+						/>
+					</View>
 				</View>
+				{/* Cancel Button */}
+				<Pressable onPress={() => navigation.goBack()}>
+					<Text style={tw`text-primary-500`}>Cancel</Text>
+				</Pressable>
 			</View>
+			{/* Content */}
 			<View style={tw`flex-1 items-center justify-center`}>
+				<Button variant="primary" onPress={() => setLoading((v) => !v)}>
+					<Text>Toggle loading</Text>
+				</Button>
 				<Text style={tw`font-bold text-white`}>Stuff</Text>
 			</View>
-		</SafeAreaView>
+		</View>
 	);
 };
 
