b72346408267a2df85dee1693b4db2643ff47e63
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2022-08-23 11:51:36 +0300

    Less margin on header

--- apps/mobile/src/navigation/index.tsx
+++ apps/mobile/src/navigation/index.tsx
@@ -49,6 +49,7 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> = Stac
 declare global {
 	// eslint-disable-next-line @typescript-eslint/no-namespace
 	namespace ReactNavigation {
+		// eslint-disable-next-line @typescript-eslint/no-empty-interface
 		interface RootParamList extends RootStackParamList {}
 	}
 }
