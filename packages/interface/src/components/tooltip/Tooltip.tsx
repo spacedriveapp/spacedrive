c207642429336001fb76b8c689db08bcaff4910c
Oscar Beaumont <oscar@otbeaumont.me>
2022-08-30 14:38:45 +0800

    make typecheck pass

--- packages/interface/src/components/tooltip/Tooltip.tsx
+++ packages/interface/src/components/tooltip/Tooltip.tsx
@@ -1,24 +1,18 @@
-
 import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 import React from 'react';
-export const Tooltip = ({children,label}: {children:React.ReactNode, label:string})=>{
 
-
+export const Tooltip = ({ children, label }: { children: React.ReactNode; label: string }) => {
 	return (
-	
-			<TooltipPrimitive.Provider >
-			  <TooltipPrimitive.Root >
-				<TooltipPrimitive.Trigger asChild  >		
-					<span>
-						{children}
-					</span>		
-					</TooltipPrimitive.Trigger>
-				<TooltipPrimitive.Content className='text-sm  rounded   px-2 py-1 mb-[2px]  bg-gray-300 dark:!bg-gray-500 dark:text-gray-100'>
-				  <TooltipPrimitive.Arrow className='fill-gray-300 dark:!fill-gray-500'/>
-				{label}
+		<TooltipPrimitive.Provider>
+			<TooltipPrimitive.Root>
+				<TooltipPrimitive.Trigger asChild>
+					<span>{children}</span>
+				</TooltipPrimitive.Trigger>
+				<TooltipPrimitive.Content className="text-sm  rounded   px-2 py-1 mb-[2px]  bg-gray-300 dark:!bg-gray-500 dark:text-gray-100">
+					<TooltipPrimitive.Arrow className="fill-gray-300 dark:!fill-gray-500" />
+					{label}
 				</TooltipPrimitive.Content>
-			  </TooltipPrimitive.Root>
-			</TooltipPrimitive.Provider>
-	
-	)
-}
+			</TooltipPrimitive.Root>
+		</TooltipPrimitive.Provider>
+	);
+};
