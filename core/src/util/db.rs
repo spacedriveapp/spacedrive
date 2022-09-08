efb451ee36229319db214b626603fa6685df8d93
Oscar Beaumont <oscar@otbeaumont.me>
2022-09-09 06:57:07 +0800

    Better db migrations (#381)
    * use prisma migration engine for migrations
    
    * squash migrations + remove excessive logging

--- core/src/util/db.rs
+++ core/src/util/db.rs
@@ -1,137 +1,89 @@
-use crate::prisma::{self, migration, PrismaClient};
-use data_encoding::HEXLOWER;
+use crate::prisma::{self, PrismaClient};
+use enumflags2::BitFlags;
 use include_dir::{include_dir, Dir};
-use prisma_client_rust::{raw, NewClientError};
-use ring::digest::{Context, SHA256};
+use migration_core::{
+	commands::apply_migrations,
+	json_rpc::types::ApplyMigrationsInput,
+	migration_connector::{ConnectorError, ConnectorParams},
+};
+use prisma_client_rust::NewClientError;
+use quaint::prelude::*;
+use sql_migration_connector::SqlMigrationConnector;
+use std::path::Path;
 use thiserror::Error;
+use tokio::fs::{create_dir, remove_dir_all};
+use tracing::debug;
 
-const INIT_MIGRATION: &str = include_str!("../../prisma/migrations/migration_table/migration.sql");
 static MIGRATIONS_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/prisma/migrations");
 
 /// MigrationError represents an error that occurring while opening a initialising and running migrations on the database.
 #[derive(Error, Debug)]
 pub enum MigrationError {
 	#[error("An error occurred while initialising a new database connection: {0}")]
-	DatabaseInitialization(#[from] NewClientError),
-	#[error("An error occurred with the database while applying migrations: {0}")]
-	DatabaseError(#[from] prisma_client_rust::QueryError),
-	#[error("An error occurred reading the embedded migration files. {0}. Please report to Spacedrive developers!")]
-	InvalidEmbeddedMigration(&'static str),
+	NewClient(#[from] NewClientError),
+	#[error("The temporary file path for the database migrations is invalid.")]
+	InvalidDirectory,
+	#[error("An error occurred creating the temporary directory for the migrations: {0}")]
+	CreateDir(std::io::Error),
+	#[error("An error occurred extracting the migrations to the temporary directory: {0}")]
+	ExtractMigrations(std::io::Error),
+	#[error("An error occurred creating the database connection for migrations: {0}")]
+	Quiant(#[from] quaint::error::Error),
+	#[error("An error occurred running the migrations: {0}")]
+	Connector(#[from] ConnectorError),
+	#[error("An error occurred removing the temporary directory for the migrations: {0}")]
+	RemoveDir(std::io::Error),
 }
 
 /// load_and_migrate will load the database from the given path and migrate it to the latest version of the schema.
-pub async fn load_and_migrate(db_url: &str) -> Result<PrismaClient, MigrationError> {
+pub async fn load_and_migrate(
+	base_path: &Path,
+	db_url: &str,
+) -> Result<PrismaClient, MigrationError> {
 	let client = prisma::new_client_with_url(db_url).await?;
+	let temp_migrations_dir = base_path.join("./migrations_temp");
+	let migrations_directory_path = temp_migrations_dir
+		.to_str()
+		.ok_or(MigrationError::InvalidDirectory)?
+		.to_string();
 
-	let migrations_table_missing = client
-		._query_raw::<serde_json::Value>(raw!(
-			"SELECT name FROM sqlite_master WHERE type='table' AND name='_migrations'"
-		))
-		.exec()
-		.await?
-		.is_empty();
-
-	if migrations_table_missing {
-		client._execute_raw(raw!(INIT_MIGRATION)).exec().await?;
+	if temp_migrations_dir.exists() {
+		remove_dir_all(&migrations_directory_path)
+			.await
+			.map_err(MigrationError::RemoveDir)?;
 	}
 
-	let mut migration_directories = MIGRATIONS_DIR
-		.dirs()
-		.map(|dir| {
-			dir.path()
-				.file_name()
-				.ok_or(MigrationError::InvalidEmbeddedMigration(
-					"File has malformed name",
-				))
-				.and_then(|name| {
-					name.to_str()
-						.ok_or(MigrationError::InvalidEmbeddedMigration(
-							"File name contains malformed characters",
-						))
-						.map(|name| (name, dir))
-				})
-		})
-		.filter_map(|v| match v {
-			Ok((name, _)) if name == "migration_table" => None,
-			Ok((name, dir)) => match name[..14].parse::<i64>() {
-				Ok(timestamp) => Some(Ok((name, timestamp, dir))),
-				Err(_) => Some(Err(MigrationError::InvalidEmbeddedMigration(
-					"File name is incorrectly formatted",
-				))),
-			},
-			Err(v) => Some(Err(v)),
-		})
-		.collect::<Result<Vec<_>, _>>()?;
+	create_dir(&temp_migrations_dir)
+		.await
+		.map_err(MigrationError::CreateDir)?;
+	MIGRATIONS_DIR
+		.extract(&temp_migrations_dir)
+		.map_err(MigrationError::ExtractMigrations)?;
 
-	// We sort the migrations so they are always applied in the correct order
-	migration_directories.sort_by(|(_, a_time, _), (_, b_time, _)| a_time.cmp(b_time));
+	let mut connector = match &ConnectionInfo::from_url(&db_url)? {
+		ConnectionInfo::Sqlite { .. } => SqlMigrationConnector::new_sqlite(),
+		ConnectionInfo::InMemorySqlite { .. } => unreachable!(), // This is how it is in the Prisma Rust tests
+	};
+	connector.set_params(ConnectorParams {
+		connection_string: db_url.to_string(),
+		preview_features: BitFlags::empty(),
+		shadow_database_connection_string: None,
+	})?;
 
-	for (name, _, dir) in migration_directories {
-		let migration_file_raw = dir
-			.get_file(dir.path().join("./migration.sql"))
-			.ok_or(MigrationError::InvalidEmbeddedMigration(
-				"Failed to find 'migration.sql' file in '{}' migration subdirectory",
-			))?
-			.contents_utf8()
-			.ok_or(
-				MigrationError::InvalidEmbeddedMigration(
-					"Failed to open the contents of 'migration.sql' file in '{}' migration subdirectory",
-				)
-			)?;
+	let output = apply_migrations(
+		ApplyMigrationsInput {
+			migrations_directory_path,
+		},
+		&mut connector,
+	)
+	.await?;
 
-		// Generate SHA256 checksum of migration
-		let mut checksum = Context::new(&SHA256);
-		checksum.update(migration_file_raw.as_bytes());
-		let checksum = HEXLOWER.encode(checksum.finish().as_ref());
+	remove_dir_all(temp_migrations_dir)
+		.await
+		.map_err(MigrationError::RemoveDir)?;
 
-		// get existing migration by checksum, if it doesn't exist run the migration
-		if client
-			.migration()
-			.find_unique(migration::checksum::equals(checksum.clone()))
-			.exec()
-			.await?
-			.is_none()
-		{
-			// Create migration record
-			client
-				.migration()
-				.create(name.to_string(), checksum.clone(), vec![])
-				.exec()
-				.await?;
-
-			// Split the migrations file up into each individual step and apply them all
-			let steps = migration_file_raw.split(';').collect::<Vec<&str>>();
-			let step_count = steps.len();
-			let steps = &steps[0..step_count - 1];
-
-			for (i, step) in steps.iter().enumerate() {
-				match client._execute_raw(raw!(*step)).exec().await {
-					Ok(_) => {}
-					Err(e) => {
-						// remove the failed migration record so next time it will be retried
-						// potentially an issue if steps were already applied, look into generating down migrations
-						client
-							.migration()
-							.delete(migration::checksum::equals(checksum.clone()))
-							.exec()
-							.await?;
-
-						// TODO: Show UI alert with error message
-						panic!("Error applying migration step: {}", e);
-					}
-				}
-				// Note: there isn't much point storing the steps in the db if we don't generate down migrations and write logic to run the already applied steps in reverse for a failed migration.
-				// for now if a migration fails we abort entirely (see above panic)
-				client
-					.migration()
-					.update(
-						migration::checksum::equals(checksum.clone()),
-						vec![migration::steps_applied::set(i as i32 + 1)],
-					)
-					.exec()
-					.await?;
-			}
-		}
+	for migration in output.applied_migration_names {
+		debug!("Applied migration '{}'", migration);
 	}
 
 	Ok(client)
