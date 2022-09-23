1c85505c03a14b2ac4ae145e2b8d5cc0f506da6d
Brendan Allan <brendonovich@outlook.com>
2022-09-23 14:17:29 +0800

    extract long type to please clippy

--- apps/mobile/rust/src/lib.rs
+++ apps/mobile/rust/src/lib.rs
@@ -11,9 +11,9 @@ use tokio::{
 #[allow(dead_code)]
 pub(crate) static RUNTIME: Lazy<Runtime> = Lazy::new(|| Runtime::new().unwrap());
 
+type LazyNode = Lazy<Mutex<Option<(Arc<Node>, Arc<Router>)>>>;
 #[allow(dead_code)]
-pub(crate) static NODE: Lazy<Mutex<Option<(Arc<Node>, Arc<Router>)>>> =
-	Lazy::new(|| Mutex::new(None));
+pub(crate) static NODE: LazyNode = Lazy::new(|| Mutex::new(None));
 
 #[allow(dead_code)]
 pub(crate) static CLIENT_CONTEXT: Lazy<ClientContext> = Lazy::new(|| ClientContext {
