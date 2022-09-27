fe497ce104c037315d1212029754c1934503bb03
Brendan Allan <brendonovich@outlook.com>
2022-09-27 12:28:32 +0800

    make rust 1.64 requirement more concrete

--- core/src/util/db.rs
+++ core/src/util/db.rs
@@ -22,13 +22,18 @@ pub async fn load_and_migrate(db_url: &str) -> Result<PrismaClient, MigrationErr
 		.map_err(Box::new)?;
 
 	#[cfg(debug_assertions)]
-	client
-		._db_push(
-			std::env::var("SD_FORCE_RESET_DB")
-				.map(|v| v == "true")
-				.unwrap_or(false),
-		)
-		.await?;
+	{
+		let mut builder = client._db_push();
+
+		if std::env::var("SD_FORCE_RESET_DB")
+			.map(|v| v == "true")
+			.unwrap_or(false)
+		{
+			builder = builder.force_reset();
+		}
+
+		builder.await?;
+	}
 
 	#[cfg(not(debug_assertions))]
 	client._migrate_deploy().await?;
