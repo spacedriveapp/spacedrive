bbee2a8e355cec5fe098722407389d77ff8840ba
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-10-05 09:54:41 +0100

    add basic serialization functionality

--- crates/crypto/src/header/file.rs
+++ crates/crypto/src/header/file.rs
@@ -12,13 +12,13 @@ pub struct FileHeader {
 	pub keyslots: Vec<FileKeyslot>,
 }
 
-// There's no need to include `Mode` here, as master keys/keyslots should always use "memory" mode
-// Stream encryption isn't viable for 32 bytes of data
+// I chose to add the mode for uniformity, that way it's clear that master keys are encrypted differently
 // I opted to include a hashing algorithm - it's 2 additional bytes but it may save a version iteration in the future
 // This also may become the universal keyslot standard, so maybe `FileKeyslot` isn't the best name
 pub struct FileKeyslot {
 	pub version: FileKeyslotVersion,
 	pub hashing_algorithm: HashingAlgorithm,
+	pub mode: Mode,
 	pub salt: [u8; SALT_LEN],
 	pub nonce: Vec<u8>,
 	pub master_key: [u8; ENCRYPTED_MASTER_KEY_LEN], // this is encrypted so we can store it
@@ -36,6 +36,32 @@ pub enum FileKeyslotVersion {
 }
 
 impl FileHeader {
+	fn serialize_header_version(version: FileHeaderVersion) -> [u8; 2] {
+		match version {
+			FileHeaderVersion::V1 => [0x0A, 0x01],
+		}
+	}
+
+	fn serialize_keyslot_version(version: FileKeyslotVersion) -> [u8; 2] {
+		match version {
+			FileKeyslotVersion::V1 => [0x0A, 0x01],
+		}
+	}
+
+	fn serialize_algorithm(algorithm: Algorithm) -> [u8; 2] {
+		match algorithm {
+			Algorithm::XChaCha20Poly1305 => [0x0B, 0x01],
+			Algorithm::Aes256Gcm => [0x0B, 0x02],
+		}
+	}
+
+	fn serialize_mode(mode: Mode) -> [u8; 2] {
+		match mode {
+			Mode::Stream => [0x0C, 0x01],
+			Mode::Memory => [0x0C, 0x02],
+		}
+	}
+
 	pub fn serialize(&self) -> Vec<u8> {
 		todo!()
 	}
