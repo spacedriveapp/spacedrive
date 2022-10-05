435a868fbfc3e3d412539661b9746b548d154b45
Jamie Pine <ijamespine@me.com>
2022-10-04 18:15:29 -0700

    macros do rule !

--- core/src/object/kind.rs
+++ core/src/object/kind.rs
@@ -18,7 +18,7 @@ pub enum ObjectKind {
 	// A virtual filesystem directory
 	Folder = 2,
 	// A file that contains human-readable text
-	TextFile = 3,
+	Text = 3,
 	// A virtual directory int
 	Package = 4,
 	// An image file
@@ -34,57 +34,100 @@ pub enum ObjectKind {
 	// A link to another object
 	Alias = 10,
 	// Raw bytes encrypted by Spacedrive with self contained metadata
-	EncryptedBytes = 11,
+	Encrypted = 11,
 	// A link can open web pages, apps or Spaces
-	Link = 12,
+	Key = 12,
+	// A link can open web pages, apps or Spaces
+	Link = 13,
 	// A special filetype that represents a preserved webpage
-	WebPageArchive = 13,
+	WebPageArchive = 14,
 	// A widget is a mini app that can be placed in a Space at various sizes, associated Widget struct required
-	Widget = 14,
+	Widget = 15,
 	// Albums can only have one level of children, and are associated with the Album struct
-	Album = 15,
+	Album = 16,
 	// Its like a folder, but appears like a stack of files, designed for burst photos / associated groups of files
-	Collection = 16,
+	Collection = 17,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub enum Extension {
-	Other(String),
-	Video(VideoExtension),
-	Image(ImageExtension),
-	// Audio(AudioExtension),
-}
-
-impl Display for Extension {
-	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-		match self {
-			Extension::Other(s) => write!(f, "{}", s),
-			Extension::Video(v) => write!(f, "{}", v),
-			Extension::Image(v) => write!(f, "{}", v),
+macro_rules! extension_enum {
+	(
+		Extension {
+			$( $variant:ident($type:ident), )*
 		}
+	) => {
+		#[derive(Debug, Serialize, Deserialize)]
+		pub enum Extension {
+			$( $variant($type), )*
+		}
+		// convert extension to object kind
+		impl From<Extension> for ObjectKind {
+			fn from(ext: Extension) -> Self {
+				match ext {
+					$( Extension::$variant(_) => ObjectKind::$variant, )*
+				}
+			}
+		}
+		impl Display for Extension {
+			fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+				match self {
+					$( Extension::$variant(x) =>  write!(f, "{}", x), )*
+				}
+			}
+		}
+	};
+}
+
+extension_enum! {
+	Extension {
+		Unknown(String),
+		Video(VideoExtension),
+		Image(ImageExtension),
+		Audio(AudioExtension),
+		Archive(ArchiveExtension),
+		Executable(ExecutableExtension),
+		Text(TextExtension),
+		Encrypted(EncryptedExtension),
+		Key(KeyExtension),
 	}
 }
 
 /// Define a public enum with static array of all possible variants
+/// including implementations to convert to/from string
 macro_rules! enum_with_variants {
 	(
 		$(#[$enum_attr:meta])*
-		$name:ident $array:ident {
+		$enum_name:ident $static_array_name:ident {
 			$($(#[$variant_attr:meta])* $variant:ident, )*
 		}
 	) => {
 		#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
 		#[serde(rename_all = "snake_case")]
 		$(#[$enum_attr])*
-		pub enum $name {
+		// construct enum
+		pub enum $enum_name {
 			$( $(#[$variant_attr])* $variant, )*
 		}
-		pub static $array: &[$name] = &[
-			$( $name::$variant, )*
+		// a static array of all variants
+		pub static $static_array_name: &[$enum_name] = &[
+			$( $enum_name::$variant, )*
 		];
+		// convert from string
+		impl FromStr for $enum_name {
+			type Err = serde_json::Error;
+			fn from_str(s: &str) -> Result<Self, Self::Err> {
+				serde_json::from_value(Value::String(s.to_string()))
+			}
+		}
+		// convert to string
+		impl std::fmt::Display for $enum_name {
+			fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+				write!(f, "{}", serde_json::to_string(self).unwrap()) // SAFETY: This is safe
+			}
+		}
 	}
 }
 
+// video extensions
 enum_with_variants! {
 	VideoExtension ALL_VIDEO_EXTENSIONS {
 		Avi,
@@ -112,6 +155,7 @@ enum_with_variants! {
 	}
 }
 
+// image extensions
 enum_with_variants! {
 	ImageExtension _ALL_IMAGE_EXTENSIONS {
 		Jpg,
@@ -123,6 +167,7 @@ enum_with_variants! {
 		Webp,
 		Svg,
 		Ico,
+		Heic,
 	}
 }
 
@@ -137,7 +182,6 @@ enum_with_variants! {
 		Flac,
 		Ogg,
 		Opus,
-		Webm,
 	}
 }
 
@@ -171,6 +215,9 @@ enum_with_variants! {
 enum_with_variants! {
 	DocumentExtension _ALL_DOCUMENT_EXTENSIONS {
 		Pdf,
+		Key,
+		Pages,
+		Numbers,
 		Doc,
 		Docx,
 		Xls,
@@ -180,10 +227,11 @@ enum_with_variants! {
 		Odt,
 		Ods,
 		Odp,
+		Ics,
 	}
 }
 
-// Text file extensions
+// text file extensions
 enum_with_variants! {
 	TextExtension _ALL_TEXT_EXTENSIONS {
 		Txt,
@@ -197,29 +245,23 @@ enum_with_variants! {
 		Md,
 	}
 }
-// Text file extensions
+// encrypted file extensions
 enum_with_variants! {
 	EncryptedExtension _ALL_ENCRYPTED_EXTENSIONS {
-		Bytes,
-		Container,
-		Block,
+		Bit,	// Spacedrive encrypted file
+		Box,	// Spacedrive container
+		Block,// Spacedrive block storage,
 	}
 }
 
-macro_rules! impl_str_enum {
-	($($enum_name:ident)+) => { $(
-		impl FromStr for $enum_name {
-			type Err = serde_json::Error;
-			fn from_str(s: &str) -> Result<Self, Self::Err> {
-				serde_json::from_value(Value::String(s.to_string()))
-			}
-		}
-		impl std::fmt::Display for $enum_name {
-			fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-				write!(f, "{}", serde_json::to_string(self).unwrap()) // SAFETY: This is safe
-			}
-		}
-	)+ };
+// key extensions
+enum_with_variants! {
+	KeyExtension _ALL_KEY_EXTENSIONS {
+		Pgp,
+		Pub,
+		Pem,
+		P12,
+		P8,
+		Keychain,
+	}
 }
-
-impl_str_enum!(VideoExtension ImageExtension);
