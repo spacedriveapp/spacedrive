90e90e8c55696b47a1c9622881f2550db48581c3
사무엘 <kor_pixel@fastmail.com>
2022-10-07 09:09:32 +0900

    fixed the overflow

--- apps/landing/src/pages/index.page.tsx
+++ apps/landing/src/pages/index.page.tsx
@@ -82,7 +82,7 @@ function Page() {
 	}, []);
 
 	return (
-		<div className="flex flex-col items-center w-full px-4">
+		<div className="flex flex-col items-center w-full px-4 overflow-hidden">
 			<Helmet>
 				<title>Spacedrive — A file manager from the future.</title>
 				<meta
