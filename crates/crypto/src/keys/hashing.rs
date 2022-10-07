0db96038233027890c27fa4d77e274a9d98e2943
jake <77554505+brxken128@users.noreply.github.com>
2022-10-07 15:31:40 +0100

    [ENG-250] Crypto Library (#400)
    * add crypto crate with some functionality
    
    * formatting
    
    * add `argon2id` parameter levels
    
    * add descriptive comments
    
    * add stream decryption objects
    
    * add `StreamEncryptor` struct
    
    * add `StreamDecryptor`
    
    * general cleanup
    
    * add `thiserror` and error handling
    
    * add header structs
    
    * add basic serialization functionality
    
    * advance serialization
    
    * finish serialization
    
    * clean up serialization and use `impl`
    
    * finalise deserialization
    
    * add stream helper functions and remove old code
    
    * add AAD creation and retrieval
    
    * add important comment
    
    * add `ChaCha20Rng` as a CSPRNG
    
    * cleanup and crate-wide clippy lints
    
    * apply nursery lints
    
    * add in-memory encryption objects
    
    * rename `utils` to `objects`
    
    * move (de)serialization rules to separate file
    
    * add header-write helper function
    
    * add password hash helper function
    
    * add `decrypt_master_key` function
    
    * cleanup, formatting, linting
    
    * move keyslots to separate file, and rename them
    
    * add basic comments
    
    * remove `secrecy` dependency and import `protected`
    
    * add `to_array` helper function
    
    * `sd_crypto` -> `sd-crypto`
    
    * remove manual drops
    
    * add clippy allows
    
    * add `new()` for `Keyslot` and `FileHeader`
    
    * remove license
    
    * zeroize read buffer on error
    
    * magic bytes are now `ballapp`
    
    Co-authored-by: Brendan Allan <brendonovich@outlook.com>
    Co-authored-by: Jamie Pine <32987599+jamiepine@users.noreply.github.com>

---
+++ crates/crypto/src/keys/hashing.rs
@@ -0,0 +1,65 @@
+use crate::protected::Protected;
+use crate::{error::Error, primitives::SALT_LEN};
+use argon2::Argon2;
+
+// These names are not final
+// I'm considering adding an `(i32)` to each, to allow specific versioning of each parameter version
+// These will be serializable/deserializable with regards to the header/storage of this information
+#[derive(Clone, Copy)]
+pub enum Params {
+	Standard,
+	Hardened,
+	Paranoid,
+}
+
+impl Params {
+	#[must_use]
+	pub fn get_argon2_params(&self) -> argon2::Params {
+		match self {
+			// We can use `.unwrap()` here as the values are hardcoded, and this shouldn't error
+			// The values are NOT final, as we need to find a good average.
+			// It's very hardware dependant but we should aim for at least 16MB of RAM usage on standard
+			// Provided they all take one (ish) second or longer, and less than 3/4 seconds (for paranoid), they will be fine
+			// It's not so much the parameters themselves that matter, it's the duration (and ensuring that they use enough RAM to hinder ASIC brute-force attacks)
+			Self::Standard => {
+				argon2::Params::new(131_072, 8, 4, Some(argon2::Params::DEFAULT_OUTPUT_LEN))
+					.unwrap()
+			}
+			Self::Paranoid => {
+				argon2::Params::new(262_144, 8, 4, Some(argon2::Params::DEFAULT_OUTPUT_LEN))
+					.unwrap()
+			}
+			Self::Hardened => {
+				argon2::Params::new(524_288, 8, 4, Some(argon2::Params::DEFAULT_OUTPUT_LEN))
+					.unwrap()
+			}
+		}
+	}
+}
+
+// Shouldn't be called directly - call it on the `HashingAlgorithm` struct
+/// This function should NOT be called directly!
+///
+/// Call it via the `HashingAlgorithm` struct (e.g. `HashingAlgorithm::Argon2id(Params::Standard).hash()`)
+#[allow(clippy::needless_pass_by_value)]
+pub fn password_hash_argon2id(
+	password: Protected<Vec<u8>>,
+	salt: [u8; SALT_LEN],
+	params: Params,
+) -> Result<Protected<[u8; 32]>, Error> {
+	let mut key = [0u8; 32];
+
+	let argon2 = Argon2::new(
+		argon2::Algorithm::Argon2id,
+		argon2::Version::V0x13,
+		params.get_argon2_params(),
+	);
+
+	let result = argon2.hash_password_into(password.expose(), &salt, &mut key);
+
+	if result.is_ok() {
+		Ok(Protected::new(key))
+	} else {
+		Err(Error::PasswordHash)
+	}
+}
---
+++ crates/crypto/src/objects/memory.rs
@@ -0,0 +1,84 @@
+use crate::protected::Protected;
+use aead::{Aead, KeyInit, Payload};
+use aes_gcm::Aes256Gcm;
+use chacha20poly1305::XChaCha20Poly1305;
+
+use crate::{error::Error, primitives::Algorithm};
+
+// Although these two objects are identical, I think it'll be good practice to keep their usage separate.
+// One for encryption, and one for decryption. This can easily be changed if needed.
+pub enum MemoryEncryption {
+	XChaCha20Poly1305(Box<XChaCha20Poly1305>),
+	Aes256Gcm(Box<Aes256Gcm>),
+}
+
+pub enum MemoryDecryption {
+	XChaCha20Poly1305(Box<XChaCha20Poly1305>),
+	Aes256Gcm(Box<Aes256Gcm>),
+}
+
+impl MemoryEncryption {
+	#[allow(clippy::needless_pass_by_value)]
+	pub fn new(key: Protected<[u8; 32]>, algorithm: Algorithm) -> Result<Self, Error> {
+		let encryption_object = match algorithm {
+			Algorithm::XChaCha20Poly1305 => {
+				let cipher = XChaCha20Poly1305::new_from_slice(key.expose())
+					.map_err(|_| Error::MemoryModeInit)?;
+
+				Self::XChaCha20Poly1305(Box::new(cipher))
+			}
+			Algorithm::Aes256Gcm => {
+				let cipher =
+					Aes256Gcm::new_from_slice(key.expose()).map_err(|_| Error::MemoryModeInit)?;
+
+				Self::Aes256Gcm(Box::new(cipher))
+			}
+		};
+
+		Ok(encryption_object)
+	}
+
+	pub fn encrypt<'msg, 'aad>(
+		&self,
+		plaintext: impl Into<Payload<'msg, 'aad>>,
+		nonce: &[u8],
+	) -> aead::Result<Vec<u8>> {
+		match self {
+			Self::XChaCha20Poly1305(m) => m.encrypt(nonce.into(), plaintext),
+			Self::Aes256Gcm(m) => m.encrypt(nonce.into(), plaintext),
+		}
+	}
+}
+
+impl MemoryDecryption {
+	#[allow(clippy::needless_pass_by_value)]
+	pub fn new(key: Protected<[u8; 32]>, algorithm: Algorithm) -> Result<Self, Error> {
+		let decryption_object = match algorithm {
+			Algorithm::XChaCha20Poly1305 => {
+				let cipher = XChaCha20Poly1305::new_from_slice(key.expose())
+					.map_err(|_| Error::MemoryModeInit)?;
+
+				Self::XChaCha20Poly1305(Box::new(cipher))
+			}
+			Algorithm::Aes256Gcm => {
+				let cipher =
+					Aes256Gcm::new_from_slice(key.expose()).map_err(|_| Error::MemoryModeInit)?;
+
+				Self::Aes256Gcm(Box::new(cipher))
+			}
+		};
+
+		Ok(decryption_object)
+	}
+
+	pub fn decrypt<'msg, 'aad>(
+		&self,
+		ciphertext: impl Into<Payload<'msg, 'aad>>,
+		nonce: &[u8],
+	) -> aead::Result<Vec<u8>> {
+		match self {
+			Self::XChaCha20Poly1305(m) => m.decrypt(nonce.into(), ciphertext),
+			Self::Aes256Gcm(m) => m.decrypt(nonce.into(), ciphertext),
+		}
+	}
+}
