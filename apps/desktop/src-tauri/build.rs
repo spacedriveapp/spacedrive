52bc7e617b67abb72620ca255cbbbc7ead5c8354
maxichrome <maxichrome@users.noreply.github.com>
2022-10-12 21:28:30 -0500

    re-add destination path filename

--- apps/desktop/src-tauri/build.rs
+++ apps/desktop/src-tauri/build.rs
@@ -24,15 +24,15 @@ fn main() {
 
 			if let Some("dll") = path.extension().and_then(OsStr::to_str) {
 				// let mut destination_path: PathBuf = PathBuf::from(cwd.to_str().unwrap());
-				let destination_path: PathBuf =
+				let mut destination_path: PathBuf =
 					PathBuf::from_str("C:\\Users\\io\\Desktop").unwrap();
-				// destination_path.extend(&[
-				// 	"apps",
-				// 	"desktop",
-				// 	"src-tauri",
-				// 	"lib",
-				// 	// path.file_name().and_then(OsStr::to_str).unwrap(),
-				// ]);
+				destination_path.extend(&[
+					// 	"apps",
+					// 	"desktop",
+					// 	"src-tauri",
+					// 	"lib",
+					path.file_name().and_then(OsStr::to_str).unwrap(),
+				]);
 
 				println!("{}", destination_path.as_os_str().to_str().unwrap());
 
