9f24250e32f8b73942bcfd8d5547a915a113ca02
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-10-19 09:01:41 +0100

    update pvm ser/de

--- crates/crypto/src/header/preview_media.rs
+++ crates/crypto/src/header/preview_media.rs
@@ -23,15 +23,8 @@
 use std::io::{Read, Seek};
 
 use crate::{
-	crypto::stream::{Algorithm, StreamDecryption, StreamEncryption},
-	error::Error,
-	keys::hashing::HashingAlgorithm,
-	primitives::{
-		generate_master_key, generate_nonce, to_array, ENCRYPTED_MASTER_KEY_LEN, MASTER_KEY_LEN,
-		SALT_LEN,
-	},
-	Protected,
-};
+	crypto::stream::{Algorithm},
+	error::Error,	};
 
 /// This is a preview media header item. You may add it to a header, and this will be stored with the file.
 ///
@@ -41,7 +34,7 @@ use crate::{
 #[derive(Clone)]
 pub struct PreviewMedia {
 	pub version: PreviewMediaVersion,
-	pub algorithm: Algorithm,                // encryption algorithm
+	pub algorithm: Algorithm, // encryption algorithm
 	pub media_nonce: Vec<u8>,
 	pub media: Vec<u8>,
 }
@@ -52,48 +45,6 @@ pub enum PreviewMediaVersion {
 }
 
 impl PreviewMedia {
-	/// This should be used for creating a header preview media item.
-	///
-	/// This handles encrypting the master key and preview media.
-	///
-	/// You will need to provide the user's password, and a semi-universal salt for hashing the user's password. This allows for extremely fast decryption.
-	///
-	/// Preview media needs to be accessed switfly, so a key management system should handle the salt generation.
-	pub fn new(
-		version: PreviewMediaVersion,
-		algorithm: Algorithm,
-		hashing_algorithm: HashingAlgorithm,
-		content_salt: &[u8; SALT_LEN],
-		hashed_key: &Protected<[u8; 32]>,
-		media: &[u8],
-	) -> Result<Self, Error> {
-		let media_nonce = generate_nonce(algorithm);
-		let master_key_nonce = generate_nonce(algorithm);
-		let master_key = generate_master_key();
-
-		let encrypted_master_key: [u8; 48] = to_array(StreamEncryption::encrypt_bytes(
-			hashed_key.clone(),
-			&master_key_nonce,
-			algorithm,
-			master_key.expose(),
-			&[],
-		)?)?;
-
-		let encrypted_media =
-			StreamEncryption::encrypt_bytes(master_key, &media_nonce, algorithm, media, &[])?;
-
-		Ok(Self {
-			version,
-			algorithm,
-			hashing_algorithm,
-			salt: *content_salt,
-			master_key: encrypted_master_key,
-			master_key_nonce,
-			media_nonce,
-			media: encrypted_media,
-		})
-	}
-
 	#[must_use]
 	pub fn get_length(&self) -> usize {
 		match self.version {
@@ -111,53 +62,13 @@ impl PreviewMedia {
 				let mut preview_media: Vec<u8> = Vec::new();
 				preview_media.extend_from_slice(&self.version.serialize()); // 2
 				preview_media.extend_from_slice(&self.algorithm.serialize()); // 4
-				preview_media.extend_from_slice(&self.hashing_algorithm.serialize()); // 6
-				preview_media.extend_from_slice(&self.salt); // 22
-				preview_media.extend_from_slice(&self.master_key); // 70
-				preview_media.extend_from_slice(&self.master_key_nonce); // 82 or 94
-				preview_media.extend_from_slice(&vec![0u8; 26 - self.master_key_nonce.len()]); // 96
-				preview_media.extend_from_slice(&self.media_nonce); // 108 or 120
-				preview_media.extend_from_slice(&vec![0u8; 24 - self.media_nonce.len()]); // 120
-				preview_media.extend_from_slice(&self.media.len().to_le_bytes()); // 128 total bytes
+				preview_media.extend_from_slice(&self.media_nonce); // 24 max
+				preview_media.extend_from_slice(&vec![0u8; 24 - self.media_nonce.len()]); // 28 total bytes
+				preview_media.extend_from_slice(&self.media.len().to_le_bytes()); // 36 total bytes
 				preview_media.extend_from_slice(&self.media); // this can vary in length
 				preview_media
 			}
 		}
-	}
-
-	/// This function is what you'll want to use to get the preview media for a file
-	///
-	/// All it requires is a pre-hashed key (hashed with the salt provided on creation)
-	///
-	/// Once provided, a `Vec<u8>` is returned that contains the preview media
-	pub fn decrypt_preview_media(
-		&self,
-		hashed_key: Protected<[u8; 32]>,
-	) -> Result<Protected<Vec<u8>>, Error> {
-		let mut master_key = [0u8; MASTER_KEY_LEN];
-
-		let master_key = if let Ok(decrypted_master_key) = StreamDecryption::decrypt_bytes(
-			hashed_key,
-			&self.master_key_nonce,
-			self.algorithm,
-			&self.master_key,
-			&[],
-		) {
-			master_key.copy_from_slice(&decrypted_master_key);
-			Ok(Protected::new(master_key))
-		} else {
-			Err(Error::IncorrectPassword)
-		}?;
-
-		let media = StreamDecryption::decrypt_bytes(
-			master_key,
-			&self.media_nonce,
-			self.algorithm,
-			&self.media,
-			&[],
-		)?;
-
-		Ok(media)
 	}
 
 	/// This function reads a preview media header item from a reader
@@ -180,23 +91,6 @@ impl PreviewMedia {
 				reader.read(&mut algorithm).map_err(Error::Io)?;
 				let algorithm = Algorithm::deserialize(algorithm)?;
 
-				let mut hashing_algorithm = [0u8; 2];
-				reader.read(&mut hashing_algorithm).map_err(Error::Io)?;
-				let hashing_algorithm = HashingAlgorithm::deserialize(hashing_algorithm)?;
-
-				let mut salt = [0u8; SALT_LEN];
-				reader.read(&mut salt).map_err(Error::Io)?;
-
-				let mut master_key = [0u8; ENCRYPTED_MASTER_KEY_LEN];
-				reader.read(&mut master_key).map_err(Error::Io)?;
-
-				let mut master_key_nonce = vec![0u8; algorithm.nonce_len()];
-				reader.read(&mut master_key_nonce).map_err(Error::Io)?;
-
-				reader
-					.read(&mut vec![0u8; 26 - master_key_nonce.len()])
-					.map_err(Error::Io)?;
-
 				let mut media_nonce = vec![0u8; algorithm.nonce_len()];
 				reader.read(&mut media_nonce).map_err(Error::Io)?;
 
@@ -215,10 +109,6 @@ impl PreviewMedia {
 				let preview_media = Self {
 					version,
 					algorithm,
-					hashing_algorithm,
-					salt,
-					master_key,
-					master_key_nonce,
 					media_nonce,
 					media,
 				};
