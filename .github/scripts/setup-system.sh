8d23baa0e44e8308f6b34ec7e34e9c3c7870e3c2
PyRo1121 <o.latham@me.com>
2022-10-20 14:30:09 -0400

    Backed ffmpeg requirement to ^4.4.0 due to not all linux distro support ffmpeg5 natively as of yet. Minor rewrite of distro detection in setup-system.sh due to which 'package manager' seemed loose for detecting a distro

--- .github/scripts/setup-system.sh
+++ .github/scripts/setup-system.sh
@@ -11,12 +11,6 @@ trap 'script_failure $LINENO' ERR
 
 echo "Setting up your system for Spacedrive development!"
 
-if ! which cargo &> /dev/null; then
-        echo "Rust was not detected on your system. Ensure the 'rustc' and 'cargo' binaries are in your \$PATH."
-        exit 1
-fi
-
-
 
 if [ "$1" == "mobile" ]; then
         echo "Setting up for mobile development!"
@@ -72,7 +66,9 @@ elif [ -f /etc/debian_version -o "$DISTRO" == "Debian" -o "$DISTRO" == "Ubuntu" 
         DEBIAN_FFMPEG_DEPS="libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavutil-dev libswscale-dev libswresample-dev ffmpeg" # FFMPEG dependencies
         DEBIAN_TAURI_DEPS="libwebkit2gtk-4.0-dev build-essential curl wget libssl-dev libgtk-3-dev libayatana-appindicator3-dev librsvg2-dev" # Tauri dependencies
         DEBIAN_BINDGEN_DEPS="pkg-config clang" # Bindgen dependencies - it's used by a dependency of Spacedrive
-
+        if ! which pnpm &> /dev/null; then
+                echo "PNPM was not detected on your system. Ensure the 'pnpm' command is in your \$PATH. You are not able to use Yarn or NPM." && curl -fsSL https://get.pnpm.io/install.sh | sh -
+        fi
         sudo apt-get -y update
         sudo apt-get -y install ${SPACEDRIVE_CUSTOM_APT_FLAGS:-} $DEBIAN_TAURI_DEPS $DEBIAN_FFMPEG_DEPS $DEBIAN_BINDGEN_DEPS
 
@@ -85,6 +81,9 @@ elif [ -f /etc/os-release -o "$DISTRO" == "openSUSE" ]; then
         if ! which pnpm &> /dev/null; then
                 echo "PNPM was not detected on your system. Ensure the 'pnpm' command is in your \$PATH. You are not able to use Yarn or NPM." && curl -fsSL https://get.pnpm.io/install.sh | sh -
         fi
+        if ! which cargo &> /dev/null; then
+                echo "Rust was not detected on your system. Ensure the 'rustc' and 'cargo' binaries are in your \$PATH." && curl https://sh.rustup.rs -sSf | sh -s -- -y  && source $HOME/.cargo/env
+        fi
         sudo zypper up
         sudo zypper in $SUSE_TAURI_DEPS $SUSE_FFMPEG_DEPS $SUSE_BINDGEN_DEPS
         sudo zypper in -t pattern devel_basis
@@ -94,7 +93,9 @@ elif [ -f /usr/lib/os-release -o "$DISTRO" == "Arch" ]; then
         ARCH_TAURI_DEPS="webkit2gtk base-devel curl wget openssl appmenu-gtk-module gtk3 libappindicator-gtk3 librsvg libvips" # Tauri deps https://tauri.studio/guides/getting-started/setup/linux#1-system-dependencies
         ARCH_FFMPEG_DEPS="ffmpeg" # FFMPEG dependencies
         ARCH_BINDGEN_DEPS="clang" # Bindgen dependencies - it's used by a dependency of Spacedrive
-
+        if ! which pnpm &> /dev/null; then
+                echo "PNPM was not detected on your system. Ensure the 'pnpm' command is in your \$PATH. You are not able to use Yarn or NPM." && curl -fsSL https://get.pnpm.io/install.sh | sh -
+        fi
         sudo pacman -Syu
         sudo pacman -S --needed $ARCH_TAURI_DEPS $ARCH_FFMPEG_DEPS $ARCH_BINDGEN_DEPS
 
@@ -103,7 +104,9 @@ elif [ -f /etc/redhat-release -o "$DISTRO" == "RedHat" -o "$DISTRO" == "CentOS" 
         FEDORA_TAURI_DEPS="webkit2gtk3-devel.x86_64 openssl-devel curl wget libappindicator-gtk3 librsvg2-devel" # Tauri dependencies
         FEDORA_FFMPEG_DEPS="ffmpeg ffmpeg-devel" # FFMPEG dependencies
         FEDORA_BINDGEN_DEPS="clang" # Bindgen dependencies - it's used by a dependency of Spacedrive
-
+        if ! which pnpm &> /dev/null; then
+                echo "PNPM was not detected on your system. Ensure the 'pnpm' command is in your \$PATH. You are not able to use Yarn or NPM." && curl -fsSL https://get.pnpm.io/install.sh | sh -
+        fi
         sudo dnf check-update
         sudo dnf install $FEDORA_TAURI_DEPS $FEDORA_FFMPEG_DEPS $FEDORA_BINDGEN_DEPS
         sudo dnf group install "C Development Tools and Libraries"
