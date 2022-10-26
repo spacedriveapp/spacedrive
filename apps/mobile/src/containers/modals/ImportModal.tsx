1bdeaeca3c5c48da7ead9a31d1aa1a65b0296064
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2022-10-26 21:02:08 +0300

    Media Library permission stuff

--- apps/mobile/src/containers/modals/ImportModal.tsx
+++ apps/mobile/src/containers/modals/ImportModal.tsx
@@ -1,19 +1,25 @@
 import { BottomSheetModal } from '@gorhom/bottom-sheet';
 import { useLibraryMutation } from '@sd/client';
-import * as MediaLibrary from 'expo-media-library';
+import * as ML from 'expo-media-library';
 import { forwardRef, useCallback } from 'react';
-import { Text, View } from 'react-native';
+import { Alert, Platform, Text, View } from 'react-native';
 import DocumentPicker from 'react-native-document-picker';
 import RFS from 'react-native-fs';
 import { Modal } from '~/components/layout/Modal';
 import { Button } from '~/components/primitive/Button';
+import useForwardedRef from '~/hooks/useForwardedRef';
 import tw from '~/lib/tailwind';
 
 const ImportModal = forwardRef<BottomSheetModal, unknown>((_, ref) => {
+	const modalRef = useForwardedRef(ref);
+
 	const { mutate: createLocation } = useLibraryMutation('locations.create', {
-		onError(error, variables, context) {
-			// TODO: Toast message
-			console.log(error);
+		onError: (error, variables, context) => {
+			console.error(error);
+		},
+		onSettled: (data, error, variables, context) => {
+			// Close the modal
+			modalRef.current?.close();
 		}
 	});
 
@@ -28,39 +34,74 @@ const ImportModal = forwardRef<BottomSheetModal, unknown>((_, ref) => {
 				indexer_rules_ids: []
 			});
 		} catch (err) {
-			console.warn(err);
+			console.error(err);
 		}
 	}, [createLocation]);
 
-	// const [status, requestPermission] = MediaLibrary.usePermissions();
-	// console.log(status);
-
+	// Temporary until we decide on the user flow
 	const handlePhotosButton = useCallback(async () => {
-		const permission = await MediaLibrary.requestPermissionsAsync();
-		console.log(permission);
+		// Check if we have full access to the photos library
+		let permission = await ML.getPermissionsAsync();
+		// {"accessPrivileges": "none", "canAskAgain": true, "expires": "never", "granted": false, "status": "undetermined"}
 
-		const assets = await MediaLibrary.getAssetsAsync({ mediaType: MediaLibrary.MediaType.photo });
-		assets.assets.map(async (i) => {
-			console.log((await MediaLibrary.getAssetInfoAsync(i)).localUri);
-		});
-		// console.log(await MediaLibrary.getAssetInfoAsync({id: }))
-	}, []);
+		if (
+			permission.status === ML.PermissionStatus.UNDETERMINED ||
+			(permission.status === ML.PermissionStatus.DENIED && permission.canAskAgain)
+		) {
+			permission = await ML.requestPermissionsAsync();
+		}
 
-	const testFN = useCallback(async () => {
-		const URL = decodeURIComponent(RFS.DocumentDirectoryPath + '/libraries');
-		RFS.readdir(URL).then((files) => {
-			files.forEach((file) => {
-				console.log(file);
-			});
+		// Permission Denied
+		if (permission.status === ML.PermissionStatus.DENIED) {
+			Alert.alert(
+				'Permission required',
+				'You need to grant access to your photos library to import your photos/videos.'
+			);
+			return;
+		}
+
+		// Limited Permission (Can't access path)
+		if (permission.accessPrivileges === 'limited') {
+			Alert.alert(
+				'Limited access',
+				'You need to grant full access to your photos library to import your photos/videos.'
+			);
+			return;
+		}
+
+		// Permission Granted
+		// TODO: Find the paths??
+		const libraryPath = Platform.select({
+			android: '',
+			ios: RFS.MainBundlePath + '/Media/DCIM'
 		});
-	}, []);
+
+		createLocation({
+			path: libraryPath,
+			indexer_rules_ids: []
+		});
+
+		// const assets = await ML.getAssetsAsync({ mediaType: ML.MediaType.photo });
+		// assets.assets.map(async (i) => {
+		// 	console.log((await ML.getAssetInfoAsync(i)).localUri);
+		// });
+	}, [createLocation]);
+
+	// const testFN = useCallback(async () => {
+	// 	const URL = decodeURIComponent(RFS.DocumentDirectoryPath + '/libraries');
+	// 	RFS.readdir(URL).then((files) => {
+	// 		files.forEach((file) => {
+	// 			console.log(file);
+	// 		});
+	// 	});
+	// }, []);
 
 	return (
 		<Modal ref={ref} snapPoints={['20%']}>
 			<View style={tw`flex-1 px-6 pt-1 pb-2 bg-gray-600`}>
-				<Button size="md" variant="primary" style={tw`my-2`} onPress={testFN}>
+				{/* <Button size="md" variant="primary" style={tw`my-2`} onPress={testFN}>
 					<Text>TEST</Text>
-				</Button>
+				</Button> */}
 				<Button size="md" variant="primary" style={tw`my-2`} onPress={handleFilesButton}>
 					<Text>Import from Files</Text>
 				</Button>
