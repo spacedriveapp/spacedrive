23f10c9ed6c7ea71d30c3dbff55c2a5af8e7c35b
Brendan Allan <brendonovich@outlook.com>
2022-10-27 16:34:45 +0800

    use bleeding rspc

--- crates/sync/example/web/src/index.tsx
+++ crates/sync/example/web/src/index.tsx
@@ -1,16 +1,15 @@
 /* @refresh reload */
 import { Suspense } from 'react';
-import { render } from 'react-dom';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
 import './index.css';
 import { queryClient, rspc, rspcClient } from './utils/rspc';
 
-const root = createRoot(document.getElementById('root') as HTMLElement).render(
-	<rspc.Provider client={rspcClient} queryClient={queryClient}>
-		<Suspense fallback={null}>
-			<App />
-		</Suspense>
-	</rspc.Provider>
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <rspc.Provider client={rspcClient} queryClient={queryClient}>
+    <Suspense fallback={null}>
+      <App />
+    </Suspense>
+  </rspc.Provider>
 );
