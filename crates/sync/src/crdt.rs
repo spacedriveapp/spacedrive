b713c896a099f1e4605bf5da5fc8517b735cbc0b
Brendan Allan <brendonovich@outlook.com>
2022-10-28 14:12:28 +0800

    More Sync Stuff (#438)
    * broken sync example
    
    * move to react
    
    * filter owned ops more
    
    * update deps
    
    * working sync
    
    * relation operations
    
    * fix pnpm lock
    
    * please rustfmt
    
    * fix api
    
    * compare strings properly
    
    * please clippy
    
    * feature gate logging
    
    * use bleeding rspc in example
    
    * use bleeding rspc
    
    * use generated rspc client

--- crates/sync/src/crdt.rs
+++ crates/sync/src/crdt.rs
@@ -1,63 +1,79 @@
 use std::fmt::Debug;
 
+use rspc::Type;
 use serde::{Deserialize, Serialize};
 use serde_json::{Map, Value};
 use uhlc::NTP64;
 use uuid::Uuid;
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, Type)]
+pub enum RelationOperationData {
+	Create,
+	Update { field: String, value: Value },
+	Delete,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Type)]
+pub struct RelationOperation {
+	pub relation_item: Uuid,
+	pub relation_group: Uuid,
+	pub relation: String,
+	pub data: RelationOperationData,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Type)]
 pub enum SharedOperationCreateData {
 	Unique(Map<String, Value>),
 	Atomic,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, Type)]
 pub enum SharedOperationData {
 	Create(SharedOperationCreateData),
 	Update { field: String, value: Value },
 	Delete,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, Type)]
 pub struct SharedOperation {
-	pub record_id: Value, // Uuid,
+	pub record_id: Uuid,
 	pub model: String,
 	pub data: SharedOperationData,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, Type)]
 pub enum OwnedOperationData {
 	Create(Map<String, Value>),
 	Update(Map<String, Value>),
 	Delete,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, Type)]
 pub struct OwnedOperationItem {
 	pub id: Value,
 	pub data: OwnedOperationData,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, Type)]
 pub struct OwnedOperation {
 	pub model: String,
 	pub items: Vec<OwnedOperationItem>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, Type)]
 #[serde(untagged)]
 pub enum CRDTOperationType {
 	Shared(SharedOperation),
-	// Relation(RelationOperation),
+	Relation(RelationOperation),
 	Owned(OwnedOperation),
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Type)]
 pub struct CRDTOperation {
 	pub node: Uuid,
 	pub timestamp: NTP64,
 	pub id: Uuid,
-	#[serde(flatten)]
+	// #[serde(flatten)]
 	pub typ: CRDTOperationType,
 }
 
