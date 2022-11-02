ec72c8fb5f96b4a8aca8b0e08002738f24fb4aa1
Jamie Pine <ijamespine@me.com>
2022-11-02 12:28:50 -0700

    small improvements to libraries settings

--- packages/interface/src/screens/settings/node/LibrariesSettings.tsx
+++ packages/interface/src/screens/settings/node/LibrariesSettings.tsx
@@ -1,7 +1,7 @@
-import { useBridgeMutation, useBridgeQuery } from '@sd/client';
+import { useBridgeMutation, useBridgeQuery, useCurrentLibrary } from '@sd/client';
 import { LibraryConfigWrapped } from '@sd/client';
-import { Button, ButtonLink, Card } from '@sd/ui';
-import { DotsSixVertical, Pen, Trash } from 'phosphor-react';
+import { Button, ButtonLink, Card, tw } from '@sd/ui';
+import { Database, DotsSixVertical, Link, Pen, Pencil, Trash } from 'phosphor-react';
 import { useState } from 'react';
 
 import CreateLibraryDialog from '../../../components/dialog/CreateLibraryDialog';
@@ -9,7 +9,9 @@ import DeleteLibraryDialog from '../../../components/dialog/DeleteLibraryDialog'
 import { SettingsContainer } from '../../../components/settings/SettingsContainer';
 import { SettingsHeader } from '../../../components/settings/SettingsHeader';
 
-function LibraryListItem(props: { library: LibraryConfigWrapped }) {
+const Pill = tw.span`px-1.5 ml-2 py-[2px] rounded text-xs font-medium bg-accent`;
+
+function LibraryListItem(props: { library: LibraryConfigWrapped; current: boolean }) {
 	const [openDeleteModal, setOpenDeleteModal] = useState(false);
 
 	const deleteLibrary = useBridgeMutation('library.delete', {
@@ -22,15 +24,21 @@ function LibraryListItem(props: { library: LibraryConfigWrapped }) {
 		<Card>
 			<DotsSixVertical weight="bold" className="mt-[15px] mr-3 opacity-30" />
 			<div className="flex-1 my-0.5">
-				<h3 className="font-semibold">{props.library.config.name}</h3>
+				<h3 className="font-semibold">
+					{props.library.config.name}
+					{props.current && <Pill>Current</Pill>}
+				</h3>
 				<p className="mt-0.5 text-xs text-ink-dull">{props.library.uuid}</p>
 			</div>
 			<div className="flex flex-row items-center space-x-2">
-				<ButtonLink size="icon" to="/settings/library" variant="gray">
-					<Pen className="w-4 h-4" />
+				<Button className="!p-1.5" onClick={() => {}} variant="gray">
+					<Database className="w-4 h-4" />
+				</Button>
+				<ButtonLink className="!p-1.5" to="/settings/library" variant="gray">
+					<Pencil className="w-4 h-4" />
 				</ButtonLink>
 				<DeleteLibraryDialog libraryUuid={props.library.uuid}>
-					<Button size="icon" variant="gray">
+					<Button className="!p-1.5" variant="gray">
 						<Trash className="w-4 h-4" />
 					</Button>
 				</DeleteLibraryDialog>
@@ -42,6 +50,8 @@ function LibraryListItem(props: { library: LibraryConfigWrapped }) {
 export default function LibrarySettings() {
 	const { data: libraries } = useBridgeQuery(['library.list']);
 	const [open, setOpen] = useState(false);
+
+	const { library: currentLibrary } = useCurrentLibrary();
 
 	return (
 		<SettingsContainer>
@@ -60,9 +70,19 @@ export default function LibrarySettings() {
 			/>
 
 			<div className="space-y-2">
-				{libraries?.map((library) => (
-					<LibraryListItem key={library.uuid} library={library} />
-				))}
+				{libraries
+					?.sort((a, b) => {
+						if (a.uuid === currentLibrary?.uuid) return -1;
+						if (b.uuid === currentLibrary?.uuid) return 1;
+						return 0;
+					})
+					.map((library) => (
+						<LibraryListItem
+							current={library.uuid === currentLibrary?.uuid}
+							key={library.uuid}
+							library={library}
+						/>
+					))}
 			</div>
 		</SettingsContainer>
 	);
