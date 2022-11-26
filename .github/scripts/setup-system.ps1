905194176dc9847d3f30a8e3bac71c5d367ed692
maxichrome <maxichrome@users.noreply.github.com>
2022-11-26 03:19:27 -0600

    wip: more setup script i am going clinically insane
    Co-authored-by: ly <maplefins@users.noreply.github.com>

--- .github/scripts/setup-system.ps1
+++ .github/scripts/setup-system.ps1
@@ -3,30 +3,65 @@ param (
 	[Switch] $ci
 )
 
-# Get temp path
-# TODO: files are not cleaned up here there is better solution
-$temp = [System.IO.Path]::GetTempPath()
+$tempFiles = @()
 
-# Check if shell can exexute cmdlet
+# Check if shell can execute cmdlet
 function Test-CommandExists {
-	param ($command)
+	param (
+		[string] $command
+	)
 
-	return if (Get-Command $command -errorAction SilentlyContinue) { $true } else { $false }
+	return $(if (Get-Command $command -errorAction SilentlyContinue) { $true } else { $false })
+}
+
+function New-TempFile {
+	param (
+		[string] $Extension
+	)
+
+	$filePath = "$([System.IO.Path]::GetTempFilename()).$Extension"
+	$tempFiles += $filePath
+
+	return $filePath
+}
+
+function Remove-TempFiles {
+	foreach ($path in $tempFiles) {
+		Remove-Item -Force -Path $path
+	}
+}
+
+function Update-EnvironmentVariable {
+	param (
+		[string]$variableName
+	)
+
+	$value = [System.Environment]::GetEnvironmentVariable($variableName, [System.EnvironmentVariableTarget]::User)
+
+	if ($variableName -ieq "Path") {
+		$systemPath = [System.Environment]::GetEnvironmentVariable($variableName, [System.EnvironmentVariableTarget]::Machine)
+		$value = "$systemPath$value"
+	}
+
+	[System.Environment]::SetEnvironmentVariable($variableName, $value, [System.EnvironmentVariableTarget]::Process)
 }
 
 # Required by Tauri: VS build tools + Windows SDK
 function Install-VSTools {
 	$downloadUri = "https://aka.ms/vs/17/release/vs_buildtools.exe"
+	$executablePath = New-TempFile -Extension "exe"
 
-	Start-BitsTransfer -Source $downloadUri -Destination "$temp\vs_buildtools.exe"
-	& "$temp\vs_buildtools.exe" --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK.19041 --passive | Out-Null
+	Start-BitsTransfer -Source $downloadUri -Destination $executablePath
+	& $executablePath --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK.19041 --passive | Out-Null
 }
 
 function Install-Rustup {
 	$downloadUri = "https://win.rustup.rs/"
+	$executablePath = New-TempFile -Extension "exe"
 
-	Start-BitsTransfer -Source $downloadUri -Destination "$temp\rustup-init.exe"
-	Start-Process -FilePath "$temp\rustup-init.exe" -ArgumentList "-y" -PassThru -Wait -Verb RunAs
+	Start-BitsTransfer -Source $downloadUri -Destination $executablePath
+	Start-Process -FilePath $executablePath -ArgumentList "-y" -PassThru -Wait -Verb RunAs
+	Update-EnvironmentVariable "Path"
 }
 
 function Install-Pnpm {
@@ -37,9 +72,61 @@ function Install-Pnpm {
 	# Working around issue in pnpm that dosen't set %PNPM_HOME% correctly
 	$pnpmHome = [System.Environment]::GetEnvironmentVariable("PNPM_HOME", [System.EnvironmentVariableTarget]::User)
 	[System.Environment]::SetEnvironmentVariable("PNPM_HOME", $pnpmHome, [System.EnvironmentVariableTarget]::User)
+	Update-EnvironmentVariable "PNPM_HOME"
 }
 
-Install-Pnpm
+try {
+	# greeting
+	Write-Host "Spacedrive Development Environment Setup" -ForegroundColor Magenta
+	Write-Host @"
+
+To set up your machine for Spacedrive development, this script will check for the following and install if necessary:
+
+	- Visual Studio build tools
+	- Cargo
+	- pnpm
+	- FFmpeg
+
+"@
+
+	# vs step
+	Write-Host "Checking for VS build tools..." -ForegroundColor Yellow
+	$vswherePath = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+	$hasVSwhere = Test-Path -Path $vswherePath
+	if ($hasVSwhere -eq $false) {
+		Write-Host "VS build tools not found. Installing."
+		Install-VSTools
+	}
+	Write-Host "VS build tools are installed."
+
+	# rustup step
+	Write-Host "Checking for Cargo..." -ForegroundColor Yellow
+	$hasCargo = Test-CommandExists cargo
+	if ($hasCargo -eq $false) {
+		Write-Host "Cargo not found. Installing."
+		Install-Rustup
+	}
+	Write-Host "Cargo is installed."
+
+	# pnpm step
+	Write-Host "Checking for pnpm..." -ForegroundColor Yellow
+	$hasPnpm = Test-CommandExists "pnpm"
+	if ($hasPnpm -eq $false) {
+		Write-Host "pnpm not found. Installing."
+		Install-Pnpm
+	}
+	Write-Host "pnpm is installed."
+
+	Write-Host "Using pnpm to install the latest version of Node..."
+	Start-Process -FilePath "pnpm" -ArgumentList "env", "use", "--global", "latest" -Wait -PassThru -Verb RunAs
+
+	# fin
+	Write-Host
+	Write-Host "Your machine has been set up for Spacedrive development! (You may need to restart your shell)"
+	Read-Host "Press ENTER to exit"
+} finally {
+	Remove-TempFiles
+}
 
 # Toolchain (Spacedrive):
 #	Cargo (via rustup)
