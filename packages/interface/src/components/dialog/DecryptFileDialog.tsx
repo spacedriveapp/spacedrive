154a73818b6750f3ca60a95ece7a65623bbeebb7
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-12-13 11:30:07 +0000

    add ui for decrypting with password+saving to key manager

--- packages/interface/src/components/dialog/DecryptFileDialog.tsx
+++ packages/interface/src/components/dialog/DecryptFileDialog.tsx
@@ -1,8 +1,11 @@
+import { RadioGroup } from '@headlessui/react';
 import { useLibraryMutation } from '@sd/client';
-import { Button, Dialog } from '@sd/ui';
+import { Button, Dialog, Input, Switch } from '@sd/ui';
+import { Eye, EyeSlash, Info } from 'phosphor-react';
 import { useState } from 'react';
 
 import { usePlatform } from '../../util/Platform';
+import { Tooltip } from '../tooltip/Tooltip';
 import { GenericAlertDialogProps } from './AlertDialog';
 
 interface DecryptDialogProps {
@@ -18,6 +21,11 @@ export const DecryptFileDialog = (props: DecryptDialogProps) => {
 	const { location_id, object_id } = props;
 	const decryptFile = useLibraryMutation('files.decryptFiles');
 	const [outputPath, setOutputpath] = useState('');
+	const [decryptType, setDecryptType] = useState('key');
+	const [password, setPassword] = useState('');
+	const [saveToKeyManager, setSaveToKeyManager] = useState(true);
+	const [showPassword, setShowPassword] = useState(false);
+	const PasswordCurrentEyeIcon = showPassword ? EyeSlash : Eye;
 
 	return (
 		<>
@@ -64,6 +72,63 @@ export const DecryptFileDialog = (props: DecryptDialogProps) => {
 						);
 				}}
 			>
+				<RadioGroup value={decryptType} onChange={setDecryptType} className="mt-2">
+					<span className="text-xs font-bold">Key Type</span>
+					<div className="flex flex-row gap-2 mt-2">
+						<RadioGroup.Option value="key">
+							{({ checked }) => (
+								<Button type="button" size="sm" variant={checked ? 'accent' : 'gray'}>
+									Key Manager
+								</Button>
+							)}
+						</RadioGroup.Option>
+						<RadioGroup.Option value="password">
+							{({ checked }) => (
+								<Button type="button" size="sm" variant={checked ? 'accent' : 'gray'}>
+									Password
+								</Button>
+							)}
+						</RadioGroup.Option>
+					</div>
+				</RadioGroup>
+
+				{decryptType === 'password' && (
+					<>
+						<div className="relative flex flex-grow mt-3 mb-2">
+							<Input
+								className={`flex-grow w-max !py-0.5`}
+								placeholder="Password"
+								onChange={(e) => setPassword(e.target.value)}
+								value={password}
+								type={showPassword ? 'text' : 'password'}
+							/>
+							<Button
+								onClick={() => setShowPassword(!showPassword)}
+								size="icon"
+								className="border-none absolute right-[5px] top-[5px]"
+								type="button"
+							>
+								<PasswordCurrentEyeIcon className="w-4 h-4" />
+							</Button>
+						</div>
+
+						<div className="relative flex flex-grow mt-3 mb-2">
+							<div className="space-x-2">
+								<Switch
+									className="bg-app-selected"
+									size="sm"
+									checked={saveToKeyManager}
+									onCheckedChange={setSaveToKeyManager}
+								/>
+							</div>
+							<span className="ml-3 text-xs font-medium mt-0.5">Save to Key Manager</span>
+							<Tooltip label="This key will be saved to the key manager">
+								<Info className="w-4 h-4 ml-1.5 text-ink-faint mt-0.5" />
+							</Tooltip>
+						</div>
+					</>
+				)}
+
 				<div className="grid w-full grid-cols-2 gap-4 mt-4 mb-3">
 					<div className="flex flex-col">
 						<span className="text-xs font-bold">Output file</span>
