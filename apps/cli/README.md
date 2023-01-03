b5920fed0f14b9c3d2d92e31c3ee5f69a6f6e7f5
Brendan Allan <brendonovich@outlook.com>
2023-01-04 02:11:11 +0800

    move crypto cli to cli app

---
+++ apps/cli/README.md
@@ -0,0 +1,4 @@
+# CLI
+
+Basic CLI for interacting with encrypted files.
+Will be expanded to a general Spacedrive CLI in the future.
---
+++ apps/cli/src/main.rs
@@ -0,0 +1,83 @@
+use anyhow::{Context, Result};
+use clap::Parser;
+use indoc::printdoc;
+use sd_crypto::header::file::FileHeader;
+use std::{fs::File, path::PathBuf};
+
+#[derive(Parser)]
+struct Args {
+	#[arg(help = "the file path to get details for")]
+	path: PathBuf,
+}
+
+fn main() -> Result<()> {
+	let args = Args::parse();
+
+	let mut reader = File::open(args.path).context("unable to open file")?;
+	let (header, aad) = FileHeader::from_reader(&mut reader)?;
+	print_details(&header, &aad);
+
+	Ok(())
+}
+
+fn print_details(header: &FileHeader, aad: &[u8]) {
+	printdoc! {"
+        Header version: {version}
+        Encryption algorithm: {algorithm}
+        AAD (hex): {hex}
+    ",
+		version = header.version,
+		algorithm = header.algorithm,
+		hex = hex::encode(aad)
+	};
+
+	header.keyslots.iter().enumerate().for_each(|(i, k)| {
+		printdoc! {"
+            Keyslot {index}:
+              Version: {version}
+              Algorithm: {algorithm}
+              Hashing algorithm: {hashing_algorithm}
+              Salt (hex): {salt}
+              Master Key (hex, encrypted): {master}
+              Master key nonce (hex): {nonce}
+        ",
+			index = i + i,
+			version = k.version,
+			algorithm = k.algorithm,
+			hashing_algorithm = k.hashing_algorithm,
+			salt = hex::encode(k.salt),
+			master = hex::encode(k.master_key),
+			nonce = hex::encode(k.nonce.clone())
+		};
+	});
+
+	header.metadata.iter().for_each(|m| {
+		printdoc! {"
+            Metadata:
+              Version: {version}
+              Algorithm: {algorithm}
+              Encrypted size: {size}
+              Nonce (hex): {nonce}
+        ",
+			version = m.version,
+			algorithm = m.algorithm,
+			size = m.metadata.len(),
+			nonce = hex::encode(m.metadata_nonce.clone())
+		}
+	});
+
+	header.preview_media.iter().for_each(|p| {
+		printdoc! {"
+            Preview Media:
+              Version: {version}
+              Algorithm: {algorithm}
+              Encrypted size: {size}
+              Nonce (hex): {nonce}
+        ",
+			version = p.version,
+			algorithm = p.algorithm,
+			size = p.media.len(),
+			nonce = hex::encode(p.media_nonce.clone())
+		};
+	});
+}
