8c3307c26910648ae0814e715f762910c82e6eb5
Brendan Allan <brendonovich@outlook.com>
2023-01-05 12:22:49 +0800

    move back to alias for landing

--- apps/landing/vite.config.ts
+++ apps/landing/vite.config.ts
@@ -6,25 +6,23 @@ import esm from 'vite-plugin-esmodule';
 import md, { Mode } from 'vite-plugin-markdown';
 import ssr from 'vite-plugin-ssr/plugin';
 import svg from 'vite-plugin-svgr';
-import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
 	plugins: [
-		tsconfigPaths(),
 		react(),
 		ssr({ prerender: true }),
 		svg(),
 		md({ mode: [Mode.REACT] }),
 		visualizer()
 	],
-	// resolve: {
-	// 	alias: [
-	// 		{
-	// 			find: '@sd/',
-	// 			replacement: path.join(__dirname, '../../packages/')
-	// 		}
-	// 	]
-	// },
+	resolve: {
+		alias: [
+			{
+				find: '@sd/',
+				replacement: path.join(__dirname, '../../packages/')
+			}
+		]
+	},
 	server: {
 		port: 8003
 	},
