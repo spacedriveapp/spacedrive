666c6631155dd50f047104b59a0e2152ed1bdb7a
Brendan Allan <brendonovich@outlook.com>
2023-01-04 20:32:46 -0800

    Fix Vercel Deployments (#503)

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -2,14 +2,4 @@
 // const plugin = require('tailwindcss/plugin');
 // const defaultTheme = require('tailwindcss/defaultTheme');
 
-module.exports = require('./style/tailwind')();
-
-module.exports = {
-    theme: {
-        extend: {
-            zIndex: {
-                '55': '55',
-            }
-        }
-    }
-}
+module.exports = require('./style/tailwind');
