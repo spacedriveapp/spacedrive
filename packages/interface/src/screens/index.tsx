db4369f0db0846376688e53cf21020c2bf6618ea
Brendan Allan <brendonovich@outlook.com>
2023-01-19 21:01:56 +0800

    refactor routes

---
+++ packages/interface/src/screens/index.tsx
@@ -0,0 +1,33 @@
+import { Navigate, Route, RouteProps } from 'react-router-dom';
+import { lazyEl } from '~/util';
+
+import settingsScreens from './settings';
+
+const routes: RouteProps[] = [
+	{
+		index: true,
+		element: <Navigate to="overview" relative="path" />
+	},
+	{
+		path: 'overview',
+		element: lazyEl(() => import('./Overview'))
+	},
+	{ path: 'content', element: lazyEl(() => import('./Content')) },
+	{ path: 'photos', element: lazyEl(() => import('./Photos')) },
+	{ path: 'debug', element: lazyEl(() => import('./Debug')) },
+	{ path: 'location/:id', element: lazyEl(() => import('./LocationExplorer')) },
+	{ path: 'tag/:id', element: lazyEl(() => import('./TagExplorer')) },
+	{
+		path: 'settings',
+		element: lazyEl(() => import('./settings/Layout')),
+		children: settingsScreens
+	}
+];
+
+export const screens = (
+	<>
+		{routes.map((route) => (
+			<Route key={route.path} {...route} />
+		))}
+	</>
+);
