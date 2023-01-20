44a63ae3a0c72e189c3937c8011c041e27246538
Brendan Allan <brendonovich@outlook.com>
2023-01-20 14:53:47 +0800

    more formatting

--- apps/desktop/src-tauri/native/macos/Sources/webview.swift
+++ apps/desktop/src-tauri/native/macos/Sources/webview.swift
@@ -2,7 +2,7 @@ import WebKit
 
 @_cdecl("reload_webview")
 public func reloadWebview(webviewPtr: UnsafePointer<WKWebView>) -> () {
-    let webview = webviewPtr.pointee;
+	let webview = webviewPtr.pointee;
 	webview.window!.orderOut(webview);
 	webview.reload();
 	webview.window!.makeKey();
