144023b202b2c22b9fef9024648550594b7b2eef
Jamie Pine <ijamespine@me.com>
2023-02-02 16:30:55 -0800

    fix onboarding img size

--- packages/interface/src/components/onboarding/OnboardingStart.tsx
+++ packages/interface/src/components/onboarding/OnboardingStart.tsx
@@ -1,9 +1,8 @@
 import AppLogo from '@sd/assets/images/logo.png';
-import { getOnboardingStore, useOnboardingStore } from '@sd/client';
-import { Button } from '@sd/ui';
 import { FC, useEffect } from 'react';
 import { useNavigate } from 'react-router';
-
+import { getOnboardingStore, useOnboardingStore } from '@sd/client';
+import { Button } from '@sd/ui';
 // import { useUnlockOnboardingScreen } from './OnboardingProgress';
 import { OnboardingContainer, OnboardingDescription, OnboardingImg } from './OnboardingRoot';
 
@@ -12,7 +11,7 @@ export default function OnboardingStart() {
 
 	return (
 		<OnboardingContainer>
-			<OnboardingImg src={AppLogo} className="w-32 h-32 mb-8" />
+			<OnboardingImg src={AppLogo} className="flex-shrink-0 mb-8 h-36 w-36" />
 
 			<h1 className="mb-2 text-4xl font-bold text-center text-ink">
 				The file explorer from the future.
