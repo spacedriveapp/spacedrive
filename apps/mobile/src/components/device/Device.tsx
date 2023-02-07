35a48b165a0d2f9de35069f32abc23ce0d5efb69
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2023-02-07 15:03:46 +0300

    Update Device.tsx
    remove placeholder data

--- apps/mobile/src/components/device/Device.tsx
+++ apps/mobile/src/components/device/Device.tsx
@@ -1,101 +1,7 @@
 import { Cloud, Desktop, DeviceMobileCamera, Laptop, Lock } from 'phosphor-react-native';
 import { FlatList, Text, View } from 'react-native';
-import { ExplorerItem } from '@sd/client';
 import tw from '~/lib/tailwind';
 import FileItem from '../explorer/FileItem';
-
-const placeholderFileItems: ExplorerItem[] = [
-	{
-		type: 'Path',
-		item: {
-			date_created: '2020-01-01T00:00:00.000Z',
-			date_indexed: '2020-01-01T00:00:00.000Z',
-			date_modified: '2020-01-01T00:00:00.000Z',
-			extension: '',
-			cas_id: '3',
-			integrity_checksum: '',
-			id: 2,
-			name: 'Documents',
-			key_id: null,
-			is_dir: true,
-			location_id: 1,
-			materialized_path: '/Documents',
-			object_id: 5,
-			parent_id: 1,
-			object: {
-				extension: 'tsx',
-				id: 3,
-				pub_id: [3],
-				name: 'App.tsx',
-				key_id: null,
-				date_created: '2020-01-01T00:00:00.000Z',
-				date_indexed: '2020-01-01T00:00:00.000Z',
-				date_modified: '2020-01-01T00:00:00.000Z',
-				favorite: false,
-				has_thumbnail: false,
-				has_thumbstrip: false,
-				has_video_preview: false,
-				hidden: false,
-				important: false,
-				ipfs_id: '',
-				kind: 2,
-				note: '',
-				size_in_bytes: '0'
-			}
-		},
-		has_thumbnail: false
-	},
-	{
-		type: 'Object',
-		has_thumbnail: false,
-		item: {
-			date_created: '2020-01-01T00:00:00.000Z',
-			date_indexed: '2020-01-01T00:00:00.000Z',
-			date_modified: '2020-01-01T00:00:00.000Z',
-			extension: '',
-			id: 1,
-			pub_id: [1],
-			name: 'Minecraft',
-			key_id: null,
-			favorite: false,
-			file_paths: [],
-			has_thumbnail: false,
-			has_thumbstrip: false,
-			has_video_preview: false,
-			hidden: false,
-			important: false,
-			ipfs_id: '',
-			kind: 4,
-			note: '',
-			size_in_bytes: '0'
-		}
-	},
-	{
-		type: 'Object',
-		has_thumbnail: false,
-		item: {
-			date_created: '2020-01-01T00:00:00.000Z',
-			date_indexed: '2020-01-01T00:00:00.000Z',
-			date_modified: '2020-01-01T00:00:00.000Z',
-			extension: '',
-			id: 5,
-			pub_id: [5],
-			name: 'Minecraft',
-			key_id: null,
-			favorite: false,
-			file_paths: [],
-			has_thumbnail: false,
-			has_thumbstrip: false,
-			has_video_preview: false,
-			hidden: false,
-			important: false,
-			ipfs_id: '',
-			kind: 5,
-			note: '',
-			size_in_bytes: '0'
-		}
-	}
-];
 
 type DeviceProps = {
 	name: string;
@@ -129,7 +35,7 @@ const Device = ({ name, size, type }: DeviceProps) => {
 				<Text style={tw`ml-2 text-sm font-semibold text-ink-dull`}>{size}</Text>
 			</View>
 			<FlatList
-				data={placeholderFileItems}
+				data={[]}
 				renderItem={({ item }) => <FileItem data={item} />}
 				keyExtractor={(item) => item.item.id.toString()}
 				horizontal
