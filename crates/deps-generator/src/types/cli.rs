f684b7f787b8dd5c211d879499581413b97724cb
brxken128 <77554505+brxken128@users.noreply.github.com>
2023-02-16 14:30:15 +0000

    Revert "experimentally update ci"
    This reverts commit c04897d902c9a55baf14d532f4590bc711f08c16.

--- crates/deps-generator/src/types/cli.rs
+++ crates/deps-generator/src/types/cli.rs
@@ -16,7 +16,6 @@ pub enum Action {
 
 #[derive(Args)]
 pub struct FrontendArgs {
-	// could source this from `$GITHUB_SHA` if not set
 	#[arg(help = "the git revision")]
 	pub revision: String,
 	#[arg(help = "the output path")]
@@ -25,7 +24,6 @@ pub struct FrontendArgs {
 
 #[derive(Args)]
 pub struct BackendArgs {
-	// could use `Cargo.toml` as the default from current dir (if not set)
 	#[arg(help = "path to the cargo manifest")]
 	pub manifest_path: PathBuf,
 	#[arg(help = "the output path")]
