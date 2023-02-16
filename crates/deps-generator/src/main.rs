821109e1bffebe2cebc157140251fe4ad6971fc6
brxken128 <77554505+brxken128@users.noreply.github.com>
2023-02-16 10:40:41 +0000

    add `deps-generator` crate

---
+++ crates/deps-generator/src/main.rs
@@ -0,0 +1,106 @@
+use anyhow::Result;
+use cargo_metadata::CargoOpt;
+use clap::{Args, Parser, Subcommand};
+use std::{fs::File, path::PathBuf};
+use types::{backend::BackendDependency, frontend::FrontendDependency};
+
+pub mod types;
+
+#[derive(Parser)]
+struct Arguments {
+	#[command(subcommand)]
+	action: Action,
+}
+
+#[derive(Subcommand)]
+enum Action {
+	Frontend(FrontendArgs),
+	Backend(BackendArgs),
+}
+
+#[derive(Args)]
+struct FrontendArgs {
+	#[arg(help = "the git revision")]
+	revision: String,
+	#[arg(help = "the output path")]
+	path: PathBuf,
+}
+
+#[derive(Args)]
+struct BackendArgs {
+	#[arg(help = "path to the cargo manifest")]
+	manifest_path: PathBuf,
+	#[arg(help = "the output path")]
+	output_path: PathBuf,
+}
+
+const FOSSA_BASE_URL: &str =
+	"https://app.fossa.com/api/revisions/git%2Bgithub.com%2Fspacedriveapp%2Fspacedrive%24";
+
+#[tokio::main]
+async fn main() -> Result<()> {
+	let args = Arguments::parse();
+
+	match args.action {
+		Action::Frontend(sub_args) => write_frontend_deps(sub_args.revision, sub_args.path).await,
+		Action::Backend(sub_args) => {
+			write_backend_deps(sub_args.manifest_path, sub_args.output_path)
+		}
+	}
+}
+
+fn write_backend_deps(manifest_path: PathBuf, output_path: PathBuf) -> Result<()> {
+	let cmd = cargo_metadata::MetadataCommand::new()
+		.manifest_path(manifest_path)
+		.features(CargoOpt::AllFeatures)
+		.exec()?;
+
+	let deps: Vec<BackendDependency> = cmd
+		.packages
+		.into_iter()
+		.filter_map(|p| {
+			if !cmd.workspace_members.iter().any(|t| &p.id == t) {
+				let dep = BackendDependency {
+					title: p.name,
+					description: p.description,
+					url: p.repository,
+					version: p.version.to_string(),
+					authors: p.authors,
+					license: p.license,
+				};
+
+				Some(dep)
+			} else {
+				None
+			}
+		})
+		.collect();
+
+	let mut file = File::create(output_path)?;
+	serde_json::to_writer(&mut file, &deps)?;
+
+	Ok(())
+}
+
+async fn write_frontend_deps(rev: String, path: PathBuf) -> Result<()> {
+	let url = format!("{FOSSA_BASE_URL}{rev}/dependencies");
+
+	let response = reqwest::get(url).await?.text().await?;
+	let json: Vec<types::frontend::Dependency> = serde_json::from_str(&response)?;
+
+	let deps: Vec<FrontendDependency> = json
+		.into_iter()
+		.map(|dep| FrontendDependency {
+			title: dep.project.title,
+			authors: dep.project.authors,
+			description: dep.project.description,
+			url: dep.project.url,
+			license: dep.licenses,
+		})
+		.collect();
+
+	let mut file = File::create(path)?;
+	serde_json::to_writer(&mut file, &deps)?;
+
+	Ok(())
+}
---
+++ crates/deps-generator/src/types/mod.rs
@@ -0,0 +1,2 @@
+pub mod backend;
+pub mod frontend;
