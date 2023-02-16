b58c69b909842c85dd6d3c3f4c727ed0cb6ad85a
Ericson Soares <ericson.ds999@gmail.com>
2023-02-16 17:24:27 -0300

    Using reqwest blocking feature to avoid asyncness

--- crates/deps-generator/src/main.rs
+++ crates/deps-generator/src/main.rs
@@ -13,12 +13,11 @@ pub mod types;
 const FOSSA_BASE_URL: &str =
 	"https://app.fossa.com/api/revisions/git%2Bgithub.com%2Fspacedriveapp%2Fspacedrive%24";
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
 	let args = Arguments::parse();
 
 	match args.action {
-		Action::Frontend(sub_args) => write_frontend_deps(sub_args.revision, sub_args.path).await,
+		Action::Frontend(sub_args) => write_frontend_deps(sub_args.revision, sub_args.path),
 		Action::Backend(sub_args) => {
 			write_backend_deps(sub_args.manifest_path, sub_args.output_path)
 		}
@@ -52,13 +51,13 @@ fn write_backend_deps(manifest_path: PathBuf, output_path: PathBuf) -> Result<()
 	Ok(())
 }
 
-async fn write_frontend_deps(rev: String, path: PathBuf) -> Result<()> {
+fn write_frontend_deps(rev: String, path: PathBuf) -> Result<()> {
 	let url = format!("{FOSSA_BASE_URL}{rev}/dependencies");
 
-	let response = reqwest::get(url).await?.text().await?;
+	let response = reqwest::blocking::get(url)?.text()?;
 	let json: Vec<types::frontend::Dependency> = serde_json::from_str(&response)?;
 
-	let deps: Vec<FrontendDependency> = json
+	let deps: Vec<_> = json
 		.into_iter()
 		.map(|dep| FrontendDependency {
 			title: dep.project.title,
