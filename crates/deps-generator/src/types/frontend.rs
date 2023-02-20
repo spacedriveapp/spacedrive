4cc582e08c6657ad290f2a82872e35361f4ba422
jake <77554505+brxken128@users.noreply.github.com>
2023-02-20 05:58:55 +0000

    [ENG-134] OSS Licenses and Projects (#575)
    * add `deps-generator` crate
    
    * move `clap` types to separate file
    
    * idiomatic filtering
    
    * experimentally update ci
    
    * add *really* basic dependency page
    
    * remove `license_id` from `License`
    
    * add bare minimum JSON files
    
    * Revert "experimentally update ci"
    
    This reverts commit c04897d902c9a55baf14d532f4590bc711f08c16.
    
    * re-insert comments
    
    * Using reqwest blocking feature to avoid asyncness
    
    ---------
    
    Co-authored-by: Ericson Soares <ericson.ds999@gmail.com>

---
+++ crates/deps-generator/src/types/frontend.rs
@@ -0,0 +1,36 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Dependency {
+	pub project: Project,
+	pub licenses: Vec<License>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Project {
+	// pub locator: Option<String>,
+	pub title: String,
+	pub description: Option<String>,
+	pub url: Option<String>,
+	pub authors: Vec<Option<String>>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct License {
+	pub text: Option<String>,
+	// pub license_id: Option<String>, // always null AFAIK
+	pub copyright: Option<String>,
+	// pub license_group_id: i64,
+	// pub ignored: bool, // always false from my testing
+	// pub revision_id: Option<String>,
+}
+
+#[allow(clippy::module_name_repetitions)]
+#[derive(Serialize)]
+pub struct FrontendDependency {
+	pub title: String,
+	pub description: Option<String>,
+	pub url: Option<String>,
+	pub authors: Vec<Option<String>>,
+	pub license: Vec<License>,
+}
