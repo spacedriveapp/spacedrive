5432bfe91400cd3009d89b736276ecfacf9ad08f
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2023-03-06 15:58:25 +0300

    password input

--- apps/mobile/src/components/form/Input.tsx
+++ apps/mobile/src/components/form/Input.tsx
@@ -1,6 +1,7 @@
 import { VariantProps, cva } from 'class-variance-authority';
-import { FC } from 'react';
-import { TextInputProps as RNTextInputProps, TextInput } from 'react-native';
+import { Eye, EyeSlash } from 'phosphor-react-native';
+import { useState } from 'react';
+import { Pressable, TextInputProps as RNTextInputProps, TextInput, View } from 'react-native';
 import { tw, twStyle } from '~/lib/tailwind';
 
 const input = cva(['rounded-md border text-sm leading-tight shadow-sm'], {
@@ -21,7 +22,7 @@ const input = cva(['rounded-md border text-sm leading-tight shadow-sm'], {
 
 type InputProps = VariantProps<typeof input> & RNTextInputProps;
 
-export const Input: FC<InputProps> = ({ variant, size, ...props }) => {
+export const Input = ({ variant, size, ...props }: InputProps) => {
 	const { style, ...otherProps } = props;
 	return (
 		<TextInput
@@ -29,5 +30,43 @@ export const Input: FC<InputProps> = ({ variant, size, ...props }) => {
 			style={twStyle(input({ variant, size }), style as string)}
 			{...otherProps}
 		/>
+	);
+};
+
+// Same as above but configured with password props & show/hide password button
+
+type PasswordInputProps = InputProps & {
+	isNewPassword?: boolean;
+};
+
+export const PasswordInput = ({ variant, size, ...props }: PasswordInputProps) => {
+	const { style, isNewPassword = false, ...otherProps } = props;
+
+	const [showPassword, setShowPassword] = useState(false);
+
+	const Icon = showPassword ? EyeSlash : Eye;
+
+	return (
+		<View style={tw`relative`}>
+			<TextInput
+				autoComplete={isNewPassword ? 'password-new' : 'password'}
+				textContentType={isNewPassword ? 'newPassword' : 'password'}
+				placeholder="Password"
+				secureTextEntry={!showPassword}
+				autoCorrect={false}
+				autoCapitalize="none"
+				placeholderTextColor={tw.color('ink-dull')}
+				// Do not use margin here, it will break the absolute positioning of the button.
+				// Maybe switch to flexbox?
+				style={twStyle(input({ variant, size }), style as string)}
+				{...otherProps}
+			/>
+			<Pressable
+				style={tw`absolute inset-y-[10px] right-4`}
+				onPress={() => setShowPassword((v) => !v)}
+			>
+				<Icon size={18} color="white" />
+			</Pressable>
+		</View>
 	);
 };
