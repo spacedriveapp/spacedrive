5bbfc85665f4cbbea50fd5d4ed0f43d9638dc4af
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2023-03-06 15:59:35 +0300

    new library style tweaks

--- apps/mobile/src/screens/onboarding/NewLibrary.tsx
+++ apps/mobile/src/screens/onboarding/NewLibrary.tsx
@@ -35,23 +35,25 @@ const NewLibraryScreen = ({ navigation }: OnboardingStackScreenProps<'NewLibrary
 		<OnboardingContainer>
 			<Image source={require('@sd/assets/images/Database.png')} style={tw`h-25 w-25`} />
 			<OnboardingTitle style={tw`mt-4`}>Create a Library</OnboardingTitle>
-			<OnboardingDescription style={tw`mt-4`}>
-				Libraries are a secure, on-device database. Your files remain where they are, the Library
-				catalogs them and stores all Spacedrive related data.
-			</OnboardingDescription>
-			<Controller
-				name="name"
-				control={form.control}
-				render={({ field: { onBlur, onChange, value } }) => (
-					<Input
-						style={tw`m-4 w-full`}
-						placeholder='e.g. "James Library"'
-						onBlur={onBlur}
-						onChangeText={onChange}
-						value={value}
-					/>
-				)}
-			/>
+			<View style={tw`w-full px-4`}>
+				<OnboardingDescription style={tw`mt-4`}>
+					Libraries are a secure, on-device database. Your files remain where they are, the Library
+					catalogs them and stores all Spacedrive related data.
+				</OnboardingDescription>
+				<Controller
+					name="name"
+					control={form.control}
+					render={({ field: { onBlur, onChange, value } }) => (
+						<Input
+							style={tw`my-3`}
+							placeholder='e.g. "James Library"'
+							onBlur={onBlur}
+							onChangeText={onChange}
+							value={value}
+						/>
+					)}
+				/>
+			</View>
 
 			{form.formState.errors.name && (
 				<Text style={tw`text-center text-xs font-bold text-red-500`}>
@@ -60,11 +62,11 @@ const NewLibraryScreen = ({ navigation }: OnboardingStackScreenProps<'NewLibrary
 			)}
 			<View style={tw`mt-4 flex w-full flex-row items-center justify-center`}>
 				<Button variant="accent" onPress={handleNewLibrary}>
-					<Text style={tw`text-ink text-center text-base font-medium`}>New Library</Text>
+					<Text style={tw`text-ink text-center font-medium`}>New Library</Text>
 				</Button>
 				<Text style={tw`text-ink-faint px-4 text-xs font-bold`}>OR</Text>
 				<Button onPress={handleImport} variant="outline">
-					<Text style={tw`text-ink text-center text-base font-medium`}>Import Library</Text>
+					<Text style={tw`text-ink text-center font-medium`}>Import Library</Text>
 				</Button>
 			</View>
 		</OnboardingContainer>
