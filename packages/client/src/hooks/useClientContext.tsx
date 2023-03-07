99384efeaa335af2151720ca583f83d536d2821e
brxken128 <77554505+brxken128@users.noreply.github.com>
2023-03-07 12:37:10 +0000

    remove old artifacts from `ClientContext`

--- packages/client/src/hooks/useClientContext.tsx
+++ packages/client/src/hooks/useClientContext.tsx
@@ -68,44 +68,6 @@ export const useClientContext = () => {
 
 export const useCurrentLibraryId = () => useClientContext().currentLibraryId;
 
-/**
- * @privateRemarks
- * This should **not** throw an error at all.
- *
- * It may be used in contexts where a library or `ClientContextProvider` aren't available,
- * such as during onboarding.
- *
- * We shouldn't throw an error if the user hasn't selected a telemetry sharing option,
- * and this shouldn't be used for anything other than validating that we can send usage data. It is
- * more of an optional query than anything else, and telemetry will not be shared unless this explicitly returns `true`.
- */
-export const useCurrentTelemetrySharing = () => {
-	try {
-		return useClientContext().library?.config.shareTelemetry ?? null;
-	} catch (e) {
-		return null;
-	}
-};
-
-/**
- * Used for finding the telemetry sharing settings for a specific library
- *
- * @privateRemarks
- * We shouldn't throw an error if the user hasn't selected a telemetry sharing option,
- * and this shouldn't be used for anything other than validating that we can send usage data. It is
- * more of an optional query than anything else, and telemetry will not be shared unless this explicitly returns `true`.
- */
-export const useTelemetrySharing = (props: { libraryUuid: string }) => {
-	try {
-		return (
-			useCachedLibraries().data?.find((l) => l.uuid === props.libraryUuid)?.config.shareTelemetry ??
-			null
-		);
-	} catch (e) {
-		return null;
-	}
-};
-
 export const currentLibraryCache = valtioPersist('sd-current-library', {
 	id: null as string | null
 });
