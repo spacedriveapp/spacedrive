d0a86a2cc5a0c6584d415f6cedca38d9e6cfc405
brxken128 <77554505+brxken128@users.noreply.github.com>
2023-03-07 15:17:13 +0000

    change how `Switch` updates the store in privacy settings

--- interface/app/$libraryId/settings/client/privacy.tsx
+++ interface/app/$libraryId/settings/client/privacy.tsx
@@ -1,23 +1,22 @@
 import { Switch } from '@sd/ui';
-import { getTelemetryState, useTelemetryState } from '~/../packages/client/src';
+import { telemetryStore, useTelemetryState } from '~/../packages/client/src';
 import { Heading } from '../Layout';
 import Setting from '../Setting';
 
 export default function PrivacySettings() {
 	const shareTelemetry = useTelemetryState().shareTelemetry;
-	const telemetryState = getTelemetryState();
 
 	return (
 		<>
 			<Heading title="Privacy" description="" />
 			<Setting
 				mini
-				title="Share Usage Data"
+				title="Share Telemetry and Usage Data"
 				description="Share anonymous usage data to help us improve the app."
 			>
 				<Switch
-					checked={shareTelemetry ?? undefined}
-					onCheckedChange={(e) => (telemetryState.shareTelemetry = e)}
+					checked={shareTelemetry}
+					onClick={() => (telemetryStore.shareTelemetry = !shareTelemetry)}
 					className="m-2 ml-4"
 				/>
 			</Setting>
