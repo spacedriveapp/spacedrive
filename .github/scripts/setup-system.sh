5ef70377503d8f4b3e90993004dc9d01cae8bfe3
brxken128 <77554505+brxken128@users.noreply.github.com>
2023-03-11 16:53:38 +0000

    shfmt

--- .github/scripts/setup-system.sh
+++ .github/scripts/setup-system.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-function log_err {
+function log_err() {
 	echo "$@" >&2
 }
 
-function script_failure {
+function script_failure() {
 	log_err "An error occurred$([ -z "$1" ] && " on line $1" || " (unknown)")."
 	log_err "Setup failed."
 }
@@ -63,8 +63,7 @@ if [ "$1" == "mobile" ]; then
 	fi
 
 	# Android requires python
-	if ! command -v python3 >/dev/null
-	then
+	if ! command -v python3 >/dev/null; then
 		log_err "python3 command could not be found. This is required for Android mobile development."
 		log_err "Ensure python3 is available in your \$PATH and try again."
 		exit 1
@@ -73,15 +72,15 @@ if [ "$1" == "mobile" ]; then
 	# Android targets
 	echo "Setting up Android targets for Rust..."
 
-	rustup target add armv7-linux-androideabi   # for arm
-	rustup target add aarch64-linux-android     # for arm64
-	rustup target add i686-linux-android        # for x86
-	rustup target add x86_64-linux-android      # for x86_64
-	rustup target add x86_64-unknown-linux-gnu  # for linux-x86-64
-	rustup target add aarch64-apple-darwin      # for darwin arm64 (if you have an M1 Mac)
-	rustup target add x86_64-apple-darwin       # for darwin x86_64 (if you have an Intel Mac)
-	rustup target add x86_64-pc-windows-gnu     # for win32-x86-64-gnu
-	rustup target add x86_64-pc-windows-msvc    # for win32-x86-64-msvc
+	rustup target add armv7-linux-androideabi  # for arm
+	rustup target add aarch64-linux-android    # for arm64
+	rustup target add i686-linux-android       # for x86
+	rustup target add x86_64-linux-android     # for x86_64
+	rustup target add x86_64-unknown-linux-gnu # for linux-x86-64
+	rustup target add aarch64-apple-darwin     # for darwin arm64 (if you have an M1 Mac)
+	rustup target add x86_64-apple-darwin      # for darwin x86_64 (if you have an Intel Mac)
+	rustup target add x86_64-pc-windows-gnu    # for win32-x86-64-gnu
+	rustup target add x86_64-pc-windows-msvc   # for win32-x86-64-msvc
 
 	echo "Done setting up mobile targets."
 	echo
@@ -91,11 +90,18 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	if command -v apt-get >/dev/null; then
 		echo "Detected apt!"
 		echo "Installing dependencies with apt..."
-		
-		DEBIAN_TAURI_DEPS="libwebkit2gtk-4.0-dev build-essential curl wget libssl-dev libgtk-3-dev libayatana-appindicator3-dev librsvg2-dev" # Tauri dependencies
-		DEBIAN_FFMPEG_DEPS="libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavutil-dev libswscale-dev libswresample-dev ffmpeg" # FFmpeg dependencies
-		DEBIAN_BINDGEN_DEPS="pkg-config clang" # Bindgen dependencies - it's used by a dependency of Spacedrive
-		PROTOBUF="protobuf-compiler" # Protobuf compiler
+
+		# Tauri dependencies
+		DEBIAN_TAURI_DEPS="libwebkit2gtk-4.0-dev build-essential curl wget libssl-dev libgtk-3-dev libayatana-appindicator3-dev librsvg2-dev"
+
+		# FFmpeg dependencies
+		DEBIAN_FFMPEG_DEPS="libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavutil-dev libswscale-dev libswresample-dev ffmpeg"
+
+		# Bindgen dependencies - it's used by a dependency of Spacedrive
+		DEBIAN_BINDGEN_DEPS="pkg-config clang"
+
+		# Protobuf compiler
+		PROTOBUF="protobuf-compiler"
 
 		sudo apt-get -y update
 		sudo apt-get -y install ${SPACEDRIVE_CUSTOM_APT_FLAGS:-} $DEBIAN_TAURI_DEPS $DEBIAN_FFMPEG_DEPS $DEBIAN_BINDGEN_DEPS $PROTOBUF
@@ -103,10 +109,17 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 		echo "Detected pacman!"
 		echo "Installing dependencies with pacman..."
 
-		ARCH_TAURI_DEPS="webkit2gtk base-devel curl wget openssl appmenu-gtk-module gtk3 libappindicator-gtk3 librsvg libvips" # Tauri deps https://tauri.studio/guides/getting-started/setup/linux#1-system-dependencies
-		ARCH_FFMPEG_DEPS="ffmpeg" # FFmpeg dependencies
-		ARCH_BINDGEN_DEPS="clang" # Bindgen dependencies - it's used by a dependency of Spacedrive
-		PROTOBUF="protobuf" # Protobuf compiler - https://github.com/archlinux/svntogit-packages/blob/packages/protobuf/trunk/PKGBUILD provides `libprotoc`
+		# Tauri deps https://tauri.studio/guides/getting-started/setup/linux#1-system-dependencies
+		ARCH_TAURI_DEPS="webkit2gtk base-devel curl wget openssl appmenu-gtk-module gtk3 libappindicator-gtk3 librsvg libvips"
+
+		# FFmpeg dependencies
+		ARCH_FFMPEG_DEPS="ffmpeg"
+
+		# Bindgen dependencies - it's used by a dependency of Spacedrive
+		ARCH_BINDGEN_DEPS="clang"
+
+		# Protobuf compiler - https://github.com/archlinux/svntogit-packages/blob/packages/protobuf/trunk/PKGBUILD provides `libprotoc`
+		PROTOBUF="protobuf"
 
 		sudo pacman -Syu
 		sudo pacman -S --needed $ARCH_TAURI_DEPS $ARCH_FFMPEG_DEPS $ARCH_BINDGEN_DEPS $PROTOBUF
@@ -114,10 +127,17 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 		echo "Detected dnf!"
 		echo "Installing dependencies with dnf..."
 
-		FEDORA_TAURI_DEPS="webkit2gtk3-devel.x86_64 openssl-devel curl wget libappindicator-gtk3 librsvg2-devel" # Tauri dependencies
-		FEDORA_FFMPEG_DEPS="ffmpeg ffmpeg-devel" # FFmpeg dependencies
-		FEDORA_BINDGEN_DEPS="clang" # Bindgen dependencies - it's used by a dependency of Spacedrive
-		PROTOBUF="protobuf-compiler" # Protobuf compiler
+		# Tauri dependencies
+		FEDORA_TAURI_DEPS="webkit2gtk3-devel.x86_64 openssl-devel curl wget libappindicator-gtk3 librsvg2-devel"
+
+		# FFmpeg dependencies
+		FEDORA_FFMPEG_DEPS="ffmpeg ffmpeg-devel"
+
+		# Bindgen dependencies - it's used by a dependency of Spacedrive
+		FEDORA_BINDGEN_DEPS="clang"
+
+		# Protobuf compiler
+		PROTOBUF="protobuf-compiler"
 
 		sudo dnf check-update
 		sudo dnf install $FEDORA_TAURI_DEPS $FEDORA_FFMPEG_DEPS $FEDORA_BINDGEN_DEPS $PROTOBUF
@@ -127,31 +147,31 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 		exit 1
 	fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-		if ! command -v brew >/dev/null; then
-			log_err "Homebrew was not found. Please install it using the instructions at https://brew.sh and try again."
-			exit 1
-		fi
+	if ! command -v brew >/dev/null; then
+		log_err "Homebrew was not found. Please install it using the instructions at https://brew.sh and try again."
+		exit 1
+	fi
 
-		echo "Installing Homebrew dependencies..."
+	echo "Installing Homebrew dependencies..."
 
-		if ! brew tap -q | grep -qx "spacedriveapp/deps" >/dev/null; then
-			echo "Creating Homebrew tap \`spacedriveapp/deps\`..."
-			brew tap-new spacedriveapp/deps
-		fi
+	if ! brew tap -q | grep -qx "spacedriveapp/deps" >/dev/null; then
+		echo "Creating Homebrew tap \`spacedriveapp/deps\`..."
+		brew tap-new spacedriveapp/deps
+	fi
 
-		FFMPEG_VERSION="5.0.1"
+	FFMPEG_VERSION="5.0.1"
 
-		if ! brew list --full-name -1 | grep -x "spacedriveapp/deps/ffmpeg@$FFMPEG_VERSION" >/dev/null; then
-			echo "Extracting FFmpeg version $FFMPEG_VERSION..."
+	if ! brew list --full-name -1 | grep -x "spacedriveapp/deps/ffmpeg@$FFMPEG_VERSION" >/dev/null; then
+		echo "Extracting FFmpeg version $FFMPEG_VERSION..."
 
-			brew extract -q --force --version $FFMPEG_VERSION ffmpeg spacedriveapp/deps
-			brew unlink -q ffmpeg || true
-			brew install -q "spacedriveapp/deps/ffmpeg@$FFMPEG_VERSION"
+		brew extract -q --force --version $FFMPEG_VERSION ffmpeg spacedriveapp/deps
+		brew unlink -q ffmpeg || true
+		brew install -q "spacedriveapp/deps/ffmpeg@$FFMPEG_VERSION"
 
-			brew install protobuf
+		brew install protobuf
 
-			echo "FFmpeg version $FFMPEG_VERSION has been installed and is now being used on your system."
-		fi
+		echo "FFmpeg version $FFMPEG_VERSION has been installed and is now being used on your system."
+	fi
 else
 	log_err "Your OS ($OSTYPE) is not supported by this script. We would welcome a PR or some help adding your OS to this script. https://github.com/spacedriveapp/spacedrive/issues"
 	exit 1
