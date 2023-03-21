29f0dfb3381293331a804aab0a4e296ced0f31eb
nikec <43032218+niikeec@users.noreply.github.com>
2023-03-21 06:01:48 +0100

    [Desktop, UI] Fix library dropdown overflow & navigation (#619)
    * add radix dropdown menu
    
    * [desktop] Improve Libraries Dropdown
    
    * rename alignToParent prop and fix sizing
    
    * Update pnpm-lock.yaml
    
    * Revert "Update pnpm-lock.yaml"
    
    This reverts commit 6113361c51e816788691b44aca24ac23cc723d5b.
    
    * fix pnpm lock

--- interface/app/$libraryId/Layout/Sidebar/LibrariesDropdown.tsx
+++ interface/app/$libraryId/Layout/Sidebar/LibrariesDropdown.tsx
@@ -1,22 +1,19 @@
 import clsx from 'clsx';
 import { Gear, Lock, Plus } from 'phosphor-react';
 import { useClientContext } from '@sd/client';
-import { Dropdown, dialogManager } from '@sd/ui';
+import { Dropdown, DropdownMenu, dialogManager } from '@sd/ui';
 import CreateDialog from '../../settings/node/libraries/CreateDialog';
 
 export default () => {
 	const { library, libraries, currentLibraryId } = useClientContext();
 
 	return (
-		<Dropdown.Root
-			// we override the sidebar dropdown item's hover styles
-			// because the dark style clashes with the sidebar
-			itemsClassName="dark:bg-sidebar-box dark:border-sidebar-line mt-1 dark:divide-menu-selected/30 shadow-none"
-			button={
+		<DropdownMenu.Root
+			trigger={
 				<Dropdown.Button
 					variant="gray"
 					className={clsx(
-						`text-ink w-full `,
+						`text-ink w-full`,
 						// these classname overrides are messy
 						// but they work
 						`!bg-sidebar-box !border-sidebar-line/50 active:!border-sidebar-line active:!bg-sidebar-button ui-open:!bg-sidebar-button ui-open:!border-sidebar-line ring-offset-sidebar`,
@@ -28,32 +25,43 @@ export default () => {
 					</span>
 				</Dropdown.Button>
 			}
+			// we override the sidebar dropdown item's hover styles
+			// because the dark style clashes with the sidebar
+			className="dark:bg-sidebar-box dark:border-sidebar-line dark:divide-menu-selected/30 mt-1 shadow-none"
+			alignToTrigger
+			animate
 		>
-			<Dropdown.Section>
-				{libraries.data?.map((lib) => (
-					<Dropdown.Item
-						to={`/${lib.uuid}/overview`}
-						key={lib.uuid}
-						selected={lib.uuid === currentLibraryId}
-					>
-						{lib.config.name}
-					</Dropdown.Item>
-				))}
-			</Dropdown.Section>
-			<Dropdown.Section>
-				<Dropdown.Item
-					icon={Plus}
-					onClick={() => dialogManager.create((dp) => <CreateDialog {...dp} />)}
+			{libraries.data?.map((lib) => (
+				<DropdownMenu.Item
+					to={`/${lib.uuid}/overview`}
+					key={lib.uuid}
+					selected={lib.uuid === currentLibraryId}
 				>
-					New Library
-				</Dropdown.Item>
-				<Dropdown.Item icon={Gear} to="settings/library">
-					Manage Library
-				</Dropdown.Item>
-				<Dropdown.Item icon={Lock} onClick={() => alert('TODO: Not implemented yet!')}>
-					Lock
-				</Dropdown.Item>
-			</Dropdown.Section>
-		</Dropdown.Root>
+					{lib.config.name}
+				</DropdownMenu.Item>
+			))}
+			<DropdownMenu.Separator className="mx-0 my-0.5" />
+			<DropdownMenu.Item
+				label="	New Library"
+				icon={Plus}
+				iconProps={{ weight: 'bold', size: 16 }}
+				onClick={() => dialogManager.create((dp) => <CreateDialog {...dp} />)}
+				className="font-medium"
+			/>
+			<DropdownMenu.Item
+				label="Manage Library"
+				icon={Gear}
+				iconProps={{ weight: 'bold', size: 16 }}
+				to="settings/library/general"
+				className="font-medium"
+			/>
+			<DropdownMenu.Item
+				label="Lock"
+				icon={Lock}
+				iconProps={{ weight: 'bold', size: 16 }}
+				onClick={() => alert('TODO: Not implemented yet!')}
+				className="font-medium"
+			/>
+		</DropdownMenu.Root>
 	);
 };
