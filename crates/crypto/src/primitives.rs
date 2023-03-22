8e92593696b97b7a7bc7978ab91e344abfcb4c4f
jake <77554505+brxken128@users.noreply.github.com>
2023-03-22 13:27:01 +0000

    add static primitive tests - they should *never* change (after release)

--- crates/crypto/src/primitives.rs
+++ crates/crypto/src/primitives.rs
@@ -27,6 +27,7 @@ pub const BLOCK_LEN: usize = 1_048_576;
 /// This is the default AEAD tag size for all encryption algorithms used within the crate.
 pub const AEAD_TAG_LEN: usize = 16;
 
+/// Length of the AAD
 pub const AAD_LEN: usize = 32;
 
 /// The length of encrypted master keys
@@ -66,3 +67,82 @@ pub(super) const ARGON2ID_PARANOID: (u32, u32, u32) = (524_288, 8, 4);
 pub(super) const B3BALLOON_STANDARD: (u32, u32, u32) = (131_072, 2, 1);
 pub(super) const B3BALLOON_HARDENED: (u32, u32, u32) = (262_144, 2, 1);
 pub(super) const B3BALLOON_PARANOID: (u32, u32, u32) = (524_288, 2, 1);
+
+#[cfg(test)]
+mod tests {
+	use crate::primitives::{
+		AAD_LEN, AEAD_TAG_LEN, AES_256_GCM_NONCE_LEN, ARGON2ID_HARDENED, ARGON2ID_PARANOID,
+		ARGON2ID_STANDARD, B3BALLOON_HARDENED, B3BALLOON_PARANOID, B3BALLOON_STANDARD, BLOCK_LEN,
+		ENCRYPTED_KEY_LEN, KEY_LEN, SECRET_KEY_LEN, XCHACHA20_POLY1305_NONCE_LEN,
+	};
+
+	#[test]
+	fn argon2id_standard_params() {
+		assert_eq!(ARGON2ID_STANDARD, (131_072, 8, 4));
+	}
+
+	#[test]
+	fn argon2id_hardened_params() {
+		assert_eq!(ARGON2ID_HARDENED, (262_144, 8, 4));
+	}
+
+	#[test]
+	fn argon2id_paranoid_params() {
+		assert_eq!(ARGON2ID_PARANOID, (524_288, 8, 4));
+	}
+
+	#[test]
+	fn balloon_blake3_standard_params() {
+		assert_eq!(B3BALLOON_STANDARD, (131_072, 2, 1));
+	}
+
+	#[test]
+	fn balloon_blake3_hardened_params() {
+		assert_eq!(B3BALLOON_HARDENED, (262_144, 2, 1));
+	}
+
+	#[test]
+	fn balloon_blake3_paranoid_params() {
+		assert_eq!(B3BALLOON_PARANOID, (524_288, 2, 1));
+	}
+
+	#[test]
+	fn block_len() {
+		assert_eq!(BLOCK_LEN, 1_048_576);
+	}
+
+	#[test]
+	fn secret_key_len() {
+		assert_eq!(SECRET_KEY_LEN, 18);
+	}
+
+	#[test]
+	fn key_len() {
+		assert_eq!(KEY_LEN, 32);
+	}
+
+	#[test]
+	fn aead_tag_len() {
+		assert_eq!(AEAD_TAG_LEN, 16);
+	}
+
+	#[test]
+	fn encrypted_key_len() {
+		assert_eq!(ENCRYPTED_KEY_LEN, 48);
+	}
+
+	#[test]
+	fn aad_len() {
+		assert_eq!(AAD_LEN, 32);
+	}
+
+	#[test]
+	fn xchacha20_poly1305_nonce_len() {
+		assert_eq!(XCHACHA20_POLY1305_NONCE_LEN, 20);
+	}
+
+	#[test]
+	fn aes_256_gcm_nonce_len() {
+		assert_eq!(AES_256_GCM_NONCE_LEN, 8);
+	}
+}
