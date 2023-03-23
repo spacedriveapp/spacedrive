44936cbbeac1a0728553afe766c39d3cce7d7658
nikec <nikec.job@gmail.com>
2023-03-23 19:22:26 +0100

    [Desktop] Improve explorer item context menu tags

--- interface/app/$libraryId/Explorer/File/ContextMenu.tsx
+++ interface/app/$libraryId/Explorer/File/ContextMenu.tsx
@@ -1,3 +1,5 @@
+import { useVirtualizer } from '@tanstack/react-virtual';
+import clsx from 'clsx';
 import {
 	ArrowBendUpRight,
 	Copy,
@@ -12,7 +14,7 @@ import {
 	Trash,
 	TrashSimple
 } from 'phosphor-react';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useRef } from 'react';
 import {
 	ExplorerItem,
 	isObject,
@@ -25,7 +27,9 @@ import { ContextMenu, Input, dialogManager } from '@sd/ui';
 import { useExplorerParams } from '~/app/$libraryId/location/$id';
 import { showAlertDialog } from '~/components/AlertDialog';
 import { getExplorerStore, useExplorerStore } from '~/hooks/useExplorerStore';
+import { useScrolled } from '~/hooks/useScrolled';
 import { usePlatform } from '~/util/Platform';
+import CreateDialog from '../../settings/library/tags/CreateDialog';
 import { OpenInNativeExplorer } from '../ContextMenu';
 import DecryptDialog from './DecryptDialog';
 import DeleteDialog from './DeleteDialog';
@@ -158,9 +162,11 @@ export default ({ data, ...props }: Props) => {
 
 				<ContextMenu.Separator />
 
-				<ContextMenu.SubMenu label="Assign tag" icon={TagSimple}>
-					<AssignTagMenuItems objectId={objectData?.id || 0} />
-				</ContextMenu.SubMenu>
+				{objectData && (
+					<ContextMenu.SubMenu label="Assign tag" icon={TagSimple}>
+						<AssignTagMenuItems objectId={objectData.id} />
+					</ContextMenu.SubMenu>
+				)}
 
 				<ContextMenu.SubMenu label="More actions..." icon={Plus}>
 					<ContextMenu.Item
@@ -254,48 +260,96 @@ const AssignTagMenuItems = (props: { objectId: number }) => {
 
 	const tags = useLibraryQuery(['tags.list'], { suspense: true });
 	const tagsForObject = useLibraryQuery(['tags.getForObject', props.objectId], { suspense: true });
+
 	const assignTag = useLibraryMutation('tags.assign', {
 		onSuccess: () => {
 			submitPlausibleEvent({ event: { type: 'tagAssign' } });
 		}
 	});
 
+	const parentRef = useRef<HTMLDivElement>(null);
+	const rowVirtualizer = useVirtualizer({
+		count: tags.data?.length || 0,
+		getScrollElement: () => parentRef.current,
+		estimateSize: () => 30,
+		paddingStart: 2
+	});
+
+	const { isScrolled } = useScrolled(parentRef, 10);
+
 	return (
 		<>
-			{tags.data?.length === 0 && (
-				<div className="m-1 pb-10">
-					<Input autoFocus defaultValue="New tag" />
-				</div>
-			)}
-			{tags.data?.map((tag, index) => {
-				const active = !!tagsForObject.data?.find((t) => t.id === tag.id);
-
-				return (
-					<ContextMenu.Item
-						key={tag.id}
-						keybind={`${index + 1}`}
-						onClick={(e) => {
-							e.preventDefault();
-							if (props.objectId === null) return;
-
-							assignTag.mutate({
-								tag_id: tag.id,
-								object_id: props.objectId,
-								unassign: active
-							});
+			<ContextMenu.Item
+				label="New tag"
+				icon={Plus}
+				iconProps={{ size: 15 }}
+				keybind="⌘N"
+				onClick={() => {
+					dialogManager.create((dp) => <CreateDialog {...dp} assignToObject={props.objectId} />);
+				}}
+			/>
+			<ContextMenu.Separator className={clsx('mx-0 mb-0 transition', isScrolled && 'shadow')} />
+			{tags.data && tags.data.length > 0 ? (
+				<div
+					ref={parentRef}
+					style={{
+						maxHeight: `400px`,
+						height: `100%`,
+						width: `100%`,
+						overflow: 'auto'
+					}}
+				>
+					<div
+						style={{
+							height: `${rowVirtualizer.getTotalSize()}px`,
+							width: '100%',
+							position: 'relative'
 						}}
 					>
-						<div
-							className="mr-0.5 block h-[15px] w-[15px] rounded-full border"
-							style={{
-								backgroundColor: active ? tag.color || '#efefef' : 'transparent' || '#efefef',
-								borderColor: tag.color || '#efefef'
-							}}
-						/>
-						<p>{tag.name}</p>
-					</ContextMenu.Item>
-				);
-			})}
+						{rowVirtualizer.getVirtualItems().map((virtualRow) => {
+							const tag = tags.data[virtualRow.index];
+							const active = !!tagsForObject.data?.find((t) => t.id === tag?.id);
+
+							return (
+								tag && (
+									<ContextMenu.Item
+										key={virtualRow.index}
+										style={{
+											position: 'absolute',
+											top: 0,
+											left: 0,
+											width: '100%',
+											height: `${virtualRow.size}px`,
+											transform: `translateY(${virtualRow.start}px)`
+										}}
+										onClick={(e) => {
+											e.preventDefault();
+											assignTag.mutate({
+												tag_id: tag.id,
+												object_id: props.objectId,
+												unassign: active
+											});
+										}}
+									>
+										<div
+											className="mr-0.5 h-[15px] w-[15px] shrink-0 rounded-full border"
+											style={{
+												backgroundColor: active && tag.color ? tag.color : 'transparent',
+												borderColor: tag.color || '#efefef'
+											}}
+										/>
+										<span className="truncate">{tag.name}</span>
+									</ContextMenu.Item>
+								)
+							);
+						})}
+					</div>
+				</div>
+			) : (
+				<div className="text-ink-faint py-1 text-center text-xs">
+					{tags.data ? 'No tags' : 'Failed to load tags'}
+				</div>
+			)}
 		</>
 	);
 };
