94a1f07094a3d2cb7f9b8a1b4168605566fee4ff
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-03-29 02:14:54 -0300

    Improve the way react-devtools script is injected in dev mode (#641)
    * Improve the way react-devtools script is injected in dev mode
     - Implement a simple vite plugin to inject the reactdevtools script tag before serving index.html
     - Install reactdevtools and concurrently in desktop workspace
     - Replace tauri beforeDevCommand with concurrently for managing background process
    
    * Revert adding concurrently and executing react-devtools alongside tauri desktop app
     - Add instructions on how to run desktop app alongside react-devtools to CONTRIBUTING.md
    
    * Remove unnecessary 'react-devtools' script
     - Update wording in CONTRIBUTING.md to clarify how to launch 'react-devtools'

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -15,6 +15,20 @@ export default defineConfig({
 		tsconfigPaths(),
 		react(),
 		svg({ svgrOptions: { icon: true } }),
+		{
+			name: 'devtools-plugin',
+			transformIndexHtml(html) {
+				const isDev = process.env.NODE_ENV === 'development';
+				if (isDev) {
+					const devtoolsScript = `<script src="http://localhost:8097"></script>`;
+					const headTagIndex = html.indexOf('</head>');
+					if (headTagIndex > -1) {
+						return html.slice(0, headTagIndex) + devtoolsScript + html.slice(headTagIndex);
+					}
+				}
+				return html;
+			}
+		},
 		createHtmlPlugin({
 			minify: true
 		})
