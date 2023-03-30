60a0137f6591623340ef2d1eada50bc110b3bf1d
nikec <nikec.job@gmail.com>
2023-03-30 16:12:59 +0200

    Add label and error to form field

--- packages/ui/src/forms/FormField.tsx
+++ packages/ui/src/forms/FormField.tsx
@@ -1,28 +1,44 @@
+import clsx from 'clsx';
 import { PropsWithChildren, useId } from 'react';
 import { useFormContext } from 'react-hook-form';
 
 export interface UseFormFieldProps extends PropsWithChildren {
 	name: string;
-	// label: string;
+	label?: string;
+	required?: boolean;
+	className?: string;
 }
 
 export const useFormField = <P extends UseFormFieldProps>(props: P) => {
-	const { name, ...otherProps } = props;
+	const { name, label, required, className, ...otherProps } = props;
 	const id = useId();
 
 	return {
-		formFieldProps: { id, name },
+		formFieldProps: { id, name, label, required, className },
 		childProps: { ...otherProps, id, name }
 	};
 };
 
-interface FormFieldProps {
-	name: string;
+interface FormFieldProps extends UseFormFieldProps {
+	id: string;
 }
 
-export const FormField = ({ name, children }: PropsWithChildren<FormFieldProps>) => {
+export const FormField = (props: FormFieldProps) => {
 	const ctx = useFormContext();
-	const _ = ctx.getFieldState(name);
+	const state = ctx.getFieldState(props.name);
 
-	return <>{children}</>;
+	return (
+		<div className={clsx(props.className)}>
+			{props.label && (
+				<label htmlFor={props.id} className="mb-1 flex text-sm font-medium">
+					{props.label}
+					{props.required && <span className="ml-1 text-red-500">*</span>}
+				</label>
+			)}
+			{props.children}
+			{state.error?.message && (
+				<span className="mt-1 text-xs text-red-500">{state.error?.message}</span>
+			)}
+		</div>
+	);
 };
