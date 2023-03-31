2ca9857f88b38e07ffbe525e4e6d31e2c78fad39
Brendan Allan <brendonovich@outlook.com>
2023-03-31 18:35:26 +0800

    standardise vite configs

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,43 +1,15 @@
-import { relativeAliasResolver } from '@sd/config/vite';
-import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
-import { defineConfig } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
-import svg from 'vite-plugin-svgr';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { name, version } from './package.json';
+import { mergeConfig } from 'vite';
+import baseConfig from '../../packages/config/vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default mergeConfig(baseConfig, {
 	server: {
 		port: 8002
 	},
 	plugins: [
-		tsconfigPaths(),
-		react(),
-		svg({ svgrOptions: { icon: true } }),
-		createHtmlPlugin({
-			minify: true
-		}),
 		visualizer({
 			gzipSize: true,
 			brotliSize: true
 		})
-	],
-	css: {
-		modules: {
-			localsConvention: 'camelCaseOnly'
-		}
-	},
-	resolve: {
-		alias: [relativeAliasResolver]
-	},
-	root: 'src',
-	define: {
-		pkgJson: { name, version }
-	},
-	build: {
-		outDir: '../dist',
-		assetsDir: '.'
-	}
+	]
 });
---
+++ packages/config/vite/index.ts
@@ -0,0 +1,31 @@
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import svg from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import relativeAliasResolver from './relativeAliasResolver';
+
+export default defineConfig({
+	plugins: [
+		tsconfigPaths(),
+		react(),
+		svg({ svgrOptions: { icon: true } }),
+		createHtmlPlugin({
+			minify: true
+		})
+	],
+	css: {
+		modules: {
+			localsConvention: 'camelCaseOnly'
+		}
+	},
+	resolve: {
+		alias: [relativeAliasResolver]
+	},
+	root: 'src',
+	build: {
+		outDir: '../dist',
+		assetsDir: '.'
+	}
+});
