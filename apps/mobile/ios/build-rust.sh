016f040eaeb80e58bd76a7509d15e023c5b83143
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2023-03-31 14:52:30 +0300

    clean up and disable android for now

--- apps/mobile/ios/build-rust.sh
+++ apps/mobile/ios/build-rust.sh
@@ -11,20 +11,23 @@ fi
 
 # TODO: Also do this for non-Apple Silicon Macs
 if [[ $SPACEDRIVE_CI == "1" ]]; then
-  # IF SPACEDRIVE_CI is "1", build for x86_64-apple-ios 
+   
+  # Required for CI
   export PATH="$HOME/.cargo/bin:$PATH"
   export PROTOC=/usr/local/bin/protoc
+
+  cargo build -p sd-mobile-ios --target x86_64-apple-ios
+  
   if [[ $PLATFORM_NAME = "iphonesimulator" ]]
   then
-    cargo build -p sd-mobile-ios --target x86_64-apple-ios
     lipo -create -output $TARGET_DIRECTORY/libsd_mobile_ios-iossim.a $TARGET_DIRECTORY/x86_64-apple-ios/debug/libsd_mobile_ios.a
   else
-    cargo build -p sd-mobile-ios --target x86_64-apple-ios
     lipo -create -output $TARGET_DIRECTORY/libsd_mobile_ios-ios.a $TARGET_DIRECTORY/x86_64-apple-ios/debug/libsd_mobile_ios.a
   fi
   exit 0
 fi
 
+# Required for M1 Mac builds (?)
 export PROTOC=/opt/homebrew/bin/protoc
 
 if [[ $PLATFORM_NAME = "iphonesimulator" ]]
