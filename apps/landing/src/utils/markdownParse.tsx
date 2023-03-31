8d20fcfe33bfb16f9eb1e1e12de3170661b905bb
vloe <vloe.dev@gmail.com>
2023-03-31 15:36:20 +0200

    added title icons for slots (#660)
    slot icons now works w phosphor

--- apps/landing/src/utils/markdownParse.tsx
+++ apps/landing/src/utils/markdownParse.tsx
@@ -1,5 +1,7 @@
 import parseMarkdownMetadata from 'markdown-yaml-metadata-parser';
 import { marked } from 'marked';
+import { Fire, Info } from 'phosphor-react';
+import { renderToString } from 'react-dom/server';
 
 export interface MarkdownPageData {
 	name?: string;
@@ -51,9 +53,19 @@ export function parseMarkdown(markdownRaw: string): MarkdownParsed {
 
 				switch (kind) {
 					case 'slot':
-						return `<div class="slot-block ${name}"><h5 class="slot-block-title">${
-							extra || name
-						}</h5><p class="slot-block-content">${content}</p></div>`;
+						return (
+							// prettier-ignore
+							`<div class="slot-block ${name}">
+                                                                <div class="slot-block-title-container">
+                                                                        ${name === 'warning'
+                                                                                ? renderToString(<Fire className="slot-block-title-icon" />)
+                                                                                : renderToString(<Info className="slot-block-title-icon" />)
+                                                                        }
+                                                                        <h5 class="slot-block-title">${extra || name}</h5>
+                                                                </div>
+                                                                <p class="slot-block-content">${content}</p>
+                                                        </div>`
+						);
 						break;
 
 					default:
