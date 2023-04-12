0ce717d34b3ff4ba1923fa266834e21dfe572779
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-04-12 03:47:51 +0000

    [ENG-291] Location settings (#689)
    * Implement indexer rule editing for locations
    - Partially implement IndexerRuleEditor (missing indexer rule creation)
    - Update AddLocationDialog to use IndexerRuleEditor instead of checkboxes
    - Enable IndexerRuleEditor on the Location settings page
    - Improve error handling on the Location settings page
    
    * Change location data retrieve logic on settings page
     - Improve error handling on settings page (don't send NaN to backend)
     - Add disabled prop to Form component
     - Wait for data before allowing edits on location settings page
     - Change some snake_case object properties to camelCase
     - Fix a small error in rspc client that transformed any falsy argument value to null
    
    * Remove console.log
    
    * Fix issue with errors only being handled during the first load of the location settings page

--- packages/client/src/rspc.ts
+++ packages/client/src/rspc.ts
@@ -56,13 +56,13 @@ const libraryHooks = hooks.createHooks<
 					const libraryId = currentLibraryCache.id;
 					if (libraryId === null)
 						throw new Error('Attempted to do library operation with no library set!');
-					return [keyAndInput[0], { library_id: libraryId, arg: keyAndInput[1] || null }];
+					return [keyAndInput[0], { library_id: libraryId, arg: keyAndInput[1] ?? null }];
 				},
 				doMutation: (keyAndInput, next) => {
 					const libraryId = currentLibraryCache.id;
 					if (libraryId === null)
 						throw new Error('Attempted to do library operation with no library set!');
-					return next([keyAndInput[0], { library_id: libraryId, arg: keyAndInput[1] || null }]);
+					return next([keyAndInput[0], { library_id: libraryId, arg: keyAndInput[1] ?? null }]);
 				}
 			};
 		})
