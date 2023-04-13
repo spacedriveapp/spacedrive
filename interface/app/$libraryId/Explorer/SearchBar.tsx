809a6159464f3c803d0279909a150373f5eadfbd
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2023-04-13 12:33:13 +0300

    [ENG-450] TopBar revamp and restructure (#701)
    * TopBar revamp and restructure
    
    * file tweaks
    
    * Update package.json
    
    * Typecheck missed
    
    * TopBar revamp and restructure
    
    file tweaks
    
    Update package.json
    
    Typecheck missed
    
    * Removed search icon and layout change
    
    * move framer to interface/package.json
    
    * Increase spacing
    
    * fixes & tweaks
    
    * Update TopBar.tsx
    
    * typecheck tweak

--- interface/app/$libraryId/Explorer/SearchBar.tsx
+++ interface/app/$libraryId/Explorer/SearchBar.tsx
@@ -4,72 +4,78 @@ import { useForm } from 'react-hook-form';
 import { Input, Shortcut } from '@sd/ui';
 import { useOperatingSystem } from '~/hooks/useOperatingSystem';
 
-export default forwardRef<HTMLInputElement, ComponentPropsWithRef<'input'>>(
-	(props, forwardedRef) => {
-		const {
-			register,
-			handleSubmit,
-			reset,
-			formState: { dirtyFields }
-		} = useForm();
+interface Props extends ComponentPropsWithRef<'input'> {
+	formClassName?: string;
+}
 
-		const { ref, ...searchField } = register('searchField', {
-			onBlur: () => {
-				// if there's no text in the search bar, don't mark it as dirty so the key hint shows
-				if (!dirtyFields.searchField) reset();
-			}
-		});
+export default forwardRef<HTMLInputElement, Props>((props, forwardedRef) => {
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { dirtyFields }
+	} = useForm();
 
-		const platform = useOperatingSystem(false);
-		const os = useOperatingSystem(true);
+	const { ref, ...searchField } = register('searchField', {
+		onBlur: () => {
+			// if there's no text in the search bar, don't mark it as dirty so the key hint shows
+			if (!dirtyFields.searchField) reset();
+		}
+	});
 
-		useEffect(() => {
-			const keyboardSearchFocus = (event: KeyboardEvent) => {
-				if (typeof forwardedRef !== 'function') {
-					if ((event.key === 'f' && event.metaKey) || event.ctrlKey) {
-						event.preventDefault();
-						forwardedRef?.current?.focus();
-					} else if (forwardedRef?.current === document.activeElement && event.key === 'Escape') {
-						forwardedRef.current?.blur();
-					}
+	const platform = useOperatingSystem(false);
+	const os = useOperatingSystem(true);
+
+	useEffect(() => {
+		const keyboardSearchFocus = (event: KeyboardEvent) => {
+			if (typeof forwardedRef !== 'function') {
+				if ((event.key === 'f' && event.metaKey) || event.ctrlKey) {
+					event.preventDefault();
+					forwardedRef?.current?.focus();
+				} else if (forwardedRef?.current === document.activeElement && event.key === 'Escape') {
+					forwardedRef.current?.blur();
+					reset();
 				}
-			};
-			document.addEventListener('keydown', keyboardSearchFocus);
-			return () => {
-				document.removeEventListener('keydown', keyboardSearchFocus);
-			};
-			// eslint-disable-next-line react-hooks/exhaustive-deps
-		}, []);
+			}
+		};
+		document.addEventListener('keydown', keyboardSearchFocus);
+		return () => {
+			document.removeEventListener('keydown', keyboardSearchFocus);
+		};
+	}, [forwardedRef, reset]);
 
-		return (
-			<form onSubmit={handleSubmit(() => null)} className="relative flex h-7">
-				<Input
-					ref={(el) => {
-						ref(el);
-						if (typeof forwardedRef === 'function') forwardedRef(el);
-						else if (forwardedRef) forwardedRef.current = el;
-					}}
-					placeholder="Search"
-					className={clsx('w-32 transition-all focus-within:w-52', props.className)}
-					size="sm"
-					{...searchField}
-					right={
-						<div
-							className={clsx(
-								'pointer-events-none flex h-7 items-center space-x-1 opacity-70 group-focus-within:hidden'
-							)}
-						>
-							{platform === 'browser' ? (
-								<Shortcut chars="⌘F" aria-label={'Press Command-F to focus search bar'} />
-							) : os === 'macOS' ? (
-								<Shortcut chars="⌘F" aria-label={'Press Command-F to focus search bar'} />
-							) : (
-								<Shortcut chars="CTRL+F" aria-label={'Press CTRL-F to focus search bar'} />
-							)}
-						</div>
-					}
-				/>
-			</form>
-		);
-	}
-);
+	return (
+		<form
+			data-tauri-drag-region
+			onSubmit={handleSubmit(() => null)}
+			className={`relative flex h-7 ${props.formClassName}`}
+		>
+			<Input
+				ref={(el) => {
+					ref(el);
+					if (typeof forwardedRef === 'function') forwardedRef(el);
+					else if (forwardedRef) forwardedRef.current = el;
+				}}
+				placeholder="Search"
+				className={clsx('w-52 transition-all duration-200 focus-within:w-60', props.className)}
+				size="sm"
+				{...searchField}
+				right={
+					<div
+						className={clsx(
+							'pointer-events-none flex h-7 items-center space-x-1 opacity-70 group-focus-within:hidden'
+						)}
+					>
+						{platform === 'browser' ? (
+							<Shortcut chars="⌘F" aria-label={'Press Command-F to focus search bar'} />
+						) : os === 'macOS' ? (
+							<Shortcut chars="⌘F" aria-label={'Press Command-F to focus search bar'} />
+						) : (
+							<Shortcut chars="CTRL+F" aria-label={'Press CTRL-F to focus search bar'} />
+						)}
+					</div>
+				}
+			/>
+		</form>
+	);
+});
--- interface/app/$libraryId/Explorer/TopBar.tsx
+++ interface/app/$libraryId/Explorer/TopBar.tsx
@@ -1,229 +1,102 @@
 import clsx from 'clsx';
-import {
-	ArrowsClockwise,
-	CaretLeft,
-	CaretRight,
-	Columns,
-	Key,
-	MonitorPlay,
-	Rows,
-	SidebarSimple,
-	SlidersHorizontal,
-	SquaresFour,
-	Tag
-} from 'phosphor-react';
-import { useEffect, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { CaretLeft, CaretRight } from 'phosphor-react';
+import { useRef } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Popover, Tooltip } from '@sd/ui';
-import { getExplorerStore, useExplorerStore } from '~/hooks/useExplorerStore';
-import { useOperatingSystem } from '~/hooks/useOperatingSystem';
-import { KeybindEvent } from '~/util/keybind';
-import { KeyManager } from '../KeyManager';
-import OptionsPanel from './OptionsPanel';
+import { RoutePaths, groupKeys, useToolBarRouteOptions } from '~/hooks/useToolBarOptions';
 import SearchBar from './SearchBar';
 import TopBarButton from './TopBarButton';
 
 export const TOP_BAR_HEIGHT = 46;
 
-export type TopBarProps = {
-	showSeparator?: boolean;
-};
-
-export default (props: TopBarProps) => {
+export default () => {
 	const TOP_BAR_ICON_STYLE = 'm-0.5 w-5 h-5 text-ink-dull';
-	const platform = useOperatingSystem(false);
-	const os = useOperatingSystem(true);
-	const store = useExplorerStore();
 	const navigate = useNavigate();
 
-	//create function to focus on search box when cmd+k is pressed
 	const searchRef = useRef<HTMLInputElement>(null);
-
-	const focusSearchBar = (bar: HTMLInputElement, e?: Event): boolean => {
-		bar.focus();
-
-		e?.preventDefault();
-		return false;
-	};
-
-	useEffect(() => {
-		const searchBar = searchRef.current;
-
-		if (searchBar === null || !searchBar) return;
-
-		const handleKeybindAction = (e: KeybindEvent) => {
-			if (e.detail.action === 'open_search') {
-				return focusSearchBar(searchBar, e);
-			}
-		};
-
-		const handleDOMKeydown = (e: KeyboardEvent) => {
-			if (e.target === searchBar && e.key === 'Escape') {
-				(e.target as HTMLInputElement).blur();
-				e.preventDefault();
-				return;
-			}
-
-			const isBrowser = platform === 'browser';
-			// use cmd on macOS and ctrl on Windows
-			const hasModifier = os === 'macOS' ? e.metaKey : e.ctrlKey;
-
-			if (
-				// allow slash on all platforms
-				(e.key === '/' &&
-					!(document.activeElement instanceof HTMLInputElement) &&
-					!(document.activeElement instanceof HTMLTextAreaElement)) ||
-				// only do the cmd-f keybind check on browser to allow for native keybind functionality
-				// this is particularly useful for power-user niche use cases,
-				// like how macOS lets you redefine keybinds for apps
-				(isBrowser && hasModifier && e.key === 'f')
-			) {
-				document.dispatchEvent(new KeybindEvent('open_search'));
-				e.preventDefault();
-				return;
-			}
-		};
-
-		document.addEventListener('keydown', handleDOMKeydown);
-		document.addEventListener('keybindexec', handleKeybindAction);
-
-		return () => {
-			document.removeEventListener('keydown', handleDOMKeydown);
-			document.removeEventListener('keybindexec', handleKeybindAction);
-		};
-	}, [os, platform]);
+	const { pathname } = useLocation();
+	const getPageName = pathname.split('/')[2] as RoutePaths;
+	const { toolBarRouteOptions } = useToolBarRouteOptions();
 
 	return (
-		<>
-			<div
-				data-tauri-drag-region
-				className={clsx(
-					'max-w duration-250 absolute left-0 z-50 flex w-full shrink-0 items-center overflow-hidden border-b border-transparent bg-app pl-3 transition-[background-color] transition-[border-color] ease-out',
-					props.showSeparator && 'top-bar-blur !bg-app/90'
-				)}
-				style={{ height: TOP_BAR_HEIGHT }}
-			>
-				<div className="flex">
-					<Tooltip label="Navigate back">
-						<TopBarButton onClick={() => navigate(-1)}>
-							<CaretLeft weight="bold" className={TOP_BAR_ICON_STYLE} />
-						</TopBarButton>
-					</Tooltip>
-					<Tooltip label="Navigate forward">
-						<TopBarButton onClick={() => navigate(1)}>
-							<CaretRight weight="bold" className={TOP_BAR_ICON_STYLE} />
-						</TopBarButton>
-					</Tooltip>
-				</div>
+		<div
+			data-tauri-drag-region
+			className={clsx(
+				'duration-250 absolute top-0 z-20 flex grid h-[46px] w-full shrink-0 grid-cols-3 items-center justify-center overflow-hidden border-b border-sidebar-divider bg-app px-5 transition-[background-color] transition-[border-color] ease-out'
+			)}
+		>
+			<div data-tauri-drag-region className="flex ">
+				<Tooltip label="Navigate back">
+					<TopBarButton onClick={() => navigate(-1)}>
+						<CaretLeft weight="bold" className={TOP_BAR_ICON_STYLE} />
+					</TopBarButton>
+				</Tooltip>
+				<Tooltip label="Navigate forward">
+					<TopBarButton onClick={() => navigate(1)}>
+						<CaretRight weight="bold" className={TOP_BAR_ICON_STYLE} />
+					</TopBarButton>
+				</Tooltip>
+			</div>
 
-				<div data-tauri-drag-region className="flex grow flex-row justify-center">
-					<div className="mx-8 flex">
-						<Tooltip label="Grid view">
-							<TopBarButton
-								rounding="left"
-								active={store.layoutMode === 'grid'}
-								onClick={() => (getExplorerStore().layoutMode = 'grid')}
-							>
-								<SquaresFour className={TOP_BAR_ICON_STYLE} />
-							</TopBarButton>
-						</Tooltip>
-						<Tooltip label="List view">
-							<TopBarButton
-								rounding="none"
-								active={store.layoutMode === 'rows'}
-								onClick={() => (getExplorerStore().layoutMode = 'rows')}
-							>
-								<Rows className={TOP_BAR_ICON_STYLE} />
-							</TopBarButton>
-						</Tooltip>
-						<Tooltip label="Columns view">
-							<TopBarButton
-								rounding="none"
-								active={store.layoutMode === 'columns'}
-								onClick={() => (getExplorerStore().layoutMode = 'columns')}
-							>
-								<Columns className={TOP_BAR_ICON_STYLE} />
-							</TopBarButton>
-						</Tooltip>
-						<Tooltip label="Media view">
-							<TopBarButton
-								rounding="right"
-								active={store.layoutMode === 'media'}
-								onClick={() => (getExplorerStore().layoutMode = 'media')}
-							>
-								<MonitorPlay className={TOP_BAR_ICON_STYLE} />
-							</TopBarButton>
-						</Tooltip>
-					</div>
+			<SearchBar formClassName="justify-center" ref={searchRef} />
 
-					<SearchBar ref={searchRef} />
-
-					<div className="mx-8 flex space-x-2">
-						<Tooltip label="Key Manager">
-							<Popover
-								className="focus:outline-none"
-								trigger={
-									<TopBarButton>
-										<Key className={TOP_BAR_ICON_STYLE} />
-									</TopBarButton>
+			<div data-tauri-drag-region className="flex flex-row justify-end w-full">
+				<div data-tauri-drag-region className="flex gap-0">
+					{toolBarRouteOptions[getPageName].options.map((group) => {
+						return (Object.keys(group) as groupKeys[]).map((groupKey) => {
+							return group[groupKey]?.map(
+								({ icon, onClick, popOverComponent, toolTipLabel, topBarActive }, index) => {
+									const groupCount = Object.keys(group).length;
+									const groupIndex = Object.keys(group).indexOf(groupKey);
+									const roundingCondition =
+										index === 0
+											? 'left'
+											: index === (group[groupKey]?.length as number) - 1
+											? 'right'
+											: 'none';
+									return (
+										<div data-tauri-drag-region key={toolTipLabel} className="flex items-center">
+											<Tooltip label={toolTipLabel}>
+												{popOverComponent ? (
+													<Popover
+														className="focus:outline-none"
+														trigger={
+															<TopBarButton
+																rounding={roundingCondition}
+																active={topBarActive}
+																onClick={onClick}
+															>
+																{icon}
+															</TopBarButton>
+														}
+													>
+														<div className="block w-[250px] ">{popOverComponent}</div>
+													</Popover>
+												) : (
+													<TopBarButton
+														rounding={roundingCondition}
+														active={topBarActive}
+														onClick={onClick ?? undefined}
+													>
+														{icon}
+													</TopBarButton>
+												)}
+											</Tooltip>
+											{index === (group[groupKey]?.length as number) - 1 &&
+												groupCount !== groupIndex + 1 && (
+													<div
+														data-tauri-drag-region
+														className="mx-4 h-[15px] w-0 border-l border-zinc-600"
+													/>
+												)}
+										</div>
+									);
 								}
-							>
-								<div className="block w-[350px]">
-									<KeyManager />
-								</div>
-							</Popover>
-						</Tooltip>
-						<Tooltip label="Tag Assign Mode">
-							<TopBarButton
-								onClick={() => (getExplorerStore().tagAssignMode = !store.tagAssignMode)}
-								active={store.tagAssignMode}
-							>
-								<Tag
-									weight={store.tagAssignMode ? 'fill' : 'regular'}
-									className={TOP_BAR_ICON_STYLE}
-								/>
-							</TopBarButton>
-						</Tooltip>
-						<Tooltip label="Regenerate thumbs (temp)">
-							<TopBarButton>
-								<ArrowsClockwise className={TOP_BAR_ICON_STYLE} />
-							</TopBarButton>
-						</Tooltip>
-					</div>
-				</div>
-				<div className="mr-3 flex space-x-2">
-					<Tooltip label="Explorer display options" position="left">
-						<Popover
-							className="focus:outline-none"
-							trigger={
-								<TopBarButton className="my-2">
-									<SlidersHorizontal className={TOP_BAR_ICON_STYLE} />
-								</TopBarButton>
-							}
-						>
-							<div className="block w-[250px] ">
-								<OptionsPanel />
-							</div>
-						</Popover>
-					</Tooltip>
-
-					<Tooltip
-						label={store.showInspector ? 'Hide Inspector' : 'Show Inspector'}
-						position="left"
-					>
-						<TopBarButton
-							active={store.showInspector}
-							onClick={() => (getExplorerStore().showInspector = !store.showInspector)}
-							className="my-2"
-						>
-							<SidebarSimple
-								weight={store.showInspector ? 'fill' : 'regular'}
-								className={clsx(TOP_BAR_ICON_STYLE, 'scale-x-[-1]')}
-							/>
-						</TopBarButton>
-					</Tooltip>
+							);
+						});
+					})}
 				</div>
 			</div>
-		</>
+		</div>
 	);
 };
