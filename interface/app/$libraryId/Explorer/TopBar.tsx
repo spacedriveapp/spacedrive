18938a89fc327265587ca978d8c9633e57e13297
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2023-04-14 13:54:55 +0300

    [ENG-458] - Top Bar Responsiveness (#705)
    * Topbar responsiveness
    
    remove duplicate code
    
    Searchbar: remove form hook and replace with a regular state.
    
    onblur
    
    * Applied feedback + added back in top bar blur

--- interface/app/$libraryId/Explorer/TopBar.tsx
+++ interface/app/$libraryId/Explorer/TopBar.tsx
@@ -1,28 +1,58 @@
 import clsx from 'clsx';
 import { CaretLeft, CaretRight } from 'phosphor-react';
 import { useRef } from 'react';
+import { useLayoutEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { Popover, Tooltip } from '@sd/ui';
 import { RoutePaths, groupKeys, useToolBarRouteOptions } from '~/hooks/useToolBarOptions';
 import SearchBar from './SearchBar';
 import TopBarButton from './TopBarButton';
+import TopBarMobile from './TopBarMobile';
 
 export const TOP_BAR_HEIGHT = 46;
 
 export default () => {
 	const TOP_BAR_ICON_STYLE = 'm-0.5 w-5 h-5 text-ink-dull';
 	const navigate = useNavigate();
-
+	const topBarRef = useRef<HTMLDivElement>(null);
 	const searchRef = useRef<HTMLInputElement>(null);
 	const { pathname } = useLocation();
 	const getPageName = pathname.split('/')[2] as RoutePaths;
 	const { toolBarRouteOptions } = useToolBarRouteOptions();
+	const [topBarWidth, setTopBarWidth] = useState(0);
+	const countToolOptions = toolBarRouteOptions[getPageName].options.reduce(
+		(totalOptionsCount, option) => {
+			const allTools = ([].concat as any)(...Object.values(option));
+			return totalOptionsCount + allTools.length;
+		},
+		0
+	);
+
+	useLayoutEffect(() => {
+		const topBar = topBarRef.current;
+		const searchBar = searchRef.current;
+		const getTopBarWidth = () => {
+			if (topBar && searchBar) {
+				const { width } = topBar.getBoundingClientRect();
+				setTopBarWidth(width);
+			}
+		};
+		getTopBarWidth();
+		window.addEventListener('resize', getTopBarWidth);
+		return () => {
+			window.removeEventListener('resize', getTopBarWidth);
+		};
+	}, [topBarRef]);
+
+	const topBarCondition =
+		(topBarWidth < 1000 && countToolOptions >= 8) || (topBarWidth < 600 && countToolOptions >= 6);
 
 	return (
 		<div
 			data-tauri-drag-region
+			ref={topBarRef}
 			className={clsx(
-				'duration-250 absolute top-0 z-20 flex grid h-[46px] w-full shrink-0 grid-cols-3 items-center justify-center overflow-hidden border-b border-sidebar-divider bg-app px-5 transition-[background-color] transition-[border-color] ease-out'
+				'duration-250 top-bar-blur absolute top-0 z-20  grid h-[46px] w-full shrink-0 grid-cols-3 items-center justify-center overflow-hidden border-b border-sidebar-divider bg-app/90 px-5 transition-[background-color,border-color] ease-out'
 			)}
 		>
 			<div data-tauri-drag-region className="flex ">
@@ -41,21 +71,29 @@ export default () => {
 			<SearchBar formClassName="justify-center" ref={searchRef} />
 
 			<div data-tauri-drag-region className="flex flex-row justify-end w-full">
-				<div data-tauri-drag-region className="flex gap-0">
+				<div data-tauri-drag-region className={`gap-0 ${topBarCondition ? 'hidden' : 'flex'}`}>
 					{toolBarRouteOptions[getPageName].options.map((group) => {
 						return (Object.keys(group) as groupKeys[]).map((groupKey) => {
 							return group[groupKey]?.map(
-								({ icon, onClick, popOverComponent, toolTipLabel, topBarActive }, index) => {
+								(
+									{ icon, onClick, popOverComponent, toolTipLabel, topBarActive, individual },
+									index
+								) => {
 									const groupCount = Object.keys(group).length;
 									const groupIndex = Object.keys(group).indexOf(groupKey);
-									const roundingCondition =
-										index === 0
-											? 'left'
-											: index === (group[groupKey]?.length as number) - 1
-											? 'right'
-											: 'none';
+									const roundingCondition = individual
+										? 'both'
+										: index === 0
+										? 'left'
+										: index === (group[groupKey]?.length as number) - 1
+										? 'right'
+										: 'none';
 									return (
-										<div data-tauri-drag-region key={toolTipLabel} className="flex items-center">
+										<div
+											data-tauri-drag-region
+											key={toolTipLabel}
+											className={`flex items-center ${individual && 'mx-1'}`}
+										>
 											<Tooltip label={toolTipLabel}>
 												{popOverComponent ? (
 													<Popover
@@ -96,6 +134,7 @@ export default () => {
 						});
 					})}
 				</div>
+				<TopBarMobile className={`${topBarCondition ? 'flex' : 'hidden'}`} />
 			</div>
 		</div>
 	);
