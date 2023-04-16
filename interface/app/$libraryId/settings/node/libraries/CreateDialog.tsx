623789ca6604af4e62f0d4d7bbb19bdd109b9ac3
nikec <nikec.job@gmail.com>
2023-04-16 16:40:57 +0200

    Add advanced options to create lib dialog

--- interface/app/$libraryId/settings/node/libraries/CreateDialog.tsx
+++ interface/app/$libraryId/settings/node/libraries/CreateDialog.tsx
@@ -1,5 +1,7 @@
+import * as Tabs from '@radix-ui/react-tabs';
 import { useQueryClient } from '@tanstack/react-query';
-import { Controller } from 'react-hook-form';
+import { useEffect } from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import {
 	HASHING_ALGOS,
@@ -8,16 +10,26 @@ import {
 	useBridgeMutation,
 	usePlausibleEvent
 } from '@sd/client';
-import { Dialog, DialogSteps, RadioGroup, UseDialogProps, forms, useDialog } from '@sd/ui';
+import {
+	Dialog,
+	DialogSteps,
+	RadioGroup,
+	SelectOption,
+	UseDialogProps,
+	forms,
+	useDialog
+} from '@sd/ui';
 import { shareTelemetry } from '~/app/onboarding/privacy';
 
-const { Input, z, useZodForm, PasswordInput } = forms;
+const { Input, z, useZodForm, PasswordInput, Select } = forms;
 
 const nameSchema = z.object({ name: z.string().min(1) });
 const passwordSchema = z
 	.object({
 		password: z.string(),
-		password_validate: z.string()
+		password_validate: z.string(),
+		algorithm: z.enum(['XChaCha20Poly1305', 'Aes256Gcm']),
+		hashing_algorithm: hashingAlgoSlugSchema
 	})
 	.superRefine((data, ctx) => {
 		if (data.password && data.password !== data.password_validate) {
@@ -30,15 +42,60 @@ const passwordSchema = z
 	});
 const privacySchema = z.object({ share_telemetry: shareTelemetry.schema });
 
-const schema = nameSchema
-	.and(passwordSchema)
-	.and(privacySchema)
-	.and(
-		z.object({
-			algorithm: z.enum(['XChaCha20Poly1305', 'Aes256Gcm']),
-			hashing_algorithm: hashingAlgoSlugSchema
-		})
+const schema = nameSchema.and(passwordSchema).and(privacySchema);
+type FormSchema = (typeof schema)['_output'];
+
+const PasswordStep = () => {
+	const form = useFormContext<FormSchema>();
+
+	useEffect(() => {
+		!form.getValues('password') && form.setFocus('password');
+	}, []);
+
+	return (
+		<Tabs.Root defaultValue="password">
+			<Tabs.List className="mb-4 inline-flex gap-0.5 rounded-md border border-app-line bg-app-selected/40 p-0.5 dark:bg-app">
+				<Tabs.Trigger
+					className="rounded px-2 py-1 text-sm transition hover:bg-app hover:shadow radix-state-active:bg-app radix-state-active:shadow dark:hover:bg-app-input dark:radix-state-active:bg-app-input"
+					value="password"
+				>
+					Password
+				</Tabs.Trigger>
+				<Tabs.Trigger
+					className="rounded px-2 py-1 text-sm transition hover:bg-app hover:shadow radix-state-active:bg-app radix-state-active:shadow dark:hover:bg-app-input dark:radix-state-active:bg-app-input"
+					value="encryption"
+				>
+					Encryption
+				</Tabs.Trigger>
+			</Tabs.List>
+
+			<Tabs.Content className="space-y-4 outline-none" value="password" tabIndex={-1}>
+				<PasswordInput {...form.register('password')} label="Password" showStrength />
+				<PasswordInput
+					{...form.register('password_validate', {
+						onBlur: () => form.trigger('password_validate')
+					})}
+					label="Confirm password"
+				/>
+			</Tabs.Content>
+			<Tabs.Content className="space-y-4 outline-none" value="encryption" tabIndex={-1}>
+				<Select control={form.control} name="algorithm" label="Algorithm" size="md">
+					<SelectOption value="XChaCha20Poly1305">XChaCha20-Poly1305</SelectOption>
+					<SelectOption value="Aes256Gcm">AES-256-GCM</SelectOption>
+				</Select>
+
+				<Select control={form.control} name="hashing_algorithm" label="Hashing Algorithm" size="md">
+					<SelectOption value="Argon2id-s">Argon2id (standard)</SelectOption>
+					<SelectOption value="Argon2id-h">Argon2id (hardened)</SelectOption>
+					<SelectOption value="Argon2id-p">Argon2id (paranoid)</SelectOption>
+					<SelectOption value="BalloonBlake3-s">BLAKE3-Balloon (standard)</SelectOption>
+					<SelectOption value="BalloonBlake3-h">BLAKE3-Balloon (hardened)</SelectOption>
+					<SelectOption value="BalloonBlake3-p">BLAKE3-Balloon (paranoid)</SelectOption>
+				</Select>
+			</Tabs.Content>
+		</Tabs.Root>
 	);
+};
 
 export default (props: UseDialogProps) => {
 	const dialog = useDialog(props);
@@ -77,7 +134,7 @@ export default (props: UseDialogProps) => {
 
 	const password = form.watch('password');
 
-	const steps: DialogSteps<(typeof schema)['_output']> = [
+	const steps: DialogSteps<FormSchema> = [
 		{
 			schema: nameSchema,
 			title: 'Library name',
@@ -98,17 +155,7 @@ export default (props: UseDialogProps) => {
 			description: 'Choose a password for your new library.',
 			skippable: !password,
 			ctaLabel: !password ? 'Continue without' : undefined,
-			body: (
-				<>
-					<PasswordInput {...form.register('password')} label="Password" autoFocus showStrength />
-					<PasswordInput
-						{...form.register('password_validate', {
-							onBlur: () => form.trigger('password_validate')
-						})}
-						label="Confirm password"
-					/>
-				</>
-			)
+			body: <PasswordStep />
 		},
 		{
 			schema: privacySchema,
