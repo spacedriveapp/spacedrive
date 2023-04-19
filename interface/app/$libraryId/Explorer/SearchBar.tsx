2a4188577467ff49bca5451111d68f41e02dad58
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-04-19 13:22:19 -0300

    Misc UI fixes  - Solve React dev warning due to not using ref in a fowardRef  - Show title in alertDialog

--- interface/app/$libraryId/Explorer/SearchBar.tsx
+++ interface/app/$libraryId/Explorer/SearchBar.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { ComponentPropsWithRef, forwardRef, useEffect } from 'react';
+import { ComponentPropsWithRef, forwardRef, useEffect, useImperativeHandle } from 'react';
 import { useRef, useState } from 'react';
 import { Input, Shortcut } from '@sd/ui';
 import { useOperatingSystem } from '~/hooks/useOperatingSystem';
@@ -8,11 +8,13 @@ interface Props extends ComponentPropsWithRef<'input'> {
 	formClassName?: string;
 }
 
-export default forwardRef<HTMLInputElement, Props>((props) => {
+export default forwardRef<HTMLInputElement, Props>((props, ref) => {
 	const [searchValue, setSearchValue] = useState('');
 	const platform = useOperatingSystem(false);
 	const os = useOperatingSystem(true);
 	const searchRef = useRef<HTMLInputElement>(null);
+
+	useImperativeHandle(ref, () => searchRef.current!);
 
 	useEffect(() => {
 		const keyboardSearchFocus = (event: KeyboardEvent) => {
