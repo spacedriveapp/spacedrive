fe9240fd5044210aaea77ddbf74bbe45bb0a3a81
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-05-02 15:42:24 -0300

    Fix some ctrl+c/ctrl+v typos

--- apps/desktop/src-tauri/build.js
+++ apps/desktop/src-tauri/build.js
@@ -6,11 +6,11 @@ process.env.BACKGROUND_FILE_NAME = path.basename(process.env.BACKGROUND_FILE);
 process.env.BACKGROUND_CLAUSE = `set background picture of opts to file ".background:${process.env.BACKGROUND_FILE_NAME}"`;
 
 const child = spawn('pnpm', ['tauri', 'build'], { stdio: 'inherit' });
-process.on('SIGTERM', () => proc.kill('SIGTERM'));
-process.on('SIGINT', () => proc.kill('SIGINT'));
-process.on('SIGBREAK', () => proc.kill('SIGBREAK'));
-process.on('SIGHUP', () => proc.kill('SIGHUP'));
-proc.on('exit', (code, signal) => {
+process.on('SIGTERM', () => child.kill('SIGTERM'));
+process.on('SIGINT', () => child.kill('SIGINT'));
+process.on('SIGBREAK', () => child.kill('SIGBREAK'));
+process.on('SIGHUP', () => child.kill('SIGHUP'));
+child.on('exit', (code, signal) => {
 	let exitCode = code;
 	// exit code could be null when OS kills the process(out of memory, etc) or due to node handling it
 	// but if the signal is SIGINT the user exited the process so we want exit code 0
--- apps/desktop/src-tauri/dev.js
+++ apps/desktop/src-tauri/dev.js
@@ -59,11 +59,11 @@ if (isWindows) {
 }
 
 const child = spawn('pnpm', ['tauri', 'dev'], { stdio: 'inherit' });
-process.on('SIGTERM', () => proc.kill('SIGTERM'));
-process.on('SIGINT', () => proc.kill('SIGINT'));
-process.on('SIGBREAK', () => proc.kill('SIGBREAK'));
-process.on('SIGHUP', () => proc.kill('SIGHUP'));
-proc.on('exit', (code, signal) => {
+process.on('SIGTERM', () => child.kill('SIGTERM'));
+process.on('SIGINT', () => child.kill('SIGINT'));
+process.on('SIGBREAK', () => child.kill('SIGBREAK'));
+process.on('SIGHUP', () => child.kill('SIGHUP'));
+child.on('exit', (code, signal) => {
 	let exitCode = code;
 	// exit code could be null when OS kills the process(out of memory, etc) or due to node handling it
 	// but if the signal is SIGINT the user exited the process so we want exit code 0
