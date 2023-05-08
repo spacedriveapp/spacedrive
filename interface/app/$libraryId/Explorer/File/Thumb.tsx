2a5d5be4c0d73e1fd4c3e297d30338fde58499e9
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2023-05-09 01:31:49 +0300

    Job Manager improvement & refactor (#783)
    * d
    
    * wip
    
    * Nit: margin and padding tweak
    
    * UI design tweaks
    
    * Update Job.tsx
    
    * Improve UI
    
    * [WIP] - Refactor job manager
    
    * remove invalidate explorer event on thumb generation. the event above performs atomic updates on the front end when new thumbnails are generated, now just need to make that work
    
    * prettier formatting + removed unused imports
    
    * UI tweaks
    
    * progress bar width adjustment
    
    * tweaks
    
    * fix em
    
    * fix thumbnail generation
    
    * fix progress bar
    
    * fix time
    
    ---------
    
    Co-authored-by: Jamie Pine <ijamespine@me.com>
    Co-authored-by: Brendan Allan <brendonovich@outlook.com>
    Co-authored-by: Oscar Beaumont <oscar@otbeaumont.me>

--- interface/app/$libraryId/Explorer/File/Thumb.tsx
+++ interface/app/$libraryId/Explorer/File/Thumb.tsx
@@ -1,6 +1,6 @@
 import * as icons from '@sd/assets/icons';
 import clsx from 'clsx';
-import { memo, useLayoutEffect, useRef, useState } from 'react';
+import { memo, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { ExplorerItem, isKeyOf, useLibraryContext } from '@sd/client';
 import { useExplorerStore } from '~/hooks/useExplorerStore';
 import { useIsDark, usePlatform } from '~/util/Platform';
@@ -48,11 +48,21 @@ function Thumb({ size, cover, ...props }: ThumbProps) {
 	const [thumbSize, setThumbSize] = useState<null | VideoThumbSize>(null);
 	const { library } = useLibraryContext();
 	const [thumbLoaded, setThumbLoaded] = useState<boolean>(false);
-	const { locationId } = useExplorerStore();
-	const { cas_id, isDir, kind, hasThumbnail, extension } = getExplorerItemData(props.data);
+	const { locationId, newThumbnails } = useExplorerStore();
+
+	const { cas_id, isDir, kind, hasThumbnail, newThumb, extension } = getExplorerItemData(
+		props.data,
+		newThumbnails
+	);
 
 	// Allows disabling thumbnails when they fail to load
 	const [useThumb, setUseThumb] = useState<boolean>(hasThumbnail);
+
+	// When new thumbnails are generated, reset the useThumb state
+	// If it fails to load, it will be set back to false by the error handler in the img
+	useEffect(() => {
+		if (newThumb) setUseThumb(true);
+	}, [newThumb]);
 
 	useLayoutEffect(() => {
 		const img = thumbImg.current;
@@ -139,9 +149,9 @@ function Thumb({ size, cover, ...props }: ThumbProps) {
 								kind === 'Video' ? 'rounded' : 'rounded-sm',
 								classes.checkers,
 								size &&
-									(kind === 'Video'
-										? 'border-x-0 border-black'
-										: size > 60 && 'border-2 border-app-line'),
+								(kind === 'Video'
+									? 'border-x-0 border-black'
+									: size > 60 && 'border-2 border-app-line'),
 								props.className
 							)}
 						/>
@@ -151,11 +161,11 @@ function Thumb({ size, cover, ...props }: ThumbProps) {
 									cover
 										? {}
 										: thumbSize
-										? {
+											? {
 												marginTop: Math.floor(thumbSize.height / 2) - 2,
 												marginLeft: Math.floor(thumbSize.width / 2) - 2
-										  }
-										: { display: 'none' }
+											}
+											: { display: 'none' }
 								}
 								className={clsx(
 									cover
@@ -181,4 +191,4 @@ function Thumb({ size, cover, ...props }: ThumbProps) {
 	);
 }
 
-export default memo(Thumb);
+export default Thumb;
