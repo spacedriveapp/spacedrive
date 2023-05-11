5340947bb8734d49234b9a451ed36d0be093f8a2
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-05-11 03:48:46 -0300

    Github CI input/output are always string  - Attempt to fix Windows Clippy

--- crates/macos/build.rs
+++ crates/macos/build.rs
@@ -1,7 +1,9 @@
+#[cfg(target_os = "macos")]
 use std::env;
 
 fn main() {
-	if cfg!(target_os = "macos") {
+	#[cfg(target_os = "macos")]
+	{
 		let deployment_target =
 			env::var("MACOSX_DEPLOYMENT_TARGET").unwrap_or_else(|_| String::from("10.15"));
 
