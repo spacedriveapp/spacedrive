77ff422c8c050d9d3fb8a6b9287b6f302e0aab1e
Brendan Allan <brendonovich@outlook.com>
2023-05-14 02:20:55 +0800

    build output api

--- apps/landing/vite.config.ts
+++ apps/landing/vite.config.ts
@@ -1,3 +1,4 @@
+import vercelSsr from '@magne4000/vite-plugin-vercel-ssr';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
@@ -6,16 +7,19 @@ import { defineConfig } from 'vite';
 import md, { Mode } from 'vite-plugin-markdown';
 import ssr from 'vite-plugin-ssr/plugin';
 import svg from 'vite-plugin-svgr';
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
 	// prettier-ignore
 	// Prettier reeeally wants to one-line this -- I AM PUTTING MY FOOT DOWN AND SAYING NO!
 	plugins: [
 		react(),
-		ssr({ prerender: true }),
 		svg(),
 		md({ mode: [Mode.REACT] }),
 		visualizer(),
+		ssr({ prerender: true }),
+		vercel(),
+		vercelSsr()
 	],
 	css: {
 		modules: {
