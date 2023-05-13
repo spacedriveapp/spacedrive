868756e293d9755a089bb83ab1c84627a8362800
Brendan Allan <brendonovich@outlook.com>
2023-05-14 02:50:09 +0800

    maybe idk

--- apps/landing/src/pages/blog/index.page.tsx
+++ apps/landing/src/pages/blog/index.page.tsx
@@ -15,37 +15,35 @@ function Page({ posts }: { posts: BlogPosts }) {
 				<p className="fade-in-heading animation-delay-1">Get the latest from Spacedrive.</p>
 			</section>
 			<section className="animation-delay-2 grid grid-cols-1 gap-4 will-change-transform fade-in sm:grid-cols-1 lg:grid-cols-1">
-				{postsArray.map((post) => {
-					return (
-						<a
-							key={post.slug}
-							href={`/blog/${post.slug}`}
-							className="relative z-0 mb-8 flex cursor-pointer flex-col gap-2 overflow-hidden rounded-xl border border-gray-500 transition-colors"
-						>
-							{post.image && (
-								<img
-									src={`/${post.image}`}
-									alt=""
-									className="inset-0 -z-10 m-0 w-full rounded-t-xl object-cover md:h-96"
-								/>
-							)}
-							<div className="p-8">
-								<h2 className="text2xl m-0 md:text-4xl">{post.title}</h2>
-								<small className="m-0">{post.readTime} minute read.</small>
-								{/* <p className="line-clamp-3 my-2">{post.excerpt}</p> */}
-								<p className="m-0 text-white">
-									by {post.author} &middot;{' '}
-									{new Date(post.date ?? '').toLocaleDateString()}
-								</p>
-								<div className="mt-4 flex flex-wrap gap-2">
-									{post.tags?.map((tag) => (
-										<BlogTag key={tag} name={tag} />
-									))}
-								</div>
+				{postsArray.map((post) => (
+					<a
+						key={post.slug}
+						href={`/blog/${post.slug}`}
+						className="relative z-0 mb-8 flex cursor-pointer flex-col gap-2 overflow-hidden rounded-xl border border-gray-500 transition-colors"
+					>
+						{post.image && (
+							<img
+								src={`/${post.image}`}
+								alt=""
+								className="inset-0 -z-10 m-0 w-full rounded-t-xl object-cover md:h-96"
+							/>
+						)}
+						<div className="p-8">
+							<h2 className="text2xl m-0 md:text-4xl">{post.title}</h2>
+							<small className="m-0">{post.readTime} minute read.</small>
+							{/* <p className="line-clamp-3 my-2">{post.excerpt}</p> */}
+							<p className="m-0 text-white">
+								by {post.author} &middot;{' '}
+								{new Date(post.date ?? '').toLocaleDateString()}
+							</p>
+							<div className="mt-4 flex flex-wrap gap-2">
+								{post.tags?.map((tag) => (
+									<BlogTag key={tag} name={tag} />
+								))}
 							</div>
-						</a>
-					);
-				})}
+						</div>
+					</a>
+				))}
 			</section>
 		</div>
 	);
