db1df4eccdaab05e6731b36f9cdab35c1d49f3c2
Brendan Allan <brendonovich@outlook.com>
2023-05-14 17:15:14 +0800

    fix landing

--- apps/landing/vite.config.ts
+++ apps/landing/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
-// import esm from 'vite-plugin-esmodule';
 import md, { Mode } from 'vite-plugin-markdown';
 import ssr from 'vite-plugin-ssr/plugin';
 import svg from 'vite-plugin-svgr';
