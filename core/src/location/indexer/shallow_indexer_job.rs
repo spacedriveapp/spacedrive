44696452509b9f7ca3a959d3badba96de6ee7e0a
Brendan Allan <brendonovich@outlook.com>
2023-05-16 09:12:00 +0800

    Orphan Object Remover (#821)
    * orphan remover task
    
    * loop to do multiple passes
    
    * struct-ify

--- core/src/location/indexer/shallow_indexer_job.rs
+++ core/src/location/indexer/shallow_indexer_job.rs
@@ -186,6 +186,8 @@ impl StatefulJob for ShallowIndexerJob {
 				.await
 				.map_err(IndexerError::from)?;
 
+		ctx.library.orphan_remover.invoke().await;
+
 		// Filter out paths that are already in the databases
 		let new_paths = found_paths
 			.into_iter()
