0ad47d4f2b241f6ea958e3e5b093c53d096dbe0d
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2023-05-19 13:58:36 +0300

    Notice component and removed slot

--- apps/landing/src/components/mdx/Alert.tsx
+++
@@ -1,3 +0,0 @@
-export function Alert() {
-	return <div></div>;
-}
--- apps/landing/src/components/mdx/index.tsx
+++ apps/landing/src/components/mdx/index.tsx
@@ -1,6 +1,7 @@
 import NextImage, { ImageProps } from 'next/image';
+import Notice from './Notice';
 
 const MDXImage = (props: ImageProps) => <NextImage {...props} />;
 
 export const BlogMDXComponents = { MDXImage };
-export const DocMDXComponents = { MDXImage };
+export const DocMDXComponents = { MDXImage, Notice };
