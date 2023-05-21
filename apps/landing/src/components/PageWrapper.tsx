a9a87821d80e5e71402b91ef5747d5a715e07e04
Utku <74243531+utkubakir@users.noreply.github.com>
2023-05-21 17:16:53 +0300

    fix landing (#844)
    fix blooms

--- apps/landing/src/components/PageWrapper.tsx
+++ apps/landing/src/components/PageWrapper.tsx
@@ -5,7 +5,7 @@ export default function PageWrapper({ children }: { children: React.ReactNode })
 	return (
 		<>
 			<NavBar />
-			<main className="dark z-10 m-auto max-w-[100rem] dark:bg-black dark:text-white">
+			<main className="dark z-10 m-auto max-w-[100rem] overflow-hidden dark:bg-black dark:text-white">
 				{children}
 			</main>
 			<Footer />
