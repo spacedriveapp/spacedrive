36b7bf686a7c2edc01e4d17e9b1c03a02adee600
Utku <74243531+utkubakir@users.noreply.github.com>
2023-05-21 23:03:16 +0300

    blooms... (#845)

--- apps/landing/src/components/PageWrapper.tsx
+++ apps/landing/src/components/PageWrapper.tsx
@@ -3,12 +3,12 @@ import NavBar from './NavBar';
 
 export default function PageWrapper({ children }: { children: React.ReactNode }) {
 	return (
-		<>
+		<div className="overflow-hidden">
 			<NavBar />
-			<main className="dark z-10 m-auto max-w-[100rem] overflow-hidden dark:bg-black dark:text-white">
+			<main className="dark z-10 m-auto max-w-[100rem] dark:bg-black dark:text-white">
 				{children}
 			</main>
 			<Footer />
-		</>
+		</div>
 	);
 }
