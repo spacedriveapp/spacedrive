2fcfe6d3702ee888ed0864afee40b46ae751f012
brxken128 <77554505+brxken128@users.noreply.github.com>
2023-05-23 09:22:40 +0100

    new KM (it's awesome)

--- core/src/crypto/error.rs
+++ core/src/crypto/error.rs
@@ -9,4 +9,38 @@ pub enum CryptoError {
 
 	#[error("generic key manager error")]
 	KeyManager,
+
+	#[error("the key specified was not found")]
+	KeyNotFound,
+	#[error("the key manager is not unlocked")]
+	NotUnlocked,
+	#[error("the key is already mounted")]
+	AlreadyMounted,
+
+	#[error("there was an error during a conversion")]
+	Conversion,
+
+	#[error("the test vector failed (password is likely incorrect)")]
+	IncorrectPassword,
+	#[error("there was an issue while unlocking the key manager")]
+	Unlock,
+
+	#[error("an unsupported operation was attempted")]
+	Unsupported,
+
+	#[error("the word provided is too short")]
+	WordTooShort,
+
+	#[error("the specified file already exists and would be overwritten")]
+	FileAlreadyExists,
+	#[error("the specified file doesn't exist")]
+	FileDoesntExist,
+	#[error("the specified file is too large")]
+	FileTooLarge,
+
+	#[error("database error: {0}")]
+	Database(#[from] prisma_client_rust::QueryError),
+
+	#[error("async IO error: {0}")]
+	IoAsync(#[from] tokio::io::Error),
 }
