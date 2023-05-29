b43f1badd0001be0c07944262f25eb4b14382850
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2023-05-29 15:24:21 +0300

    Update Link.tsx

--- interface/app/$libraryId/Layout/Sidebar/Link.tsx
+++ interface/app/$libraryId/Layout/Sidebar/Link.tsx
@@ -5,7 +5,7 @@ import { NavLink, NavLinkProps } from 'react-router-dom';
 import { useOperatingSystem } from '~/hooks/useOperatingSystem';
 
 const styles = cva(
-	'max-w flex grow flex-row items-center gap-0.5 truncate rounded px-2 py-1 text-sm font-medium outline-none ring-offset-sidebar focus:ring-2 focus:ring-accent focus:ring-offset-2',
+	'max-w flex grow flex-row items-center gap-0.5 truncate rounded px-2 py-1 ring-inset ring-transparent text-sm font-medium outline-none ring-offset-0 focus:ring-1 focus:ring-accent focus:ring-offset-0',
 	{
 		variants: {
 			active: {
@@ -35,9 +35,7 @@ export default ({
 				clsx(
 					styles({ active: isActive, transparent: os === 'macOS' }),
 					disabled && 'pointer-events-none opacity-50',
-					className,
-					os === 'browser' &&
-						'!ring-1 !ring-inset ring-transparent focus:ring-accent focus:!ring-offset-0'
+					className
 				)
 			}
 			{...props}
