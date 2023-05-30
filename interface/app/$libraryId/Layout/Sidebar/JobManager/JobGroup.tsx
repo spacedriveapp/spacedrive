eb37c20014f9d73b52c48ffee01d6d3f3c74965f
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2023-05-30 17:30:03 +0300

    Fill fallback value - was causing null error of page

--- interface/app/$libraryId/Layout/Sidebar/JobManager/JobGroup.tsx
+++ interface/app/$libraryId/Layout/Sidebar/JobManager/JobGroup.tsx
@@ -72,10 +72,13 @@ function JobGroup({ data, clearJob }: JobGroupProps) {
 							<div className="truncate">
 								<p className="truncate font-semibold">
 									{allJobsCompleted
-										? `Added location "${
-												data.metadata.init.location.name || ''
-										  }"`
-										: `Indexing "${data.metadata.init.location.name || ''}"`}
+										? `Added location ${
+												data.metadata?.init?.location?.name || ''
+										  }`
+										: `Indexing ${
+												data.metadata?.init?.location?.name ||
+												'added location'
+										  }`}
 								</p>
 								<p className="my-[2px] text-ink-faint">
 									<b>{tasks.total} </b>
