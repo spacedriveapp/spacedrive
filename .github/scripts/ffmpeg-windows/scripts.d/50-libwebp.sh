f17a10fa510b4eab6e61e1da3971377caf8ca237
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-05-30 20:08:57 +0200

    Revert libwebp to a static build

--- .github/scripts/ffmpeg-windows/scripts.d/50-libwebp.sh
+++ .github/scripts/ffmpeg-windows/scripts.d/50-libwebp.sh
@@ -7,28 +7,36 @@ ffbuild_dockerbuild() {
   git-mini-clone "$SCRIPT_REPO" "$SCRIPT_COMMIT" webp
   cd webp
 
-  mkdir build && cd build
+  ./autogen.sh
 
-  cmake .. -G Ninja \
-    -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
-    -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_SHARED_LIBS=ON \
-    -DWEBP_LINK_STATIC=ON \
-    -DWEBP_BUILD_ANIM_UTILS=OFF \
-    -DWEBP_BUILD_CWEBP=OFF \
-    -DWEBP_BUILD_DWEBP=OFF \
-    -DWEBP_BUILD_GIF2WEBP=OFF \
-    -DWEBP_BUILD_IMG2WEBP=OFF \
-    -DWEBP_BUILD_VWEBP=OFF \
-    -DWEBP_BUILD_WEBPINFO=OFF \
-    -DWEBP_BUILD_WEBPMUX=OFF \
-    -DWEBP_BUILD_EXTRAS=OFF \
-    -DWEBP_BUILD_WEBP_JS=OFF
+  local myconf=(
+    --prefix="$FFBUILD_PREFIX"
+    --enable-shared
+    --disable-static
+    --with-pic
+    --enable-libwebpmux
+    --disable-libwebpextras
+    --disable-libwebpdemux
+    --disable-sdl
+    --disable-gl
+    --disable-png
+    --disable-jpeg
+    --disable-tiff
+    --disable-gif
+  )
 
-  ninja -j"$(nproc)"
+  if [[ $TARGET == win* || $TARGET == linux* ]]; then
+    myconf+=(
+      --host="$FFBUILD_TOOLCHAIN"
+    )
+  else
+    echo "Unknown target"
+    return -1
+  fi
 
-  ninja install
+  export CFLAGS="$CFLAGS -fcommon"
 
-  mv "$FFBUILD_PREFIX/bin"/*.dll "$FFBUILD_PREFIX/lib"
+  ./configure "${myconf[@]}"
+  make -j"$(nproc)"
+  make install
 }
