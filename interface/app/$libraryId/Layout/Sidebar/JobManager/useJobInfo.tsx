d5eed41e3b3e4fe1ece04787aa1e853dcc4002de
Jamie Pine <32987599+jamiepine@users.noreply.github.com>
2023-06-02 03:30:39 -0700

    Update useJobInfo.tsx
    better

--- interface/app/$libraryId/Layout/Sidebar/JobManager/useJobInfo.tsx
+++ interface/app/$libraryId/Layout/Sidebar/JobManager/useJobInfo.tsx
@@ -1,14 +1,8 @@
 import { JobReport } from '@sd/client';
 import {
-	Copy,
 	Fingerprint,
 	Folder,
-	Image,
-	LockSimple,
-	LockSimpleOpen,
-	Scissors,
-	Trash,
-	TrashSimple
+	Image
 } from 'phosphor-react';
 import { TextItems } from './JobContainer';
 
@@ -23,18 +17,31 @@ export default function useJobInfo(job: JobReport,
 	const isRunning = job.status === 'Running';
 	const isQueued = job.status === 'Queued';
 	const indexedPath = job.metadata?.data?.indexed_path;
-	const taskCount = numberWithCommas(job.task_count);
-	const completedTaskCount = numberWithCommas(job.completed_task_count);
-	const fileCount = appendPlural(job.task_count, 'file');
+	const taskCount = comma(job.task_count);
+	const completedTaskCount = comma(job.completed_task_count);
+	// const fileCount = plural(job.task_count, 'file');
+	const meta = job.metadata;
 
 	return ({
+		indexer: {
+			name: isRunning || isQueued
+				? `Indexing files ${indexedPath && `at ${indexedPath}`}`
+				: `Indexed files  ${indexedPath && `at ${indexedPath}`}`
+			,
+			icon: Folder,
+			textItems: [[
+				{ text: `${comma(job.completed_task_count)} of ${comma(job.task_count)} ${plural(job.task_count, 'task')}` },
+				{ text: `${comma(meta?.total_indexed_directories)} ${plural(meta?.total_indexed_directories, 'folder')}` },
+				{ text: `${comma(meta?.total_indexed_files)} ${plural(meta?.total_indexed_files, 'file')}` },
+			]]
+		},
 		thumbnailer: {
-			name: `${isRunning || isQueued
+			name: isRunning || isQueued
 				? 'Generating thumbnails'
 				: 'Generated thumbnails'
-				}`,
+			,
 			icon: Image,
-			textItems: [[{ text: `${completedTaskCount} of ${taskCount} ${appendPlural(job.task_count, 'thumbnail')} generated` }]]
+			textItems: [[{ text: `${completedTaskCount} of ${taskCount} ${plural(job.task_count, 'thumbnail')} generated` }]]
 		},
 		file_identifier: {
 			name: `${isRunning || isQueued
@@ -43,38 +50,23 @@ export default function useJobInfo(job: JobReport,
 				}`,
 			icon: Fingerprint,
 			textItems: [[
-				{ text: `${numberWithCommas(job.metadata?.total_orphan_paths)} ${appendPlural(job.metadata?.total_orphan_paths, 'file')}` },
-				{ text: `${numberWithCommas(job.metadata?.total_objects_created)} ${appendPlural(job.metadata?.total_objects_created, 'Object')} created` },
-				{ text: `${numberWithCommas(job.metadata?.total_objects_linked)} ${appendPlural(job.metadata?.total_objects_linked, 'Object')} linked` }
+				{ text: `${comma(meta?.total_orphan_paths)} ${plural(meta?.total_orphan_paths, 'file')}` },
+				{ text: `${comma(meta?.total_objects_created)} ${plural(meta?.total_objects_created, 'Object')} created` },
+				{ text: `${comma(meta?.total_objects_linked)} ${plural(meta?.total_objects_linked, 'Object')} linked` }
 			]]
 		},
-		indexer: {
-			name: `${isRunning || isQueued
-				? 'Indexing files'
-				: 'Indexed files'
-				}`,
-			icon: Folder,
-			textItems: [[
-				{ text: `${numberWithCommas(job.completed_task_count)} of ${numberWithCommas(job.task_count)} ${appendPlural(job.task_count, 'task')}` },
-				{ text: `${numberWithCommas(job.metadata?.total_indexed_directories)} ${appendPlural(job.metadata?.total_indexed_directories, 'folder')}` },
-				{ text: `${numberWithCommas(job.metadata?.total_indexed_files)} ${appendPlural(job.metadata?.total_indexed_files, 'file')}` },
-			]]
-		}
-
 		// Repeat the similar pattern for all subtext fields
 	})
 }
 
-
-
-function appendPlural(count: number, name?: string) {
+function plural(count: number, name?: string) {
 	if (count === 1) {
 		return name || '';
 	}
 	return `${name || ''}s`;
 }
 
-function numberWithCommas(x: number) {
+function comma(x: number) {
 	if (!x) return 0;
 	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
