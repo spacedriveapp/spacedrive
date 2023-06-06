e3eeab55138589a43e52e1284cd33ad808c1795d
nikec <43032218+niikeec@users.noreply.github.com>
2023-06-06 08:55:56 +0200

    [ENG 655] Explorer restructure (#858)
    * wip
    
    * wip 2
    
    * Grid list single selection
    
    * core & pnpm-lock
    
    * Merge branch 'main'
    
    Conflicts:
    	interface/app/$libraryId/Explorer/index.tsx
    
    * missing import from merge
    
    * fix total_orphan_paths bug
    
    * add top bar context
    
    * missing pieces of merge
    
    * missing pieces of merge
    
    * missing divs
    
    * Fill fallback value - was causing null error of page
    
    * spelling fixes
    
    * notice light theme, list view update, other explorer updates
    
    * Update pnpm-lock
    
    * Remove procedure
    
    * fix light menu ink color
    
    * fix list view scrolled state
    
    * Change layout default
    
    * Remove unused imports
    
    * remove keys
    
    * empty notice & context menu overview
    
    * Fix prevent selection while context menu is up
    
    * Fix scroll with keys
    
    * Empty notice icon
    
    * Add light icons
    
    * Context menu and fixed list view scroll
    
    * Fix name column sizing
    
    * top/bottom scroll position
    
    * Tag assign only when objectData
    
    * Fix list view locked state
    
    * fix ci
    
    * shamefully ignore eslint
    
    ---------
    
    Co-authored-by: Jamie Pine <ijamespine@me.com>
    Co-authored-by: ameer2468 <33054370+ameer2468@users.noreply.github.com>
    Co-authored-by: Jamie Pine <32987599+jamiepine@users.noreply.github.com>
    Co-authored-by: Utku Bakir <74243531+utkubakir@users.noreply.github.com>

--- interface/app/$libraryId/Explorer/ViewContext.ts
+++ interface/app/$libraryId/Explorer/ViewContext.ts
@@ -1,15 +1,25 @@
-import { RefObject, createContext, useContext } from 'react';
+import { ReactNode, RefObject, createContext, useContext } from 'react';
 import { ExplorerItem } from '@sd/client';
 
-interface Context {
-	data: ExplorerItem[];
+export type ExplorerViewSelection = number | number[];
+
+export interface ExplorerViewContext<T = ExplorerViewSelection> {
+	items: ExplorerItem[] | null;
 	scrollRef: RefObject<HTMLDivElement>;
-	isFetchingNextPage?: boolean;
-	onLoadMore?(): void;
-	hasNextPage?: boolean;
+	selected?: T;
+	onSelectedChange?: (selected: T) => void;
+	overscan?: number;
+	onLoadMore?: () => void;
+	rowsBeforeLoadMore?: number;
+	top?: number;
+	multiSelect?: boolean;
+	contextMenu?: ReactNode;
+	setIsContextMenuOpen?: (isOpen: boolean) => void;
+	selectable?: boolean;
+	padding?: number | { x?: number; y?: number };
 }
 
-export const ViewContext = createContext<Context | null>(null);
+export const ViewContext = createContext<ExplorerViewContext | null>(null);
 
 export const useExplorerViewContext = () => {
 	const ctx = useContext(ViewContext);
