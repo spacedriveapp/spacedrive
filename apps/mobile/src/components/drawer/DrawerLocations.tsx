6dbd61adef9a54e7ca31339f859e949b9d377224
Utku <74243531+utkubakir@users.noreply.github.com>
2023-06-08 19:06:17 +0300

    [MOB-3] Small fixes and improvements (#813)
    * add location button
    
    * add tag button
    
    * library manager arrow points right when open
    
    * wip create lib modal
    
    * handle .spacedrive file in location
    
    * fix location screen title
    
    * remove create lib dialog and use a modal instead
    
    * clean tsconfig.tsbuildinfo too
    
    * update some packages
    
    * modal paddings
    
    * fix onboarding animations

--- apps/mobile/src/components/drawer/DrawerLocations.tsx
+++ apps/mobile/src/components/drawer/DrawerLocations.tsx
@@ -36,7 +36,7 @@ type DrawerLocationsProp = {
 const DrawerLocations = ({ stackName }: DrawerLocationsProp) => {
 	const navigation = useNavigation<DrawerNavigationHelpers>();
 
-	const importModalRef = useRef<ModalRef>(null);
+	const modalRef = useRef<ModalRef>(null);
 
 	const { data: locations } = useLibraryQuery(['locations.list'], { keepPreviousData: true });
 
@@ -62,7 +62,7 @@ const DrawerLocations = ({ stackName }: DrawerLocationsProp) => {
 					))}
 				</View>
 				{/* Add Location */}
-				<Pressable onPress={() => importModalRef.current?.present()}>
+				<Pressable onPress={() => modalRef.current?.present()}>
 					<View style={tw`mt-1 rounded border border-dashed border-app-line/80`}>
 						<Text style={tw`p-2 text-center text-xs font-bold text-gray-400`}>
 							Add Location
@@ -70,7 +70,7 @@ const DrawerLocations = ({ stackName }: DrawerLocationsProp) => {
 					</View>
 				</Pressable>
 			</CollapsibleView>
-			<ImportModal ref={importModalRef} />
+			<ImportModal ref={modalRef} />
 		</>
 	);
 };
