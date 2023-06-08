3c5ab27a3f6b375e960b7927d5f82017d4cf0ccb
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-06-08 04:26:59 +0100

    Move final copy dll logic to external script

--- .github/scripts/ffmpeg-windows/copy_dll.sh
+++ .github/scripts/ffmpeg-windows/copy_dll.sh
@@ -11,13 +11,39 @@ if [ -z "${FFBUILD_PREFIX:-}" ]; then
   exit 1
 fi
 
-find "$FFBUILD_PREFIX/bin" -name '*.dll' -print0 | while IFS= read -r -d '' _dll; do
-  _dir="$(dirname "$_dll")"
-  _dir="${_dir#"${FFBUILD_PREFIX}/bin/"}"
-  if [ -z "$_dir" ]; then
-    _dir='.'
-  elif [ "$_dir" != '.' ]; then
-    mkdir -p "${FFBUILD_PREFIX}/lib/${_dir}"
-  fi
-  mv -v "$_dll" "${FFBUILD_PREFIX}/lib/${_dir}/"
-done
+if [ "${1:-}" = 'final' ]; then
+  find "${FFBUILD_PREFIX}/lib" -name '*.dll' -print0 | while IFS= read -r -d '' _dll; do
+    x86_64-w64-mingw32-strip -s "$_dll"
+    cp -av "$_dll" /opt/dlls/bin
+
+    _dir="$(dirname "$_dll")"
+    _dir="${_dir#"${FFBUILD_PREFIX}/lib/"}"
+    if [ -z "$_dir" ]; then
+      _dir='.'
+    fi
+
+    _name="$(basename "$_dll" '.dll')"
+
+    if [ -f "${FFBUILD_PREFIX}/lib/${_dir}/${_name}.dll.a" ]; then
+      mkdir -p "/opt/dlls/lib/${_dir}"
+      cp -av "${FFBUILD_PREFIX}/lib/${_dir}/${_name}.dll.a" "/opt/dlls/lib/${_dir}/"
+    fi
+
+    if [ -f "${FFBUILD_PREFIX}/lib/${_dir}/${_name}.lib" ]; then
+      mkdir -p "/opt/dlls/lib/${_dir}"
+      cp -av "${FFBUILD_PREFIX}/lib/${_dir}/${_name}.lib" "/opt/dlls/lib/${_dir}/"
+    fi
+  done
+else
+  find "$FFBUILD_PREFIX/bin" -name '*.dll' -print0 | while IFS= read -r -d '' _dll; do
+    _dir="$(dirname "$_dll")"
+    _dir="${_dir#"${FFBUILD_PREFIX}/bin/"}"
+    if [ -z "$_dir" ]; then
+      _dir='.'
+    elif [ "$_dir" != '.' ]; then
+      mkdir -p "${FFBUILD_PREFIX}/lib/${_dir}"
+    fi
+    mv -v "$_dll" "${FFBUILD_PREFIX}/lib/${_dir}/"
+  done
+
+fi
