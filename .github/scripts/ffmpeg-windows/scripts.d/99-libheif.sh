44efac959df0ab2034765306344796e6b102c3fa
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-06-10 16:23:37 +0100

    [ENG-630] Windows ffmpeg + libheif custom build (#871)
    * Initial Windows ffmpeg + libheif custom build
    
    * Add build steps for most of ffmpeg deps
    
    * FFmpeg deps and libheif
    
    * Fix libheif build
    
    * Fix libvpx and dlfcn + attempt to fix rav1e
    
    * Rework the whole ffmpeg-windows build system
     - New system based on https://github.com/BtbN/FFmpeg-Builds
     - Add new ffmpeg-windows workflow
     - Rename macos ffmpeg workflow
     - Adapt macos setupt script due to above name change
    
    * Forgot to update update the workflow name
    
    * Strip all libs from debug symbols
    
    * Add docs
    
    * Add libde265 deps, required by libheif
     - Make x265, svtav1 and dav1d as shared deps (used by both ffmpeg and libheif)
    
    * Add missing libheif to Linux setup script
    
    * Fix libx265 build script
    
    * Forgot to point x265 ninja install to the correct directory
    
    * Remove libaom and libsvt-av1
     - dav1d and rav1d are our default AV1 decoders/encoders
     - Quote subshell executions
     - Make libweb shared
    
    * Forgot to remove libaom and libsvt-av1 build steps
    
    * Fix typo
    
    * Try force webp to link against static libs
    
    * Revert libwebp to a static build
    
    * Dumb typo
    
    * Modify windows script to download our ffmpeg build (WIP)
    
    * Fix dlls output folder structure
    
    * Fix dumb mistake
    
    * Remove unused ffbuild_enabled
    
    * Enable core's heif feature on Windows
     - Fix windows ffmpeg build not including the headers
     - Fix windows setupt script incorrect download loagic
     - Implement build_arg to pass which repo ref to use when cloning
    
    * Fix windows setup script
    
    * Fix workflow artifact path
     - Make ffmpeg-windows dockerfile respect the FFMPEG_VERSION env
    
    * Fix Windows setup script incorrect logic for downloading ffmpeg builds
    
    * Error out when workflow_runs is empty
    
    * Fix dumb mistake
    
    * Manually define ffmpeg version for windows script
    
    * Fix ffmpeg windows build extract logic
    
    * Fix prop access in windows setup script
    
    * Revert back to a web request because nightly.link does a redirect before serving the artifact content
    
    * Fix windows setup script
    
    * Do not use nightly.link in Github CI
    
    * Fix windows setup script
    
    * Should finally fix window setup script
     - Update ffmpeg-windows deps
     - Should fix ffmpeg-windows failing to build due to mingw changes in new base image
    
    * Fix libxz failing to build due to doxygen
    
    * Fix windows setup-script not executing till the end
    
    * Fix LASTEXITCODE not defined
    
    * Remove libjxl, deps are not being compiled
    
    * Fix dll and lib copy logic
    
    * Move final copy dll logic to external script
    
    * Use main for libjxl
    
    * Change brotli from stable to main
     - Attempt to fix libjxl
    
    * Attempt fix lib copy again
    
    * Split copy_dll logic to avoid cache burst in docker
    
    * Missing file
    
    * Change how to export build files from shared deps
    
    * Replace rsync with cp
    
    * Fix copy
    
    * Fix dir not existing
    
    * Fix pkgconfig
    
    * Remove superflous files from exported ffmpeg for windows
     - Adjust dav1d to not build tools and examples
     - Adjust windows setup-script to point linker to the libs directory
    
    * Fix dav1d meson config args
    
    * Fix dumb mistake
    
    * WORK PLZ
    
    * Fix .lib file location
     - Strip all dlls
    
    * Formatter

---
+++ .github/scripts/ffmpeg-windows/scripts.d/99-libheif.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SCRIPT_REPO='https://github.com/strukturag/libheif.git'
+SCRIPT_TAG='v1.16.2'
+
+ffbuild_dockerbuild() {
+  git-mini-clone "$SCRIPT_REPO" "$SCRIPT_TAG" libheif
+
+  cd libheif
+
+  mkdir build && cd build
+
+  cmake \
+    -GNinja \
+    -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX='/opt/dlls' \
+    -DCMAKE_INSTALL_BINDIR='/opt/dlls/bin' \
+    -DCMAKE_INSTALL_LIBDIR='/opt/dlls/lib' \
+    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_TESTING=OFF \
+    -DWITH_EXAMPLES=OFF \
+    -DWITH_FUZZERS=OFF \
+    -DWITH_REDUCED_VISIBILITY=ON \
+    -DWITH_DEFLATE_HEADER_COMPRESSION=ON \
+    -DWITH_AOM_DECODER_PLUGIN=OFF \
+    -DWITH_AOM_ENCODER_PLUGIN=OFF \
+    -DWITH_DAV1D_PLUGIN=OFF \
+    -DWITH_LIBDE265_PLUGIN=OFF \
+    -DWITH_RAV1E_PLUGIN=OFF \
+    -DWITH_SvtEnc_PLUGIN=OFF \
+    -DWITH_X265_PLUGIN=OFF \
+    ..
+
+  ninja -j"$(nproc)"
+  ninja install
+}
---
+++ apps/server/docker/entrypoint.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+set -eu
+
+# Shortcircuit for non-default commands.
+# The last part inside the "{}" is a workaround for the following bug in ash/dash:
+# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=874264
+if [ -n "${1:-}" ] && [ "${1#-}" = "${1}" ] \
+  && [ -n "$(command -v -- "${1}")" ] \
+  && { ! [ -f "${1}" ] || [ -x "${1}" ]; }; then
+  exec "$@"
+fi
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo "This container requires executing as root for initial setup, privilages are dropped after" 1>&2
+  exit 1
+fi
+
+echo "Configure unprivileged user"
+addgroup --system --gid "${PGID}" spacedrive
+adduser --system --disabled-password \
+  --uid "${PUID}" \
+  --home /var/empty \
+  --gecos 'Spacedrive system account' \
+  --ingroup spacedrive \
+  spacedrive
+passwd -l spacedrive
+
+if [ -n "${TZ:-}" ]; then
+  echo "Set Timezone to $TZ"
+  rm -f /etc/localtime
+  ln -s "/usr/share/zoneinfo/${TZ}" /etc/localtime
+  echo "$TZ" >/etc/timezone
+fi
+
+echo "Fix spacedrive's directories permissions"
+chown -R "${PUID}:${PGID}" /data
+
+exec su spacedrive -s /bin/server -- "$@"
