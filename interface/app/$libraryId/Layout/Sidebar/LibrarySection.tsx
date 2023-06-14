49887e549c9f93e0316d18d372aa69a0b219e2a1
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2023-06-14 15:50:43 +0300

    [ENG-690] Context menus for locations and tags (#940)
    * Context menus for locations and tags
    
    * Update TagsContextMenu.tsx
    
    * tweaks
    
    * reverse back to border accent
    
    ---------
    
    Co-authored-by: Brendan Allan <brendonovich@outlook.com>

--- interface/app/$libraryId/Layout/Sidebar/LibrarySection.tsx
+++ interface/app/$libraryId/Layout/Sidebar/LibrarySection.tsx
@@ -1,19 +1,34 @@
-import { Laptop, Mobile, Server } from '@sd/assets/icons';
+import { Laptop } from '@sd/assets/icons';
 import clsx from 'clsx';
+import { useEffect, useRef, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { arraysEqual, useBridgeQuery, useLibraryQuery, useOnlineLocations } from '@sd/client';
 import { AddLocationButton } from '~/app/$libraryId/settings/library/locations/AddLocationButton';
 import { Folder } from '~/components/Folder';
 import { SubtleButton } from '~/components/SubtleButton';
 import SidebarLink from './Link';
+import LocationsContextMenu from './LocationsContextMenu';
 import Section from './Section';
+import TagsContextMenu from './TagsContextMenu';
 
 export const LibrarySection = () => {
 	const node = useBridgeQuery(['nodeState']);
-
 	const locations = useLibraryQuery(['locations.list'], { keepPreviousData: true });
 	const tags = useLibraryQuery(['tags.list'], { keepPreviousData: true });
 	const onlineLocations = useOnlineLocations();
+	const [triggeredContextItem, setTriggeredContextItem] = useState(-1);
+
+	useEffect(() => {
+		const outsideClick = () => {
+			document.addEventListener('click', () => {
+				setTriggeredContextItem(-1);
+			});
+		};
+		outsideClick();
+		return () => {
+			document.removeEventListener('click', outsideClick);
+		};
+	}, [triggeredContextItem]);
 
 	return (
 		<>
@@ -25,27 +40,27 @@ export const LibrarySection = () => {
 					</Link>
 				}
 			>
+				{/* <SidebarLink className="relative w-full group" to={`/`}>
+					<img src={Laptop} className="w-5 h-5 mr-1" />
+					<span className="truncate">Jamie's MBP</span>
+				</SidebarLink>
+				<SidebarLink className="relative w-full group" to={`/`}>
+					<img src={Mobile} className="w-5 h-5 mr-1" />
+					<span className="truncate">spacephone</span>
+				</SidebarLink>
+				<SidebarLink className="relative w-full group" to={`/`}>
+					<img src={Server} className="w-5 h-5 mr-1" />
+					<span className="truncate">titan</span>
+				</SidebarLink>
+				{(locations.data?.length || 0) < 4 && (
+					<Button variant="dotted" className="w-full mt-1">
+						Connect Node
+					</Button>
+				)} */}
 				<SidebarLink disabled className="group relative w-full" to={`/`} key={'jeff'}>
 					<img src={Laptop} className="mr-1 h-5 w-5" />
 					<span className="truncate">{node.data?.name}</span>
 				</SidebarLink>
-				{/* <SidebarLink className="group relative w-full" to={`/`}>
-					<img src={Laptop} className="mr-1 h-5 w-5" />
-					<span className="truncate">Jamie's MBP</span>
-				</SidebarLink>
-				<SidebarLink className="group relative w-full" to={`/`}>
-					<img src={Mobile} className="mr-1 h-5 w-5" />
-					<span className="truncate">spacephone</span>
-				</SidebarLink>
-				<SidebarLink className="group relative w-full" to={`/`}>
-					<img src={Server} className="mr-1 h-5 w-5" />
-					<span className="truncate">titan</span>
-				</SidebarLink>
-				{(locations.data?.length || 0) < 4 && (
-					<Button variant="dotted" className="mt-1 w-full">
-						Connect Node
-					</Button>
-				)} */}
 			</Section>
 			<Section
 				name="Locations"
@@ -57,25 +72,31 @@ export const LibrarySection = () => {
 			>
 				{locations.data?.map((location) => {
 					const online = onlineLocations?.some((l) => arraysEqual(location.pub_id, l));
-
 					return (
-						<SidebarLink
-							className="group relative w-full"
-							to={`location/${location.id}`}
-							key={location.id}
-						>
-							<div className="relative -mt-0.5 mr-1 shrink-0 grow-0">
-								<Folder size={18} />
-								<div
-									className={clsx(
-										'absolute bottom-0.5 right-0 h-1.5 w-1.5 rounded-full',
-										online ? 'bg-green-500' : 'bg-red-500'
-									)}
-								/>
-							</div>
+						<LocationsContextMenu key={location.id} locationId={location.id}>
+							<SidebarLink
+								onContextMenu={() => setTriggeredContextItem(location.id)}
+								className={clsx(
+									triggeredContextItem === location.id
+										? 'border-accent'
+										: 'border-transparent',
+									'group relative w-full border'
+								)}
+								to={`location/${location.id}`}
+							>
+								<div className="relative -mt-0.5 mr-1 shrink-0 grow-0">
+									<Folder size={18} />
+									<div
+										className={clsx(
+											'absolute bottom-0.5 right-0 h-1.5 w-1.5 rounded-full',
+											online ? 'bg-green-500' : 'bg-red-500'
+										)}
+									/>
+								</div>
 
-							<span className="truncate">{location.name}</span>
-						</SidebarLink>
+								<span className="truncate">{location.name}</span>
+							</SidebarLink>
+						</LocationsContextMenu>
 					);
 				})}
 				{(locations.data?.length || 0) < 4 && <AddLocationButton className="mt-1" />}
@@ -90,24 +111,29 @@ export const LibrarySection = () => {
 					}
 				>
 					<div className="mb-2 mt-1">
-						{tags.data?.slice(0, 6).map((tag, index) => (
-							<SidebarLink key={index} to={`tag/${tag.id}`} className="">
-								<div
-									className="h-[12px] w-[12px] shrink-0 rounded-full"
-									style={{ backgroundColor: tag.color || '#efefef' }}
-								/>
-								<span className="ml-1.5 truncate text-sm">{tag.name}</span>
-							</SidebarLink>
+						{tags.data?.slice(0, 6).map((tag) => (
+							<TagsContextMenu tagId={tag.id} key={tag.id}>
+								<SidebarLink
+									onContextMenu={() => setTriggeredContextItem(tag.id)}
+									className={clsx(
+										triggeredContextItem === tag.id
+											? 'border-accent'
+											: 'border-transparent',
+										'border'
+									)}
+									to={`tag/${tag.id}`}
+								>
+									<div
+										className="h-[12px] w-[12px] shrink-0 rounded-full"
+										style={{ backgroundColor: tag.color || '#efefef' }}
+									/>
+									<span className="ml-1.5 truncate text-sm">{tag.name}</span>
+								</SidebarLink>
+							</TagsContextMenu>
 						))}
 					</div>
 				</Section>
 			)}
-			{/* <Section name="Debug">
-				<SidebarLink to="sync">
-					<Icon component={ArrowsClockwise} />
-					Sync
-				</SidebarLink>
-			</Section> */}
 		</>
 	);
 };
