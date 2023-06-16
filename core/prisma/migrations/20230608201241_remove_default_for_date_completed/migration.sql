334ca3c9f3599ec1e808dc7c92fe7e4f307d3eff
Oscar Beaumont <oscar@otbeaumont.me>
2023-06-16 12:58:58 +0800

    fix migrations

--- core/prisma/migrations/20230608201241_remove_default_for_date_completed/migration.sql
+++ core/prisma/migrations/20230608201241_remove_default_for_date_completed/migration.sql
@@ -4,9 +4,6 @@
   - A unique constraint covering the columns `[pub_id]` on the table `indexer_rule` will be added. If there are existing duplicate values, this will fail.
 
 */
--- AlterTable
-ALTER TABLE "indexer_rule" ADD COLUMN "pub_id" BLOB;
-
 -- RedefineTables
 PRAGMA foreign_keys=OFF;
 CREATE TABLE "new_job" (
