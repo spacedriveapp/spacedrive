f05cfbd0d973fdbd0f1ac1792be546ea21732a83
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-06-17 02:23:45 -0300

    [ENG-594] `Open With` Windows + fixes (#945)
    * Windows `Open With` WIP
     - Listing applications capable of hanling a file type is working
     - Openning a file with a selected application is failing with unspecified error HRESULT(0x80004005) for some reason
    
    * Fix file not opening due to COM not being initialized
     - Fix `no apps available` style
    
    * Remove unwrap
    
    * Fix `Open With` due to changes in main
    
    * Fix macOS `Open With`
    
    * Fix Windows `Open With` due to changes in main
     - Sort linux `Open With` entries, to ensure consistent app order
    
    * Fix macOS again
    
    * Update core.ts
    
    * Fix windows CI being rate limited
    
    * Clippy
    
    * Fix CoUninitialize not being called
    
    * minor formatting
    
    * Implement feedback
     - Improve performance of listing apps that can handle a certain file type in Linux
    
    * Fix broken feedback change
     - Small perf improvement to windows crate
    
    * Some improvements to windows crate

--- apps/desktop/src-tauri/src/file.rs
+++ apps/desktop/src-tauri/src/file.rs
@@ -50,59 +50,53 @@ pub async fn open_file_path(
 }
 
 #[derive(Serialize, Type)]
-#[serde(tag = "t", content = "c")]
-#[allow(dead_code)]
-pub enum OpenWithApplication {
-	File {
-		id: i32,
-		name: String,
-		#[cfg(target_os = "linux")]
-		url: std::path::PathBuf,
-		#[cfg(not(target_os = "linux"))]
-		url: String,
-	},
-	Error(i32, String),
+pub struct OpenWithApplication {
+	id: i32,
+	name: String,
+	#[cfg(target_os = "linux")]
+	url: std::path::PathBuf,
+	#[cfg(not(target_os = "linux"))]
+	url: String,
 }
 
 #[tauri::command(async)]
 #[specta::specta]
-#[allow(unused_variables)]
 pub async fn get_file_path_open_with_apps(
 	library: uuid::Uuid,
 	ids: Vec<i32>,
 	node: tauri::State<'_, Arc<Node>>,
 ) -> Result<Vec<OpenWithApplication>, ()> {
-	let Some(library) = node.library_manager.get_library(library).await else {
-        return Err(())
-    };
+	let Some(library) = node.library_manager.get_library(library).await
+		else {
+			return Ok(vec![]);
+		};
 
-	let Ok(paths) = library.get_file_paths(ids).await.map_err(|e| {error!("{e:#?}");})
-	else {
-		return Err(());
-	};
+	let Ok(paths) = library
+		.get_file_paths(ids).await
+		.map_err(|e| {error!("{e:#?}");})
+		else {
+			return Ok(vec![]);
+		};
 
 	#[cfg(target_os = "macos")]
 	return Ok(paths
 		.into_iter()
 		.flat_map(|(id, path)| {
-			if let Some(path) = path {
-				unsafe {
-					sd_desktop_macos::get_open_with_applications(&path.to_str().unwrap().into())
-				}
+			let Some(path) = path
+				else {
+					error!("File not found in database");
+					return vec![];
+				};
+
+			unsafe { sd_desktop_macos::get_open_with_applications(&path.to_str().unwrap().into()) }
 				.as_slice()
 				.iter()
-				.map(|app| OpenWithApplication::File {
+				.map(|app| OpenWithApplication {
 					id,
 					name: app.name.to_string(),
 					url: app.url.to_string(),
 				})
 				.collect::<Vec<_>>()
-			} else {
-				vec![OpenWithApplication::Error(
-					id,
-					"File not found in database".into(),
-				)]
-			}
 		})
 		.collect());
 
@@ -111,133 +105,170 @@ pub async fn get_file_path_open_with_apps(
 		use sd_desktop_linux::{DesktopEntry, HandlerType, SystemApps};
 
 		// TODO: cache this, and only update when the underlying XDG desktop apps changes
-		let system_apps = SystemApps::populate().map_err(|_| ())?;
+		let Ok(system_apps) = SystemApps::populate()
+			.map_err(|e| { error!("{e:#?}"); })
+			else {
+				return Ok(vec![]);
+			};
 
 		return Ok(paths
 			.into_iter()
 			.flat_map(|(id, path)| {
-				if let Some(path) = path {
-					let Some(name) = path.file_name()
-						.and_then(|name| name.to_str())
-						.map(|name| name.to_string())
+				let Some(path) = path
 					else {
-						return vec![OpenWithApplication::Error(
-							id,
-							"Failed to extract file name".into(),
-						)]
+						error!("File not found in database");
+						return vec![];
 					};
 
-					system_apps
-						.get_handlers(HandlerType::Ext(name))
-						.iter()
-						.map(|handler| {
-							handler
-								.get_path()
-								.map(|path| {
-									DesktopEntry::try_from(&path)
-										.map(|entry| OpenWithApplication::File {
-											id,
-											name: entry.name,
-											url: path,
-										})
-										.unwrap_or_else(|e| {
-											error!("{e:#?}");
-											OpenWithApplication::Error(
-												id,
-												"Failed to parse desktop entry".into(),
-											)
-										})
-								})
-								.unwrap_or_else(|e| {
-									error!("{e:#?}");
-									OpenWithApplication::Error(
+				let Some(name) = path.file_name()
+					.and_then(|name| name.to_str())
+					.map(|name| name.to_string())
+					else {
+						error!("Failed to extract file name");
+						return vec![];
+					};
+
+				system_apps
+					.get_handlers(HandlerType::Ext(name))
+					.map(|handler| {
+						handler
+							.get_path()
+							.map_err(|e| {
+								error!("{e:#?}");
+							})
+							.and_then(|path| {
+								DesktopEntry::try_from(&path)
+									// TODO: Ignore desktop entries that have commands that don't exist/aren't available in path
+									.map(|entry| OpenWithApplication {
 										id,
-										"Failed to get path from desktop entry".into(),
-									)
-								})
-						})
-						.collect::<Vec<_>>()
-				} else {
-					vec![OpenWithApplication::Error(
-						id,
-						"File not found in database".into(),
-					)]
-				}
+										name: entry.name,
+										url: path,
+									})
+									.map_err(|e| {
+										error!("{e:#?}");
+									})
+							})
+					})
+					.collect::<Result<Vec<_>, _>>()
+					.unwrap_or(vec![])
 			})
 			.collect());
 	}
 
+	#[cfg(windows)]
+	return Ok(paths
+		.into_iter()
+		.flat_map(|(id, path)| {
+			let Some(path) = path
+				else {
+					error!("File not found in database");
+					return vec![];
+				};
+
+			let Some(ext) = path.extension()
+				else {
+					error!("Failed to extract file extension");
+					return vec![];
+				};
+
+			sd_desktop_windows::list_apps_associated_with_ext(ext)
+				.map_err(|e| {
+					error!("{e:#?}");
+				})
+				.map(|handlers| {
+					handlers
+						.iter()
+						.filter_map(|handler| {
+							let (Ok(name), Ok(url)) = (
+							unsafe { handler.GetUIName() }.map_err(|e| { error!("{e:#?}");})
+								.and_then(|name| unsafe { name.to_string() }
+								.map_err(|e| { error!("{e:#?}");})),
+							unsafe { handler.GetName() }.map_err(|e| { error!("{e:#?}");})
+								.and_then(|name| unsafe { name.to_string() }
+								.map_err(|e| { error!("{e:#?}");})),
+						) else {
+							error!("Failed to get handler info");
+							return None
+						};
+
+							Some(OpenWithApplication { id, name, url })
+						})
+						.collect::<Vec<_>>()
+				})
+				.unwrap_or(vec![])
+		})
+		.collect());
+
 	#[allow(unreachable_code)]
-	Err(())
+	Ok(vec![])
 }
 
 type FileIdAndUrl = (i32, String);
 
 #[tauri::command(async)]
 #[specta::specta]
-#[allow(unused_variables)]
 pub async fn open_file_path_with(
 	library: uuid::Uuid,
 	file_ids_and_urls: Vec<FileIdAndUrl>,
 	node: tauri::State<'_, Arc<Node>>,
 ) -> Result<(), ()> {
-	let Some(library) = node.library_manager.get_library(library).await else {
-        return Err(())
-    };
+	let Some(library) = node.library_manager.get_library(library).await
+		else {
+			return Err(())
+		};
 
 	let url_by_id = file_ids_and_urls.into_iter().collect::<HashMap<_, _>>();
 	let ids = url_by_id.keys().copied().collect::<Vec<_>>();
 
-	#[cfg(target_os = "macos")]
-	{
-		library
-			.get_file_paths(ids)
-			.await
-			.map(|paths| {
-				paths.iter().for_each(|(id, path)| {
-					if let Some(path) = path {
+	library
+		.get_file_paths(ids)
+		.await
+		.map_err(|e| {
+			error!("{e:#?}");
+		})
+		.and_then(|paths| {
+			paths
+				.iter()
+				.map(|(id, path)| {
+					let (Some(path), Some(url)) = (
+						#[cfg(windows)]
+						path.as_ref(),
+						#[cfg(not(windows))]
+						path.as_ref().and_then(|path| path.to_str()),
+						url_by_id.get(id)
+					)
+						else {
+							error!("File not found in database");
+							return Err(());
+						};
+
+					#[cfg(target_os = "macos")]
+					return {
 						unsafe {
 							sd_desktop_macos::open_file_path_with(
-								&path.to_str().unwrap().into(),
-								&url_by_id
-									.get(id)
-									.expect("we just created this hashmap")
-									.as_str()
-									.into(),
+								&path.into(),
+								&url.as_str().into(),
 							)
-						}
-					}
-				})
-			})
-			.map_err(|e| {
-				error!("{e:#?}");
-			})
-	}
+						};
+						Ok(())
+					};
 
-	#[cfg(target_os = "linux")]
-	{
-		library
-			.get_file_paths(ids)
-			.await
-			.map(|paths| {
-				paths.iter().for_each(|(id, path)| {
-					if let Some(path) = path.as_ref().and_then(|path| path.to_str()) {
-						if let Err(e) = sd_desktop_linux::Handler::assume_valid(
-							url_by_id
-								.get(id)
-								.expect("we just created this hashmap")
-								.as_str()
-								.into(),
-						)
+					#[cfg(target_os = "linux")]
+					return sd_desktop_linux::Handler::assume_valid(url.into())
 						.open(&[path])
-						{
+						.map_err(|e| {
 							error!("{e:#?}");
-						}
-					}
+						});
+
+					#[cfg(windows)]
+					return sd_desktop_windows::open_file_path_with(path, url).map_err(|e| {
+						error!("{e:#?}");
+					});
+
+					#[allow(unreachable_code)]
+					Err(())
 				})
-			})
-			.map_err(|e| {
-				error!("{e:#?}");
-			})
-	}
+				.collect::<Result<Vec<_>, _>>()
+				.map(|_| ())
+		})
 }
