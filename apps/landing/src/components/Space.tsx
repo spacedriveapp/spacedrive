00f9952a358285af6ed3d721988de4ba198b9e82
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2023-06-19 12:08:12 +0300

    fix types & lint & pnpm-lock file

--- apps/landing/src/components/Space.tsx
+++ apps/landing/src/components/Space.tsx
@@ -1,37 +1,45 @@
-'use client'
-import React, {useRef, useState} from "react";
-import {Canvas, useFrame} from "@react-three/fiber";
-import * as random from "maath/random/dist/maath-random.cjs";
-import {Points, PointMaterial, Trail} from '@react-three/drei'
-import * as THREE from 'three'
-
+import { PointMaterial, Points, Trail } from '@react-three/drei';
+import { Canvas, useFrame } from '@react-three/fiber';
+import * as random from 'maath/random/dist/maath-random.cjs';
+import React, { useRef, useState } from 'react';
+import * as THREE from 'three';
 
 const Stars = (props: any) => {
 	const ref = useRef<THREE.Mesh>();
-	const [sphere] = useState(() => random.inSphere(new Float32Array(35000), { radius: 1 }))
+	const [sphere] = useState(() => random.inSphere(new Float32Array(35000), { radius: 1 }));
 	useFrame((state, delta) => {
-		if(ref.current) {
-			ref.current.rotation.x -= delta / 100
-			ref.current.rotation.y -= delta / 100
+		if (ref.current) {
+			ref.current.rotation.x -= delta / 100;
+			ref.current.rotation.y -= delta / 100;
 		}
-	})
+	});
 	return (
 		<group rotation={[0, 0, Math.PI / 4]}>
 			<Points ref={ref} positions={sphere} stride={3} frustumCulled={false} {...props}>
-				<PointMaterial transparent color="#ffffff" size={0.001} sizeAttenuation={true} depthWrite={false} />
+				<PointMaterial
+					transparent
+					color="#ffffff"
+					size={0.001}
+					sizeAttenuation={true}
+					depthWrite={false}
+				/>
 			</Points>
 		</group>
-	)
-}
+	);
+};
 
 function ShootingStar() {
-	const ref = useRef<any>()
+	const ref = useRef<any>();
 	useFrame((state) => {
-		const t = state.clock.getElapsedTime() * 0.5
-		if(ref.current) {
-			ref.current.position.set(Math.sin(t) * 4, Math.atan(t) * Math.cos(t / 2) * 2, Math.cos(t) * 4)
+		const t = state.clock.getElapsedTime() * 0.5;
+		if (ref.current) {
+			ref.current.position.set(
+				Math.sin(t) * 4,
+				Math.atan(t) * Math.cos(t / 2) * 2,
+				Math.cos(t) * 4
+			);
 		}
-	})
+	});
 	return (
 		<Trail width={0.05} length={8} color={new THREE.Color(2, 1, 10)} attenuation={(t) => t * t}>
 			<mesh ref={ref}>
@@ -39,19 +47,18 @@ function ShootingStar() {
 				<meshBasicMaterial color={[10, 1, 10]} toneMapped={false} />
 			</mesh>
 		</Trail>
-	)
+	);
 }
-
 
 const Space: React.FunctionComponent = () => {
 	return (
-		<div className="absolute z-0 w-screen h-screen opacity-50">
-			<Canvas camera={{ position: [0, 0, 0]}}>
+		<div className="absolute z-0 h-screen w-screen opacity-50">
+			<Canvas camera={{ position: [0, 0, 0] }}>
 				<ShootingStar />
 				<ShootingStar />
 				<Stars />
 			</Canvas>
 		</div>
-	)
-}
+	);
+};
 export default Space;
