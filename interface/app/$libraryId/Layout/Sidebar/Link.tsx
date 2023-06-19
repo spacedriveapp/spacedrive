da51392140766e6ee031d087dbc09762a5fe3320
Abe M <abe.malla8@gmail.com>
2023-06-19 05:24:14 -0700

    Fix window flash when cmd+clicking on a link

--- interface/app/$libraryId/Layout/Sidebar/Link.tsx
+++ interface/app/$libraryId/Layout/Sidebar/Link.tsx
@@ -28,10 +28,18 @@ const Link = forwardRef<
 
 	return (
 		<NavLink
-			onClick={(e) => (disabled ? e.preventDefault() : onClick?.(e))}
+			onClick={(e) => {
+				// Prevent default action if Command (metaKey) or Control is pressed
+				if (e.metaKey || e.ctrlKey) {
+					e.preventDefault();
+				}
+				if (!disabled) {
+					onClick?.(e);
+				}
+			}}
 			className={({ isActive }) =>
 				clsx(
-					"ring-0", // Remove ugly outline ring on Chrome Windows & Linux
+					'ring-0', // Remove ugly outline ring on Chrome Windows & Linux
 					styles({ active: isActive, transparent: os === 'macOS' }),
 					disabled && 'pointer-events-none opacity-50',
 					className
