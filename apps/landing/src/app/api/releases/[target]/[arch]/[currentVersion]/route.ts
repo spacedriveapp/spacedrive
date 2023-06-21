5fbcfef5b7a13ad9c42735eb719f97834582d56d
Brendan Allan <brendonovich@outlook.com>
2023-06-21 20:20:53 +0800

    types

--- apps/landing/src/app/api/releases/[target]/[arch]/[currentVersion]/route.ts
+++ apps/landing/src/app/api/releases/[target]/[arch]/[currentVersion]/route.ts
@@ -3,14 +3,18 @@ import { NextResponse } from 'next/server';
 
 export const runtime = 'edge';
 
+type Channel = 'stable' | 'beta';
+type TauriTarget = 'linux' | 'windows' | 'darwin';
+type TauriArch = 'x86_64' | 'i686' | 'aarch64' | 'armv7';
+
 export async function GET(
 	_: Request,
 	{
 		params
 	}: {
 		params: {
-			target: string;
-			arch: string;
+			target: TauriTarget;
+			arch: TauriArch;
 			currentVersion: string;
 		};
 	}
