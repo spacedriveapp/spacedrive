25c1f7c7d9a8fd97d2854e4573a816fd56dabed4
James Pine <ijamespine@me.com>
2023-06-22 05:05:31 -0700

    here's my seed

--- core/src/object/tag/seed.rs
+++ core/src/object/tag/seed.rs
@@ -5,7 +5,24 @@ use crate::library::Library;
 /// Shouldn't be called more than once!
 pub async fn new_library(library: &Library) -> prisma_client_rust::Result<()> {
 	// remove type after tags are added
-	let tags: [TagCreateArgs; 0] = [];
+	let tags = [
+		TagCreateArgs {
+			name: "Keepsafe".to_string(),
+			color: "#D9188E".to_string(),
+		},
+		TagCreateArgs {
+			name: "Hidden".to_string(),
+			color: "#646278".to_string(),
+		},
+		TagCreateArgs {
+			name: "Projects".to_string(),
+			color: "#42D097".to_string(),
+		},
+		TagCreateArgs {
+			name: "Memes".to_string(),
+			color: "#A718D9".to_string(),
+		},
+	];
 
 	for tag in tags {
 		tag.exec(&library).await?;
