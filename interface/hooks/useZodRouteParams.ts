e980a4067e99203e041f535c906b03d35710a377
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-06-22 20:32:57 -0300

    Remove default argument from useZodRouteParams

--- interface/hooks/useZodRouteParams.ts
+++ interface/hooks/useZodRouteParams.ts
@@ -2,15 +2,8 @@ import { useMemo } from 'react';
 import { useParams } from 'react-router';
 import { z } from 'zod';
 
-const RouteParamsSchema = z.object({
-	id: z.coerce.number().default(0),
-	libraryId: z.string().optional()
-});
-
-export function useZodRouteParams<Z extends z.AnyZodObject = typeof RouteParamsSchema>(
-	schema?: Z
-): z.infer<Z> {
+export function useZodRouteParams<Z extends z.AnyZodObject>(schema: Z): z.infer<Z> {
 	// eslint-disable-next-line no-restricted-syntax
 	const params = useParams();
-	return useMemo(() => (schema ?? RouteParamsSchema).parse(params), [params, schema]);
+	return useMemo(() => schema.parse(params), [params, schema]);
 }
