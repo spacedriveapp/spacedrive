2cdf20462e79326ec5d7290120e746e7041914e1
Gedeon Sainrival <hello@gedeondoescode.com>
2023-06-24 16:39:53 -0400

    remove unused code

--- core/src/object/tag/seed.rs
+++ core/src/object/tag/seed.rs
@@ -1,5 +1,3 @@
-use chrono::Utc;
-
 use super::TagCreateArgs;
 use crate::library::Library;
 
@@ -7,28 +5,23 @@ use crate::library::Library;
 /// Shouldn't be called more than once!
 pub async fn new_library(library: &Library) -> prisma_client_rust::Result<()> {
 	// remove type after tags are added
-	let now = Utc::now().into();
 
 	let tags = [
 		TagCreateArgs {
 			name: "Keepsafe".to_string(),
 			color: "#D9188E".to_string(),
-			date_created: now,
 		},
 		TagCreateArgs {
 			name: "Hidden".to_string(),
 			color: "#646278".to_string(),
-			date_created: now,
 		},
 		TagCreateArgs {
 			name: "Projects".to_string(),
 			color: "#42D097".to_string(),
-			date_created: now,
 		},
 		TagCreateArgs {
 			name: "Memes".to_string(),
 			color: "#A718D9".to_string(),
-			date_created: now,
 		},
 	];
 
