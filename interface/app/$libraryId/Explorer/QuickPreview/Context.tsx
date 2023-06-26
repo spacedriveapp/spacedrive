8d0a85f3083bafb04ae6ebc6e503fe0c54a8c792
Brendan Allan <brendonovich@outlook.com>
2023-06-27 05:56:40 +0800

    store quickview ref inside provider

--- interface/app/$libraryId/Explorer/QuickPreview/Context.tsx
+++ interface/app/$libraryId/Explorer/QuickPreview/Context.tsx
@@ -1,16 +1,26 @@
-import { RefObject, createContext, useContext } from "react";
+import { PropsWithChildren, RefObject, createContext, useContext, useRef } from 'react';
 
 interface QuickPreviewContext {
     ref: RefObject<HTMLDivElement>;
 }
 
-export const QuickPreviewContext = createContext<QuickPreviewContext | null>(null);
+const QuickPreviewContext = createContext<QuickPreviewContext | null>(null);
+
+export const QuickPreviewContextProvider = ({ children }: PropsWithChildren) => {
+    const ref = useRef<HTMLDivElement>(null);
+
+    return (
+        <QuickPreviewContext.Provider value={{ ref }}>
+            {children}
+            <div ref={ref} />
+        </QuickPreviewContext.Provider>
+    );
+};
 
 export const useQuickPreviewContext = () => {
     const context = useContext(QuickPreviewContext);
 
-    if (!context)
-        throw new Error("QuickPreviewContext.Provider not found!");
+    if (!context) throw new Error('QuickPreviewContext.Provider not found!');
 
     return context;
 };
