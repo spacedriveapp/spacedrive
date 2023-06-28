411f0335ba8a3d9cde08799acfb33d0f6c6b5aa5
Brendan Allan <brendonovich@outlook.com>
2023-06-28 04:07:22 +0200

    [ENG-829] Render QuickPreview in Explorer/View (#1036)
    formatting + render QuickPreview in Explorer/View

--- interface/app/$libraryId/Explorer/ParentContextMenu.tsx
+++ interface/app/$libraryId/Explorer/ParentContextMenu.tsx
@@ -11,155 +11,155 @@ import { getExplorerStore, useExplorerStore } from './store';
 import { useExplorerSearchParams } from './util';
 
 export default (props: PropsWithChildren) => {
-    const os = useOperatingSystem();
-    const keybind = keybindForOs(os);
-    const [{ path: currentPath }] = useExplorerSearchParams();
-    const { cutCopyState } = useExplorerStore();
+	const os = useOperatingSystem();
+	const keybind = keybindForOs(os);
+	const [{ path: currentPath }] = useExplorerSearchParams();
+	const { cutCopyState } = useExplorerStore();
 
-    const { parent } = useExplorerContext();
+	const { parent } = useExplorerContext();
 
-    const generateThumbsForLocation = useLibraryMutation('jobs.generateThumbsForLocation');
-    const objectValidator = useLibraryMutation('jobs.objectValidator');
-    const rescanLocation = useLibraryMutation('locations.fullRescan');
-    const copyFiles = useLibraryMutation('files.copyFiles');
-    const cutFiles = useLibraryMutation('files.cutFiles');
+	const generateThumbsForLocation = useLibraryMutation('jobs.generateThumbsForLocation');
+	const objectValidator = useLibraryMutation('jobs.objectValidator');
+	const rescanLocation = useLibraryMutation('locations.fullRescan');
+	const copyFiles = useLibraryMutation('files.copyFiles');
+	const cutFiles = useLibraryMutation('files.cutFiles');
 
-    return (
-        <CM.Root trigger={props.children}>
-            {parent?.type === 'Location' && (
-                <SharedItems.RevealInNativeExplorer locationId={parent.location.id} />
-            )}
+	return (
+		<CM.Root trigger={props.children}>
+			{parent?.type === 'Location' && (
+				<SharedItems.RevealInNativeExplorer locationId={parent.location.id} />
+			)}
 
-            <CM.Item
-                label="Share"
-                icon={Share}
-                onClick={(e) => {
-                    e.preventDefault();
+			<CM.Item
+				label="Share"
+				icon={Share}
+				onClick={(e) => {
+					e.preventDefault();
 
-                    navigator.share?.({
-                        title: 'Spacedrive',
-                        text: 'Check out this cool app',
-                        url: 'https://spacedrive.com'
-                    });
-                }}
-                disabled
-            />
+					navigator.share?.({
+						title: 'Spacedrive',
+						text: 'Check out this cool app',
+						url: 'https://spacedrive.com'
+					});
+				}}
+				disabled
+			/>
 
-            <CM.Separator />
+			<CM.Separator />
 
-            {parent?.type === 'Location' && (
-                <>
-                    <CM.Item
-                        onClick={async () => {
-                            try {
-                                await rescanLocation.mutateAsync(parent.location.id);
-                            } catch (error) {
-                                showAlertDialog({
-                                    title: 'Error',
-                                    value: `Failed to re-index location, due to an error: ${error}`
-                                });
-                            }
-                        }}
-                        label="Re-index"
-                        icon={Repeat}
-                    />
+			{parent?.type === 'Location' && (
+				<>
+					<CM.Item
+						onClick={async () => {
+							try {
+								await rescanLocation.mutateAsync(parent.location.id);
+							} catch (error) {
+								showAlertDialog({
+									title: 'Error',
+									value: `Failed to re-index location, due to an error: ${error}`
+								});
+							}
+						}}
+						label="Re-index"
+						icon={Repeat}
+					/>
 
-                    <CM.Item
-                        label="Paste"
-                        keybind={keybind([ModifierKeys.Control], ['V'])}
-                        hidden={!cutCopyState.active}
-                        onClick={async () => {
-                            const path = currentPath ?? '/';
-                            const { actionType, sourcePathId, sourceParentPath, sourceLocationId } =
-                                cutCopyState;
-                            const sameLocation =
-                                sourceLocationId === parent.location.id &&
-                                sourceParentPath === path;
-                            try {
-                                if (actionType == 'Copy') {
-                                    await copyFiles.mutateAsync({
-                                        source_location_id: sourceLocationId,
-                                        sources_file_path_ids: [sourcePathId],
-                                        target_location_id: parent.location.id,
-                                        target_location_relative_directory_path: path,
-                                        target_file_name_suffix: sameLocation ? ' copy' : null
-                                    });
-                                } else if (sameLocation) {
-                                    showAlertDialog({
-                                        title: 'Error',
-                                        value: `File already exists in this location`
-                                    });
-                                } else {
-                                    await cutFiles.mutateAsync({
-                                        source_location_id: sourceLocationId,
-                                        sources_file_path_ids: [sourcePathId],
-                                        target_location_id: parent.location.id,
-                                        target_location_relative_directory_path: path
-                                    });
-                                }
-                            } catch (error) {
-                                showAlertDialog({
-                                    title: 'Error',
-                                    value: `Failed to ${actionType.toLowerCase()} file, due to an error: ${error}`
-                                });
-                            }
-                        }}
-                        icon={Clipboard}
-                    />
-                </>
-            )}
+					<CM.Item
+						label="Paste"
+						keybind={keybind([ModifierKeys.Control], ['V'])}
+						hidden={!cutCopyState.active}
+						onClick={async () => {
+							const path = currentPath ?? '/';
+							const { actionType, sourcePathId, sourceParentPath, sourceLocationId } =
+								cutCopyState;
+							const sameLocation =
+								sourceLocationId === parent.location.id &&
+								sourceParentPath === path;
+							try {
+								if (actionType == 'Copy') {
+									await copyFiles.mutateAsync({
+										source_location_id: sourceLocationId,
+										sources_file_path_ids: [sourcePathId],
+										target_location_id: parent.location.id,
+										target_location_relative_directory_path: path,
+										target_file_name_suffix: sameLocation ? ' copy' : null
+									});
+								} else if (sameLocation) {
+									showAlertDialog({
+										title: 'Error',
+										value: `File already exists in this location`
+									});
+								} else {
+									await cutFiles.mutateAsync({
+										source_location_id: sourceLocationId,
+										sources_file_path_ids: [sourcePathId],
+										target_location_id: parent.location.id,
+										target_location_relative_directory_path: path
+									});
+								}
+							} catch (error) {
+								showAlertDialog({
+									title: 'Error',
+									value: `Failed to ${actionType.toLowerCase()} file, due to an error: ${error}`
+								});
+							}
+						}}
+						icon={Clipboard}
+					/>
+				</>
+			)}
 
-            <CM.Item
-                label="Deselect"
-                hidden={!cutCopyState.active}
-                onClick={() => {
-                    getExplorerStore().cutCopyState = {
-                        ...cutCopyState,
-                        active: false
-                    };
-                }}
-                icon={FileX}
-            />
+			<CM.Item
+				label="Deselect"
+				hidden={!cutCopyState.active}
+				onClick={() => {
+					getExplorerStore().cutCopyState = {
+						...cutCopyState,
+						active: false
+					};
+				}}
+				icon={FileX}
+			/>
 
-            {parent?.type === 'Location' && (
-                <CM.SubMenu label="More actions..." icon={Plus}>
-                    <CM.Item
-                        onClick={async () => {
-                            try {
-                                await generateThumbsForLocation.mutateAsync({
-                                    id: parent.location.id,
-                                    path: currentPath ?? '/'
-                                });
-                            } catch (error) {
-                                showAlertDialog({
-                                    title: 'Error',
-                                    value: `Failed to generate thumbanails, due to an error: ${error}`
-                                });
-                            }
-                        }}
-                        label="Regen Thumbnails"
-                        icon={Image}
-                    />
+			{parent?.type === 'Location' && (
+				<CM.SubMenu label="More actions..." icon={Plus}>
+					<CM.Item
+						onClick={async () => {
+							try {
+								await generateThumbsForLocation.mutateAsync({
+									id: parent.location.id,
+									path: currentPath ?? '/'
+								});
+							} catch (error) {
+								showAlertDialog({
+									title: 'Error',
+									value: `Failed to generate thumbanails, due to an error: ${error}`
+								});
+							}
+						}}
+						label="Regen Thumbnails"
+						icon={Image}
+					/>
 
-                    <CM.Item
-                        onClick={async () => {
-                            try {
-                                objectValidator.mutateAsync({
-                                    id: parent.location.id,
-                                    path: currentPath ?? '/'
-                                });
-                            } catch (error) {
-                                showAlertDialog({
-                                    title: 'Error',
-                                    value: `Failed to generate checksum, due to an error: ${error}`
-                                });
-                            }
-                        }}
-                        label="Generate Checksums"
-                        icon={ShieldCheck}
-                    />
-                </CM.SubMenu>
-            )}
-        </CM.Root>
-    );
+					<CM.Item
+						onClick={async () => {
+							try {
+								objectValidator.mutateAsync({
+									id: parent.location.id,
+									path: currentPath ?? '/'
+								});
+							} catch (error) {
+								showAlertDialog({
+									title: 'Error',
+									value: `Failed to generate checksum, due to an error: ${error}`
+								});
+							}
+						}}
+						label="Generate Checksums"
+						icon={ShieldCheck}
+					/>
+				</CM.SubMenu>
+			)}
+		</CM.Root>
+	);
 };
--- interface/app/$libraryId/Explorer/QuickPreview/Context.tsx
+++ interface/app/$libraryId/Explorer/QuickPreview/Context.tsx
@@ -1,26 +1,26 @@
 import { PropsWithChildren, RefObject, createContext, useContext, useRef } from 'react';
 
 interface QuickPreviewContext {
-    ref: RefObject<HTMLDivElement>;
+	ref: RefObject<HTMLDivElement>;
 }
 
 const QuickPreviewContext = createContext<QuickPreviewContext | null>(null);
 
 export const QuickPreviewContextProvider = ({ children }: PropsWithChildren) => {
-    const ref = useRef<HTMLDivElement>(null);
+	const ref = useRef<HTMLDivElement>(null);
 
-    return (
-        <QuickPreviewContext.Provider value={{ ref }}>
-            {children}
-            <div ref={ref} />
-        </QuickPreviewContext.Provider>
-    );
+	return (
+		<QuickPreviewContext.Provider value={{ ref }}>
+			{children}
+			<div ref={ref} />
+		</QuickPreviewContext.Provider>
+	);
 };
 
 export const useQuickPreviewContext = () => {
-    const context = useContext(QuickPreviewContext);
+	const context = useContext(QuickPreviewContext);
 
-    if (!context) throw new Error('QuickPreviewContext.Provider not found!');
+	if (!context) throw new Error('QuickPreviewContext.Provider not found!');
 
-    return context;
+	return context;
 };
