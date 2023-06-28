6aed53a2911d08b01ab7f75b453701c0768db96c
Brendan Allan <brendonovich@outlook.com>
2023-06-28 08:40:55 +0800

    formatting + render QuickPreview in Explorer/View

--- interface/app/$libraryId/Explorer/ContextMenu/Object/index.tsx
+++ interface/app/$libraryId/Explorer/ContextMenu/Object/index.tsx
@@ -4,55 +4,51 @@ import { ContextMenu } from '@sd/ui';
 import { FilePathItems, ObjectItems, SharedItems } from '..';
 
 interface Props {
-    data: Extract<ExplorerItem, { type: 'Object' }>;
+	data: Extract<ExplorerItem, { type: 'Object' }>;
 }
 
 export default ({ data }: Props) => {
-    const object = data.item;
-    const filePath = data.item.file_paths[0];
+	const object = data.item;
+	const filePath = data.item.file_paths[0];
 
-    return (
-        <>
-            {filePath && <FilePathItems.OpenOrDownload filePath={filePath} />}
+	return (
+		<>
+			{filePath && <FilePathItems.OpenOrDownload filePath={filePath} />}
 
-            <SharedItems.OpenQuickView item={data} />
+			<SharedItems.OpenQuickView item={data} />
 
-            <ContextMenu.Separator />
+			<ContextMenu.Separator />
 
-            <SharedItems.Details />
+			<SharedItems.Details />
 
-            <ContextMenu.Separator />
+			<ContextMenu.Separator />
 
-            {filePath && (
-                <SharedItems.RevealInNativeExplorer
-                    filePath={filePath}
-                />
-            )}
+			{filePath && <SharedItems.RevealInNativeExplorer filePath={filePath} />}
 
-            <SharedItems.Rename />
+			<SharedItems.Rename />
 
-            <ContextMenu.Separator />
+			<ContextMenu.Separator />
 
-            <SharedItems.Share />
+			<SharedItems.Share />
 
-            {(object || filePath) && <ContextMenu.Separator />}
+			{(object || filePath) && <ContextMenu.Separator />}
 
-            {object && <ObjectItems.AssignTag object={object} />}
+			{object && <ObjectItems.AssignTag object={object} />}
 
-            {filePath && (
-                <ContextMenu.SubMenu label="More actions..." icon={Plus}>
-                    <FilePathItems.Crypto filePath={filePath} />
-                    <FilePathItems.Compress filePath={filePath} />
-                    <ObjectItems.ConvertObject filePath={filePath} object={object} />
-                </ContextMenu.SubMenu>
-            )}
+			{filePath && (
+				<ContextMenu.SubMenu label="More actions..." icon={Plus}>
+					<FilePathItems.Crypto filePath={filePath} />
+					<FilePathItems.Compress filePath={filePath} />
+					<ObjectItems.ConvertObject filePath={filePath} object={object} />
+				</ContextMenu.SubMenu>
+			)}
 
-            {filePath && (
-                <>
-                    <ContextMenu.Separator />
-                    <FilePathItems.Delete filePath={filePath} />
-                </>
-            )}
-        </>
-    );
+			{filePath && (
+				<>
+					<ContextMenu.Separator />
+					<FilePathItems.Delete filePath={filePath} />
+				</>
+			)}
+		</>
+	);
 };
