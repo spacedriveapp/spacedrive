81ca62f19d9f6b0e16435c6f4bfa8ef3ef6f5b41
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-07-01 05:24:49 -0300

    Fix macOS disk stats retrieve logic  - Ignore mounted dmgs when calculation disk total/free size  - Only take into account disk mounted by macOS

--- core/src/volume.rs
+++ core/src/volume.rs
@@ -1,9 +1,8 @@
+use futures::future;
 use serde::{Deserialize, Serialize};
 use serde_with::{serde_as, DisplayFromStr};
 use specta::Type;
-use std::collections::HashMap;
-use std::sync::OnceLock;
-use std::{ffi::OsString, fmt::Display, path::PathBuf};
+use std::{ffi::OsString, fmt::Display, path::PathBuf, str::FromStr, sync::OnceLock};
 use sysinfo::{DiskExt, System, SystemExt};
 use thiserror::Error;
 use tokio::sync::Mutex;
@@ -35,14 +34,13 @@ impl Display for DiskType {
 #[derive(Serialize, Deserialize, Debug, Clone, Type)]
 pub struct Volume {
 	pub name: OsString,
-	pub mount_point: Vec<PathBuf>,
+	pub mount_point: PathBuf,
 	#[specta(type = String)]
 	#[serde_as(as = "DisplayFromStr")]
 	pub total_capacity: u64,
 	#[specta(type = String)]
 	#[serde_as(as = "DisplayFromStr")]
 	pub available_capacity: u64,
-	pub is_removable: bool,
 	pub disk_type: DiskType,
 	pub file_system: Option<String>,
 	pub is_root_filesystem: bool,
@@ -60,6 +58,27 @@ impl From<VolumeError> for rspc::Error {
 	fn from(e: VolumeError) -> Self {
 		rspc::Error::with_cause(rspc::ErrorCode::InternalServerError, e.to_string(), e)
 	}
+}
+
+#[cfg(target_os = "macos")]
+#[derive(Deserialize)]
+#[serde(rename_all = "kebab-case")]
+struct ImageSystemEntity {
+	mount_point: Option<String>,
+}
+
+#[cfg(target_os = "macos")]
+#[derive(Deserialize)]
+#[serde(rename_all = "kebab-case")]
+struct ImageInfo {
+	system_entities: Vec<ImageSystemEntity>,
+}
+
+#[cfg(target_os = "macos")]
+#[derive(Deserialize)]
+#[serde(rename_all = "kebab-case")]
+struct HDIUtilInfo {
+	images: Vec<ImageInfo>,
 }
 
 // pub async fn save_volume(library: &Library) -> Result<(), VolumeError> {
@@ -99,38 +118,89 @@ impl From<VolumeError> for rspc::Error {
 
 pub async fn get_volumes() -> Vec<Volume> {
 	let mut sys = sys_guard().lock().await;
-
-	let mut disks: HashMap<OsString, Volume> = HashMap::new();
-
 	sys.refresh_disks_list();
 
-	// for loo sys.disks
-	for disk in sys.disks() {
-		let disk_name = disk.name().to_os_string();
+	#[cfg(target_os = "linux")]
+	let disk_names_guard = Mutex::new(std::collections::HashSet::new());
+
+	// Ignore mounted DMGs
+	#[cfg(target_os = "macos")]
+	let dmgs = &tokio::process::Command::new("hdiutil")
+		.args(["info", "-plist"])
+		.output()
+		.await
+		.map_err(|err| tracing::error!("Failed to execute hdiutil: {err:#?}"))
+		.ok()
+		.and_then(|wmic_process| {
+			if wmic_process.status.success() {
+				let info: Result<HDIUtilInfo, _> = plist::from_bytes(&wmic_process.stdout);
+				match info {
+					Err(err) => {
+						tracing::error!("Failed to parse hdiutil output: {err:#?}");
+						None
+					}
+					Ok(info) => Some(
+						info.images
+							.into_iter()
+							.flat_map(|image| image.system_entities)
+							.flat_map(|entity: ImageSystemEntity| entity.mount_point)
+							.flat_map(|mount_point| PathBuf::from_str(mount_point.as_str()))
+							.collect::<std::collections::HashSet<_>>(),
+					),
+				}
+			} else {
+				tracing::error!("Command hdiutil return error");
+				None
+			}
+		});
+
+	future::join_all(sys.disks().iter().map(|disk| async {
+		let disk_name = disk.name();
+		let disk_type = disk.type_();
 		let mount_point = disk.mount_point().to_path_buf();
 
-		#[cfg(unix)]
+		#[cfg(target_os = "linux")]
 		{
 			use std::os::unix::ffi::OsStrExt;
 
 			// Ignore non-devices disks (overlay, fuse, tmpfs, etc.)
-			#[cfg(target_os = "linux")]
 			if !disk_name.as_bytes().starts_with(b"/dev") {
 				continue;
+			// Ignore proxy devices
+			} else if tokio::fs::canonicalize(disk_name.clone())
+				.await
+				.map(|path| path != disk_name)
+				.unwrap_or(true)
+			{
+				return None;
 			}
 
-			#[cfg(target_os = "macos")]
-			if !disk_name.as_bytes().starts_with(b"/Volumes") {
-				continue;
+			// Ignore multiple mounts of the same disk
+			let mut disk_names = disk_names_guard.lock().await;
+			if !disk_names.insert(disk_name) {
+				return None;
 			}
 		}
 
-		if let Some(disk) = disks.get_mut(&disk_name) {
-			disk.mount_point.push(mount_point);
-			continue;
+		#[cfg(target_os = "macos")]
+		{
+			// Ignore mounted DMGs
+			if dmgs
+				.as_ref()
+				.map(|dmgs| dmgs.contains(&mount_point))
+				.unwrap_or(false)
+			{
+				return None;
+			}
+
+			if !(mount_point.starts_with("/Volumes") || mount_point.starts_with("/System/Volumes"))
+			{
+				return None;
+			}
 		}
 
-		let total_capacity = disk.total_space();
+		#[allow(unused_mut)] // mut is used in windows
+		let mut total_capacity = disk.total_space();
 		let available_capacity = disk.available_space();
 		let is_root_filesystem = mount_point.is_absolute() && mount_point.parent().is_none();
 
@@ -147,53 +217,59 @@ pub async fn get_volumes() -> Vec<Volume> {
 				// Remove path separator from Disk letter
 				caption.pop();
 
-				let wmic_output = Command::new("cmd")
+				let wmic_output = tokio::process::Command::new("cmd")
 					.args([
 						"/C",
 						&format!("wmic logical disk where Caption='{caption}' get Size"),
 					])
 					.output()
+					.await
+					.map_err(|err| tracing::error!("Failed to execute hdiutil: {err:#?}"))
 					.ok()
 					.and_then(|wmic_process| {
 						if wmic_process.status.success() {
 							String::from_utf8(wmic_process.stdout).ok()
 						} else {
+							tracing::error!("Command wmic return error");
 							None
 						}
 					});
 
 				if let Some(wmic_output) = wmic_output {
-					if let Ok(n) = wmic_output.split("\r\r\n").collect::<Vec<&str>>()[1]
+					match wmic_output.split("\r\r\n").collect::<Vec<&str>>()[1]
 						.to_string()
 						.trim()
 						.parse::<u64>()
 					{
-						total_capacity = n;
+						Err(err) => tracing::error!("Failed to parse wmic output: {err:#?}"),
+						Ok(n) => total_capacity = n,
 					}
 				}
 			}
 		}
 
-		disks.insert(
-			disk_name.clone(),
-			Volume {
-				name: disk_name,
-				disk_type: match disk.type_() {
+		Some(Volume {
+			name: disk_name.to_os_string(),
+			disk_type: if disk.is_removable() {
+				DiskType::Removable
+			} else {
+				match disk_type {
 					sysinfo::DiskType::SSD => DiskType::SSD,
 					sysinfo::DiskType::HDD => DiskType::HDD,
 					_ => DiskType::Removable,
-				},
-				mount_point: vec![mount_point],
-				file_system: String::from_utf8(disk.file_system().to_vec()).ok(),
-				is_removable: disk.is_removable(),
-				total_capacity,
-				available_capacity,
-				is_root_filesystem,
+				}
 			},
-		);
-	}
-
-	disks.values().cloned().collect()
+			mount_point,
+			file_system: String::from_utf8(disk.file_system().to_vec()).ok(),
+			total_capacity,
+			available_capacity,
+			is_root_filesystem,
+		})
+	}))
+	.await
+	.into_iter()
+	.flatten()
+	.collect::<Vec<Volume>>()
 }
 
 // #[test]
