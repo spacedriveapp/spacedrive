b112eac6df8d1f1a5f223305952d9ae834a1da39
nikec <nikec.job@gmail.com>
2023-07-03 08:52:16 +0200

    Update useIsTextTruncated hook

--- interface/hooks/useIsTextTruncated.ts
+++ interface/hooks/useIsTextTruncated.ts
@@ -1,26 +1,11 @@
-import { RefObject, useCallback, useEffect, useState } from 'react';
-import { useCallbackToWatchResize } from './useCallbackToWatchResize';
+import { RefObject, useMemo } from 'react';
+import useResizeObserver from 'use-resize-observer';
 
-export const useIsTextTruncated = (
-	element: RefObject<HTMLElement>,
-	text: string | null
-): boolean => {
-	const determineIsTruncated = useCallback((): boolean => {
+export const useIsTextTruncated = (element: RefObject<HTMLElement>, text: string | null) => {
+	const { width } = useResizeObserver({ ref: element });
+
+	return useMemo(() => {
 		if (!element.current) return false;
 		return element.current.scrollWidth > element.current.clientWidth;
-	}, [element]);
-
-	const [isTruncated, setIsTruncated] = useState<boolean>(determineIsTruncated());
-
-	useCallbackToWatchResize(
-		() => setIsTruncated(determineIsTruncated()),
-		[determineIsTruncated],
-		element
-	);
-
-	useEffect(() => {
-		setIsTruncated(determineIsTruncated());
-	}, [element, determineIsTruncated, text]);
-
-	return isTruncated;
+	}, [element, width, text]);
 };
