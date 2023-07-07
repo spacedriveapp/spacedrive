4feca104f787cfa02276278973555128765e8854
nikec <43032218+niikeec@users.noreply.github.com>
2023-07-07 12:57:25 +0200

    [ENG-889] Display options based on selected view (#1079)
    * Update slider color
    
    * Update tooltip color
    
    * Display options based on view

--- interface/app/$libraryId/Explorer/OptionsPanel.tsx
+++ interface/app/$libraryId/Explorer/OptionsPanel.tsx
@@ -3,7 +3,6 @@ import { type SortOrder, SortOrderSchema } from '~/app/route-schemas';
 import { getExplorerConfigStore, useExplorerConfigStore } from './config';
 import { FilePathSearchOrderingKeys, getExplorerStore, useExplorerStore } from './store';
 
-const Heading = tw.div`text-ink-dull text-xs font-semibold`;
 const Subheading = tw.div`text-ink-dull mb-1 text-xs font-medium`;
 
 export const sortOptions: Record<FilePathSearchOrderingKeys, string> = {
@@ -21,106 +20,114 @@ export default () => {
 	const explorerConfig = useExplorerConfigStore();
 
 	return (
-		<div className="p-4">
-			<Subheading>Item size</Subheading>
-			{explorerStore.layoutMode === 'media' ? (
-				<Slider
-					defaultValue={[10 - explorerStore.mediaColumns]}
-					min={0}
-					max={6}
-					step={2}
-					onValueChange={([val]) => {
-						if (val !== undefined) {
-							getExplorerStore().mediaColumns = 10 - val;
+		<div className="flex w-80 flex-col gap-4 p-4">
+			{(explorerStore.layoutMode === 'grid' || explorerStore.layoutMode === 'media') && (
+				<div>
+					<Subheading>Item size</Subheading>
+					{explorerStore.layoutMode === 'grid' ? (
+						<Slider
+							onValueChange={(value) => {
+								getExplorerStore().gridItemSize = value[0] || 100;
+							}}
+							defaultValue={[explorerStore.gridItemSize]}
+							max={200}
+							step={10}
+							min={60}
+						/>
+					) : (
+						<Slider
+							defaultValue={[10 - explorerStore.mediaColumns]}
+							min={0}
+							max={6}
+							step={2}
+							onValueChange={([val]) => {
+								if (val !== undefined) {
+									getExplorerStore().mediaColumns = 10 - val;
+								}
+							}}
+						/>
+					)}
+				</div>
+			)}
+			{(explorerStore.layoutMode === 'grid' || explorerStore.layoutMode === 'media') && (
+				<div className="grid grid-cols-2 gap-2">
+					<div className="flex flex-col">
+						<Subheading>Sort by</Subheading>
+						<Select
+							value={explorerStore.orderBy}
+							size="sm"
+							className="w-full"
+							onChange={(value) =>
+								(getExplorerStore().orderBy = value as FilePathSearchOrderingKeys)
+							}
+						>
+							{Object.entries(sortOptions).map(([value, text]) => (
+								<SelectOption key={value} value={value}>
+									{text}
+								</SelectOption>
+							))}
+						</Select>
+					</div>
+
+					<div className="flex flex-col">
+						<Subheading>Direction</Subheading>
+						<Select
+							value={explorerStore.orderByDirection}
+							size="sm"
+							className="w-full"
+							onChange={(value) =>
+								(getExplorerStore().orderByDirection = value as SortOrder)
+							}
+						>
+							{SortOrderSchema.options.map((o) => (
+								<SelectOption key={o.value} value={o.value}>
+									{o.value}
+								</SelectOption>
+							))}
+						</Select>
+					</div>
+				</div>
+			)}
+
+			{explorerStore.layoutMode === 'grid' && (
+				<RadixCheckbox
+					checked={explorerStore.showBytesInGridView}
+					label="Show Object size"
+					name="showBytesInGridView"
+					onCheckedChange={(value) => {
+						if (typeof value === 'boolean') {
+							getExplorerStore().showBytesInGridView = value;
 						}
 					}}
-				/>
-			) : (
-				<Slider
-					onValueChange={(value) => {
-						getExplorerStore().gridItemSize = value[0] || 100;
-					}}
-					defaultValue={[explorerStore.gridItemSize]}
-					max={200}
-					step={10}
-					min={60}
+					className="mt-1"
 				/>
 			)}
 
-			<div className="my-2 mt-4 grid grid-cols-2 gap-2">
-				<div className="flex flex-col">
-					<Subheading>Sort by</Subheading>
-					<Select
-						value={explorerStore.orderBy}
-						size="sm"
-						className="w-full"
-						onChange={(value) =>
-							(getExplorerStore().orderBy = value as FilePathSearchOrderingKeys)
+			{explorerStore.layoutMode === 'media' && (
+				<RadixCheckbox
+					checked={explorerStore.mediaAspectSquare}
+					label="Show square thumbnails"
+					name="mediaAspectSquare"
+					onCheckedChange={(value) => {
+						if (typeof value === 'boolean') {
+							getExplorerStore().mediaAspectSquare = value;
 						}
-					>
-						{Object.entries(sortOptions).map(([value, text]) => (
-							<SelectOption key={value} value={value}>
-								{text}
-							</SelectOption>
-						))}
-					</Select>
-				</div>
-				<div className="flex flex-col">
-					<Subheading>Direction</Subheading>
-					<Select
-						value={explorerStore.orderByDirection}
-						size="sm"
-						className="w-full"
-						onChange={(value) =>
-							(getExplorerStore().orderByDirection = value as SortOrder)
-						}
-					>
-						{SortOrderSchema.options.map((o) => (
-							<SelectOption key={o.value} value={o.value}>
-								{o.value}
-							</SelectOption>
-						))}
-					</Select>
-				</div>
-			</div>
-
-			<div className="flex w-full flex-col space-y-3 pt-2">
-				{explorerStore.layoutMode === 'media' ? (
-					<RadixCheckbox
-						checked={explorerStore.mediaAspectSquare}
-						label="Show square thumbnails"
-						name="mediaAspectSquare"
-						onCheckedChange={(value) => {
-							if (typeof value === 'boolean') {
-								getExplorerStore().mediaAspectSquare = value;
-							}
-						}}
-					/>
-				) : (
-					<RadixCheckbox
-						checked={explorerStore.showBytesInGridView}
-						label="Show Object size"
-						name="showBytesInGridView"
-						onCheckedChange={(value) => {
-							if (typeof value === 'boolean') {
-								getExplorerStore().showBytesInGridView = value;
-							}
-						}}
-					/>
-				)}
-				<div>
-					<Subheading>Double click action</Subheading>
-					<Select
-						className="w-full"
-						value={explorerConfig.openOnDoubleClick ? 'openFile' : 'quickPreview'}
-						onChange={(value) => {
-							getExplorerConfigStore().openOnDoubleClick = value === 'openFile';
-						}}
-					>
-						<SelectOption value="openFile">Open File</SelectOption>
-						<SelectOption value="quickPreview">Quick Preview</SelectOption>
-					</Select>
-				</div>
+					}}
+					className="mt-1"
+				/>
+			)}
+			<div>
+				<Subheading>Double click action</Subheading>
+				<Select
+					className="w-full"
+					value={explorerConfig.openOnDoubleClick ? 'openFile' : 'quickPreview'}
+					onChange={(value) => {
+						getExplorerConfigStore().openOnDoubleClick = value === 'openFile';
+					}}
+				>
+					<SelectOption value="openFile">Open File</SelectOption>
+					<SelectOption value="quickPreview">Quick Preview</SelectOption>
+				</Select>
 			</div>
 		</div>
 	);
