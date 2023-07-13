8c013c3692edc485783403acb46be50cf2f544bb
Oscar Beaumont <oscar@otbeaumont.me>
2023-07-13 14:52:51 +0800

    mobile + P2P is a mess

--- apps/mobile/src/screens/settings/info/Debug.tsx
+++ apps/mobile/src/screens/settings/info/Debug.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import { getDebugState, toggleFeatureFlag } from '@sd/client';
+import { getDebugState, toggleFeatureFlag, useDebugState, useFeatureFlags } from '@sd/client';
 import { Button } from '~/components/primitive/Button';
 import { tw } from '~/lib/tailwind';
 import { SettingsStackScreenProps } from '~/navigation/SettingsNavigator';
 
 const DebugScreen = ({ navigation }: SettingsStackScreenProps<'Debug'>) => {
+	const debugState = useDebugState();
+	const featureFlags = useFeatureFlags();
 	return (
 		<View>
 			<Text style={tw`text-ink`}>Debug</Text>
 			<Button onPress={() => toggleFeatureFlag(['p2pPairing', 'spacedrop'])}>
 				<Text style={tw`text-ink`}>Toggle P2P</Text>
 			</Button>
-
 			<Button onPress={() => (getDebugState().rspcLogger = !getDebugState().rspcLogger)}>
 				<Text style={tw`text-ink`}>Toggle rspc logger</Text>
 			</Button>
-
+			<Text style={tw`text-ink`}>{JSON.stringify(featureFlags)}</Text>
+			<Text style={tw`text-ink`}>{JSON.stringify(debugState)}</Text>
 			<Button
 				onPress={() => {
 					navigation.popToTop();
