55a257de4cac90c932a67e4028256fd7e6a65536
brxken128 <77554505+brxken128@users.noreply.github.com>
2023-07-26 20:20:22 +0100

    cleanup migrations

--- core/prisma/migrations/20230726191938_media_data/migration.sql
+++ core/prisma/migrations/20230726191938_media_data/migration.sql
@@ -1,20 +1,14 @@
 /*
   Warnings:
 
-  - You are about to drop the column `color_space` on the `media_data` table. All the data in the column will be lost.
-  - You are about to drop the column `date_created` on the `media_data` table. All the data in the column will be lost.
-  - You are about to drop the column `date_taken` on the `media_data` table. All the data in the column will be lost.
-  - You are about to drop the column `device_make` on the `media_data` table. All the data in the column will be lost.
-  - You are about to drop the column `device_model` on the `media_data` table. All the data in the column will be lost.
-  - You are about to drop the column `device_software` on the `media_data` table. All the data in the column will be lost.
-  - You are about to drop the column `flash` on the `media_data` table. All the data in the column will be lost.
-  - You are about to drop the column `focal_length` on the `media_data` table. All the data in the column will be lost.
+  - You are about to drop the column `capture_device_make` on the `media_data` table. All the data in the column will be lost.
+  - You are about to drop the column `capture_device_model` on the `media_data` table. All the data in the column will be lost.
+  - You are about to drop the column `capture_device_software` on the `media_data` table. All the data in the column will be lost.
+  - You are about to drop the column `duration_seconds` on the `media_data` table. All the data in the column will be lost.
   - You are about to drop the column `latitude` on the `media_data` table. All the data in the column will be lost.
   - You are about to drop the column `longitude` on the `media_data` table. All the data in the column will be lost.
-  - You are about to drop the column `orientation` on the `media_data` table. All the data in the column will be lost.
   - You are about to drop the column `pixel_height` on the `media_data` table. All the data in the column will be lost.
   - You are about to drop the column `pixel_width` on the `media_data` table. All the data in the column will be lost.
-  - You are about to drop the column `shutter_speed` on the `media_data` table. All the data in the column will be lost.
 
 */
 -- AlterTable
@@ -49,7 +43,7 @@ CREATE TABLE "new_media_data" (
     "codecs" TEXT,
     CONSTRAINT "media_data_id_fkey" FOREIGN KEY ("id") REFERENCES "object" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
-INSERT INTO "new_media_data" ("codecs", "copyright", "duration", "fps", "id", "streams") SELECT "codecs", "copyright", "duration", "fps", "id", "streams" FROM "media_data";
+INSERT INTO "new_media_data" ("codecs", "fps", "id", "streams") SELECT "codecs", "fps", "id", "streams" FROM "media_data";
 DROP TABLE "media_data";
 ALTER TABLE "new_media_data" RENAME TO "media_data";
 PRAGMA foreign_key_check;
