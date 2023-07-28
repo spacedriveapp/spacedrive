821eca29d11f5a8732e260ce6228dc37b74ce6a6
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-07-28 22:54:41 +0530

    #1119 Issue: Implemented "Copy as Path" functionality
    I addressed issue #1119 by successfully implementing the "Copy as Path" functionality within the "More Actions" section of the Context Menu. This feature enhances user experience and offers a convenient way to copy file paths. While considering the valuable suggestion of potentially associating this option with the "Alt" key in the future, I opted to make it accessible through the "More Actions" section for now. All tests have passed smoothly

--- interface/app/$libraryId/Explorer/ContextMenu/FilePath/index.tsx
+++ interface/app/$libraryId/Explorer/ContextMenu/FilePath/index.tsx
@@ -51,6 +51,8 @@ export default ({ data }: Props) => {
 			{object && <ObjectItems.AssignTag object={object} />}
 
 			<ContextMenu.SubMenu label="More actions..." icon={Plus}>
+				<FilePathItems.CopyAsPath filePath={filePath} />
+
 				<FilePathItems.Crypto filePath={filePath} />
 
 				<FilePathItems.Compress filePath={filePath} />
