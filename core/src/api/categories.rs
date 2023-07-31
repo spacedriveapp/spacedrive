0a661228af0ae273129b77ba2af0274194d60950
Oscar Beaumont <oscar@otbeaumont.me>
2023-08-01 02:02:08 +0800

    parallelise `categories.list`

--- core/src/api/categories.rs
+++ core/src/api/categories.rs
@@ -1,6 +1,6 @@
-use crate::library::{get_category_count, Category};
+use crate::library::Category;
 
-use std::{collections::BTreeMap, str::FromStr};
+use std::str::FromStr;
 
 use rspc::alpha::AlphaRouter;
 use strum::VariantNames;
@@ -10,16 +10,25 @@ use super::{utils::library, Ctx, R};
 pub(crate) fn mount() -> AlphaRouter<Ctx> {
 	R.router().procedure("list", {
 		R.with2(library()).query(|(_, library), _: ()| async move {
-			let mut data = BTreeMap::new();
+			let (categories, queries): (Vec<_>, Vec<_>) = Category::VARIANTS
+				.iter()
+				.map(|category| {
+					let category = Category::from_str(category)
+						.expect("it's alright this category string exists");
+					(
+						category,
+						library.db.object().count(vec![category.to_where_param()]),
+					)
+				})
+				.unzip();
 
-			for category_str in Category::VARIANTS {
-				let category = Category::from_str(category_str)
-					.expect("it's alright this category string exists");
-
-				data.insert(category, get_category_count(&library.db, category).await);
-			}
-
-			Ok(data)
+			Ok(library
+				.db
+				._batch(queries)
+				.await?
+				.into_iter()
+				.zip(categories)
+				.collect::<Vec<_>>())
 		})
 	})
 }
