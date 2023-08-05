486a2bd552bbea96e237387310804c1e3f9d4b30
Brendan Allan <brendonovich@outlook.com>
2023-08-05 19:54:01 +0800

    disable ban-types eslint rule

--- packages/config/eslint/base.js
+++ packages/config/eslint/base.js
@@ -1,50 +1,51 @@
 const path = require('node:path');
 module.exports = {
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true
-		},
-		ecmaVersion: 12,
-		sourceType: 'module'
-	},
-	extends: [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:react-hooks/recommended',
-		'plugin:@typescript-eslint/recommended',
-		'turbo',
-		'prettier'
-	],
-	plugins: ['react'],
-	rules: {
-		'react/display-name': 'off',
-		'react/prop-types': 'off',
-		'react/no-unescaped-entities': 'off',
-		'react/react-in-jsx-scope': 'off',
-		'react-hooks/rules-of-hooks': 'warn',
-		'react-hooks/exhaustive-deps': 'warn',
-		'@typescript-eslint/no-unused-vars': 'off',
-		'@typescript-eslint/ban-ts-comment': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/no-var-requires': 'off',
-		'@typescript-eslint/no-non-null-assertion': 'off',
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-empty-interface': 'off',
-		'@typescript-eslint/no-empty-function': 'off',
-		'no-control-regex': 'off',
-		'no-mixed-spaces-and-tabs': ['warn', 'smart-tabs'],
-		'turbo/no-undeclared-env-vars': [
-			'error',
-			{
-				cwd: path.resolve(path.join(__dirname, '..', '..', '..'))
-			}
-		]
-	},
-	ignorePatterns: ['dist', '**/*.js', '**/*.json', 'node_modules'],
-	settings: {
-		react: {
-			version: 'detect'
-		}
-	}
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'turbo',
+        'prettier'
+    ],
+    plugins: ['react'],
+    rules: {
+        'react/display-name': 'off',
+        'react/prop-types': 'off',
+        'react/no-unescaped-entities': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react-hooks/rules-of-hooks': 'warn',
+        'react-hooks/exhaustive-deps': 'warn',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/ban-types': 'off',
+        'no-control-regex': 'off',
+        'no-mixed-spaces-and-tabs': ['warn', 'smart-tabs'],
+        'turbo/no-undeclared-env-vars': [
+            'error',
+            {
+                cwd: path.resolve(path.join(__dirname, '..', '..', '..'))
+            }
+        ]
+    },
+    ignorePatterns: ['dist', '**/*.js', '**/*.json', 'node_modules'],
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    }
 };
