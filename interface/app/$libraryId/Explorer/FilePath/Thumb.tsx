4ffb48a2eb1be1334d28bd5f292b9b16d874f611
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-08-08 22:30:51 +0530

    Text Viewer's Implementation
    Implemented a TEXT Viewer like PDF Viewer, which shows content of text files

--- interface/app/$libraryId/Explorer/FilePath/Thumb.tsx
+++ interface/app/$libraryId/Explorer/FilePath/Thumb.tsx
@@ -2,7 +2,7 @@ import { getIcon, iconNames } from '@sd/assets/util';
 import clsx from 'clsx';
 import { ImgHTMLAttributes, memo, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { ExplorerItem, getItemFilePath, getItemLocation, useLibraryContext } from '@sd/client';
-import { PDFViewer } from '~/components';
+import { PDFViewer, TEXTViewer } from '~/components';
 import { useCallbackToWatchResize, useIsDark } from '~/hooks';
 import { usePlatform } from '~/util/Platform';
 import { pdfViewerEnabled } from '~/util/pdfViewer';
@@ -244,17 +244,17 @@ function FileThumb({ size, cover, ...props }: ThumbProps) {
 								);
 							case 'Text':
 								return (
-									<iframe
+									<TEXTViewer
 										src={src}
 										onLoad={onLoad}
 										onError={onError}
 										className={clsx(
-											'h-full w-full border-0 rounded bg-white',
+											'h-full w-full border-0 font-mono px-2',
+											!props.mediaControls ? 'overflow-hidden' : 'overflow-auto',
 											childClassName,
 											props.className
 										)}
-										scrolling={!props.mediaControls ? 'no' : 'yes'}
-										draggable={false}
+										crossOrigin="anonymous"
 									/>
 								);
 							case 'Video':
