e65a6e31f72cf7df8063b0b61f292a95c5fbca6c
Brendan Allan <brendonovich@outlook.com>
2023-08-12 02:14:02 -0700

    Move onboarding form handling logic to Layout (#1206)
    * move onboarding form handling logic to Layout
    
    * better default handling + progress tracker
    
    * relative routes >>>
    
    * wait for libraries to load
    
    * move screen unlocks to Progress

--- interface/app/onboarding/index.tsx
+++ interface/app/onboarding/index.tsx
@@ -1,13 +1,25 @@
 import { Navigate, RouteObject } from 'react-router';
+import { getOnboardingStore } from '@sd/client';
 import Alpha from './alpha';
+import { useOnboardingContext } from './context';
 import CreatingLibrary from './creating-library';
 import NewLibrary from './new-library';
 import Privacy from './privacy';
 
+const Index = () => {
+	const obStore = getOnboardingStore();
+	const ctx = useOnboardingContext();
+
+	if (obStore.lastActiveScreen && !ctx.library)
+		return <Navigate to={obStore.lastActiveScreen} replace />;
+
+	return <Navigate to="alpha" replace />;
+};
+
 export default [
 	{
 		index: true,
-		element: <Navigate to="alpha" replace />
+		element: <Index />
 	},
 	{ path: 'alpha', element: <Alpha /> },
 	{
