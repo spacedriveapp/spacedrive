92e02bd0ca844c8bea363d34d7160d449094a87d
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-08-15 15:21:02 +0530

    added more icons to utilize

--- interface/app/$libraryId/Explorer/FilePath/LayeredFileIcon.tsx
+++ interface/app/$libraryId/Explorer/FilePath/LayeredFileIcon.tsx
@@ -17,7 +17,20 @@ const LayeredFileIcon = ({
 }) => {
 	const IconMapping = {
 		rs: <Icons.rust viewBox="0 0 16 16" height="40%" width="40%" />,
-		go: <Icons.go viewBox="0 0 16 16" height="40%" width="40%" />
+		go: <Icons.go viewBox="0 0 16 16" height="40%" width="40%" />,
+		html: <Icons.html viewBox="0 0 16 16" height="40%" width="40%" />,
+		css: <Icons.css viewBox="0 0 16 16" height="40%" width="40%" />,
+		scss: <Icons.scss viewBox="0 0 16 16" height="40%" width="40%" />,
+		js: <Icons.js viewBox="0 0 16 16" height="40%" width="40%" />,
+		jsx: <Icons.js viewBox="0 0 16 16" height="40%" width="40%" />,
+		ts: <Icons.tsx viewBox="0 0 16 16" height="40%" width="40%" />,
+		tsx: <Icons.tsx viewBox="0 0 16 16" height="40%" width="40%" />,
+		vue: <Icons.vue viewBox="0 0 16 16" height="40%" width="40%" />,
+		swift: <Icons.swift viewBox="0 0 16 16" height="40%" width="40%" />,
+		php: <Icons.php viewBox="0 0 16 16" height="40%" width="40%" />,
+		py: <Icons.python viewBox="0 0 16 16" height="40%" width="40%" />,
+		rb: <Icons.ruby viewBox="0 0 16 16" height="40%" width="40%" />,
+		sh: <Icons.shell viewBox="0 0 16 16" height="40%" width="40%" />,
 	};
 	const IconComponent = ({ extension }: { extension: keyof typeof IconMapping }) => {
 		return IconMapping[extension];
