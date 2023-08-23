5b10e30abd0ea7a8be20f6058ef6c2e3118f7759
pr <pineapplerind.info@gmail.com>
2023-08-23 14:07:15 -0400

    "see more" if locations > 5, not locations >= 5

--- interface/app/$libraryId/Layout/Sidebar/LibrarySection.tsx
+++ interface/app/$libraryId/Layout/Sidebar/LibrarySection.tsx
@@ -160,7 +160,7 @@ export const LibrarySection = () => {
 						</LocationsContextMenu>
 					);
 				})}
-				{locationsQuery.data?.[SEE_MORE_LOCATIONS_COUNT - 1] && (
+				{locationsQuery.data?.[SEE_MORE_LOCATIONS_COUNT] && (
 					<div
 						onClick={() => setSeeMoreLocations(!seeMoreLocations)}
 						className="mb-1 ml-2 mt-0.5 cursor-pointer text-center text-tiny font-semibold text-ink-faint/50 transition hover:text-accent"
