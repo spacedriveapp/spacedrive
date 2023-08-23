bfb14d6883ea54d1d4cf5e455c96b6c28c878b98
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-08-23 11:30:14 -0300

    Finish integrating with Thumbnailer  - Fix svg thumbnail size  - Fix incorrect color space while converting tiny_skia::Pixmap to image

--- crates/svg/src/lib.rs
+++ crates/svg/src/lib.rs
@@ -8,6 +8,8 @@ use usvg::{fontdb, TreeParsing, TreeTextToPath};
 
 type SvgResult<T> = Result<T, SvgError>;
 
+const THUMB_SIZE: u32 = 512;
+
 /// The maximum file size that an image can be in order to have a thumbnail generated.
 ///
 /// This value is in MiB.
@@ -25,6 +27,8 @@ pub enum SvgError {
 	Pixbuf,
 	#[error("there was an error while converting the image to an `RgbImage`")]
 	RgbImageConversion,
+	#[error("failed to calculate thumbnail size")]
+	InvalidSize,
 	#[error("the image provided is too large (over 20MiB)")]
 	TooLarge,
 }
@@ -46,15 +50,28 @@ pub async fn svg_to_dynamic_image(path: &Path) -> SvgResult<DynamicImage> {
 
 	let rtree = resvg::Tree::from_usvg(&tree);
 
-	let Some(mut pixmap) = tiny_skia::Pixmap::new(rtree.size.width().ceil() as u32, rtree.size.height().ceil() as u32) else {
+	let size = if rtree.size.width() > rtree.size.height() {
+		rtree.size.to_int_size().scale_to_width(THUMB_SIZE)
+	} else {
+		rtree.size.to_int_size().scale_to_height(THUMB_SIZE)
+	}
+	.ok_or(SvgError::InvalidSize)?;
+
+	let transform = tiny_skia::Transform::from_scale(
+		size.width() as f32 / rtree.size.width() as f32,
+		size.height() as f32 / rtree.size.height() as f32,
+	);
+
+	let Some(mut pixmap) = tiny_skia::Pixmap::new(size.width(), size.height()) else {
 		return Err(SvgError::Pixbuf)
 	};
+	rtree.render(transform, &mut pixmap.as_mut());
 
-	rtree.render(tiny_skia::Transform::default(), &mut pixmap.as_mut());
-
-	let Some(rgb_img) = image::RgbImage::from_raw(pixmap.width(), pixmap.height(), pixmap.data_mut().into()) else {
+	let Some(rgb_img) = image::RgbaImage::from_raw(
+		pixmap.width(), pixmap.height(), pixmap.data_mut().into()
+	) else {
 		return Err(SvgError::RgbImageConversion)
 	};
 
-	Ok(DynamicImage::ImageRgb8(rgb_img))
+	Ok(DynamicImage::ImageRgba8(rgb_img))
 }
