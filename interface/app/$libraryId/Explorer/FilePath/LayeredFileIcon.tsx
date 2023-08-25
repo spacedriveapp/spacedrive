b0f663b9b5d9b71b4af436c70ff73d237d81a66b
Jamie Pine <ijamespine@me.com>
2023-08-24 17:05:02 -0700

    config

--- interface/app/$libraryId/Explorer/FilePath/LayeredFileIcon.tsx
+++ interface/app/$libraryId/Explorer/FilePath/LayeredFileIcon.tsx
@@ -1,6 +1,6 @@
+import { getLayeredIcon } from '@sd/assets/util';
 import { type ImgHTMLAttributes } from 'react';
 import { type ObjectKindKey } from '@sd/client';
-import { getLayeredIcon } from '@sd/assets/util';
 
 interface LayeredFileIconProps extends ImgHTMLAttributes<HTMLImageElement> {
 	kind: ObjectKindKey;
@@ -9,14 +9,27 @@ interface LayeredFileIconProps extends ImgHTMLAttributes<HTMLImageElement> {
 
 const LayeredFileIcon = ({ kind, extension, ...props }: LayeredFileIconProps) => {
 	const iconImg = <img {...props} />;
+
+	if (kind !== 'Document' && kind !== 'Code' && kind !== 'Text' && kind !== 'Config') {
+		return iconImg;
+	}
+
 	const IconComponent = extension ? getLayeredIcon(kind, extension) : null;
+
+	const positionConfig: Record<string, string> = {
+		Text: 'flex h-full w-full items-center justify-center'
+		// Add more kinds here as needed
+	};
+
+	const positionClass =
+		positionConfig[kind] || 'flex h-full w-full items-end justify-end pb-4 pr-2';
 
 	return IconComponent == null ? (
 		iconImg
 	) : (
 		<div className="relative">
 			{iconImg}
-			<div className="absolute bottom-0 right-0 flex h-full w-full items-end justify-end pb-4 pr-2">
+			<div className={`absolute bottom-0 right-0 ${positionClass}`}>
 				<IconComponent viewBox="0 0 16 16" height="40%" width="40%" />
 			</div>
 		</div>
