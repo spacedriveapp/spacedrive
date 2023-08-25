7202478b247a7d6de7465c805137bf1ece0af9f8
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2023-08-25 14:04:39 +0300

    indeterminate progress bar

--- apps/mobile/src/components/animation/ProgressBar.tsx
+++ apps/mobile/src/components/animation/ProgressBar.tsx
@@ -12,6 +12,20 @@ type ProgressBarProps = {
 export const ProgressBar = memo((props: ProgressBarProps) => {
 	const percentage = props.pending ? 0 : Math.round((props.value / props.total) * 100);
 
+	if (props.pending) {
+		// Show indeterminate progress bar
+		return (
+			<View style={tw`h-1 overflow-hidden rounded-full bg-app-button`}>
+				<MotiView
+					style={tw`h-full w-[50%] bg-accent`}
+					from={{ left: '-50%' }}
+					animate={{ left: '100%' }}
+					transition={{ type: 'timing', duration: 1500, loop: true }}
+				/>
+			</View>
+		);
+	}
+
 	return (
 		<View style={tw`h-1 w-[94%] overflow-hidden rounded-full bg-app-button`}>
 			<MotiView
