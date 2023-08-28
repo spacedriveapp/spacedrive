048c6059e2d28d29dedd2b4562bb5f63351e9bbe
nikec <43032218+niikeec@users.noreply.github.com>
2023-08-28 13:55:05 +0200

    [ENG-988] fix layout shift (#1259)
    fix layout shift

--- interface/app/$libraryId/Explorer/index.tsx
+++ interface/app/$libraryId/Explorer/index.tsx
@@ -44,7 +44,7 @@ export default function Explorer(props: PropsWithChildren<Props>) {
 				<div className="flex-1 overflow-hidden">
 					<div
 						ref={explorer.scrollRef}
-						className="explorer-scroll relative h-screen overflow-y-auto overflow-x-hidden"
+						className="custom-scroll explorer-scroll relative h-screen overflow-x-hidden"
 						style={{
 							paddingTop: TOP_BAR_HEIGHT,
 							paddingRight: explorerStore.showInspector ? INSPECTOR_WIDTH : 0
