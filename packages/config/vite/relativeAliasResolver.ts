d75625d9db7ced811c6563552cba52ee3c9f9e82
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-08-29 23:03:29 -0300

    Fix Vite relativeAliasResolver on Windows  - Add Strawberry Perl installation to setup-system.ps1 (stop rust-analyzer from crashing on Windows)  - Remove unused imports from desktop windows crate  - Add todo for missing sync test match cases

--- packages/config/vite/relativeAliasResolver.ts
+++ packages/config/vite/relativeAliasResolver.ts
@@ -11,25 +11,28 @@ const resolver: Alias = {
 	async customResolver(source, importer) {
 		let root: null | string = null;
 
+		if (importer) importer = path.normalize(importer);
+
+		// source is the path imported on typescript, which always use / as path separator
 		const [_, sourcePath] = source.split('~/');
 
-		const relativeImporter = importer!.replace(projectPath, '');
+		const relativeImporter = importer?.replace(projectPath, '');
+		if (relativeImporter && relativeImporter.includes(`${path.sep}src${path.sep}`)) {
+			const [pkg] = relativeImporter.split(`${path.sep}src${path.sep}`);
 
-		if (relativeImporter.includes('/src/')) {
-			const [pkg] = relativeImporter.split('/src/');
+			root = `${projectPath}${pkg}${path.sep}src`;
+		} else if (importer) {
+			const pathObj = path.parse(importer);
 
-			root = `${projectPath}${pkg}/src`;
-		} else {
-			let parent = importer!;
-
-			while (parent !== '/') {
+			let parent = pathObj.dir;
+			while (parent !== pathObj.root) {
 				parent = path.dirname(parent);
 
 				let hasPkgJson = pkgJsonCache.get(parent);
 
 				if (hasPkgJson === undefined)
 					try {
-						await fs.stat(`${parent}/package.json`);
+						await fs.stat(`${parent}${path.sep}package.json`);
 						pkgJsonCache.set(parent, (hasPkgJson = true));
 					} catch {
 						pkgJsonCache.set(parent, (hasPkgJson = false));
@@ -43,12 +46,15 @@ const resolver: Alias = {
 
 			if (root === null)
 				throw new Error(`Failed to resolve import path ${source} in file ${importer}`);
+		} else {
+			throw new Error(`Failed to resolve import path ${source} in file ${importer}`);
 		}
 
-		const absolutePath = `${root}/${sourcePath}`;
+		const absolutePath = `${root}${path.sep}${sourcePath}`;
 
-		const folderItems = await fs.readdir(path.join(absolutePath, '../'));
+		const folderItems = await fs.readdir(path.join(absolutePath, '..'));
 
+		// sourcePath is derived from the path imported on typescript, which always use / as path separator
 		const item = folderItems.find((i) => i.startsWith(sourcePath.split('/').at(-1)!))!;
 
 		const fullPath = absolutePath + path.extname(item);
@@ -60,7 +66,7 @@ const resolver: Alias = {
 
 			const indexFile = directoryItems.find((i) => i.startsWith('index'));
 
-			return `${absolutePath}/${indexFile}`;
+			return `${absolutePath}${path.sep}${indexFile}`;
 		} else {
 			return fullPath;
 		}
