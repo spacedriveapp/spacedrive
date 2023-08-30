3e19f8e07f83a6fae3482c523bcbd8f1df600786
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2023-08-30 12:37:25 +0300

    Update JobContainer.tsx

--- interface/app/$libraryId/Layout/Sidebar/JobManager/JobContainer.tsx
+++ interface/app/$libraryId/Layout/Sidebar/JobManager/JobContainer.tsx
@@ -44,7 +44,7 @@ const JobContainer = forwardRef<HTMLLIElement, JobContainerProps>((props, ref) =
 			)}
 			<MetaContainer>
 				<Tooltip asChild tooltipClassName="bg-black max-w-[400px]" position="top" label={name}>
-					<p className="truncate max-w-[83%] pl-1.5 font-semibold">{name}</p>
+					<p className="truncate w-fit max-w-[83%] pl-1.5 font-semibold">{name}</p>
 				</Tooltip>
 				{textItems?.map((item, index) => {
 					// filter out undefined text so we don't render empty TextItems
