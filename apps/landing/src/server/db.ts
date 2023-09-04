881608e9a8a2685a717bf087785733fdaa6ced55
Utku <74243531+utkubakir@users.noreply.github.com>
2023-09-04 10:28:49 +0300

    Remove auth (#1286)
    remove auth
    
    Co-authored-by: jake <77554505+brxken128@users.noreply.github.com>

--- apps/landing/src/server/db.ts
+++ apps/landing/src/server/db.ts
@@ -1,72 +1,15 @@
-import { ProviderType } from '@auth/core/providers';
 import { connect } from '@planetscale/database';
-import { int, mysqlTable, primaryKey, serial, timestamp, varchar } from 'drizzle-orm/mysql-core';
+import { mysqlTable, serial, timestamp, varchar } from 'drizzle-orm/mysql-core';
 import { drizzle } from 'drizzle-orm/planetscale-serverless';
 import { env } from '~/env';
 
-export { eq, and, or, type InferModel } from 'drizzle-orm';
+export { and, eq, or, type InferModel } from 'drizzle-orm';
 
 const dbConnection = connect({
 	url: env.DATABASE_URL
 });
 
 export const db = drizzle(dbConnection);
-
-// AuthJS Schema
-
-// Planetscale moment
-const text = (name: string) =>
-	varchar(name, {
-		length: 255
-	});
-
-export const usersTable = mysqlTable('users', {
-	id: text('id').notNull().primaryKey(),
-	name: text('name'),
-	email: text('email').notNull(),
-	emailVerified: timestamp('emailVerified', { mode: 'date' }),
-	image: text('image')
-});
-
-export const accountsTable = mysqlTable(
-	'accounts',
-	{
-		userId: text('userId').notNull(),
-		//   .references(() => users.id, { onDelete: "cascade" }),
-		type: text('type').$type<ProviderType>().notNull(),
-		provider: text('provider').notNull(),
-		providerAccountId: text('providerAccountId').notNull(),
-		refresh_token: text('refresh_token'),
-		access_token: text('access_token'),
-		expires_at: int('expires_at'),
-		token_type: text('token_type'),
-		scope: text('scope'),
-		id_token: text('id_token'),
-		session_state: text('session_state')
-	},
-	(account) => ({
-		compoundKey: primaryKey(account.provider, account.providerAccountId)
-	})
-);
-
-export const sessionsTable = mysqlTable('sessions', {
-	sessionToken: text('sessionToken').notNull().primaryKey(),
-	userId: text('userId').notNull(),
-	// .references(() => users.id, { onDelete: "cascade" }),
-	expires: timestamp('expires', { mode: 'date' }).notNull()
-});
-
-export const verificationTokens = mysqlTable(
-	'verificationToken',
-	{
-		identifier: text('identifier').notNull(),
-		token: text('token').notNull(),
-		expires: timestamp('expires', { mode: 'date' }).notNull()
-	},
-	(vt) => ({
-		compoundKey: primaryKey(vt.identifier, vt.token)
-	})
-);
 
 // Spacedrive Schema
 
