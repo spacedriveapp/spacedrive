b677b6fe99ec23f2f2f50f00e6c7830d2167aaf5
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2023-09-07 16:46:16 +0300

    updated with variants

--- interface/components/Accordion.tsx
+++ interface/components/Accordion.tsx
@@ -2,25 +2,46 @@ import clsx from 'clsx';
 import { CaretDown } from 'phosphor-react';
 import { PropsWithChildren, useState } from 'react';
 
+
+type variants = 'apple' | 'default';
+
+type Styles = {
+	container: Record<variants, string>;
+	title: Record<variants, string>;
+	box: Record<variants, string>;
+  };
+
 interface Props {
-	className?: string;
-	title: string;
-	titleClassName?: string;
-	containerClassName?: string;
 	caretSize?: number;
+	title: string;
+	titleVariant?: variants;
+	boxVariant?: variants;
+	containerVariant?: variants;
 }
+
+const styles: Styles = {
+			container: {
+				apple: 'flex flex-col gap-1 rounded-b-none px-4',
+				default: 'rounded-b-md border-t border-app-line bg-app-box p-3 py-2'
+			},
+			title: {
+				apple: 'flex flex-row-reverse items-center justify-end gap-2 px-4 pb-1 pt-0 text-ink-dull',
+				default: 'flex flex-row items-center justify-between px-3 py-2'
+			},
+			box: {
+				apple: 'rounded-none border-0 bg-transparent py-0',
+				default: 'rounded-md border border-app-line bg-app-darkBox'
+			},
+	}
 
 const Accordion = (props: PropsWithChildren<Props>) => {
 	const [toggle, setToggle] = useState(false);
 
 	return (
-		<div className={clsx(props.className, 'rounded-md border border-app-line bg-app-darkBox')}>
+		<div className={styles.box[props.boxVariant ?? 'default']}>
 			<div
 				onClick={() => setToggle((t) => !t)}
-				className={clsx(
-					'flex flex-row items-center justify-between px-3 py-2',
-					props.titleClassName
-				)}
+				className={styles.title[props.titleVariant ?? 'default']}
 			>
 				<p className="text-xs">{props.title}</p>
 				<CaretDown
@@ -30,10 +51,7 @@ const Accordion = (props: PropsWithChildren<Props>) => {
 			</div>
 			{toggle && (
 				<div
-					className={clsx(
-						'rounded-b-md border-t border-app-line bg-app-box p-3 py-2',
-						props.containerClassName
-					)}
+					className={styles.container[props.containerVariant ?? 'default']}
 				>
 					{props.children}
 				</div>
