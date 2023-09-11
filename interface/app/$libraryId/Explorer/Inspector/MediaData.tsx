25f9b952c2d3a24f68b442f696364503d1a4b198
brxken128 <77554505+brxken128@users.noreply.github.com>
2023-09-11 23:49:50 +0100

    offer DD and DMS coordinate displays

--- interface/app/$libraryId/Explorer/Inspector/MediaData.tsx
+++ interface/app/$libraryId/Explorer/Inspector/MediaData.tsx
@@ -1,23 +1,60 @@
 import { MediaLocation, MediaMetadata, MediaTime, Orientation } from '@sd/client';
-
 import Accordion from '~/components/Accordion';
 import { usePlatform } from '~/util/Platform';
+
 import { MetaData } from './index';
 
 interface Props {
 	data: MediaMetadata;
 }
 
-function formatMediaTime(loc: MediaTime): string | null {
-	if (loc === 'Undefined') return null;
-	if ('Utc' in loc) return loc.Utc;
-	if ('Naive' in loc) return loc.Naive;
+function formatMediaTime(location: MediaTime): string | null {
+	if (location === 'Undefined') return null;
+	if ('Utc' in location) return location.Utc;
+	if ('Naive' in location) return location.Naive;
 	return null;
 }
 
-function formatLocation(loc: MediaLocation, dp: number): string {
-	return `${loc.latitude.toFixed(dp)}, ${loc.longitude.toFixed(dp)}`;
+function formatLocationDD(location: MediaLocation, dp: number): string {
+	return `${location.latitude.toFixed(dp)}, ${location.longitude.toFixed(dp)}`;
 }
+
+function formatLocationDMS(location: MediaLocation): string | null {
+	const lat_abs = Math.abs(getZeroWithDecimals(location.latitude));
+	const lat_d = flatTruncate(location.latitude, 0);
+	const lat_m = flatTruncate(60 * lat_abs, 0);
+	const lat_s = flatTruncate(lat_abs * 3600 - lat_m * 60, 0);
+	const lat_sign = location.latitude > 0 ? 'N' : 'S';
+
+	const long_abs = Math.abs(getZeroWithDecimals(location.latitude));
+	const long_d = flatTruncate(location.latitude, 0);
+	const long_m = flatTruncate(60 * long_abs, 0);
+	const long_s = flatTruncate(long_abs * 3600 - long_m * 60, 0);
+	const long_sign = location.latitude > 0 ? 'N' : 'S';
+
+	return `${lat_d}° ${lat_m}' ${lat_s}" ${lat_sign}, ${long_d}° ${long_m}' ${long_s}" ${long_sign}`;
+}
+
+// Truncate without rounding. The easiest way to do this is by treating it as a string, as there
+// will always be *some* edge-case when it comes to floating point arithmetic.
+function flatTruncate(num: number, places: number): number {
+	if (places > 8) return 0;
+	const x = num.toString();
+	const pos = x.indexOf('.');
+	return pos !== -1
+		? Number.parseFloat(
+				`${x.substring(0, pos)}${places != 0 ? '.' + x.substring(pos, places) : ''}`
+		  )
+		: 0;
+}
+
+function getZeroWithDecimals(num: number): number {
+	const x = num.toString();
+	const pos = x.indexOf('.');
+	return pos !== -1 ? Number.parseFloat(`${'0.' + x.substring(pos + 1)}`) : 0;
+}
+
+// return ~~(num * x) / x;
 
 const orientations = {
 	Normal: 'Normal',
@@ -53,12 +90,12 @@ function MediaData({ data }: Props) {
 									if (data.location)
 										platform.openLink(
 											`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
-												data.location.latitude
-											)}%2c${encodeURIComponent(data.location.longitude)}`
+												`${data.location.latitude},${data.location.longitude}`
+											)}}`
 										);
 								}}
 							>
-								{formatLocation(data.location, 3)}
+								{formatLocationDD(data.location, 3)}
 							</a>
 						) : (
 							'--'
