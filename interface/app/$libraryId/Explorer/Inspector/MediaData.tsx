e42077d2ca5f8f058087b2a0e89e40a30109fba9
brxken128 <77554505+brxken128@users.noreply.github.com>
2023-09-12 02:59:31 +0100

    clean up & optimise the conversion functions

--- interface/app/$libraryId/Explorer/Inspector/MediaData.tsx
+++ interface/app/$libraryId/Explorer/Inspector/MediaData.tsx
@@ -1,4 +1,10 @@
-import { MediaLocation, MediaMetadata, MediaTime, Orientation } from '@sd/client';
+import {
+	CoordinatesFormat,
+	MediaLocation,
+	MediaMetadata,
+	MediaTime,
+	useThemeStore
+} from '@sd/client';
 import Accordion from '~/components/Accordion';
 import { usePlatform } from '~/util/Platform';
 
@@ -8,53 +14,50 @@ interface Props {
 	data: MediaMetadata;
 }
 
-function formatMediaTime(location: MediaTime): string | null {
-	if (location === 'Undefined') return null;
-	if ('Utc' in location) return location.Utc;
-	if ('Naive' in location) return location.Naive;
+const formatMediaTime = (time: MediaTime): string | null => {
+	if (time === 'Undefined') return null;
+	if ('Utc' in time) return time.Utc;
+	if ('Naive' in time) return time.Naive;
 	return null;
-}
+};
 
-function formatLocationDD(location: MediaLocation, dp: number): string {
-	return `${location.latitude.toFixed(dp)}, ${location.longitude.toFixed(dp)}`;
-}
+const formatLocationDD = (loc: MediaLocation, dp: number): string => {
+	// the lack of a + here will mean that coordinates may have padding at the end
+	// google does the same (if one is larger than the other, the smaller one will be padded with zeroes)
+	return `${loc.latitude.toFixed(dp)}, ${loc.longitude.toFixed(dp)}`;
+};
 
-function formatLocationDMS(location: MediaLocation): string | null {
-	const lat_abs = Math.abs(getZeroWithDecimals(location.latitude));
-	const lat_d = flatTruncate(location.latitude, 0);
-	const lat_m = flatTruncate(60 * lat_abs, 0);
-	const lat_s = flatTruncate(lat_abs * 3600 - lat_m * 60, 0);
-	const lat_sign = location.latitude > 0 ? 'N' : 'S';
+const formatLocationDMS = (loc: MediaLocation): string => {
+	const formatCoordinatesAsDMS = (
+		coordinates: number,
+		positiveChar: string,
+		negativeChar: string
+	): string => {
+		const abs = getAbsoluteDecimals(coordinates);
+		const d = Math.trunc(coordinates);
+		const m = Math.trunc(60 * abs);
+		// adding 0.05 before rounding and truncating with `toFixed` makes it match up with google
+		const s = (abs * 3600 - m * 60 + 0.05).toFixed(1);
+		const sign = coordinates > 0 ? positiveChar : negativeChar;
+		return `${d}°${m}'${s}"${sign}`;
+	};
 
-	const long_abs = Math.abs(getZeroWithDecimals(location.latitude));
-	const long_d = flatTruncate(location.latitude, 0);
-	const long_m = flatTruncate(60 * long_abs, 0);
-	const long_s = flatTruncate(long_abs * 3600 - long_m * 60, 0);
-	const long_sign = location.latitude > 0 ? 'N' : 'S';
+	return `${formatCoordinatesAsDMS(loc.latitude, 'N', 'S')} ${formatCoordinatesAsDMS(
+		loc.longitude,
+		'E',
+		'W'
+	)}`;
+};
 
-	return `${lat_d}° ${lat_m}' ${lat_s}" ${lat_sign}, ${long_d}° ${long_m}' ${long_s}" ${long_sign}`;
-}
-
-// Truncate without rounding. The easiest way to do this is by treating it as a string, as there
-// will always be *some* edge-case when it comes to floating point arithmetic.
-function flatTruncate(num: number, places: number): number {
-	if (places > 8) return 0;
+const getAbsoluteDecimals = (num: number): number => {
 	const x = num.toString();
-	const pos = x.indexOf('.');
-	return pos !== -1
-		? Number.parseFloat(
-				`${x.substring(0, pos)}${places != 0 ? '.' + x.substring(pos, places) : ''}`
-		  )
-		: 0;
-}
+	// this becomes +0.xxxxxxxxx and is needed to convert the minutes/seconds for DMS
+	return Math.abs(Number.parseFloat('0.' + x.substring(x.indexOf('.') + 1)));
+};
 
-function getZeroWithDecimals(num: number): number {
-	const x = num.toString();
-	const pos = x.indexOf('.');
-	return pos !== -1 ? Number.parseFloat(`${'0.' + x.substring(pos + 1)}`) : 0;
-}
-
-// return ~~(num * x) / x;
+const formatLocation = (loc: MediaLocation, format: CoordinatesFormat, dp?: number): string => {
+	return format === 'dd' && dp ? formatLocationDD(loc, dp) : formatLocationDMS(loc);
+};
 
 const orientations = {
 	Normal: 'Normal',
@@ -67,8 +70,9 @@ const orientations = {
 	CW270: 'Rotated 270° clockwise'
 };
 
-function MediaData({ data }: Props) {
+const MediaData = ({ data }: Props) => {
 	const platform = usePlatform();
+	const coordinatesFormat = useThemeStore().coordinatesFormat;
 
 	return data.type === 'Image' ? (
 		<div className="flex flex-col gap-0 py-2">
@@ -78,9 +82,7 @@ function MediaData({ data }: Props) {
 				<MetaData
 					label="Location"
 					tooltipValue={
-						data.location
-							? `${data.location.latitude}, ${data.location.longitude}`
-							: '--'
+						data.location ? formatLocation(data.location, coordinatesFormat, 8) : '--'
 					}
 					value={
 						data.location ? (
@@ -90,12 +92,12 @@ function MediaData({ data }: Props) {
 									if (data.location)
 										platform.openLink(
 											`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
-												`${data.location.latitude},${data.location.longitude}`
-											)}}`
+												`${data.location.latitude}, ${data.location.longitude}`
+											)}`
 										);
 								}}
 							>
-								{formatLocationDD(data.location, 3)}
+								{formatLocation(data.location, coordinatesFormat, 4)}
 							</a>
 						) : (
 							'--'
@@ -147,6 +149,6 @@ function MediaData({ data }: Props) {
 			</Accordion>
 		</div>
 	) : null;
-}
+};
 
 export default MediaData;
