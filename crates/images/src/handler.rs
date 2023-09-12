50afdc7f64f01eeaf67f9a7b3dc119faa6344ab0
brxken128 <77554505+brxken128@users.noreply.github.com>
2023-09-12 20:25:13 +0100

    `get_ext()` inlined helper fn

--- crates/images/src/handler.rs
+++ crates/images/src/handler.rs
@@ -15,19 +15,18 @@ use std::{
 use crate::heif::HeifHandler;
 
 pub fn format_image(path: impl AsRef<Path>) -> Result<DynamicImage> {
-	let ext = path
-		.as_ref()
-		.extension()
-		.map_or_else(|| Err(Error::NoExtension), |e| Ok(e.to_ascii_lowercase()))?;
-	match_to_handler(&ext)?.handle_image(path.as_ref())
+	match_to_handler(&get_ext(&path)?)?.handle_image(path.as_ref())
 }
 
 pub fn convert_image(path: impl AsRef<Path>, desired_ext: &OsStr) -> Result<DynamicImage> {
-	let ext = path
-		.as_ref()
+	match_to_handler(&get_ext(&path)?)?.convert_image(match_to_handler(desired_ext)?, path.as_ref())
+}
+
+#[inline]
+fn get_ext(path: impl AsRef<Path>) -> Result<OsString> {
+	path.as_ref()
 		.extension()
-		.map_or_else(|| Err(Error::NoExtension), |e| Ok(e.to_ascii_lowercase()))?;
-	match_to_handler(&ext)?.convert_image(match_to_handler(desired_ext)?, path.as_ref())
+		.map_or_else(|| Err(Error::NoExtension), |e| Ok(e.to_ascii_lowercase()))
 }
 
 #[allow(clippy::useless_let_if_seq)]
