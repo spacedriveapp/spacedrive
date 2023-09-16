b6f31be1d5684ac08aabadfece5ce52ce4189d78
brxken128 <77554505+brxken128@users.noreply.github.com>
2023-09-16 04:35:42 +0100

    remove useless schema

--- packages/client/src/hooks/useUnitFormatStore.tsx
+++ packages/client/src/hooks/useUnitFormatStore.tsx
@@ -6,19 +6,13 @@ export type CoordinatesFormat = 'dms' | 'dd';
 export type DistanceFormat = 'km' | 'miles';
 export type TemperatureFormat = 'celsius' | 'fahrenheit';
 
-interface Schema {
-	coordinatesFormat: CoordinatesFormat;
-	distanceFormat: DistanceFormat;
-	temperatureFormat: TemperatureFormat;
-}
-
-const unitFormatStore = valtioPersist<Schema>('sd-display-units', {
+const unitFormatStore = valtioPersist('sd-display-units', {
 	// these are the defaults as 99% of users would want to see them this way
 	// if the `en-US` locale is detected during onboarding, the distance/temp are changed to freedom units
-	coordinatesFormat: 'dms',
-	distanceFormat: 'km',
-	temperatureFormat: 'celsius'
-} satisfies Schema);
+	coordinatesFormat: 'dms' as CoordinatesFormat,
+	distanceFormat: 'km' as DistanceFormat,
+	temperatureFormat: 'celsius' as TemperatureFormat
+});
 
 export function useUnitFormatStore() {
 	return useSnapshot(unitFormatStore);
