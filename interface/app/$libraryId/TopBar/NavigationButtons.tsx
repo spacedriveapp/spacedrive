789878bb3c9649c516ee3fa8f71f1d3d4f948e77
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2023-09-19 18:45:52 +0300

    [ENG-1125] Mouse navigation support (#1359)
    Mouse navigation support

--- interface/app/$libraryId/TopBar/NavigationButtons.tsx
+++ interface/app/$libraryId/TopBar/NavigationButtons.tsx
@@ -1,4 +1,5 @@
 import { ArrowLeft, ArrowRight } from '@phosphor-icons/react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router';
 import { Tooltip } from '@sd/ui';
 import { useKeyMatcher, useSearchStore } from '~/hooks';
@@ -10,6 +11,22 @@ export const NavigationButtons = () => {
 	const { isFocused } = useSearchStore();
 	const idx = history.state.idx as number;
 	const controlIcon = useKeyMatcher('Meta').icon;
+
+	useEffect(() => {
+		const onMouseDown = (e: MouseEvent) => {
+			e.preventDefault();
+			e.stopPropagation();
+			if (e.buttons === 8) {
+				if (idx === 0 || isFocused) return;
+				navigate(-1);
+			} else if (e.buttons === 16) {
+				if (idx === history.length - 1 || isFocused) return;
+				navigate(1);
+			}
+		};
+		window.addEventListener('mousedown', onMouseDown);
+		return () => window.removeEventListener('mousedown', onMouseDown);
+	}, [navigate, idx, isFocused]);
 
 	return (
 		<div data-tauri-drag-region className="flex">
