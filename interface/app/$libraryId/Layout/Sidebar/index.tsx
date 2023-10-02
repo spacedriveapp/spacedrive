e90996aa8a94552c155693d6ed707d166817805d
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2023-10-02 13:55:26 +0300

    Prevent sidebar scrolling with keyboard

--- interface/app/$libraryId/Layout/Sidebar/index.tsx
+++ interface/app/$libraryId/Layout/Sidebar/index.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { useEffect, useRef } from 'react';
 import { MacTrafficLights } from '~/components';
 import { useOperatingSystem } from '~/hooks';
 
@@ -11,10 +12,22 @@ export default () => {
 	const os = useOperatingSystem();
 	const showControls = window.location.search.includes('showControls');
 
+	//prevent sidebar scrolling with keyboard
+	useEffect(() => {
+		const handleKeyDown = (e: KeyboardEvent) => {
+			const arrows = ['ArrowUp', 'ArrowDown'];
+			if (arrows.includes(e.key)) {
+				e.preventDefault();
+			}
+		};
+		document.addEventListener('keydown', handleKeyDown);
+		return () => document.removeEventListener('keydown', handleKeyDown);
+	}, []);
+
 	return (
 		<div
 			className={clsx(
-				'relative flex min-h-full w-44 shrink-0 grow-0 flex-col gap-2.5 border-r border-sidebar-divider bg-sidebar px-2.5 pb-2 pt-2.5',
+				'relative flex min-h-full w-44 shrink-0 grow-0 flex-col gap-2.5  border-r border-sidebar-divider bg-sidebar px-2.5 pb-2 pt-2.5',
 				macOnly(os, 'bg-opacity-[0.65]')
 			)}
 		>
