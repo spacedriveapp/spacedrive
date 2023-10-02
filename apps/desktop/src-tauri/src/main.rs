d1cd002ad5ac7770479b759d13e2834a79fe6377
Brendan Allan <brendonovich@outlook.com>
2023-10-02 17:33:05 +0800

    use prod as default api url

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -96,7 +96,7 @@ async fn main() -> tauri::Result<()> {
 			Node::new(
 				data_dir,
 				sd_core::Env {
-					api_url: "http://localhost:3000".to_string(),
+					api_url: "https://app.spacedrive.com".to_string(),
 					client_id: CLIENT_ID.to_string(),
 					client_secret: CLIENT_SECRET.to_string(),
 				},
