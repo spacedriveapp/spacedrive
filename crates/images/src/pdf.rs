b53659629f91865fede45a46c6e4d6026ad01563
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-10-03 01:00:11 -0300

    Try to load pdfium lib from absolute path

--- crates/images/src/pdf.rs
+++ crates/images/src/pdf.rs
@@ -1,4 +1,8 @@
-use std::path::Path;
+use std::{
+	borrow::ToOwned,
+	env::current_exe,
+	path::{Path, PathBuf},
+};
 
 use crate::{consts::PDF_RENDER_SIZE, Error::PdfiumBinding, ImageHandler, Result};
 use image::DynamicImage;
@@ -6,24 +10,52 @@ use once_cell::sync::Lazy;
 use pdfium_render::prelude::{PdfPageRenderRotation, PdfRenderConfig, Pdfium};
 use tracing::error;
 
-// This path is relative to the running binary
+// This path must be relative to the running binary
 #[cfg(windows)]
-const BINDING_LOCATION: &str = "./";
+const BINDING_LOCATION: &str = ".";
 #[cfg(unix)]
 const BINDING_LOCATION: &str = if cfg!(target_os = "macos") {
-	"../Frameworks/FFMpeg.framework/Libraries/"
+	"../Frameworks/FFMpeg.framework/Libraries"
 } else {
 	"../lib/spacedrive"
 };
 
 static PDFIUM: Lazy<Option<Pdfium>> = Lazy::new(|| {
-	Pdfium::bind_to_library(Pdfium::pdfium_platform_library_name_at_path({
-		BINDING_LOCATION
-	}))
-	.or_else(|_| Pdfium::bind_to_system_library())
-	.map(Pdfium::new)
-	.map_err(|err| error!("{err:#?}"))
-	.ok()
+	let lib_name = Pdfium::pdfium_platform_library_name();
+	let lib_path = current_exe()
+		.ok()
+		.and_then(|exe_path| {
+			exe_path.parent().and_then(|parent_path| {
+				match parent_path
+					.join(BINDING_LOCATION)
+					.join(&lib_name)
+					.canonicalize()
+				{
+					Ok(lib_path) => lib_path.to_str().map(ToOwned::to_owned),
+					Err(err) => {
+						error!("{err:#?}");
+						None
+					}
+				}
+			})
+		})
+		.unwrap_or_else(|| {
+			#[allow(clippy::expect_used)]
+			PathBuf::from(BINDING_LOCATION)
+				.join(&lib_name)
+				.to_str()
+				.expect("We are converting valid strs to PathBuf then back, it should not fail")
+				.to_owned()
+		});
+
+	Pdfium::bind_to_library(lib_path)
+		.or_else(|err| {
+			error!("{err:#?}");
+			Pdfium::bind_to_system_library()
+		})
+		.map(Pdfium::new)
+		.map_err(|err| error!("{err:#?}"))
+		.ok()
 });
 
 pub struct PdfHandler {}
