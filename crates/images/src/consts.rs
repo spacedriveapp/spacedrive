38ba812bcd63a6cf34ba6095647239d49f22efff
jake <77554505+brxken128@users.noreply.github.com>
2023-10-06 12:54:57 +0100

    add bincode, huge enum and custom ser/de

--- crates/images/src/consts.rs
+++ crates/images/src/consts.rs
@@ -1,5 +1,4 @@
 use std::fmt::Display;
-
 /// The size of 1MiB in bytes
 const MIB: u64 = 1_048_576;
 
@@ -38,6 +37,8 @@ pub const SVG_MAXIMUM_FILE_SIZE: u64 = MIB * 24;
 pub const SVG_TAGRET_PX: f32 = 262_144_f32;
 
 #[cfg_attr(feature = "specta", derive(specta::Type))]
+#[cfg_attr(feature = "bincode", derive(bincode::Encode, bincode::Decode))]
+#[cfg_attr(feature = "serde", serde(rename_all = "lowercase"))]
 #[derive(Debug)]
 pub enum ConvertableExtensions {
 	Bmp,
@@ -69,7 +70,82 @@ pub enum ConvertableExtensions {
 
 impl Display for ConvertableExtensions {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		f.write_str(&format!("{self:?}").to_lowercase())
+		f.write_str(&format!("{self:?}"))
+	}
+}
+
+impl TryFrom<String> for ConvertableExtensions {
+	type Error = crate::Error;
+
+	fn try_from(value: String) -> Result<Self, Self::Error> {
+		let v = value.to_lowercase();
+		match v.as_str() {
+			"bmp" => Ok(Self::Bmp),
+			"dib" => Ok(Self::Dib),
+			"ff" => Ok(Self::Ff),
+			"gif" => Ok(Self::Gif),
+			"ico" => Ok(Self::Ico),
+			"jpg" => Ok(Self::Jpg),
+			"jpeg" => Ok(Self::Jpeg),
+			"png" => Ok(Self::Png),
+			"pnm" => Ok(Self::Pnm),
+			"qoi" => Ok(Self::Qoi),
+			"tga" => Ok(Self::Tga),
+			"icb" => Ok(Self::Icb),
+			"vda" => Ok(Self::Vda),
+			"vst" => Ok(Self::Vst),
+			"tiff" => Ok(Self::Tiff),
+			"tif" => Ok(Self::Tif),
+			"heif" => Ok(Self::Heif),
+			"heifs" => Ok(Self::Heifs),
+			"heic" => Ok(Self::Heic),
+			"heics" => Ok(Self::Heics),
+			"avif" => Ok(Self::Avif),
+			"avci" => Ok(Self::Avci),
+			"avcs" => Ok(Self::Avcs),
+			"svg" => Ok(Self::Svg),
+			"svgz" => Ok(Self::Svgz),
+			_ => Err(crate::Error::Unsupported),
+		}
+	}
+}
+
+#[cfg(feature = "serde")]
+impl serde::Serialize for ConvertableExtensions {
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+	where
+		S: serde::Serializer,
+	{
+		serializer.serialize_str(&self.to_string())
+	}
+}
+
+#[cfg(feature = "serde")]
+struct ExtensionVisitor;
+
+#[cfg(feature = "serde")]
+impl<'de> serde::de::Visitor<'de> for ExtensionVisitor {
+	type Value = ConvertableExtensions;
+
+	fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		formatter.write_str("A valid extension string`")
+	}
+
+	fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+	where
+		E: serde::de::Error,
+	{
+		Self::Value::try_from(v.to_string()).map_err(|e| E::custom(e.to_string()))
+	}
+}
+
+#[cfg(feature = "serde")]
+impl<'de> serde::Deserialize<'de> for ConvertableExtensions {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: serde::Deserializer<'de>,
+	{
+		deserializer.deserialize_str(ExtensionVisitor)
 	}
 }
 
