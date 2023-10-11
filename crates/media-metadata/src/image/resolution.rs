078490cdd54aefb0b9454bfbea4e0f95c0e2a320
jake <77554505+brxken128@users.noreply.github.com>
2023-10-11 01:58:59 +0100

    [ENG-1143] Media View sort by date taken (#1390)
    * clippy -_-
    
    * migrations
    
    * alter the migration so it just renames the `dimensions` field (no db re-creation required)
    
    * remove confusing semver addition for `libheif-sys`
    
    * remove warning on the migration as it's just a rename
    
    * add sort opts for resolution and date image was truly taken
    
    * major serde ckeanup & add epoch_time and pixel_count
    
    * rename, cleanup et optimise
    
    * clippy
    
    * ignore this mess
    
    * bindings
    
    * add explanation to schema
    
    * comment out dt test
    
    * better comment and WIP time
    
    * cleanup rust
    
    * failed timezone attempt
    
    * remove image resolution as a sort by option
    
    * update schema (and rename the dimensions table instead of dropping it)
    
    * just show raw date
    
    * add comments and update bindings
    
    * fix migration hopefully
    
    * fix broken migration
    
    ---------
    
    Co-authored-by: Jamie Pine <32987599+jamiepine@users.noreply.github.com>
    Co-authored-by: Jamie Pine <ijamespine@me.com>
    Co-authored-by: Vítor Vasconcellos <vasconcellos.dev@gmail.com>

--- crates/media-metadata/src/image/resolution.rs
+++ crates/media-metadata/src/image/resolution.rs
@@ -1,5 +1,3 @@
-use std::fmt::Display;
-
 use exif::Tag;
 
 use super::ExifReader;
@@ -7,21 +5,21 @@ use super::ExifReader;
 #[derive(
 	Default, Clone, PartialEq, Eq, Debug, serde::Serialize, serde::Deserialize, specta::Type,
 )]
-pub struct Dimensions {
+pub struct Resolution {
 	pub width: i32,
 	pub height: i32,
 }
 
-impl Dimensions {
+impl Resolution {
 	#[must_use]
 	/// Creates a new width and height container
 	///
 	/// # Examples
 	///
 	/// ```
-	/// use sd_media_metadata::image::Dimensions;
+	/// use sd_media_metadata::image::Resolution;
 	///
-	/// Dimensions::new(1920, 1080);
+	/// Resolution::new(1920, 1080);
 	/// ```
 	pub const fn new(width: i32, height: i32) -> Self {
 		Self { width, height }
@@ -37,11 +35,5 @@ impl Dimensions {
 				.get_tag(Tag::PixelYDimension)
 				.unwrap_or_else(|| reader.get_tag(Tag::YResolution).unwrap_or_default()),
 		}
-	}
-}
-
-impl Display for Dimensions {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		f.write_fmt(format_args!("{}x{}", self.width, self.height))
 	}
 }
