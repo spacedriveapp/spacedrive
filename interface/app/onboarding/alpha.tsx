c5d9fe16b086ef2c1193e0e91a2ff969a1c2b5d1
James Pine <ijamespine@me.com>
2023-10-11 21:21:58 -0700

    remove login from onboarding

--- interface/app/onboarding/alpha.tsx
+++ interface/app/onboarding/alpha.tsx
@@ -40,7 +40,7 @@ export default function OnboardingAlpha() {
 							<Discord className="h-4 w-4 fill-ink" />
 							Join Discord
 						</Button>
-						<ButtonLink to="../login" replace variant="accent">
+						<ButtonLink to="../new-library" replace variant="accent">
 							Continue
 						</ButtonLink>
 					</div>
