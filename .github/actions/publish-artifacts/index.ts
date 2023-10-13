53ef10b16ddd06a0b1043d7b288e861cded7fc47
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-10-12 23:10:50 -0300

    Fix artifact upload hopefully

--- .github/actions/publish-artifacts/index.ts
+++ .github/actions/publish-artifacts/index.ts
@@ -45,7 +45,7 @@ async function run() {
 		if (!standalonePath) throw `Standalone path not found. Files: ${files}`;
 
 		await io.mv(standalonePath, artifactPath);
-		await client.uploadArtifact(ARTIFACT_NAME, [artifactPath], './');
+		await client.uploadArtifact(ARTIFACT_NAME, [artifactPath], ARTIFACTS_DIR);
 
 		if (updaterExt) {
 			const artifactName = `Spacedrive-Updater-${OS}-${ARCH}.${updaterExt}`;
@@ -58,7 +58,11 @@ async function run() {
 			await io.mv(updaterPath, artifactPath);
 			await io.mv(`${updaterPath}.sig`, `${artifactPath}.sig`);
 
-			await client.uploadArtifact(artifactName, [`Spacedrive-Updater-*`], ARTIFACTS_DIR);
+			await client.uploadArtifact(
+				artifactName,
+				[artifactPath, `${artifactPath}.sig`],
+				ARTIFACTS_DIR
+			);
 		}
 	}
 }
