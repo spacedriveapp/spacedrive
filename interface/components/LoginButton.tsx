51d2e78892752cf93d8cd4d3664b278a21777063
Oscar Beaumont <oscar@otbeaumont.me>
2023-10-13 15:12:06 +0800

    Cancel login

--- interface/components/LoginButton.tsx
+++ interface/components/LoginButton.tsx
@@ -5,21 +5,34 @@ import { usePlatform } from '..';
 
 export function LoginButton({ children, ...props }: { onLogin?(): void } & ButtonProps) {
 	const authState = auth.useStateSnapshot();
-
 	const platform = usePlatform();
 
 	return (
-		<Button
-			variant="accent"
-			disabled={authState.status === 'loggingIn'}
-			onClick={async () => {
-				await auth.login(platform.auth);
+		<div className="flex flex-col items-center justify-center">
+			<Button
+				variant="accent"
+				disabled={authState.status === 'loggingIn'}
+				onClick={async () => {
+					await auth.login(platform.auth);
 
-				props.onLogin?.();
-			}}
-			{...props}
-		>
-			{authState.status !== 'loggingIn' ? children || 'Log in' : 'Logging In...'}
-		</Button>
+					props.onLogin?.();
+				}}
+				{...props}
+			>
+				{authState.status !== 'loggingIn' ? children || 'Log in' : 'Logging In...'}
+			</Button>
+			{authState.status === 'loggingIn' && (
+				<a
+					href="#"
+					onClick={(e) => {
+						e.preventDefault();
+						auth.cancel();
+					}}
+					className="mt-2 text-sm text-gray-200"
+				>
+					Cancel
+				</a>
+			)}
+		</div>
 	);
 }
