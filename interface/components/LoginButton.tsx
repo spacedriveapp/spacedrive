7338d7f97f620c951c45bc1e7c82605dbf84364c
Oscar Beaumont <oscar@otbeaumont.me>
2023-10-13 18:39:26 +1100

    Cancel login (#1562)
    * Cancel login
    
    * Fix light mode styling
    
    * use `text-ink` + `button`
    
    * better

--- interface/components/LoginButton.tsx
+++ interface/components/LoginButton.tsx
@@ -5,21 +5,32 @@ import { usePlatform } from '..';
 
 export function LoginButton({ children, ...props }: { onLogin?(): void } & ButtonProps) {
 	const authState = auth.useStateSnapshot();
-
 	const platform = usePlatform();
 
 	return (
-		<Button
-			variant="accent"
-			disabled={authState.status === 'loggingIn'}
-			onClick={async () => {
-				await auth.login(platform.auth);
-
-				props.onLogin?.();
-			}}
-			{...props}
-		>
-			{authState.status !== 'loggingIn' ? children || 'Log in' : 'Logging In...'}
-		</Button>
+		<div className="flex flex-col items-center justify-center">
+			<Button
+				variant="accent"
+				disabled={authState.status === 'loggingIn'}
+				onClick={async () => {
+					await auth.login(platform.auth);
+					props.onLogin?.();
+				}}
+				{...props}
+			>
+				{authState.status !== 'loggingIn' ? children || 'Log in' : 'Logging In...'}
+			</Button>
+			{authState.status === 'loggingIn' && (
+				<button
+					onClick={(e) => {
+						e.preventDefault();
+						auth.cancel();
+					}}
+					className="mt-2 text-sm text-ink-faint"
+				>
+					Cancel
+				</button>
+			)}
+		</div>
 	);
 }
