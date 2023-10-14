ee6b497671e5018cfd68dd9e6180de817bb51a53
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2023-10-14 18:27:21 +0300

    copy paste shortcut

--- interface/hooks/index.ts
+++ interface/hooks/index.ts
@@ -21,3 +21,4 @@ export * from './useZodRouteParams';
 export * from './useZodSearchParams';
 export * from './useIsTextTruncated';
 export * from './useKeyMatcher';
+export * from './useKeyCopyPaste';
