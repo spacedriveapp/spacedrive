75b2a1383ab3cae1de183cbbcae4506c7de43f93
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2023-10-18 11:32:25 +0300

    OS check for mouse navigate (#1621)
    Update useMouseNavigate.ts

--- interface/hooks/useMouseNavigate.ts
+++ interface/hooks/useMouseNavigate.ts
@@ -1,22 +1,24 @@
-import { useNavigate } from "react-router";
-import { useSearchStore } from "~/hooks";
-import { MouseEvent } from "react";
-
+import { MouseEvent } from 'react';
+import { useNavigate } from 'react-router';
+import { useOperatingSystem, useSearchStore } from '~/hooks';
 
 export const useMouseNavigate = () => {
 	const idx = history.state.idx as number;
 	const navigate = useNavigate();
-	const {isFocused} = useSearchStore();
+	const { isFocused } = useSearchStore();
+	const os = useOperatingSystem();
+	const realOs = useOperatingSystem(true);
 
 	const handler = (e: MouseEvent) => {
-			if (e.buttons === 8) {
-					if (idx === 0 || isFocused) return;
-					navigate(-1);
-				} else if (e.buttons === 16) {
-					if (idx === history.length - 1 || isFocused) return;
-					navigate(1);
-				}
-			}
+		if (os !== 'browser' || realOs !== 'macOS') return;
+		if (e.buttons === 8) {
+			if (idx === 0 || isFocused) return;
+			navigate(-1);
+		} else if (e.buttons === 16) {
+			if (idx === history.length - 1 || isFocused) return;
+			navigate(1);
+		}
+	};
 
 	return handler;
-}
+};
