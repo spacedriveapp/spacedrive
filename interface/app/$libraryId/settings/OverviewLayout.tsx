3def9047608f386d84a9b409683d6fbd43ab1225
jake <77554505+brxken128@users.noreply.github.com>
2023-10-20 08:30:33 +0100

    make the settings page static and `pt-6`

--- interface/app/$libraryId/settings/OverviewLayout.tsx
+++ interface/app/$libraryId/settings/OverviewLayout.tsx
@@ -1,25 +1,10 @@
-import clsx from 'clsx';
 import { Outlet } from 'react-router';
-import { useOperatingSystem } from '~/hooks';
-import { useWindowState } from '~/hooks/useWindowState';
 
-export const Component = () => {
-	const os = useOperatingSystem();
-	const windowState = useWindowState();
-
-	return (
-		<div
-			className={clsx(
-				'custom-scroll page-scroll relative flex h-full max-h-screen w-full grow-0 transition-[padding-top] ease-linear',
-				os === 'macOS' && windowState.isMaximized
-					? 'pt-4 duration-200'
-					: 'pt-8 duration-100'
-			)}
-		>
-			<div className="flex w-full max-w-4xl flex-col space-y-6 px-12 pb-5 pt-2">
-				<Outlet />
-				<div className="block h-4 shrink-0" />
-			</div>
+export const Component = () => (
+	<div className="custom-scroll page-scroll relative flex h-full max-h-screen w-full grow-0 pt-6">
+		<div className="flex w-full max-w-4xl flex-col space-y-6 px-12 pb-5 pt-2">
+			<Outlet />
+			<div className="block h-4 shrink-0" />
 		</div>
-	);
-};
+	</div>
+);
