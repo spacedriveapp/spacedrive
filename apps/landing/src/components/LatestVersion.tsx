798c86e2b5e6564f0b2ab17f39c504643db2dd4f
Brendan Allan <brendonovich@outlook.com>
2023-10-20 13:22:44 +0800

    remove LatestVersion

--- apps/landing/src/components/LatestVersion.tsx
+++
@@ -1,7 +0,0 @@
-import { getLatestRelease, githubFetch } from "~/app/api/github";
-
-export async function LatestVersion() {
-    const release = await githubFetch(getLatestRelease);
-    
-    return (<>Alpha v{ release.tag_name }</>)
-}
