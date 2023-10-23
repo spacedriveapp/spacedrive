f0ec90d1c95c34e0a0f91a97904fd387ec782557
Tilo K <tiloklarenbeek@gmail.com>
2023-10-23 17:49:34 +0200

    [ENG-860] Added file counts to deletemenu via shortcut

--- interface/hooks/useKeyDeleteFile.tsx
+++ interface/hooks/useKeyDeleteFile.tsx
@@ -2,6 +2,7 @@ import { useKey } from 'rooks';
 import type { ExplorerItem } from '@sd/client';
 import { dialogManager } from '@sd/ui';
 import DeleteDialog from '~/app/$libraryId/Explorer/FilePath/DeleteDialog';
+import { dir } from 'console';
 
 export const useKeyDeleteFile = (selectedItems: Set<ExplorerItem>, locationId?: number | null) => {
 	return useKey('Delete', (e) => {
@@ -10,13 +11,20 @@ export const useKeyDeleteFile = (selectedItems: Set<ExplorerItem>, locationId?: 
 		if (!locationId) return;
 
 		const pathIds: number[] = [];
+		let dirCount = 0;
+		let fileCount = 0;
 
 		for (const item of selectedItems) {
-			if (item.type === 'Path') pathIds.push(item.item.id);
+			if (item.type === 'Path'){
+				 pathIds.push(item.item.id);
+
+				 dirCount += item.item.is_dir ? 1 : 0;
+				 fileCount += item.item.is_dir ? 0 : 1;
+			}
 		}
 
 		dialogManager.create((dp) => (
-			<DeleteDialog {...dp} locationId={locationId} pathIds={pathIds} />
+			<DeleteDialog {...dp} locationId={locationId} pathIds={pathIds} dirCount={dirCount} fileCount={fileCount}/>
 		));
 	});
 };
