c908ca81ca27ba2d69798ab28a3ab1059737779b
jake <77554505+brxken128@users.noreply.github.com>
2023-10-28 06:10:25 +0100

    dynamically-updated app manifest

---
+++ apps/landing/src/app/manifest.tsx
@@ -0,0 +1,21 @@
+import { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+	return {
+		name: 'Spacedrive',
+		theme_color: '#E751ED',
+		start_url: '/',
+		icons: [
+			{
+				src: '/images/logo-192x192.png',
+				sizes: '192x192',
+				type: 'image/png'
+			},
+			{
+				src: '/images/logo-512x512.png',
+				sizes: 'any',
+				type: 'image/png'
+			}
+		]
+	};
+}
