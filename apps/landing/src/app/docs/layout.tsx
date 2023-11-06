8a4281edd28338bd4b28b3b33fa9fb3a19e79a05
Utku <74243531+utkubakir@users.noreply.github.com>
2023-11-06 19:30:35 +0300

    Fix search bar (#1749)
    layout messing scss order somehow

--- apps/landing/src/app/docs/layout.tsx
+++ apps/landing/src/app/docs/layout.tsx
@@ -8,9 +8,6 @@ import {
 } from './Sidebar/MobileSidebar';
 
 import 'katex/dist/katex.min.css';
-import '@docsearch/css';
-// This must be imported after the docsearch css
-import '~/styles/search.scss';
 
 import { Breadcrumbs } from './Breadcrumbs';
 
