34ee178d2a3dcac1cfb9706a498f69d78ded343d
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2023-11-08 15:57:43 -0500

    fix

--- apps/landing/src/components/mdx/index.tsx
+++ apps/landing/src/components/mdx/index.tsx
@@ -1,8 +1,13 @@
+import { MDXComponents } from 'mdx/types';
 import NextImage, { ImageProps } from 'next/image';
 
 import Notice from './Notice';
 
 const Image = (props: ImageProps) => <NextImage {...props} />;
 
-export const BlogMDXComponents = { Image };
-export const DocMDXComponents = { Image, Notice };
+export const BlogMDXComponents = {
+	img: Image, // we remap 'img' to 'Image'
+	Image
+} as MDXComponents;
+
+export const DocMDXComponents = { img: Image, Image, Notice } as MDXComponents;
