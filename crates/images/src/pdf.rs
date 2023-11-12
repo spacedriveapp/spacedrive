96cda576bce2a7ca23d5b1e7d607188db4c6de29
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-11-12 19:47:10 -0300

    [ENG-1359] Revert "Fix pdf thumb crashing app (#1460)" (#1766)
    Revert "[ENG-1220] Fix pdf thumb crashing app (#1460)"
    
    This reverts commit 9be7d93e98e39cc42b341aec77ec453db4673c55.

--- crates/images/src/pdf.rs
+++ crates/images/src/pdf.rs
@@ -6,6 +6,7 @@ use std::{
 
 use crate::{consts::PDF_RENDER_WIDTH, Error::PdfiumBinding, ImageHandler, Result};
 use image::DynamicImage;
+use once_cell::sync::Lazy;
 use pdfium_render::prelude::{PdfPageRenderRotation, PdfRenderConfig, Pdfium};
 use tracing::error;
 
@@ -19,64 +20,59 @@ const BINDING_LOCATION: &str = if cfg!(target_os = "macos") {
 	"../lib/spacedrive"
 };
 
-// FIX-ME: This is slow, but using Lazy with thread_safe was causing concurrency bugs that crashed the app
-thread_local! {
-	static PDFIUM: Option<Pdfium> = {
-		let lib_name = Pdfium::pdfium_platform_library_name();
-		let lib_path = current_exe()
-			.ok()
-			.and_then(|exe_path| {
-				exe_path.parent().and_then(|parent_path| {
-					match parent_path
-						.join(BINDING_LOCATION)
-						.join(&lib_name)
-						.canonicalize()
-					{
-						Ok(lib_path) => lib_path.to_str().map(ToOwned::to_owned),
-						Err(err) => {
-							error!("{err:#?}");
-							None
-						}
-					}
-				})
-			})
-			.unwrap_or_else(|| {
-				#[allow(clippy::expect_used)]
-				PathBuf::from(BINDING_LOCATION)
+static PDFIUM: Lazy<Option<Pdfium>> = Lazy::new(|| {
+	let lib_name = Pdfium::pdfium_platform_library_name();
+	let lib_path = current_exe()
+		.ok()
+		.and_then(|exe_path| {
+			exe_path.parent().and_then(|parent_path| {
+				match parent_path
+					.join(BINDING_LOCATION)
 					.join(&lib_name)
-					.to_str()
-					.expect("We are converting valid strs to PathBuf then back, it should not fail")
-					.to_owned()
-			});
-
-		Pdfium::bind_to_library(lib_path)
-			.or_else(|err| {
-				error!("{err:#?}");
-				Pdfium::bind_to_system_library()
+					.canonicalize()
+				{
+					Ok(lib_path) => lib_path.to_str().map(ToOwned::to_owned),
+					Err(err) => {
+						error!("{err:#?}");
+						None
+					}
+				}
 			})
-			.map(Pdfium::new)
-			.map_err(|err| error!("{err:#?}"))
-			.ok()
-	};
-}
+		})
+		.unwrap_or_else(|| {
+			#[allow(clippy::expect_used)]
+			PathBuf::from(BINDING_LOCATION)
+				.join(&lib_name)
+				.to_str()
+				.expect("We are converting valid strs to PathBuf then back, it should not fail")
+				.to_owned()
+		});
+
+	Pdfium::bind_to_library(lib_path)
+		.or_else(|err| {
+			error!("{err:#?}");
+			Pdfium::bind_to_system_library()
+		})
+		.map(Pdfium::new)
+		.map_err(|err| error!("{err:#?}"))
+		.ok()
+});
 
 pub struct PdfHandler {}
 
 impl ImageHandler for PdfHandler {
 	fn handle_image(&self, path: &Path) -> Result<DynamicImage> {
-		PDFIUM.with(|maybe_pdfium| {
-			let pdfium = maybe_pdfium.as_ref().ok_or(PdfiumBinding)?;
+		let pdfium = PDFIUM.as_ref().ok_or(PdfiumBinding)?;
 
-			let render_config = PdfRenderConfig::new()
-				.set_target_width(PDF_RENDER_WIDTH)
-				.rotate_if_landscape(PdfPageRenderRotation::Degrees90, true);
+		let render_config = PdfRenderConfig::new()
+			.set_target_width(PDF_RENDER_WIDTH)
+			.rotate_if_landscape(PdfPageRenderRotation::Degrees90, true);
 
-			Ok(pdfium
-				.load_pdf_from_file(path, None)?
-				.pages()
-				.first()?
-				.render_with_config(&render_config)?
-				.as_image())
-		})
+		Ok(pdfium
+			.load_pdf_from_file(path, None)?
+			.pages()
+			.first()?
+			.render_with_config(&render_config)?
+			.as_image())
 	}
 }
