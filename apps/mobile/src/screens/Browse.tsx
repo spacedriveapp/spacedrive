c8dbf715fd28cd26fad5ccbe6df0a89f42521542
Utku <74243531+utkubakir@users.noreply.github.com>
2023-11-22 03:49:52 +0300

    Mobile changes (#1784)
    * privacy link
    
    * remove spacedrop screens, add browse
    
    * icon
    
    * changes and network screen
    
    * bump react native

---
+++ apps/mobile/src/screens/Browse.tsx
@@ -0,0 +1,6 @@
+import { View } from 'react-native';
+import { BrowseStackScreenProps } from '~/navigation/tabs/BrowseStack';
+
+export default function BrowseScreen({ navigation, route }: BrowseStackScreenProps<'Browse'>) {
+	return <View></View>;
+}
