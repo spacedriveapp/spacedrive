4c967f3e76fd6480a9485362ac2c69019f943b7b
jake <77554505+brxken128@users.noreply.github.com>
2023-11-23 15:54:43 +0000

    old errors

--- crates/fda/src/error.rs
+++ crates/fda/src/error.rs
@@ -1,12 +1,5 @@
-// use std::path::PathBuf;
-
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-	// #[error("unable to access path: {0}")]
-	// PermissionDenied(PathBuf),
-
-	// #[error("the path provided is invalid and likely doesn't exist: \"{0}\"")]
-	// PathInvalid(PathBuf),
 	#[cfg(target_os = "macos")]
 	#[error("there was an error while prompting for full disk access")]
 	FDAPromptError,
