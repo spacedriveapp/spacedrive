21a279cecdac778c78e181e50624c6d3c8336a2c
Brendan Allan <brendonovich@outlook.com>
2023-12-21 13:58:03 +0800

    [ENG-1512] Replace sync operation types with single type (#1900)
    * replace multiple op types with crdt_operation
    
    * cleanup

--- core/src/p2p/sync/mod.rs
+++ core/src/p2p/sync/mod.rs
@@ -70,11 +70,9 @@ mod originator {
 					instance: Uuid::new_v4(),
 					timestamp: sync::NTP64(0),
 					id: Uuid::new_v4(),
-					typ: sd_sync::CRDTOperationType::Shared(sd_sync::SharedOperation {
-						record_id: serde_json::Value::Null,
-						model: "name".to_string(),
-						data: sd_sync::SharedOperationData::Create,
-					}),
+					record_id: serde_json::Value::Null,
+					model: "name".to_string(),
+					data: sd_sync::CRDTOperationData::Create,
 				}]);
 
 				let mut cursor = std::io::Cursor::new(original.to_bytes());
