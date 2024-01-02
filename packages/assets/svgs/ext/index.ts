f346072347e84b4afc9a2a8fea43156a10c5fd1d
Oscar Beaumont <oscar@otbeaumont.me>
2024-01-02 10:59:48 +0800

    fix types

--- packages/assets/svgs/ext/index.ts
+++ packages/assets/svgs/ext/index.ts
@@ -1,11 +1,11 @@
 /*
  * This file exports a object which contains Different Kinds of Icons.
  */
-import { type FC as FunctionComponent, type SVGProps } from 'react';
+import { type FC as FunctionComponent, type LazyExoticComponent, type SVGProps } from 'react';
 
 import * as Code from './Code';
 import * as Extras from './Extras';
 
 export const LayeredIcons: Partial<
-	Record<string, Record<string, FunctionComponent<SVGProps<SVGSVGElement>>>>
+	Record<string, Record<string, LazyExoticComponent<FunctionComponent<SVGProps<SVGSVGElement>>>>>
 > = { Code, Extras };
