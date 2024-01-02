c5843523c8898f6e157bb8cbe59e403cd0c00c19
Oscar Beaumont <oscar@otbeaumont.me>
2024-01-02 17:32:02 +0800

    Okay so HMR does work

--- interface/app/demo.solid.tsx
+++ interface/app/demo.solid.tsx
@@ -3,6 +3,8 @@
 import { createSignal } from 'solid-js';
 import { render } from 'solid-js/web';
 
+import { Demo2 } from './demo2.solid';
+
 function Demo() {
 	const [count, setCount] = createSignal(0);
 
@@ -10,6 +12,7 @@ function Demo() {
 		<div class="absolute top-0 z-[99999] bg-red-500">
 			<button onClick={() => setCount(count() + 1)}>Click me</button>
 			<div>Hello from Solid: {count()}</div>
+			<Demo2 />
 		</div>
 	);
 }
---
+++ interface/app/demo2.solid.tsx
@@ -0,0 +1,5 @@
+/** @jsxImportSource solid-js */
+
+export function Demo2() {
+	return <h1>Hello</h1>;
+}
