7078bc05597ec2a6445cc13c8aa97f0773b05ee1
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2024-01-11 01:28:35 +0300

    [MOB-50] Locations screen (#1928)
    locations screen

--- apps/mobile/src/components/header/Header.tsx
+++ apps/mobile/src/components/header/Header.tsx
@@ -1,25 +1,51 @@
 import { useNavigation } from '@react-navigation/native';
-import { MagnifyingGlass } from 'phosphor-react-native';
+import { ArrowLeft, MagnifyingGlass } from 'phosphor-react-native';
 import { lazy } from 'react';
 import { Pressable, Text, View } from 'react-native';
 import { tw } from '~/lib/tailwind';
 
-//Not all pages have a library manager - so lazy load it for performance
+//Not all pages use these components - so we lazy load for performance
 const BrowseLibraryManager = lazy(() => import('../browse/DrawerLibraryManager'));
+const Search = lazy(() => import('../inputs/Search'));
 
 interface Props {
-	title?: string;
-	showLibrary?: boolean;
+	title?: string; //title of the page
+	showLibrary?: boolean; //show the library manager
+	searchType?: 'explorer' | 'location'; //Temporary
+	navBack?: boolean; //navigate back to the previous screen
 }
 
 // Default header with search bar and button to open drawer
-export default function Header({ title, showLibrary }: Props) {
+export default function Header({ title, showLibrary, searchType, navBack }: Props) {
 	const navigation = useNavigation();
+
+	const SearchType = () => {
+		switch (searchType) {
+			case 'explorer':
+				return 'Explorer'; //TODO
+			case 'location':
+				return <Search placeholder="Location name..." />;
+			default:
+				return null;
+		}
+	};
+
 	return (
-		<View style={tw`relative h-fit w-full border-b border-app-line/50 bg-mobile-header pt-10`}>
-			<View style={tw`mx-auto mt-5 h-fit w-full justify-center px-7 pb-5`}>
-				<View style={tw`w-full flex-row items-center justify-between`}>
-					<Text style={tw`text-[24px] font-bold text-white`}>{title}</Text>
+		<View style={tw`relative w-full pt-10 border-b h-fit border-app-line/50 bg-mobile-header`}>
+			<View style={tw`justify-center w-full pb-5 mx-auto mt-5 h-fit px-7`}>
+				<View style={tw`flex-row items-center justify-between w-full`}>
+					<View style={tw`flex-row items-center gap-5`}>
+						{navBack && (
+							<Pressable
+								onPress={() => {
+									navigation.goBack();
+								}}
+							>
+								<ArrowLeft size={23} color={tw.color('ink')} />
+							</Pressable>
+						)}
+						<Text style={tw`text-[24px] font-bold text-white`}>{title}</Text>
+					</View>
 					<Pressable onPress={() => navigation.navigate('Search')}>
 						<MagnifyingGlass
 							size={20}
@@ -29,6 +55,7 @@ export default function Header({ title, showLibrary }: Props) {
 					</Pressable>
 				</View>
 				{showLibrary && <BrowseLibraryManager style="mt-4" />}
+				{searchType && <SearchType />}
 			</View>
 		</View>
 	);
---
+++ apps/mobile/src/stores/searchStore.ts
@@ -0,0 +1,24 @@
+import { proxy, useSnapshot } from "valtio";
+
+
+/**
+ This is subject to change, but the idea is to have a global store for search
+ that can be used by any component.
+
+ once the new mobile design is implemented this is likely to change
+ */
+
+const searchStore = proxy({
+	search: '',
+	setSearch: (search: string) => {
+		searchStore.search = search;
+	}
+})
+
+export function useSearchStore() {
+	return useSnapshot(searchStore);
+}
+
+export function getSearchStore() {
+	return searchStore;
+}
