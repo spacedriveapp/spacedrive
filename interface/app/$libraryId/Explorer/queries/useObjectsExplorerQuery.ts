45c8d714ad019fcaa31b8a763a289aba6572f727
Brendan Allan <brendonovich@outlook.com>
2024-01-11 17:15:21 +0800

    use offset pagination

--- interface/app/$libraryId/Explorer/queries/useObjectsExplorerQuery.ts
+++ interface/app/$libraryId/Explorer/queries/useObjectsExplorerQuery.ts
@@ -2,13 +2,13 @@ import { ObjectOrder, ObjectSearchArgs, useLibraryQuery } from '@sd/client';
 
 import { UseExplorerSettings } from '../useExplorer';
 import { useExplorerQuery } from './useExplorerQuery';
-import { useObjectsInfiniteQuery } from './useObjectsInfiniteQuery';
+import { useObjectsOffsetInfiniteQuery } from './useObjectsOffsetInfiniteQuery';
 
 export function useObjectsExplorerQuery(props: {
 	arg: ObjectSearchArgs;
 	explorerSettings: UseExplorerSettings<ObjectOrder>;
 }) {
-	const query = useObjectsInfiniteQuery(props);
+	const query = useObjectsOffsetInfiniteQuery(props);
 
 	const count = useLibraryQuery(['search.objectsCount', { filters: props.arg.filters }], {
 		enabled: query.isSuccess
