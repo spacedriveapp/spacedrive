b8312bb95ec8f35a8811420d0371de3eecfdc81a
jake <77554505+brxken128@users.noreply.github.com>
2024-01-20 15:50:34 +0000

    chore: iterate over paths

--- core/src/util/clear_localstorage.rs
+++ core/src/util/clear_localstorage.rs
@@ -2,6 +2,11 @@ use directories::BaseDirs;
 use tokio::fs;
 use tracing::{info, warn};
 
+#[cfg(target_os = "macos")]
+const EXTRA_DIRS: [&str; 2] = ["Library/WebKit/Spacedrive", "Library/Caches/Spacedrive"];
+#[cfg(target_os = "linux")]
+const EXTRA_DIRS: [&str; 1] = [".cache/spacedrive"];
+
 pub async fn clear_localstorage() {
 	if let Some(base_dirs) = BaseDirs::new() {
 		let data_dir = BaseDirs::data_dir(&base_dirs).join("com.spacedrive.desktop"); // app identifier, maybe tie this into something?
@@ -11,25 +16,17 @@ pub async fn clear_localstorage() {
 			.map_err(|_| warn!("Unable to delete the localstorage directory"))
 			.ok();
 
-		#[cfg(target_os = "macos")]
-		fs::remove_dir_all(BaseDirs::home_dir(&base_dirs).join("Library/WebKit/Spacedrive"))
-			.await
-			.map_err(|_| warn!("Unable to delete the WebKit localstorage directory"))
-			.ok();
+		#[cfg(any(target_os = "macos", target_os = "linux"))]
+		for path in EXTRA_DIRS {
+			fs::remove_dir_all(BaseDirs::home_dir(&base_dirs).join(path))
+				.await
+				.map_err(|_| warn!("Unable to delete {path}"))
+				.ok();
 
-		#[cfg(target_os = "macos")]
-		fs::remove_dir_all(BaseDirs::home_dir(&base_dirs).join("Library/Caches/Spacedrive"))
-			.await
-			.map_err(|_| warn!("Unable to delete the Spacedrive cache directory"))
-			.ok();
+			info!("Deleted {path}");
+		}
 
-		#[cfg(target_os = "linux")]
-		fs::remove_dir_all(BaseDirs::home_dir(&base_dirs).join(".cache/spacedrive"))
-			.await
-			.map_err(|_| warn!("Unable to delete the Spacedrive cache directory"))
-			.ok();
-
-		info!("Cleared localstorage successfully")
+		info!("Cleared localstorage fully")
 	} else {
 		warn!("Unable to source the base directories in order to clear localstorage")
 	}
