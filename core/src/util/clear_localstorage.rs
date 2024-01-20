d74b604b29c59bd4fef8987cc2b55edca1f30eb9
jake <77554505+brxken128@users.noreply.github.com>
2024-01-20 15:16:33 +0000

    feat: delete cache directories also

--- core/src/util/clear_localstorage.rs
+++ core/src/util/clear_localstorage.rs
@@ -12,12 +12,21 @@ pub async fn clear_localstorage() {
 			.ok();
 
 		#[cfg(target_os = "macos")]
-		let webkit_dir = BaseDirs::home_dir(&base_dirs).join("Library/WebKit/Spacedrive"); // macos stores some localstorage stuff here
-
-		#[cfg(target_os = "macos")]
-		fs::remove_dir_all(webkit_dir)
+		fs::remove_dir_all(BaseDirs::home_dir(&base_dirs).join("Library/WebKit/Spacedrive"))
 			.await
 			.map_err(|_| warn!("Unable to delete the WebKit localstorage directory"))
+			.ok();
+
+		#[cfg(target_os = "macos")]
+		fs::remove_dir_all(BaseDirs::home_dir(&base_dirs).join("Library/Caches/Spacedrive"))
+			.await
+			.map_err(|_| warn!("Unable to delete the Spacedrive cache directory"))
+			.ok();
+
+		#[cfg(target_os = "linux")]
+		fs::remove_dir_all(BaseDirs::home_dir(&base_dirs).join(".cache/spacedrive"))
+			.await
+			.map_err(|_| warn!("Unable to delete the Spacedrive cache directory"))
 			.ok();
 
 		info!("Cleared localstorage successfully")
