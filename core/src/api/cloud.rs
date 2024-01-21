93c904831a267aa489ef4903485a06743e182f18
Julian Braha <julianbraha@gmail.com>
2024-01-21 12:49:59 +0000

    Reuse aws s3 client across procedure calls

--- core/src/api/cloud.rs
+++ core/src/api/cloud.rs
@@ -4,6 +4,11 @@ use reqwest::Response;
 use rspc::alpha::AlphaRouter;
 use serde::de::DeserializeOwned;
 
+use aws_config::{Region, SdkConfig};
+use aws_credential_types::provider::future;
+use aws_sdk_s3::config::{Credentials, ProvideCredentials, SharedCredentialsProvider};
+use once_cell::sync::OnceCell;
+
 use uuid::Uuid;
 
 use super::{utils::library, Ctx, R};
@@ -14,6 +19,44 @@ async fn parse_json_body<T: DeserializeOwned>(response: Response) -> Result<T, r
 		rspc::Error::new(
 			rspc::ErrorCode::InternalServerError,
 			"JSON conversion failed".to_string(),
+		)
+	})
+}
+
+#[derive(Debug)]
+pub struct CredentialsProvider(sd_cloud_api::locations::authorise::Response);
+
+impl ProvideCredentials for CredentialsProvider {
+	fn provide_credentials<'a>(&'a self) -> future::ProvideCredentials<'a>
+	where
+		Self: 'a,
+	{
+		future::ProvideCredentials::ready(Ok(Credentials::new(
+			self.0.access_key_id.clone(),
+			self.0.secret_access_key.clone(),
+			Some(self.0.session_token.clone()),
+			None, // TODO: Get this from the SD Cloud backend
+			"sd-cloud",
+		)))
+	}
+
+	fn fallback_on_interrupt(&self) -> Option<Credentials> {
+		None
+	}
+}
+
+static AWS_S3_CLIENT: OnceCell<aws_sdk_s3::Client> = OnceCell::new();
+
+// Reuse the client between procedure calls
+fn get_aws_s3_client(
+	token: sd_cloud_api::locations::authorise::Response,
+) -> &'static aws_sdk_s3::Client {
+	AWS_S3_CLIENT.get_or_init(|| {
+		aws_sdk_s3::Client::new(
+			&SdkConfig::builder()
+				.region(Region::new("us-west-1")) // TODO: From cloud config
+				.credentials_provider(SharedCredentialsProvider::new(CredentialsProvider(token)))
+				.build(),
 		)
 	})
 }
@@ -120,12 +163,7 @@ mod library {
 }
 
 mod locations {
-	use aws_config::{Region, SdkConfig};
-	use aws_credential_types::provider::future;
-	use aws_sdk_s3::{
-		config::{Credentials, ProvideCredentials, SharedCredentialsProvider},
-		primitives::ByteStream,
-	};
+	use aws_sdk_s3::primitives::ByteStream;
 	use http_body::Full;
 	use serde::{Deserialize, Serialize};
 	use specta::Type;
@@ -167,28 +205,6 @@ mod locations {
 				// static AUTH_TOKEN: Lazy<Mutex<Option<AuthoriseResponse>>> =
 				// 	Lazy::new(|| Mutex::new(None));
 
-				#[derive(Debug)]
-				pub struct CredentialsProvider(sd_cloud_api::locations::authorise::Response);
-
-				impl ProvideCredentials for CredentialsProvider {
-					fn provide_credentials<'a>(&'a self) -> future::ProvideCredentials<'a>
-					where
-						Self: 'a,
-					{
-						future::ProvideCredentials::ready(Ok(Credentials::new(
-							self.0.access_key_id.clone(),
-							self.0.secret_access_key.clone(),
-							Some(self.0.session_token.clone()),
-							None, // TODO: Get this from the SD Cloud backend
-							"sd-cloud",
-						)))
-					}
-
-					fn fallback_on_interrupt(&self) -> Option<Credentials> {
-						None
-					}
-				}
-
 				#[derive(Type, Deserialize)]
 				pub struct TestingParams {
 					id: String,
@@ -213,15 +229,8 @@ mod locations {
 
 					println!("{token:?}"); // TODO
 
-					// TODO: Reuse the client between procedure calls
-					let client = aws_sdk_s3::Client::new(
-						&SdkConfig::builder()
-							.region(Region::new("us-west-1")) // TODO: From cloud config
-							.credentials_provider(SharedCredentialsProvider::new(
-								CredentialsProvider(token),
-							))
-							.build(),
-					);
+					// Initializes the client on the first call. Retrieves the same client on subsequent calls.
+					let client = get_aws_s3_client(token);
 
 					client
 						.put_object()
