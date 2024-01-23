3eab995111e83c49a51d14f584d3c0aa9b34a19d
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2024-01-23 17:42:08 +0000

    fix typo

--- core/src/location/indexer/indexer_job.rs
+++ core/src/location/indexer/indexer_job.rs
@@ -446,7 +446,7 @@ impl StatefulJob for IndexerJobInit {
 					vec![
 						ScanProgress::ChunkCount(more_steps.len() - to_walk_count),
 						ScanProgress::Message(format!(
-							"Scanned more {} files or directories; \
+							"Scanned {} more files or directories; \
 							{} more directories to scan and more {} entries to update",
 							new_metadata.total_paths,
 							to_walk_count,
