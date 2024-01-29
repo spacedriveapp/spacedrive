03b328ddf008a5e0e08ac7b3dcff9bc45bd6faf1
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2024-01-29 13:51:49 +0300

    fix overview search

--- interface/app/$libraryId/search/SearchBar.tsx
+++ interface/app/$libraryId/search/SearchBar.tsx
@@ -1,4 +1,6 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router';
+import { createSearchParams } from 'react-router-dom';
 import { useDebouncedCallback } from 'use-debounce';
 import { Input, ModifierKeys, Shortcut } from '@sd/ui';
 import { useOperatingSystem } from '~/hooks';
@@ -7,10 +9,14 @@ import { keybindForOs } from '~/util/keybinds';
 import { useSearchContext } from './context';
 import { useSearchStore } from './store';
 
-export default () => {
+interface Props {
+	redirectToSearch?: boolean;
+}
+
+export default ({ redirectToSearch }: Props) => {
 	const search = useSearchContext();
 	const searchRef = useRef<HTMLInputElement>(null);
-
+	const navigate = useNavigate();
 	const searchStore = useSearchStore();
 
 	const os = useOperatingSystem(true);
@@ -55,6 +61,14 @@ export default () => {
 
 	const updateDebounce = useDebouncedCallback((value: string) => {
 		search.setSearch(value);
+		if (redirectToSearch) {
+			navigate({
+				pathname: '../search',
+				search: createSearchParams({
+					search: value
+				}).toString()
+			});
+		}
 	}, 300);
 
 	function updateValue(value: string) {
@@ -70,10 +84,12 @@ export default () => {
 		<Input
 			ref={searchRef}
 			placeholder="Search"
-			className="mx-2 w-48 transition-all duration-200 focus-within:w-60"
+			className="w-48 mx-2 transition-all duration-200 focus-within:w-60"
 			size="sm"
 			value={value}
-			onChange={(e) => updateValue(e.target.value)}
+			onChange={(e) => {
+				updateValue(e.target.value);
+			}}
 			onBlur={() => {
 				if (search.rawSearch === '' && !searchStore.interactingWithSearchOptions) {
 					clearValue();
@@ -82,7 +98,7 @@ export default () => {
 			}}
 			onFocus={() => search.setSearchBarFocused(true)}
 			right={
-				<div className="pointer-events-none flex h-7 items-center space-x-1 opacity-70 group-focus-within:hidden">
+				<div className="flex items-center space-x-1 pointer-events-none h-7 opacity-70 group-focus-within:hidden">
 					{
 						<Shortcut
 							chars={keybind([ModifierKeys.Control], ['F'])}
