f9e827512f9827012bb84afd6e3072b15697981e
jake <77554505+brxken128@users.noreply.github.com>
2024-01-29 12:32:48 +0000

    [ENG-1540] Remove `localStorage` if no libraries are created (#1945)
    * feat: clear localstorage on start if no libraries are found
    
    * chore: add a check to see if the node is for desktop or server use
    
    this will prevent desktop/app localstorage from being cleared if it's a server being hosted
    
    * fix: add secondary localstorage directory for macos
    
    for some reason the app/onboarding progress is kept in `Library/WebKit/Spacedrive` (at least in dev?) so it also needs clearing
    
    * feat: delete cache directories also
    
    * chore: iterate over paths
    
    * feat: add support for windows `localStorage` by using `data_local_dir`
    
    * docs/style: better comments and logs
    
    * feat: remove both `AppData` local and roaming on Windows
    
    * fix: mobile builds
    
    * chore: tweak error message for windows

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -4,6 +4,7 @@ use crate::{
 	api::{CoreEvent, Router},
 	location::LocationManagerError,
 	object::media::thumbnail::actor::Thumbnailer,
+	util::clear_localstorage::clear_localstorage,
 };
 
 #[cfg(feature = "ai")]
@@ -82,6 +83,7 @@ impl Node {
 	pub async fn new(
 		data_dir: impl AsRef<Path>,
 		env: env::Env,
+		desktop: bool,
 	) -> Result<(Arc<Node>, Arc<Router>), NodeError> {
 		let data_dir = data_dir.as_ref();
 
@@ -108,6 +110,11 @@ impl Node {
 		let (locations, locations_actor) = location::Locations::new();
 		let (jobs, jobs_actor) = job::Jobs::new();
 		let libraries = library::Libraries::new(data_dir.join("libraries")).await?;
+
+		if desktop && libraries.get_all().await.is_empty() {
+			clear_localstorage().await;
+		}
+
 		let (p2p, p2p_actor) = p2p::P2PManager::new(config.clone(), libraries.clone()).await?;
 		let node = Arc::new(Node {
 			data_dir: data_dir.to_path_buf(),
