9ff33c80d08c4e1768d86ff77e3f84611ae1e062
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2024-02-01 14:58:36 +0300

    Settings screen designed

--- apps/mobile/src/components/settings/SettingsItem.tsx
+++ apps/mobile/src/components/settings/SettingsItem.tsx
@@ -1,37 +1,46 @@
 import { CaretRight, Icon } from 'phosphor-react-native';
-import { Pressable, Text, View, ViewStyle } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 import { tw, twStyle } from '~/lib/tailwind';
 
 type SettingsItemProps = {
 	title: string;
 	onPress?: () => void;
-	leftIcon?: Icon;
+	leftIcon: Icon;
 	rightArea?: React.ReactNode;
+	rounded?: 'top' | 'bottom';
 };
 
 export function SettingsItem(props: SettingsItemProps) {
+	//due to SectionList limitation of not being able to modify each section individually
+	//we have to use this 'hacky' way to make the top and bottom rounded
+	const borderRounded =
+		props.rounded === 'top' ? 'rounded-t-md' : props.rounded === 'bottom' && 'rounded-b-md';
+	const border =
+		props.rounded === 'top'
+			? 'border-t border-r border-l border-app-input'
+			: props.rounded === 'bottom'
+			? 'border-b border-app-input border-r border-l'
+			: 'border-app-input border-l border-r';
 	return (
 		<Pressable onPress={props.onPress}>
-			<View style={tw`flex flex-row items-center justify-between bg-app-darkBox px-4`}>
-				<View style={tw`flex flex-row items-center py-3`}>
-					{props.leftIcon &&
-						props.leftIcon({ size: 20, color: tw.color('ink'), style: tw`mr-3` })}
-					<Text style={tw`text-[14px] font-medium text-ink`}>{props.title}</Text>
+			<View style={twStyle(' border-app-input bg-sidebar-box', borderRounded, border)}>
+				<View style={tw`h-auto flex-row items-center`}>
+					<View
+						style={tw`ml-4 mr-5 h-8 w-8 items-center justify-center rounded-full bg-app-input`}
+					>
+						{props.leftIcon({ size: 20, color: tw.color('ink-dull') })}
+					</View>
+					<View
+						style={twStyle(
+							`flex-1 flex-row items-center justify-between py-4`,
+							borderRounded !== 'rounded-b-md' && 'border-b border-app-input'
+						)}
+					>
+						<Text style={tw`text-sm font-medium text-ink`}>{props.title}</Text>
+						<CaretRight style={tw`mr-4`} size={16} color={tw.color('ink')} />
+					</View>
 				</View>
-				{props.rightArea ? (
-					props.rightArea
-				) : (
-					<CaretRight size={16} color={tw.color('ink')} />
-				)}
 			</View>
 		</Pressable>
-	);
-}
-
-export function SettingsItemDivider(props: { style?: ViewStyle }) {
-	return (
-		<View style={twStyle('bg-app-darkLine', props.style)}>
-			<View style={tw`mx-3 border-b border-b-app-darkLine`} />
-		</View>
 	);
 }
