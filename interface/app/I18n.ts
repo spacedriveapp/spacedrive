63a2b4276766f0ddcbe45731bef5ffd7b7c6b141
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2024-02-01 20:21:28 -0500

    language detector

--- interface/app/I18n.ts
+++ interface/app/I18n.ts
@@ -1,12 +1,12 @@
 import i18n from 'i18next';
-// import LanguageDetector from 'i18next-browser-languagedetector';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import * as resources from 'virtual:i18next-loader';
 
 i18n
-	// // detect user language
-	// // learn more: https://github.com/i18next/i18next-browser-languageDetector
-	// .use(LanguageDetector)
+	// detect user language
+	// learn more: https://github.com/i18next/i18next-browser-languageDetector
+	.use(LanguageDetector)
 	// pass the i18n instance to react-i18next.
 	.use(initReactI18next)
 	// init i18next
