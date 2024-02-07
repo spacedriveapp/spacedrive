bda9a1b6ee5d7682a6a65b02333e9578f3fb0901
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2024-02-07 20:25:04 +0300

    [MOB-55] Video animation for onboarding on mobile and desktop (#2065)
    * video animation for onboarding on mobile and desktop
    
    run assets gen
    
    cleanup
    
    declare mp4 type
    
    * update metro config to transform video files from sd assets
    
    * test ci without native video exclude
    
    * casing?
    
    * remove to add back again due to github
    
    * add videos back
    
    * versions
    
    * no need to transform
    
    ---------
    
    Co-authored-by: Utku Bakir <74243531+utkubakir@users.noreply.github.com>

--- interface/app/onboarding/Layout.tsx
+++ interface/app/onboarding/Layout.tsx
@@ -1,5 +1,7 @@
 import { BloomOne } from '@sd/assets/images';
+import { sdintro } from '@sd/assets/videos';
 import clsx from 'clsx';
+import { useState } from 'react';
 import { Navigate, Outlet } from 'react-router';
 import { useDebugState } from '@sd/client';
 import DragRegion from '~/components/DragRegion';
@@ -13,7 +15,7 @@ import Progress from './Progress';
 export const Component = () => {
 	const os = useOperatingSystem();
 	const debugState = useDebugState();
-
+	const [showIntro, setShowIntro] = useState(true);
 	const ctx = useContextValue();
 
 	if (ctx.libraries.isLoading) return null;
@@ -27,20 +29,35 @@ export const Component = () => {
 					'flex h-screen flex-col bg-sidebar text-ink'
 				)}
 			>
+				{showIntro && (
+					<div className="absolute left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-[#1F212C]">
+						<video
+							width={700}
+							className="mx-auto"
+							autoPlay
+							onEnded={() => {
+								setShowIntro(false);
+							}}
+							muted
+							controls={false}
+							src={sdintro}
+						/>
+					</div>
+				)}
 				<DragRegion className="z-50 h-9" />
-				<div className="-mt-5 flex grow flex-col gap-8 p-10">
-					<div className="flex grow flex-col items-center justify-center">
+				<div className="flex flex-col gap-8 p-10 -mt-5 grow">
+					<div className="flex flex-col items-center justify-center grow">
 						<Outlet />
 					</div>
 					<Progress />
 				</div>
 				<div className="flex justify-center p-4">
-					<p className="text-xs text-ink-dull opacity-50">
+					<p className="text-xs opacity-50 text-ink-dull">
 						&copy; {new Date().getFullYear()} Spacedrive Technology Inc.
 					</p>
 				</div>
 				<div className="absolute -z-10">
-					<div className="relative h-screen w-screen">
+					<div className="relative w-screen h-screen">
 						<img src={BloomOne} className="absolute h-[2000px] w-[2000px]" />
 						{/* <img src={BloomThree} className="absolute w-[2000px] h-[2000px] -right-[200px]" /> */}
 					</div>
