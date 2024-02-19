04eb6d13e28118eceea341c5e7c5faf34d1d58e2
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2024-02-19 15:28:37 +0300

    run tests

--- interface/app/onboarding/Layout.tsx
+++ interface/app/onboarding/Layout.tsx
@@ -1,7 +1,7 @@
 import { BloomOne } from '@sd/assets/images';
 import { introvideobg, sdintro } from '@sd/assets/videos';
 import clsx from 'clsx';
-import { useState } from 'react';
+import { useLayoutEffect, useState } from 'react';
 import { Navigate, Outlet } from 'react-router';
 import { useDebugState } from '@sd/client';
 import DragRegion from '~/components/DragRegion';
@@ -17,7 +17,19 @@ export const Component = () => {
 	const debugState = useDebugState();
 	// FIX-ME: Intro video breaks onboarding for the web and Linux versions
 	const [showIntro, setShowIntro] = useState(os === 'macOS' || os === 'windows');
+	const [windowSize, setWindowSize] = useState({
+		width: window.innerWidth,
+		height: window.innerHeight
+	});
 	const ctx = useContextValue();
+
+	useLayoutEffect(() => {
+		const handleResize = () => {
+			setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+		};
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
 
 	if (ctx.libraries.isLoading) return null;
 	if (ctx.library?.uuid !== undefined) return <Navigate to={`/${ctx.library.uuid}`} replace />;
@@ -31,7 +43,17 @@ export const Component = () => {
 				)}
 			>
 				{showIntro && (
-					<div className="introbg absolute left-0 top-0 z-50 flex h-screen w-screen items-center justify-center">
+					<div className="absolute top-0 left-0 z-50 flex items-center justify-center w-screen h-screen">
+						<svg
+							width="100%"
+							height="100%"
+							className="absolute left-0 top-0 z-[-1]"
+							viewBox={`0 0 ${windowSize.width} ${windowSize.height}`}
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<rect width="100%" height="100%" fill="#2E2F38" />
+						</svg>
 						<video
 							style={{
 								position: 'absolute',
@@ -40,6 +62,7 @@ export const Component = () => {
 								height: '100vh',
 								zIndex: -1
 							}}
+							preload="auto"
 							src={introvideobg}
 							muted
 							controls={false}
@@ -57,19 +80,19 @@ export const Component = () => {
 					</div>
 				)}
 				<DragRegion className="z-50 h-9" />
-				<div className="-mt-5 flex grow flex-col gap-8 p-10">
-					<div className="flex grow flex-col items-center justify-center">
+				<div className="flex flex-col gap-8 p-10 -mt-5 grow">
+					<div className="flex flex-col items-center justify-center grow">
 						<Outlet />
 					</div>
 					<Progress />
 				</div>
 				<div className="flex justify-center p-4">
-					<p className="text-xs text-ink-dull opacity-50">
+					<p className="text-xs opacity-50 text-ink-dull">
 						&copy; {new Date().getFullYear()} Spacedrive Technology Inc.
 					</p>
 				</div>
 				<div className="absolute -z-10">
-					<div className="relative h-screen w-screen">
+					<div className="relative w-screen h-screen">
 						<img src={BloomOne} className="absolute h-[2000px] w-[2000px]" />
 						{/* <img src={BloomThree} className="absolute w-[2000px] h-[2000px] -right-[200px]" /> */}
 					</div>
