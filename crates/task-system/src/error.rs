712756bf6ef64ea95e9aca1157aa2415da2bde05
Ericson Soares <ericson.ds999@gmail.com>
2024-02-23 12:52:20 -0300

    Stale deps cleanup

--- crates/task-system/src/error.rs
+++ crates/task-system/src/error.rs
@@ -1,14 +1,11 @@
 use sd_utils::error::FileIOError;
 
-use rmp_serde::{decode, encode};
 use thiserror::Error;
 
 use super::task::TaskId;
 
 #[derive(Debug, Error)]
 pub enum Error {
-	#[error("missing loader for task kind: {0}")]
-	MissingLoader(&'static str),
 	#[error("task not found: {0}")]
 	TaskNotFound(TaskId),
 	#[error("task aborted <id='{0}'>")]
@@ -18,14 +15,6 @@ pub enum Error {
 	#[error("forced abortion for task <id='{0}'> timed out")]
 	TaskForcedAbortTimeout(TaskId),
 
-	#[error("serialize error: {0}")]
-	Serialize(#[from] encode::Error),
-	#[error("deserialize error: {0}")]
-	Deserialize(#[from] decode::Error),
-
 	#[error(transparent)]
 	FileIO(#[from] FileIOError),
-
-	#[error("task store file not found")]
-	TaskStoreFileNotFound,
 }
