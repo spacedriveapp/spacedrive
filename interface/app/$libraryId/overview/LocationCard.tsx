1d42d4de64be05915fe57ea69411c0e00e9065fe
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2024-02-27 19:44:49 +0300

    [ENG-1640] Correct locations folder icon (#2130)
    * Show correct icon for locations list
    
    * run lint fix

--- interface/app/$libraryId/overview/LocationCard.tsx
+++ interface/app/$libraryId/overview/LocationCard.tsx
@@ -1,9 +1,8 @@
 import { ReactComponent as Ellipsis } from '@sd/assets/svgs/ellipsis.svg';
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import { byteSize } from '@sd/client';
-import { Button, Card, CircularProgress, tw } from '@sd/ui';
+import { Button, Card, tw } from '@sd/ui';
 import { Icon } from '~/components';
-import { useIsDark } from '~/hooks';
 
 type LocationCardProps = {
 	name: string;
