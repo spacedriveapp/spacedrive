8061ce79d06d53a1f01fc0b3fbbb30b706baa413
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2024-03-22 20:46:50 -0300

    Double Cypress timeouts when running on CI

--- apps/web/cypress.config.ts
+++ apps/web/cypress.config.ts
@@ -1,5 +1,16 @@
 import { defineConfig } from 'cypress';
 
+const ci_specific = {
+	// Double all the default timeouts
+	// https://docs.cypress.io/guides/references/configuration#Timeouts
+	defaultCommandTimeout: 4000 * 2,
+	execTimeout: 60000 * 2,
+	taskTimeout: 60000 * 2,
+	pageLoadTimeout: 60000 * 2,
+	requestTimeout: 5000 * 2,
+	responseTimeout: 30000 * 2
+};
+
 export default defineConfig({
 	e2e: {
 		baseUrl: 'http://localhost:8002',
@@ -7,5 +18,6 @@ export default defineConfig({
 			// implement node event listeners here
 		}
 	},
-	video: true
+	video: true,
+	...(process.env.CI === 'true' ? ci_specific : {})
 });
