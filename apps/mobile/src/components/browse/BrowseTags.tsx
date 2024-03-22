00da54028903e210ed18b45c0cee417f440f5069
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2024-03-22 04:44:22 +0300

    Update BrowseTags.tsx

--- apps/mobile/src/components/browse/BrowseTags.tsx
+++ apps/mobile/src/components/browse/BrowseTags.tsx
@@ -28,6 +28,7 @@ const BrowseTags = () => {
 				<Text style={tw`text-lg font-bold text-white`}>Tags</Text>
 				<View style={tw`flex-row gap-3`}>
 					<Button
+						testID="navigate-tags-screen"
 						onPress={() => {
 							navigation.navigate('Tags');
 						}}
