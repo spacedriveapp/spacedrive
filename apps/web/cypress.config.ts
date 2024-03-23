2e5ea75e1f3eadb79a62dc243be410d9c5766345
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2024-03-23 14:43:01 -0300

    Enable test retries  - Log cypress default config during test run

--- apps/web/cypress.config.ts
+++ apps/web/cypress.config.ts
@@ -1,6 +1,8 @@
 import { dirname, join, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { inspect } from 'node:util';
 import { defineConfig } from 'cypress';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -12,13 +14,16 @@ const ci_specific = {
 	taskTimeout: 60000 * 2,
 	pageLoadTimeout: 60000 * 2,
 	requestTimeout: 5000 * 2,
-	responseTimeout: 30000 * 2
+	responseTimeout: 30000 * 2,
+	// Enable test retries
+	// https://docs.cypress.io/guides/guides/test-retries#Configure-retry-attempts-for-all-modes
+	retries: 2
 };
 
-export default defineConfig({
+const config = defineConfig({
 	e2e: {
 		baseUrl: 'http://localhost:8002',
-		setupNodeEvents(on, config) {
+		setupNodeEvents(on) {
 			on('task', {
 				repoRoot: () => {
 					return resolve(join(__dirname, '../../'));
@@ -29,3 +34,7 @@ export default defineConfig({
 	video: true,
 	...(process.env.CI === 'true' ? ci_specific : {})
 });
+
+console.log('Cypress default config:', inspect(config, { depth: null, colors: true }));
+
+export default config;
