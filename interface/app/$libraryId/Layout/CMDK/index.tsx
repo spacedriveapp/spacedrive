8f3d62501687482a3e7a8bfde7c5408edfe2f2b6
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2024-03-25 15:04:06 -0700

    add 2 more actions + locations to cmd

--- interface/app/$libraryId/Layout/CMDK/index.tsx
+++ interface/app/$libraryId/Layout/CMDK/index.tsx
@@ -1,18 +1,36 @@
 import './CMDK.css';
 import './CMDK.scss';
 
+import clsx from 'clsx';
 import { useEffect, useState } from 'react';
 import CommandPalette, { filterItems, getItemIndex } from 'react-cmdk';
 import { useNavigate } from 'react-router';
 import { createSearchParams } from 'react-router-dom';
+import {
+	arraysEqual,
+	useCache,
+	useLibraryContext,
+	useLibraryQuery,
+	useNodes,
+	useOnlineLocations
+} from '@sd/client';
+import { dialogManager } from '@sd/ui';
+import { Icon } from '~/components';
 import Sparkles from '~/components/Sparkles';
+import { usePlatform } from '~/util/Platform';
 
 import { explorerStore } from '../../Explorer/store';
+import { AddLocationDialog } from '../../settings/library/locations/AddLocationDialog';
+import { openDirectoryPickerDialog } from '../../settings/library/locations/openDirectoryPickerDialog';
+import CreateDialog from '../../settings/library/tags/CreateDialog';
 import CMDKLocations from './pages/CMDKLocations';
 import CMDKTags from './pages/CMDKTags';
 
 const CMDK = () => {
 	const [isOpen, setIsOpen] = useState<boolean>(false);
+
+	const platform = usePlatform();
+	const libraryId = useLibraryContext().library.uuid;
 
 	useEffect(() => {
 		function handleKeyDown(e: KeyboardEvent) {
@@ -51,11 +69,11 @@ const CMDK = () => {
 	const [page, setPage] = useState<'root' | 'locations' | 'tags'>('root');
 	const [search, setSearch] = useState('');
 
-	// const locationsQuery = useLibraryQuery(['locations.list'], { keepPreviousData: true });
-	// useNodes(locationsQuery.data?.nodes);
-	// const locations = useCache(locationsQuery.data?.items);
+	const locationsQuery = useLibraryQuery(['locations.list'], { keepPreviousData: true });
+	useNodes(locationsQuery.data?.nodes);
+	const locations = useCache(locationsQuery.data?.items);
 
-	// const onlineLocations = useOnlineLocations();
+	const onlineLocations = useOnlineLocations();
 
 	function handleClose(open: boolean) {
 		setIsOpen(open);
@@ -140,8 +158,45 @@ const CMDK = () => {
 					// 	onClick: () => {}
 					// }
 				]
-			}
-			// TODO: Might look nice if we showed some items and maybe saved searches
+			},
+			{
+				heading: 'Actions',
+				id: 'actions',
+				items: [
+					// {
+					// 	id: 'create-folder',
+					// 	children: 'Create folder',
+					// 	icon: 'FolderPlusIcon',
+					// 	onClick: () => {}
+					// },
+					{
+						id: 'create-tag',
+						children: 'Create tag',
+						icon: 'TagIcon',
+						onClick: () => {
+							dialogManager.create((dp) => <CreateDialog {...dp} />);
+						}
+					},
+					{
+						id: 'add-location',
+						children: 'Add location',
+						icon: 'FolderIcon',
+						onClick: async () => {
+							const path = await openDirectoryPickerDialog(platform);
+							if (path !== '') {
+								dialogManager.create((dp) => (
+									<AddLocationDialog
+										path={path ?? ''}
+										libraryId={libraryId}
+										{...dp}
+									/>
+								));
+							}
+						}
+					}
+				]
+			},
+			// TODO: Might look nice if we showed some items and maybe saved searches here
 			// {
 			// 	heading: `Searching for "${search}"`,
 			// 	id: 'search',
@@ -162,50 +217,33 @@ const CMDK = () => {
 			// 		// 	: ([] as any)
 			// 	]
 			// }
-			// {
-			// 	heading: 'Locations',
-			// 	id: 'locations',
-			// 	items: locations
-			// 		? locations.map((location) => ({
-			// 				id: location.id,
-			// 				children: location.name,
-			// 				icon: () => (
-			// 					<div className="relative -mt-0.5 mr-1 shrink-0 grow-0">
-			// 						<Icon name="Folder" size={22} />
-			// 						<div
-			// 							className={clsx(
-			// 								'absolute bottom-0.5 right-0 size-1.5 rounded-full',
-			// 								onlineLocations.some((l) =>
-			// 									arraysEqual(location.pub_id, l)
-			// 								)
-			// 									? 'bg-green-500'
-			// 									: 'bg-red-500'
-			// 							)}
-			// 						/>
-			// 					</div>
-			// 				),
-			// 				href: `#`
-			// 			}))
-			// 		: ([] as any)
-			// },
-			// {
-			// 	heading: 'Actions',
-			// 	id: 'actions',
-			// 	items: [
-			// 		{
-			// 			id: 'new-folder',
-			// 			children: 'New folder',
-			// 			icon: 'FolderPlusIcon',
-			// 			onClick: () => {}
-			// 		},
-			// 		{
-			// 			id: 'new-tag',
-			// 			children: 'New tag',
-			// 			icon: 'TagIcon',
-			// 			onClick: () => {}
-			// 		}
-			// 	]
-			// }
+			// This is technically a duplicate of "Go to Location", but it looks cool.
+			{
+				heading: 'Locations',
+				id: 'locations',
+				items: locations
+					? locations.map((location) => ({
+							id: location.id,
+							children: location.name,
+							icon: () => (
+								<div className="relative -mt-0.5 mr-1 shrink-0 grow-0">
+									<Icon name="Folder" size={22} />
+									<div
+										className={clsx(
+											'absolute bottom-0.5 right-0 size-1.5 rounded-full',
+											onlineLocations.some((l) =>
+												arraysEqual(location.pub_id, l)
+											)
+												? 'bg-green-500'
+												: 'bg-red-500'
+										)}
+									/>
+								</div>
+							),
+							onClick: () => navigate(`location/${location.id}`)
+						}))
+					: ([] as any)
+			}
 		],
 		search
 	);
