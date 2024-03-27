6fe61e20006d3606f5044745e40a9d7eee556a94
nikec <nikec.job@gmail.com>
2024-03-27 12:27:12 +0100

    better size handling

--- interface/app/$libraryId/Explorer/OptionsPanel/ListView/IconSize.tsx
+++ interface/app/$libraryId/Explorer/OptionsPanel/ListView/IconSize.tsx
@@ -1,11 +1,13 @@
+import { useMemo } from 'react';
 import { Slider } from '@sd/ui';
 import { useLocale } from '~/hooks';
 
 import { Subheading } from '..';
 import { useExplorerContext } from '../../Context';
 import { LIST_VIEW_ICON_SIZES } from '../../View/ListView/useTable';
+import { getSizes } from './util';
 
-const sizes = Object.keys(LIST_VIEW_ICON_SIZES) as (keyof typeof LIST_VIEW_ICON_SIZES)[];
+const sizes = getSizes(LIST_VIEW_ICON_SIZES);
 
 export const IconSize = () => {
 	const { t } = useLocale();
@@ -13,16 +15,21 @@ export const IconSize = () => {
 	const explorer = useExplorerContext();
 	const settings = explorer.useSettingsSnapshot();
 
+	const defaultValue = useMemo(
+		() => sizes.findIndex((size) => size[0] === settings.listViewIconSize),
+		[settings.listViewIconSize]
+	);
+
 	return (
 		<div>
 			<Subheading>{t('icon_size')}</Subheading>
 			<Slider
 				step={1}
 				max={sizes.length - 1}
-				defaultValue={[parseInt(settings.listViewIconSize)]}
+				defaultValue={[defaultValue]}
 				onValueChange={([value]) => {
 					const size = value !== undefined && sizes[value];
-					if (size) explorer.settingsStore.listViewIconSize = size;
+					if (size) explorer.settingsStore.listViewIconSize = size[0];
 				}}
 			/>
 		</div>
--- interface/app/$libraryId/Explorer/OptionsPanel/ListView/TextSize.tsx
+++ interface/app/$libraryId/Explorer/OptionsPanel/ListView/TextSize.tsx
@@ -1,11 +1,13 @@
+import { useMemo } from 'react';
 import { Slider } from '@sd/ui';
 import { useLocale } from '~/hooks';
 
 import { Subheading } from '..';
 import { useExplorerContext } from '../../Context';
 import { LIST_VIEW_TEXT_SIZES } from '../../View/ListView/useTable';
+import { getSizes } from './util';
 
-const sizes = Object.keys(LIST_VIEW_TEXT_SIZES) as (keyof typeof LIST_VIEW_TEXT_SIZES)[];
+const sizes = getSizes(LIST_VIEW_TEXT_SIZES);
 
 export const TextSize = () => {
 	const { t } = useLocale();
@@ -13,16 +15,21 @@ export const TextSize = () => {
 	const explorer = useExplorerContext();
 	const settings = explorer.useSettingsSnapshot();
 
+	const defaultValue = useMemo(
+		() => sizes.findIndex((size) => size[0] === settings.listViewTextSize),
+		[settings.listViewTextSize]
+	);
+
 	return (
 		<div>
 			<Subheading>{t('text_size')}</Subheading>
 			<Slider
 				step={1}
 				max={sizes.length - 1}
-				defaultValue={[parseInt(settings.listViewTextSize)]}
+				defaultValue={[defaultValue]}
 				onValueChange={([value]) => {
 					const size = value !== undefined && sizes[value];
-					if (size) explorer.settingsStore.listViewTextSize = size;
+					if (size) explorer.settingsStore.listViewTextSize = size[0];
 				}}
 			/>
 		</div>
