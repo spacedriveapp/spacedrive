7e149bc088c538e2be7ffb0dd485407c197f631d
nikec <43032218+niikeec@users.noreply.github.com>
2024-03-27 11:27:23 +0100

    [ENG-1696] Fix Asian characters text wrapping (#2253)
    no wrap

--- packages/ui/src/Select.tsx
+++ packages/ui/src/Select.tsx
@@ -9,7 +9,7 @@ import { forwardRef, PropsWithChildren } from 'react';
 
 export const selectStyles = cva(
 	[
-		'flex items-center justify-between rounded-md border py-0.5 pl-3 pr-[10px] text-sm',
+		'flex items-center justify-between whitespace-nowrap rounded-md border py-0.5 pl-3 pr-[10px] text-sm',
 		'shadow-sm outline-none transition-all focus:ring-2',
 		'text-ink radix-placeholder:text-ink-faint'
 	],
@@ -56,7 +56,9 @@ export const Select = forwardRef(
 				<RS.Trigger
 					className={selectStyles({ size: props.size, className: props.className })}
 				>
-					<RS.Value placeholder={props.placeholder} />
+					<span className="truncate">
+						<RS.Value placeholder={props.placeholder} />
+					</span>
 					<RS.Icon className="ml-2">
 						<ChevronDouble className="text-ink-dull" />
 					</RS.Icon>
