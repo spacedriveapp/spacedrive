383d914554559070e00658975567e5a2844d5772
Oscar Beaumont <oscar@otbeaumont.me>
2024-04-08 14:08:13 +0800

    fix

--- crates/sd-indexer/src/rules/rules.rs
+++ crates/sd-indexer/src/rules/rules.rs
@@ -19,6 +19,18 @@ pub struct IndexerRule {
 }
 
 impl IndexerRule {
+	#[cfg(test)]
+	pub fn new(name: String, default: bool, rules: Vec<RulePerKind>) -> Self {
+		Self {
+			id: None,
+			name,
+			default,
+			rules,
+			date_created: Utc::now(),
+			date_modified: Utc::now(),
+		}
+	}
+
 	pub async fn apply(
 		&self,
 		source: impl AsRef<Path>,
@@ -79,19 +91,6 @@ mod tests {
 	use globset::{Glob, GlobSetBuilder};
 	use tempfile::tempdir;
 	use tokio::fs;
-
-	impl IndexerRule {
-		pub fn new(name: String, default: bool, rules: Vec<RulePerKind>) -> Self {
-			Self {
-				id: None,
-				name,
-				default,
-				rules,
-				date_created: Utc::now(),
-				date_modified: Utc::now(),
-			}
-		}
-	}
 
 	async fn check_rule(indexer_rule: &IndexerRule, path: impl AsRef<Path>) -> bool {
 		indexer_rule
