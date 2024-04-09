c7c966b2f8a3ed95cdd936050e135ee4d6d12654
Brendan Allan <brendonovich@outlook.com>
2024-04-09 11:14:51 +0800

    [ENG-1723] better sync status (#2299)
    * better sync status
    
    * cleanup

--- core/src/api/sync.rs
+++ core/src/api/sync.rs
@@ -78,15 +78,26 @@ pub(crate) fn mount() -> AlphaRouter<Ctx> {
 		.procedure("active", {
 			R.with2(library())
 				.subscription(|(_, library), _: ()| async move {
+					#[derive(serde::Serialize, specta::Type)]
+					#[specta(rename = "SyncStatus")]
+					struct Data {
+						ingest: bool,
+						cloud_send: bool,
+						cloud_receive: bool,
+						cloud_ingest: bool,
+					}
+
 					async_stream::stream! {
 						let cloud_sync = &library.cloud.sync;
 						let sync = &library.sync.shared;
 
 						loop {
-							yield sync.active.load(Ordering::Relaxed)
-								|| cloud_sync.send_active.load(Ordering::Relaxed)
-								|| cloud_sync.receive_active.load(Ordering::Relaxed)
-								|| cloud_sync.ingest_active.load(Ordering::Relaxed);
+							yield Data {
+							  ingest: sync.active.load(Ordering::Relaxed),
+								cloud_send: cloud_sync.send_active.load(Ordering::Relaxed),
+								cloud_receive: cloud_sync.receive_active.load(Ordering::Relaxed),
+								cloud_ingest: cloud_sync.ingest_active.load(Ordering::Relaxed),
+							};
 
 							tokio::select! {
 								_ = cloud_sync.notifier.notified() => {},
