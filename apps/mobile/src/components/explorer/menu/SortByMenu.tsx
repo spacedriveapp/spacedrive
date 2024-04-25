d9ab82e175aa29584a7f5eb6be92f7467d2432a8
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2024-04-25 10:16:04 -0400

    some ideas

--- apps/mobile/src/components/explorer/menu/SortByMenu.tsx
+++ apps/mobile/src/components/explorer/menu/SortByMenu.tsx
@@ -1,33 +1,51 @@
 import { ArrowDown, ArrowUp } from 'phosphor-react-native';
-import { useState } from 'react';
 import { Text, View } from 'react-native';
+import { FilePathOrder, MediaDataOrder, ObjectOrder } from '@sd/client';
 import { Menu, MenuItem } from '~/components/primitive/Menu';
 import { tw } from '~/lib/tailwind';
+import { getExplorerStore, useExplorerStore } from '~/stores/explorerStore';
 
-const sortOptions = {
+const sortOptions: Record<FieldNames, string> = {
 	name: 'Name',
-	kind: 'Kind',
-	favorite: 'Favorite',
-	date_created: 'Date Created',
-	date_modified: 'Date Modified',
-	date_last_opened: 'Date Last Opened'
+	dateCreated: 'Date Created',
+	dateModified: 'Date Modified',
+	dateIndexed: 'Date Indexed',
+	sizeInBytes: 'Size',
+	dateAccessed: 'Date Accessed',
+	epochTime: 'Date Taken',
+	kind: 'Kind'
 };
 
-type SortByType = keyof typeof sortOptions;
+// type SortByType = FilePathOrder | ObjectOrder | MediaDataOrder;
+// get keys of FilePathOrder
+
+type ExtractFieldName<T> = T extends { field: infer F } ? F : never;
+type SortByKeys =
+	| ExtractFieldName<FilePathOrder>
+	| ExtractFieldName<ObjectOrder>
+	| ExtractFieldName<MediaDataOrder>;
+
+type ExcludeMultiple<T, K> = T extends K ? never : T;
+
+type FieldNames = ExcludeMultiple<SortByKeys, 'object' | 'mediaData'>;
 
 const ArrowUpIcon = () => <ArrowUp weight="bold" size={16} color={tw.color('ink-dull')} />;
 const ArrowDownIcon = () => <ArrowDown weight="bold" size={16} color={tw.color('ink-dull')} />;
 
 const SortByMenu = () => {
-	const [sortBy, setSortBy] = useState<SortByType>('name');
-	const [sortDirection, setSortDirection] = useState('asc' as 'asc' | 'desc');
+	// TODO: get settings from explorer store here
+
+	const explorerStore = useExplorerStore();
+
+	const sortDirection = explorerStore.orderDirection;
+	const orderKey = explorerStore.orderKey;
 
 	return (
 		<Menu
 			trigger={
 				<View style={tw`flex flex-row items-center`}>
-					<Text style={tw`mr-0.5 font-medium text-ink-dull`}>{sortOptions[sortBy]}</Text>
-					{sortDirection === 'asc' ? <ArrowUpIcon /> : <ArrowDownIcon />}
+					<Text style={tw`mr-0.5 font-medium text-ink-dull`}>{sortDirection}</Text>
+					{sortDirection === 'Asc' ? <ArrowUpIcon /> : <ArrowDownIcon />}
 				</View>
 			}
 		>
@@ -35,8 +53,8 @@ const SortByMenu = () => {
 				<MenuItem
 					key={value}
 					icon={
-						value === sortBy
-							? sortDirection === 'asc'
+						value === orderKey
+							? sortDirection === 'Asc'
 								? ArrowUpIcon
 								: ArrowDownIcon
 							: undefined
@@ -44,13 +62,15 @@ const SortByMenu = () => {
 					text={text}
 					value={value}
 					onSelect={() => {
-						if (value === sortBy) {
-							setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
+						if (value === orderKey) {
+							// pressing value again, so we change order direction
+							getExplorerStore().orderDirection =
+								sortDirection === 'Asc' ? 'Desc' : 'Asc';
 							return;
 						}
 						// Reset sort direction to descending
-						sortDirection === 'asc' && setSortDirection('desc');
-						setSortBy(value as SortByType);
+						// sortDirection === 'Asc' && getExplorerStore().orderDirection = 'Desc';
+						getExplorerStore().orderKey = value;
 					}}
 				/>
 			))}
