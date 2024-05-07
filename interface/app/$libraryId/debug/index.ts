825464efe1a3f94e83596086690acb445f88e2af
Brendan Allan <brendonovich@outlook.com>
2024-05-07 13:19:26 +0800

    sync settings page

--- interface/app/$libraryId/debug/index.ts
+++ interface/app/$libraryId/debug/index.ts
@@ -2,7 +2,6 @@ import { RouteObject } from 'react-router';
 
 export const debugRoutes = [
 	{ path: 'cloud', lazy: () => import('./cloud') },
-	{ path: 'sync', lazy: () => import('./sync') },
 	{ path: 'actors', lazy: () => import('./actors') },
 	{
 		path: 'p2p',
