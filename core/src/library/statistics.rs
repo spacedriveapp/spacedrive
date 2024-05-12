ccbc1640cb8360756ca4ec019b6b262e2c950f58
James Pine <ijamespine@me.com>
2024-05-11 18:14:39 -0700

    fix error msg actually tho

--- core/src/library/statistics.rs
+++ core/src/library/statistics.rs
@@ -38,7 +38,7 @@ pub async fn update_library_statistics(
 		.exec()
 		.await
 		.unwrap_or_else(|err| {
-			error!("Failed to get locations", err);
+			error!("Failed to get locations: {:#?}", err);
 			vec![]
 		})
 		.into_iter()
