8f2276489f45649bb0a73afdf699a8abd5839217
Matheus Consoli <matheus.consoli7@gmail.com>
2024-05-24 13:44:43 -0300

    Display captured backtrace when available

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -260,7 +260,6 @@ async fn main() -> tauri::Result<()> {
 
 					handle.windows().iter().for_each(|(_, window)| {
 						if should_clear_localstorage {
-							println!("bruh?");
 							for webview in window.webviews() {
 								webview.eval("localStorage.clear();").ok();
 							}
