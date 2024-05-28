f339794b53b013963a9f139b790a24bb3340d570
Oscar Beaumont <oscar@otbeaumont.me>
2024-05-28 22:11:18 +0800

    debug

--- crates/p2p/src/hooks/quic/transport.rs
+++ crates/p2p/src/hooks/quic/transport.rs
@@ -484,7 +484,10 @@ async fn start(
 					debug!("established inbound stream with '{}'", identity);
 				});
 			},
-			event = swarm.select_next_some() => match event {
+			event = swarm.select_next_some() => match {
+				println!("{:?}", event); // TODO
+				event
+			} {
 				SwarmEvent::ConnectionEstablished { peer_id, endpoint, connection_id, .. } => {
 					if let Some((addr, socket_addr)) = manual_addr_dial_attempts.remove(&connection_id) {
 						let mut control = control.clone();
