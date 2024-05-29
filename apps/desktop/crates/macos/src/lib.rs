a22de4f9cdf391922ba632d6c6dfc83afdd19f90
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2024-05-29 00:23:08 -0300

    Allow app to toggle app nap

--- apps/desktop/crates/macos/src/lib.rs
+++ apps/desktop/crates/macos/src/lib.rs
@@ -9,10 +9,10 @@ pub enum AppThemeType {
 	Dark = 1 as Int,
 }
 
+swift!(pub fn disable_app_nap(reason: &SRString) -> Bool);
+swift!(pub fn enable_app_nap() -> Bool);
 swift!(pub fn lock_app_theme(theme_type: Int));
 swift!(pub fn set_titlebar_style(window: &NSObject, is_fullscreen: Bool));
-// swift!(pub fn setup_disk_watcher(window: &NSObject, transparent: Bool, large: Bool));
-// swift!(pub fn disk_event_callback(mounted: Bool, path: &SRString));
 swift!(pub fn reload_webview(webview: &NSObject));
 
 #[repr(C)]
@@ -30,20 +30,3 @@ pub fn open_file_paths_with(file_urls: &[String], with_url: &str) {
 	let file_url = file_urls.join("\0");
 	unsafe { open_file_path_with(&file_url.as_str().into(), &with_url.into()) }
 }
-
-// main!(|_| {
-// 	unsafe { setup_disk_watcher() };
-// 	print!("Waiting for disk events... ");
-// 	Ok(())
-// });
-
-// #[no_mangle]
-// pub extern "C" fn disk_event_callback(mounted: Bool, path: *const SRString) {
-// 	let mounted_str = if mounted { "mounted" } else { "unmounted" };
-
-// 	// Convert the raw pointer to a reference
-// 	let path_ref = unsafe { &*path };
-// 	let path_str = path_ref.to_string(); // Assuming SRString has a to_string method
-
-// 	println!("Disk at path {} was {}", path_str, mounted_str);
-// }
