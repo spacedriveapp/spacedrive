3f10fd74aae7ab14869911ee0fa9337504636447
Matheus Consoli <matheus.consoli7@gmail.com>
2024-06-18 02:41:32 -0300

    wip

--- crates/utils/src/db.rs
+++ crates/utils/src/db.rs
@@ -135,6 +135,7 @@ impl<'a, T> OptionalField for &'a Option<T> {
 }
 
 /// If `data` is `Some(t)` returns `Ok(t)`, otherwise returns a `MissingFieldError(field)`
+#[inline(always)]
 pub fn maybe_missing<T: OptionalField>(
 	data: T,
 	field: &'static str,
