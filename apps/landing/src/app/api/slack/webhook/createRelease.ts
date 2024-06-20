44d2d8b43986d54d9ef16bc625903bf9ef8d7bc1
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2024-06-20 16:47:38 +0300

    Fix GH api commit link (#2560)
    * Update createRelease.ts
    
    * another

--- apps/landing/src/app/api/slack/webhook/createRelease.ts
+++ apps/landing/src/app/api/slack/webhook/createRelease.ts
@@ -63,7 +63,7 @@ export async function createModal(
 								type: 'plain_text',
 								text: 'View Commit'
 							},
-							url: `${github.REPO_API}/commit/${commit}`
+							url: `${github.REPO_API}/commits/${commit}`
 						},
 						text: {
 							type: 'mrkdwn',
@@ -267,7 +267,7 @@ export async function handleSubmission(
 								type: 'plain_text',
 								text: 'View Commit'
 							},
-							url: `https://github.com/${env.GITHUB_ORG}/${env.GITHUB_REPO}/commit/${commit}`
+							url: `https://github.com/${env.GITHUB_ORG}/${env.GITHUB_REPO}/commits/${commit}`
 						}
 					]
 				}
