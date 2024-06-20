9e70661ad56a8b9198002201db08adc1f50b09d3
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2024-06-20 00:47:22 -0300

    Update core.ts

--- packages/client/src/core.ts
+++ packages/client/src/core.ts
@@ -658,7 +658,7 @@ export type TextMatch = { contains: string } | { startsWith: string } | { endsWi
  */
 export type ThumbKey = { shard_hex: string; cas_id: CasId; base_directory_str: string }
 
-export type UpdateThumbnailerPreferences = { background_processing_percentage: number }
+export type UpdateThumbnailerPreferences = Record<string, never>
 
 export type VideoProps = { pixel_format: string | null; color_range: string | null; bits_per_channel: number | null; color_space: string | null; color_primaries: string | null; color_transfer: string | null; field_order: string | null; chroma_location: string | null; width: number; height: number; aspect_ratio_num: number | null; aspect_ratio_den: number | null; properties: string[] }
 
