0581f06188ae5dfe042c5ff23b9879672b36ce00
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2024-06-29 17:19:46 +0300

    optional link/href

--- apps/landing/src/app/NewBanner.tsx
+++ apps/landing/src/app/NewBanner.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 
 export interface NewBannerProps {
 	headline: string;
-	href: string;
-	link: string;
+	href?: string;
+	link?: string;
 	className?: string;
 }
 
@@ -14,7 +14,7 @@ export function NewBanner(props: NewBannerProps) {
 
 	return (
 		<Link
-			href={href}
+			href={href ?? '/'}
 			className={clsx(
 				props.className,
 				'news-banner-border-gradient news-banner-glow animation-delay-1 fade-in-whats-new z-10 mb-5 flex w-fit flex-row rounded-full bg-black/10 px-5 py-2.5 text-xs backdrop-blur-md  transition hover:bg-purple-900/20 sm:w-auto sm:text-base'
@@ -24,10 +24,14 @@ export function NewBanner(props: NewBannerProps) {
 				<Newspaper weight="fill" className="text-white " size={20} />
 				<p className="font-regular truncate text-white">{headline}</p>
 			</div>
-			<div role="separator" className="h-22 mx-4 w-px bg-zinc-700/70" />
-			<span className="font-regular shrink-0 bg-gradient-to-r from-violet-400 to-fuchsia-400 bg-clip-text text-transparent decoration-primary-600">
-				{link} <span aria-hidden="true">&rarr;</span>
-			</span>
+			{link && (
+				<>
+					<div role="separator" className="h-22 mx-4 w-px bg-zinc-700/70" />
+					<span className="font-regular shrink-0 bg-gradient-to-r from-violet-400 to-fuchsia-400 bg-clip-text text-transparent decoration-primary-600">
+						{link} <span aria-hidden="true">&rarr;</span>
+					</span>
+				</>
+			)}
 		</Link>
 	);
 }
