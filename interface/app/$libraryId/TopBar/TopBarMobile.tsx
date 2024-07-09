f2d095d38c21329e8eb708693e09e7f117182631
Lynx <141365347+iLynxcat@users.noreply.github.com>
2024-07-09 04:59:34 -0500

    Use default props for Top Bar icons (#2593)
    * Refactor top bar items to use default props
    
    * oops! revert top bar icon size to 18px

--- interface/app/$libraryId/TopBar/TopBarMobile.tsx
+++ interface/app/$libraryId/TopBar/TopBarMobile.tsx
@@ -3,7 +3,7 @@ import React, { forwardRef, HTMLAttributes } from 'react';
 import { Popover, usePopover } from '@sd/ui';
 
 import TopBarButton, { TopBarButtonProps } from './TopBarButton';
-import { ToolOption, TOP_BAR_ICON_CLASSLIST } from './TopBarOptions';
+import { ToolOption, TOP_BAR_ICON_DEFAULT_PROPS } from './TopBarOptions';
 
 const GroupTool = forwardRef<
 	HTMLButtonElement,
@@ -40,7 +40,7 @@ export default ({ toolOptions, className }: Props) => {
 				popover={popover}
 				trigger={
 					<TopBarButton>
-						<DotsThreeCircle className={TOP_BAR_ICON_CLASSLIST} />
+						<DotsThreeCircle {...TOP_BAR_ICON_DEFAULT_PROPS} />
 					</TopBarButton>
 				}
 			>
