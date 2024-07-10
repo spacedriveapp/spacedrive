428c13d4b74230613214a2331130581ec5e1ad3e
Arnab Chakraborty <11457760+Rocky43007@users.noreply.github.com>
2024-07-11 02:07:58 +0300

    Adds `intl` namespace to mobile

--- packages/client/src/utils/index.ts
+++ packages/client/src/utils/index.ts
@@ -120,11 +120,10 @@ export type UnionToIntersection<U> = (U extends never ? never : (arg: U) => neve
 
 export type UnionToTuple<T> =
 	UnionToIntersection<T extends never ? never : (t: T) => T> extends (_: never) => infer W
-		? [...UnionToTuple<Exclude<T, W>>, W]
-		: [];
+	? [...UnionToTuple<Exclude<T, W>>, W]
+	: [];
 
 export function formatNumber(n: number | bigint) {
-	n = Number(n); // Note by @Rocky43007: We have to do this because otherwise, Mobile breaks because it doesn't know how to handle Big Integers.
 	if (!n) return '0';
 	return Intl.NumberFormat().format(n);
 }
