fe6efaa4e44ff9f048622eb575f19e181eddb04e
Ericson Soares <ericson.ds999@gmail.com>
2024-08-02 18:53:29 -0300

    Setup client for Cloud Services in Node

---
+++ core/crates/cloud-services/src/error.rs
@@ -0,0 +1,21 @@
+use std::{io, net::AddrParseError};
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+	#[error("Couldn't parse Cloud Services API address URL: {0}")]
+	InvalidUrl(reqwest::Error),
+	#[error("Failed to initialize http client: {0}")]
+	HttpClientInit(reqwest::Error),
+	#[error("Failed to request Cloud Services API address from Auth Server route: {0}")]
+	FailedToRequestApiAddress(reqwest::Error),
+	#[error("Auth Server's Cloud Services API address route returned an error: {0}")]
+	AuthServerError(reqwest::Error),
+	#[error(
+		"Failed to extract response body from Auth Server's Cloud Services API address route: {0}"
+	)]
+	FailedToExtractApiAddress(reqwest::Error),
+	#[error("Failed to parse auth server's Cloud Services API address: {0}")]
+	FailedToParseApiAddress(#[from] AddrParseError),
+	#[error("Failed to create endpoint: {0}")]
+	FailedToCreateEndpoint(io::Error),
+}
