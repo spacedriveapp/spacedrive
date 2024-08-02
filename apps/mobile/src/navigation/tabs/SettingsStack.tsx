f7e939c94a2b65333c146b4f4582dbd4b1a575c1
Arnab Chakraborty <11457760+Rocky43007@users.noreply.github.com>
2024-08-03 00:22:26 +0300

    Working Auth on Mobile
    You can now log in to the new Auth system from your mobile device.
    
    WARNING: Currently, password rendering isn't hashed. It is being worked on.

--- apps/mobile/src/navigation/tabs/SettingsStack.tsx
+++ apps/mobile/src/navigation/tabs/SettingsStack.tsx
@@ -2,8 +2,11 @@ import { CompositeScreenProps } from '@react-navigation/native';
 // import KeysSettingsScreen from '~/screens/settings/library/KeysSettings';
 
 import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack';
+import { useEffect, useState } from 'react';
 import Header from '~/components/header/Header';
 import SearchHeader from '~/components/header/SearchHeader';
+import AccountLogin from '~/screens/settings/client/AccountSettings/AccountLogin';
+import AccountProfile from '~/screens/settings/client/AccountSettings/AccountProfile';
 import AppearanceSettingsScreen from '~/screens/settings/client/AppearanceSettings';
 import ExtensionsSettingsScreen from '~/screens/settings/client/ExtensionsSettings';
 import GeneralSettingsScreen from '~/screens/settings/client/GeneralSettings';
@@ -24,6 +27,12 @@ import SettingsScreen from '~/screens/settings/Settings';
 import { TabScreenProps } from '../TabNavigator';
 
 const Stack = createNativeStackNavigator<SettingsStackParamList>();
+export type User = {
+	email: string;
+	id: string;
+	timejoined: number;
+	roles: string[];
+};
 
 export default function SettingsStack() {
 	return (
@@ -40,6 +49,16 @@ export default function SettingsStack() {
 				name="GeneralSettings"
 				component={GeneralSettingsScreen}
 				options={{ header: () => <Header navBack title="General" /> }}
+			/>
+			<Stack.Screen
+				name="AccountLogin"
+				component={AccountLogin}
+				options={{ header: () => <Header navBack title="Account" /> }}
+			/>
+			<Stack.Screen
+				name="AccountProfile"
+				component={AccountProfile}
+				options={{ header: () => <Header navBack title="Account" /> }}
 			/>
 			<Stack.Screen
 				name="LibrarySettings"
@@ -129,6 +148,8 @@ export type SettingsStackParamList = {
 	Settings: undefined;
 	// Client
 	GeneralSettings: undefined;
+	AccountLogin: undefined;
+	AccountProfile: undefined;
 	LibrarySettings: undefined;
 	AppearanceSettings: undefined;
 	PrivacySettings: undefined;
