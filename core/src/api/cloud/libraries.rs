41e21a375a018b1ee473d108606ddf832f7b8ed1
Arnab Chakraborty <11457760+Rocky43007@users.noreply.github.com>
2024-08-07 23:14:56 +0300

    Forgot library delete D:

--- core/src/api/cloud/libraries.rs
+++ core/src/api/cloud/libraries.rs
@@ -2,7 +2,6 @@ use crate::{api::utils::library, invalidate_query};
 use rspc::alpha::AlphaRouter;
 use sd_cloud_schema::{auth::AccessToken, devices, libraries};
 use tracing::debug;
-use uuid::Uuid;
 
 use crate::{
 	api::{Ctx, R},
@@ -49,7 +48,7 @@ pub fn mount() -> AlphaRouter<Ctx> {
 			}
 
 			R.with2(library())
-				.mutation(|(node, library), args: LibrariesCreateArgs | async move {
+				.mutation(|(node, library), args: LibrariesCreateArgs| async move {
 					let req = libraries::create::Request {
 						name: library.config().await.name.to_string(),
 						access_token: args.access_token,
@@ -67,75 +66,17 @@ pub fn mount() -> AlphaRouter<Ctx> {
 					Ok(())
 				})
 		})
-		.procedure("join", {
-			R.mutation(|node, library_id: Uuid| async move {
-				// let Some(cloud_library) =
-				// 	sd_cloud_api::library::get(node.cloud_api_config().await, library_id).await?
-				// else {
-				// 	return Err(rspc::Error::new(
-				// 		rspc::ErrorCode::NotFound,
-				// 		"Library not found".to_string(),
-				// 	));
-				// };
+		.procedure("delete", {
+			R.mutation(|node, req: libraries::delete::Request| async move {
+				super::handle_comm_error(
+					try_get_cloud_services_client!(node)?
+						.libraries()
+						.delete(req)
+						.await,
+					"Failed to delete library;",
+				)??;
 
-				// let library = node
-				// 	.libraries
-				// 	.create_with_uuid(
-				// 		library_id,
-				// 		LibraryName::new(cloud_library.name).map_err(|e| {
-				// 			rspc::Error::new(rspc::ErrorCode::InternalServerError, e.to_string())
-				// 		})?,
-				// 		None,
-				// 		false,
-				// 		None,
-				// 		&node,
-				// 		true,
-				// 	)
-				// 	.await?;
-				// node.libraries
-				// 	.edit(
-				// 		library.id,
-				// 		None,
-				// 		MaybeUndefined::Undefined,
-				// 		MaybeUndefined::Value(cloud_library.id),
-				// 		None,
-				// 	)
-				// 	.await?;
-
-				// let node_config = node.config.get().await;
-				// let instances = sd_cloud_api::library::join(
-				// 	node.cloud_api_config().await,
-				// 	library_id,
-				// 	library.instance_uuid,
-				// 	library.identity.to_remote_identity(),
-				// 	node_config.id,
-				// 	node_config.identity.to_remote_identity(),
-				// 	node.p2p.peer_metadata(),
-				// )
-				// .await?;
-
-				// for instance in instances {
-				// 	crate::cloud::sync::receive::upsert_instance(
-				// 		library.id,
-				// 		&library.db,
-				// 		&library.sync,
-				// 		&node.libraries,
-				// 		&instance.uuid,
-				// 		instance.identity,
-				// 		&instance.node_id,
-				// 		RemoteIdentity::from_str(&instance.node_remote_identity)
-				// 			.expect("malformed remote identity in the DB"),
-				// 		instance.metadata,
-				// 	)
-				// 	.await?;
-				// }
-
-				// invalidate_query!(library, "cloud.library.get");
-				// invalidate_query!(library, "cloud.library.list");
-
-				// Ok(LibraryConfigWrapped::from_library(&library).await)
-
-				debug!("TODO: Functionality not implemented. Joining will be removed in the future, but for now, it's a no-op");
+				debug!("Deleted library");
 
 				Ok(())
 			})
