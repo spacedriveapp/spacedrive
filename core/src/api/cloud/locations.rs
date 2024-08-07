8718de0928d4fff42ba1949725ecb8d0a81e835e
Ericson Soares <ericson.ds999@gmail.com>
2024-08-07 03:21:01 -0300

    Some new abstractions and tweaks to develop cloud services calls

---
+++ core/src/api/cloud/locations.rs
@@ -0,0 +1,28 @@
+use crate::api::{Ctx, R};
+
+use rspc::alpha::AlphaRouter;
+
+pub fn mount() -> AlphaRouter<Ctx> {
+	R.router()
+		.procedure("list", {
+			R.query(|node, _: ()| async move {
+				sd_cloud_api::locations::list(node.cloud_api_config().await)
+					.await
+					.map_err(Into::into)
+			})
+		})
+		.procedure("create", {
+			R.mutation(|node, name: String| async move {
+				sd_cloud_api::locations::create(node.cloud_api_config().await, name)
+					.await
+					.map_err(Into::into)
+			})
+		})
+		.procedure("remove", {
+			R.mutation(|node, id: String| async move {
+				sd_cloud_api::locations::create(node.cloud_api_config().await, id)
+					.await
+					.map_err(Into::into)
+			})
+		})
+}
