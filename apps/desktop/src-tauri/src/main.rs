47a582864b07d8f9d5b2e8953a4950970fd97d45
Arnab Chakraborty <11457760+Rocky43007@users.noreply.github.com>
2024-08-15 23:22:15 +0300

    Event listener actually listens

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -225,6 +225,16 @@ async fn main() -> tauri::Result<()> {
 		.setup(move |app| {
 			// We need a the app handle to determine the data directory now.
 			// This means all the setup code has to be within `setup`, however it doesn't support async so we `block_on`.
+			let handle = app.handle().clone();
+			app.listen("deep-link://new-url", move |event| {
+				let deep_link_event = DeepLinkEvent {
+					data: event.payload().to_string(),
+				};
+				println!("Deep link event={:?}", deep_link_event);
+
+				handle.emit("deeplink", deep_link_event).unwrap();
+			});
+
 			block_in_place(|| {
 				block_on(async move {
 					builder.mount_events(app);
@@ -244,15 +254,15 @@ async fn main() -> tauri::Result<()> {
 						Err(err) => (None, Err(NodeError::Logger(err))),
 					};
 
-					let handle = app.handle().clone();
-					app.listen("deep-link://new-url", move |event| {
-						let deep_link_event = DeepLinkEvent {
-							data: event.payload().to_string(),
-						};
-						debug!(?deep_link_event, "Deep link event;",);
+					// let handle = app.handle().clone();
+					// app.listen("deep-link://new-url", move |event| {
+					// 	let deep_link_event = DeepLinkEvent {
+					// 		data: event.payload().to_string(),
+					// 	};
+					// 	debug!(?deep_link_event, "Deep link event;",);
 
-						handle.emit("deeplink", deep_link_event).unwrap();
-					});
+					// 	handle.emit("deeplink", deep_link_event).unwrap();
+					// });
 
 					let handle = app.handle();
 					let (node, router) = match result {
