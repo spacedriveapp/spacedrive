8cf67d0a0d0c386273594a12b54a5bdfda0a266a
Arnab Chakraborty <11457760+Rocky43007@users.noreply.github.com>
2024-08-30 07:36:11 -0400

    Update Tauri Version & Try fixing CORS issues

--- interface/app/$libraryId/settings/client/account/index.tsx
+++ interface/app/$libraryId/settings/client/account/index.tsx
@@ -18,14 +18,13 @@ type User = {
 export const Component = () => {
 	const { t } = useLocale();
 	const [userInfo, setUserInfo] = useState<User | null>(null);
-	const me = useBridgeQuery(['auth.me'], { retry: false });
-	const authStore = auth.useStateSnapshot();
 	useEffect(() => {
 		async function _() {
 			const user_data = await fetch('http://localhost:9420/api/user', {
 				method: 'GET'
 			});
 			const data = await user_data.json();
+			console.log("Data from user (auth API)", data);
 			return data;
 		}
 		_().then((data) => {
@@ -63,110 +62,107 @@ export const Component = () => {
 				description={t('spacedrive_cloud_description')}
 			/>
 			<div className="flex flex-col justify-between gap-5 lg:flex-row">
-				{userInfo === null ? (
-					<Tabs />
-				) : (
-					<Profile authStore={authStore} email={userInfo.email} />
-				)}
+				{userInfo === null ? <Tabs /> : <Profile email={userInfo.email} />}
 			</div>
-			{useFeatureFlag('hostedLocations') && <HostedLocationsPlayground />}
+			{/* {useFeatureFlag('hostedLocations') && <HostedLocationsPlayground />} */}
 		</>
 	);
 };
 
-function HostedLocationsPlayground() {
-	const locations = useBridgeQuery(['cloud.locations.list'], { retry: false });
+// Not supporting this feature for now
+// function HostedLocationsPlayground() {
+// 	const locations = useBridgeQuery(['cloud.locations.list'], { retry: false });
 
-	const [locationName, setLocationName] = useState('');
-	const [path, setPath] = useState('');
-	const createLocation = useBridgeMutation('cloud.locations.create', {
-		onSuccess(data) {
-			// console.log('DATA', data); // TODO: Optimistic UI
+// 	const [locationName, setLocationName] = useState('');
+// 	const [path, setPath] = useState('');
+// 	const createLocation = useBridgeMutation('cloud.locations.create', {
+// 		onSuccess(data) {
+// 			// console.log('DATA', data); // TODO: Optimistic UI
 
-			locations.refetch();
-			setLocationName('');
-		}
-	});
-	const removeLocation = useBridgeMutation('cloud.locations.remove', {
-		onSuccess() {
-			// TODO: Optimistic UI
+// 			locations.refetch();
+// 			setLocationName('');
+// 		}
+// 	});
+// 	const removeLocation = useBridgeMutation('cloud.locations.remove', {
+// 		onSuccess() {
+// 			// TODO: Optimistic UI
 
-			locations.refetch();
-		}
-	});
+// 			locations.refetch();
+// 		}
+// 	});
 
-	useEffect(() => {
-		if (path === '' && locations.data?.[0]) {
-			setPath(`location/${locations.data[0].id}/hello.txt`);
-		}
-	}, [path, locations.data]);
+// 	useEffect(() => {
+// 		if (path === '' && locations.data?.[0]) {
+// 			setPath(`location/${locations.data[0].id}/hello.txt`);
+// 		}
+// 	}, [path, locations.data]);
 
-	const isLoading = createLocation.isLoading || removeLocation.isLoading;
+// 	const isLoading = createLocation.isLoading || removeLocation.isLoading;
 
-	return (
-		<>
-			<Heading
-				rightArea={
-					<div className="flex-row space-x-2">
-						{/* TODO: We need UI for this. I wish I could use `prompt` for now but Tauri doesn't have it :( */}
-						<div className="flex flex-row space-x-4">
-							<Input
-								className="grow"
-								value={locationName}
-								onInput={(e) => setLocationName(e.currentTarget.value)}
-								placeholder="My sick location"
-								disabled={isLoading}
-							/>
+// 	return (
+// 		<>
+// 			<Heading
+// 				rightArea={
+// 					<div className="flex-row space-x-2">
+// 						{/* TODO: We need UI for this. I wish I could use `prompt` for now but Tauri doesn't have it :( */}
+// 						<div className="flex flex-row space-x-4">
+// 							<Input
+// 								className="grow"
+// 								value={locationName}
+// 								onInput={(e) => setLocationName(e.currentTarget.value)}
+// 								placeholder="My sick location"
+// 								disabled={isLoading}
+// 							/>
 
-							<Button
-								variant="accent"
-								size="sm"
-								onClick={() => {
-									if (locationName === '') return;
-									createLocation.mutate(locationName);
-								}}
-								disabled={isLoading}
-							>
-								Create Location
-							</Button>
-						</div>
-					</div>
-				}
-				title="Hosted Locations"
-				description="Augment your local storage with our cloud!"
-			/>
+// 							<Button
+// 								variant="accent"
+// 								size="sm"
+// 								onClick={() => {
+// 									if (locationName === '') return;
+// 									createLocation.mutate(locationName);
+// 								}}
+// 								disabled={isLoading}
+// 							>
+// 								Create Location
+// 							</Button>
+// 						</div>
+// 					</div>
+// 				}
+// 				title="Hosted Locations"
+// 				description="Augment your local storage with our cloud!"
+// 			/>
 
-			{/* TODO: Cleanup this mess + styles */}
-			{locations.status === 'loading' ? <div>Loading!</div> : null}
-			{locations.status !== 'loading' && locations.data?.length === 0 ? (
-				<div>Looks like you don't have any!</div>
-			) : (
-				<div>
-					{locations.data?.map((location) => (
-						<div key={location.id} className="flex flex-row space-x-5">
-							<h1>{location.name}</h1>
-							<Button
-								variant="accent"
-								size="sm"
-								onClick={() => removeLocation.mutate(location.id)}
-								disabled={isLoading}
-							>
-								Delete
-							</Button>
-						</div>
-					))}
-				</div>
-			)}
+// 			{/* TODO: Cleanup this mess + styles */}
+// 			{locations.status === 'loading' ? <div>Loading!</div> : null}
+// 			{locations.status !== 'loading' && locations.data?.length === 0 ? (
+// 				<div>Looks like you don't have any!</div>
+// 			) : (
+// 				<div>
+// 					{locations.data?.map((location) => (
+// 						<div key={location.id} className="flex flex-row space-x-5">
+// 							<h1>{location.name}</h1>
+// 							<Button
+// 								variant="accent"
+// 								size="sm"
+// 								onClick={() => removeLocation.mutate(location.id)}
+// 								disabled={isLoading}
+// 							>
+// 								Delete
+// 							</Button>
+// 						</div>
+// 					))}
+// 				</div>
+// 			)}
 
-			<div>
-				<p>Path to save when clicking 'Do the thing':</p>
-				<Input
-					className="grow"
-					value={path}
-					onInput={(e) => setPath(e.currentTarget.value)}
-					disabled={isLoading}
-				/>
-			</div>
-		</>
-	);
-}
+// 			<div>
+// 				<p>Path to save when clicking 'Do the thing':</p>
+// 				<Input
+// 					className="grow"
+// 					value={path}
+// 					onInput={(e) => setPath(e.currentTarget.value)}
+// 					disabled={isLoading}
+// 				/>
+// 			</div>
+// 		</>
+// 	);
+// }
