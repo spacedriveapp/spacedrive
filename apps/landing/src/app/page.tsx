61c5105bb28de604e6c1e6d32ec47190b920b561
lynx <141365347+iLynxcat@users.noreply.github.com>
2024-08-31 16:43:06 -0500

    The return of `&nbsp;`

--- apps/landing/src/app/page.tsx
+++ apps/landing/src/app/page.tsx
@@ -8,6 +8,8 @@ import { toTitleCase } from '~/utils/misc';
 
 import { getLatestRelease, getReleaseFrontmatter, githubFetch } from './api/github';
 
+const NBSP = '\xA0';
+
 export const metadata = {
 	title: 'Spacedrive — A file manager from the future.',
 	description:
@@ -50,9 +52,9 @@ export default async function Page() {
 					href={`https://github.com/spacedriveapp/spacedrive`}
 				/>
 				<h1 className="fade-in-heading z-30 mb-3 bg-gradient-to-b from-[#EFF1FB] to-[#B8CEE0] bg-[length:100%_50%] bg-clip-text bg-repeat px-2 text-center text-4xl font-bold leading-[1.15] tracking-tight text-transparent md:text-5xl lg:text-6xl">
-					Sync, manage, and discover.
+					{`Sync, manage, and discover.`.replaceAll(' ', NBSP)}
 					<br />
-					Across all your devices.
+					{`Across all your devices.`.replaceAll(' ', NBSP)}
 				</h1>
 				<p className="animation-delay-1 fade-in-heading text-md leading-2 z-30 mb-8 mt-1 max-w-4xl text-center text-gray-450 lg:text-lg lg:leading-8">
 					Your files, always within reach. Experience seamless synchronization, intuitive
