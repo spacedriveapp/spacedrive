729582851c9dd7f33892e8944bc6628cce11e002
Arnab Chakraborty <11457760+Rocky43007@users.noreply.github.com>
2024-08-31 09:20:56 -0400

    Update CTA Button Styling, again

--- apps/landing/src/components/cta-buttons/Button.tsx
+++ apps/landing/src/components/cta-buttons/Button.tsx
@@ -5,7 +5,7 @@ export function DownloadButton({ name, link }: { name: string; link: string }) {
 	return (
 		<Link
 			href={link}
-			className="z-30 inline-flex cursor-pointer items-center justify-center gap-[8px] rounded-[12px] border-[1.5px] border-[#88D7FF] px-[16px] py-[10px] shadow-[0px_4px_5px_0px_rgba(168,213,255,0.25),0px_0px_39.7px_0px_rgba(75,173,255,0.50)]"
+			className="z-30 inline-flex cursor-pointer items-center justify-center gap-x-[8px] rounded-[12px] border-[1.5px] border-[#88D7FF] px-[16px] py-[10px] shadow-[0px_4px_5px_0px_rgba(168,213,255,0.25),0px_0px_39.7px_0px_rgba(75,173,255,0.50)]"
 			style={{
 				backgroundImage: `url('images/misc/NoisePattern.png'), linear-gradient(180deg, #42B2FD -22.29%, #0078F0 99.3%)`,
 				backgroundColor: 'lightgray',
@@ -15,8 +15,10 @@ export function DownloadButton({ name, link }: { name: string; link: string }) {
 				backgroundBlendMode: 'overlay, normal'
 			}}
 		>
-			<ArrowCircleDown />
-			Download for {name}
+			<ArrowCircleDown fill="white" height={20} width={20} />
+			<p className="text-center text-[16px] font-[600] leading-normal text-white">
+				Download for {name}
+			</p>
 		</Link>
 	);
 }
--- apps/landing/src/components/discord-button.tsx
+++ apps/landing/src/components/discord-button.tsx
@@ -5,7 +5,7 @@ export function DiscordButton() {
 	return (
 		<Link
 			href="https://discord.gg/gTaF2Z44f5"
-			className="z-30 inline-flex cursor-pointer items-center justify-center gap-[8px] rounded-[12px] px-[16px] py-[10px] text-ink-faint"
+			className="z-30 inline-flex cursor-pointer items-center justify-center gap-x-[8px] rounded-[12px] px-[16px] py-[10px] text-ink-faint"
 			style={{
 				backgroundColor: 'rgba(33, 52, 72, 0.46)',
 				backgroundImage: `url('images/misc/NoisePattern.png')`,
@@ -16,8 +16,10 @@ export function DiscordButton() {
 				border: '1.5px rgba(73, 94, 115, 0.40)'
 			}}
 		>
-			<Discord />
-			Chat on Discord
+			<Discord fill="white" className="opacity-60" />
+			<p className="text-center text-[16px] font-[600] leading-normal text-white opacity-80">
+				Chat on Discord
+			</p>
 		</Link>
 	);
 }
