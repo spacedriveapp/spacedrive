978195994e02b7f0d4251dbd3d18eb922432da9d
lynx <141365347+iLynxcat@users.noreply.github.com>
2024-09-01 05:04:22 -0500

    better desktop navbar

--- apps/landing/src/components/global-nav-bar/index.tsx
+++ apps/landing/src/components/global-nav-bar/index.tsx
@@ -1,16 +1,29 @@
 'use client';
 
 import { List, X } from '@phosphor-icons/react';
+import clsx from 'clsx';
 import { AnimatePresence, motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
-import { PropsWithChildren, useState } from 'react';
+import { ReactNode, useState } from 'react';
 import appFullLogo from '~/assets/app_full_logo.svg?url';
 import { CtaPrimaryButton } from '~/components/cta-primary-button';
 
 import '~/styles/navbar.css';
 
 import { useCurrentPlatform } from '~/utils/current-platform';
+import { ExternalLinkRegex } from '~/utils/regex-external-link';
+
+const NAVIGATION_ITEMS: { label: string; href: string; adornment?: string }[] = [
+	{ label: 'Explorer', href: '/' },
+	{ label: 'Cloud', href: '/cloud' },
+	{ label: 'Teams', href: '/teams' },
+	{ label: 'Assistant', href: '/assistant', adornment: 'New' },
+	{ label: 'Store', href: '/store' },
+	{ label: 'Use Cases', href: '/use-cases' },
+	{ label: 'Blog', href: '/blog' },
+	{ label: 'Docs', href: '/docs' }
+];
 
 export function NavBar() {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -21,41 +34,36 @@ export function NavBar() {
 		<>
 			{/* Main Navbar */}
 			<motion.nav
-				className="fixed z-[110] w-full p-4"
+				className={clsx(
+					'fixed top-0 z-[110] mx-4 mt-2 w-[calc(100%-2rem)]',
+					'overflow-hidden rounded-xl bg-[#141419]/95 shadow-[0px_-10px_20px_0px_rgba(40,134,213,0.05)] backdrop-blur backdrop-saturate-[1.8]'
+				)}
+				style={{
+					border: '1px rgba(30, 30, 38, 0.00)'
+				}}
 				initial={{ opacity: 1 }}
 				animate={{ opacity: isMenuOpen ? 0 : 1 }}
 				transition={{ duration: 0.2 }}
 			>
-				<div
-					className="noise noise-strong flex w-full min-w-[300px] items-center justify-between overflow-hidden rounded-[10px] bg-[#141419] px-[24px] py-[12px] shadow-[0px_-10px_20px_0px_rgba(40,134,213,0.05)]"
-					style={{
-						border: '1px rgba(30, 30, 38, 0.00)'
-					}}
-				>
+				<div className="noise noise-faded noise-sm flex flex-wrap items-center justify-between gap-x-8 overflow-hidden px-6 py-3">
 					{/* Spacedrive Logo and Links */}
-					<div className="flex items-center gap-[20px]">
-						<Link href="/" className="flex flex-row items-center gap-1">
+					<div className="flex items-center gap-[1.125rem]">
+						<Link href="/">
 							<Image
 								alt="Spacedrive"
 								src={appFullLogo}
 								width={200}
 								height={55}
-								className="z-30 mr-[6px] h-[3.5rem] w-auto"
+								className="z-30 mr-[6px] h-[3.5rem] w-auto select-none"
 							/>
 						</Link>
-						<div className="hidden items-center gap-[10px] whitespace-nowrap xl:flex">
-							<NavLink link="/explorer">Explorer</NavLink>
-							<NavLink link="/cloud">Cloud</NavLink>
-							<NavLink link="/teams">Teams</NavLink>
-							<NavLink link="/assistant">
-								Assistant <NewIcon />
-							</NavLink>
-							<NavLink link="/store">Store</NavLink>
-							<NavLink link="/use-cases">Use Cases</NavLink>
-							<NavLink link="/blog">Blog</NavLink>
-							<NavLink link="/docs/product/getting-started/introduction">
-								Docs
-							</NavLink>
+
+						<div className="hidden items-center whitespace-nowrap xl:flex">
+							{NAVIGATION_ITEMS.map(({ label, href, adornment }) => (
+								<NavLink key={`nav-main-${label}-${href}`} href={href}>
+									{label} {adornment && <AdornmentBadge />}
+								</NavLink>
+							))}
 						</div>
 					</div>
 
@@ -111,18 +119,11 @@ export function NavBar() {
 
 							{/* Nav Links */}
 							<div className="flex flex-col items-start space-y-4 p-4">
-								<NavLink link="/explorer">Explorer</NavLink>
-								<NavLink link="/cloud">Cloud</NavLink>
-								<NavLink link="/teams">Teams</NavLink>
-								<NavLink link="/assistant">
-									Assistant <NewIcon />
-								</NavLink>
-								<NavLink link="/store">Store</NavLink>
-								<NavLink link="/use-cases">Use Cases</NavLink>
-								<NavLink link="/blog">Blog</NavLink>
-								<NavLink link="/docs/product/getting-started/introduction">
-									Docs
-								</NavLink>
+								{NAVIGATION_ITEMS.map(({ label, href, adornment }) => (
+									<NavLink key={`nav-sub-${label}-${href}`} href={href}>
+										{label} {adornment && <AdornmentBadge />}
+									</NavLink>
+								))}
 								<CtaPrimaryButton glow={'sm'} platform={currentPlatform} />
 							</div>
 						</motion.div>
@@ -133,22 +134,32 @@ export function NavBar() {
 	);
 }
 
-function NavLink(props: PropsWithChildren<{ link?: string }>) {
+interface NavLinkProps {
+	href: string;
+	target?: string;
+	children: ReactNode;
+}
+
+function NavLink({
+	href,
+	target = href.match(ExternalLinkRegex)?.length ? '_blank' : undefined,
+	children
+}: NavLinkProps) {
 	return (
 		<Link
-			href={props.link ?? '#'}
-			target={props.link?.startsWith('http') ? '_blank' : undefined}
-			className="inline-flex cursor-pointer items-center justify-center gap-[6px] p-4 text-[11pt] text-gray-300 no-underline transition hover:text-gray-50"
+			href={href ?? '#'}
+			target={target}
+			className="inline-flex cursor-pointer flex-row items-center justify-center gap-x-1.5 px-2.5 py-2 text-base text-gray-300 no-underline transition hover:text-gray-50"
 			rel="noreferrer"
 		>
-			{props.children}
+			{children}
 		</Link>
 	);
 }
 
-function NewIcon() {
+function AdornmentBadge() {
 	return (
-		<div className="flex items-center justify-center rounded-[4px] bg-[#3397EB] px-1 py-[0.01rem] text-xs">
+		<div className="flex items-center justify-center rounded bg-[#3397EB] px-1 py-0.5 align-middle text-xs font-bold leading-none text-white">
 			NEW
 		</div>
 	);
