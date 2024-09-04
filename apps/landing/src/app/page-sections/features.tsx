68d66b0cb2fde130946a2cea43f17567cfd69582
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2024-09-04 18:50:33 +0300

    new art & more

--- apps/landing/src/app/page-sections/features.tsx
+++ apps/landing/src/app/page-sections/features.tsx
@@ -4,11 +4,10 @@ import React from 'react';
 
 export const Features = () => {
 	return (
-		<div className="relative mx-auto flex h-auto w-full max-w-[1200px] flex-col flex-wrap p-4 md:flex-row">
+		<div className="relative mx-auto flex h-auto w-full max-w-[1200px] flex-col flex-wrap gap-14 p-4 md:flex-row md:gap-0">
 			{/** Lines & middle circle */}
-			<div className="absolute inset-x-0 mx-auto hidden h-[90%] w-px bg-gradient-to-b from-transparent via-[#6C708F] to-transparent md:flex" />
-			<div className="absolute hidden h-px w-full self-center bg-gradient-to-r from-transparent via-[#6C708F] to-transparent md:flex" />
-			<div className="absolute inset-0 z-10 mx-auto hidden size-3 self-center rounded-full bg-[#636783] md:flex" />
+			<div className="absolute inset-x-0 mx-auto hidden h-[90%] w-px bg-gradient-to-b from-transparent via-[#6C708F]/30 to-transparent md:flex" />
+			<div className="absolute hidden h-px w-full self-center bg-gradient-to-r from-transparent via-[#6C708F]/30 to-transparent md:flex" />
 			{/** Features */}
 			{info.map((item, index) => (
 				<Feature
@@ -27,27 +26,22 @@ interface Props {
 	imageSrc: string;
 	className?: string;
 	titleClassName?: string;
-	size?: { width: number; height: number };
+	size?: { width: number; height?: number };
+	scale?: number;
 }
 
 const Feature = ({ title, description, className, titleClassName, imageSrc, size }: Props) => {
 	const imageSize = size ?? { width: 500, height: 500 };
 	return (
-		<div className={clsx('flex h-[580px] flex-[50%] flex-col gap-3 pl-16 pt-16', className)}>
+		<div className={clsx('flex h-[580px] flex-[50%] flex-col gap-3 pt-16 md:pl-16', className)}>
 			<div className="flex flex-col gap-1">
 				<h1 className={clsx('text-2xl font-semibold', titleClassName)}>{title}</h1>
 				<p className="w-full max-w-[390px] text-ink-faint">{description}</p>
 			</div>
 			{/* Container needed to force <Image> into custom sizes */}
-			<div
-				className="w-full mx-auto"
-				style={{
-					width: imageSize.width,
-					height: imageSize.height
-				}}
-			>
+			<div className="w-auto h-auto mx-auto">
 				<Image
-					className="px-8 mt-6"
+					className="px-8 mt-8"
 					loading="eager"
 					layout="responsive"
 					width={imageSize.width}
@@ -65,7 +59,7 @@ const info: {
 	title: string;
 	description: string;
 	imageSrc: string;
-	size?: { width: number; height?: number };
+	scale?: number;
 }[] = [
 	{
 		title: 'Spacedrop',
@@ -75,10 +69,6 @@ const info: {
 	},
 	{
 		title: 'Tags',
-		size: {
-			width: 340,
-			height: 400
-		},
 		description:
 			'Organize and find your files faster by assigning custom tags to your folders and documents. Simplify your data management with easy categorization.',
 		imageSrc: '/images/bento/tags.webp'
@@ -93,6 +83,6 @@ const info: {
 		title: 'Extensions',
 		description:
 			'Install add-ons to customize Spacedrive with extra features and integrations, tailoring it to your unique workflow.',
-		imageSrc: ''
+		imageSrc: '/images/bento/extensions.webp'
 	}
 ];
