ed8c59eb02c3bd2eb6a82dfea6b4b7fcd6ecb4a6
Ericson Soares <ericson.ds999@gmail.com>
2024-09-10 23:04:50 -0300

    Adding accepted TLS signature verifiers

--- core/crates/cloud-services/src/client.rs
+++ core/crates/cloud-services/src/client.rs
@@ -197,7 +197,21 @@ impl CloudServices {
 					}
 
 					fn supported_verify_schemes(&self) -> Vec<rustls::SignatureScheme> {
-						vec![]
+						vec![
+							rustls::SignatureScheme::RSA_PKCS1_SHA1,
+							rustls::SignatureScheme::ECDSA_SHA1_Legacy,
+							rustls::SignatureScheme::RSA_PKCS1_SHA256,
+							rustls::SignatureScheme::ECDSA_NISTP256_SHA256,
+							rustls::SignatureScheme::RSA_PKCS1_SHA384,
+							rustls::SignatureScheme::ECDSA_NISTP384_SHA384,
+							rustls::SignatureScheme::RSA_PKCS1_SHA512,
+							rustls::SignatureScheme::ECDSA_NISTP521_SHA512,
+							rustls::SignatureScheme::RSA_PSS_SHA256,
+							rustls::SignatureScheme::RSA_PSS_SHA384,
+							rustls::SignatureScheme::RSA_PSS_SHA512,
+							rustls::SignatureScheme::ED25519,
+							rustls::SignatureScheme::ED448,
+						]
 					}
 				}
 
