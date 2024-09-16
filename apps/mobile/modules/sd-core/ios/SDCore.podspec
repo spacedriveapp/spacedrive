043a11f0b95d42c737bc9e24553e7a58f94bbf19
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2024-09-16 17:13:51 -0300

    Finally ios successfully compiles with ffmpeg enabled  - Change SDCore.podspec to add extra libraries required by ffmpeg  - Fix heif linking for ios in config.toml template  - Add symlink logic for extra libraries required to compile ios in build-rust.sh

--- apps/mobile/modules/sd-core/ios/SDCore.podspec
+++ apps/mobile/modules/sd-core/ios/SDCore.podspec
@@ -5,8 +5,8 @@ Pod::Spec.new do |s|
   s.version = "0.0.0"
   s.summary = "Spacedrive core for React Native"
   s.description = "Spacedrive core for React Native"
-  s.author = "Oscar Beaumont"
-  s.license = "APGL-3.0"
+  s.author = "Spacedrive Technology Inc"
+  s.license = "AGPL-3.0"
   s.platform = :ios, "14.0"
   s.source = { git: "https://github.com/spacedriveapp/spacedrive" }
   s.homepage = "https://www.spacedrive.com"
@@ -25,10 +25,25 @@ Pod::Spec.new do |s|
     :execution_position => :before_compile,
   }
 
+  # Add libraries
+  ffmpeg_libraries = [
+    "-lmp3lame", "-lsoxr", "-ltheora", "-lopus", "-lvorbisenc", "-lx265",
+    "-lpostproc", "-ltheoraenc", "-ltheoradec", "-lde265", "-lvorbisfile",
+    "-logg", "-lSvtAv1Enc", "-lvpx", "-lhdr10plus", "-lx264", "-lvorbis",
+    "-lzimg", "-lsoxr-lsr", "-liconv", "-lbz2", "-llzma"
+  ].join(' ')
+
+  # Add frameworks
+  ffmpeg_frameworks = [
+    "-framework AudioToolbox",
+    "-framework VideoToolbox",
+    "-framework AVFoundation"
+  ].join(' ')
+
   s.xcconfig = {
     "LIBRARY_SEARCH_PATHS" => '"' + JSON.parse(`cargo metadata`)["target_directory"].to_s + '"',
-    "OTHER_LDFLAGS[sdk=iphoneos*]" => "$(inherited) -lsd_mobile_ios",
-    "OTHER_LDFLAGS[sdk=iphonesimulator*]" => "$(inherited) -lsd_mobile_iossim",
+    "OTHER_LDFLAGS[sdk=iphoneos*]" => "$(inherited) -lsd_mobile_ios #{ffmpeg_libraries} #{ffmpeg_frameworks}",
+    "OTHER_LDFLAGS[sdk=iphonesimulator*]" => "$(inherited) -lsd_mobile_iossim #{ffmpeg_libraries} #{ffmpeg_frameworks}",
   }
 
   s.source_files = "**/*.{h,m,mm,swift,hpp,cpp}"
--- apps/mobile/modules/sd-core/ios/build-rust.sh
+++ apps/mobile/modules/sd-core/ios/build-rust.sh
@@ -13,6 +13,22 @@ err() {
   exit 1
 }
 
+symlink_libs() {
+  if [ $# -ne 2 ]; then
+    err "Invalid number of arguments. Usage: symlink_libs <dir1> <dir2>"
+  fi
+
+  if [ ! -d "$1" ]; then
+    err "Directory '$1' does not exist."
+  fi
+
+  if [ ! -d "$2" ]; then
+    err "Directory '$2' does not exist."
+  fi
+
+  find "$1" -type f -name '*.a' -exec ln -sf "{}" "$2" \;
+}
+
 if [ -z "${HOME:-}" ]; then
   HOME="$(CDPATH='' cd -- "$(osascript -e 'set output to (POSIX path of (path to home folder))')" && pwd -P)"
   export HOME
@@ -21,35 +37,47 @@ fi
 echo "Building 'sd-mobile-ios' library..."
 
 __dirname="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)"
+DEPS="${__dirname}/../../../.deps/"
+DEPS="$(CDPATH='' cd -- "$DEPS" && pwd -P)"
+CARGO_CONFIG="${__dirname}/../../../../../.cargo"
+CARGO_CONFIG="$(CDPATH='' cd -- "$CARGO_CONFIG" && pwd -P)/config.toml"
 
 # Ensure target dir exists
 TARGET_DIRECTORY="${__dirname}/../../../../../target"
 mkdir -p "$TARGET_DIRECTORY"
 TARGET_DIRECTORY="$(CDPATH='' cd -- "$TARGET_DIRECTORY" && pwd -P)"
 
+TARGET_CONFIG=debug
 if [ "${CONFIGURATION:-}" = "Release" ]; then
   set -- --release
+  TARGET_CONFIG=release
 fi
 
-# Reset Path to sane value to allow rust to compile
-export PATH
-PATH="${CARGO_HOME:-"${HOME}/.cargo"}/bin:$(brew --prefix)/bin:$(dirname "$(xcrun --find lipo)"):$(env -i /bin/bash --noprofile --norc -c 'echo $PATH')"
+trap 'if [ -e "${CARGO_CONFIG}.bak" ]; then mv "${CARGO_CONFIG}.bak" "$CARGO_CONFIG"; fi' EXIT
 
+# Required for `cargo` to correctly compile the library
+RUST_PATH="${CARGO_HOME:-"${HOME}/.cargo"}/bin:$(brew --prefix)/bin:$(env -i /bin/bash --noprofile --norc -c 'echo $PATH')"
 if [ "${PLATFORM_NAME:-}" = "iphonesimulator" ]; then
   case "$(uname -m)" in
     "arm64" | "aarch64") # M series
-      cargo build -p sd-mobile-ios --target aarch64-apple-ios-sim "$@"
-      lipo -create -output "$TARGET_DIRECTORY"/libsd_mobile_iossim.a "$TARGET_DIRECTORY"/aarch64-apple-ios-sim/debug/libsd_mobile_ios.a
+      sed -i.bak "s|FFMPEG_DIR = { force = true, value = \".*\" }|FFMPEG_DIR = { force = true, value = \"${DEPS}/aarch64-apple-ios-sim\" }|" "$CARGO_CONFIG"
+      env CARGO_FEATURE_STATIC=1 PATH="$RUST_PATH" cargo build -p sd-mobile-ios --target aarch64-apple-ios-sim "$@"
+      lipo -create -output "$TARGET_DIRECTORY"/libsd_mobile_iossim.a "${TARGET_DIRECTORY}/aarch64-apple-ios-sim/${TARGET_CONFIG}/libsd_mobile_ios.a"
+      symlink_libs "${DEPS}/aarch64-apple-ios-sim/lib" "$TARGET_DIRECTORY"
       ;;
     "x86_64") # Intel
-      cargo build -p sd-mobile-ios --target x86_64-apple-ios "$@"
-      lipo -create -output "$TARGET_DIRECTORY"/libsd_mobile_iossim.a "$TARGET_DIRECTORY"/x86_64-apple-ios/debug/libsd_mobile_ios.a
+      sed -i.bak "s|FFMPEG_DIR = { force = true, value = \".*\" }|FFMPEG_DIR = { force = true, value = \"${DEPS}/x86_64-apple-ios\" }|" "$CARGO_CONFIG"
+      env CARGO_FEATURE_STATIC=1 PATH="$RUST_PATH" cargo build -p sd-mobile-ios --target x86_64-apple-ios "$@"
+      lipo -create -output "$TARGET_DIRECTORY"/libsd_mobile_iossim.a "${TARGET_DIRECTORY}/x86_64-apple-ios/${TARGET_CONFIG}/libsd_mobile_ios.a"
+      symlink_libs "${DEPS}/x86_64-apple-ios/lib" "$TARGET_DIRECTORY"
       ;;
     *)
       err 'Unsupported architecture.'
       ;;
   esac
 else
-  cargo build -p sd-mobile-ios --target aarch64-apple-ios "$@"
-  lipo -create -output "$TARGET_DIRECTORY"/libsd_mobile_ios.a "$TARGET_DIRECTORY"/aarch64-apple-ios/release/libsd_mobile_ios.a
+  sed -i.bak "s|FFMPEG_DIR = { force = true, value = \".*\" }|FFMPEG_DIR = { force = true, value = \"${DEPS}/aarch64-apple-ios\" }|" "$CARGO_CONFIG"
+  env CARGO_FEATURE_STATIC=1 PATH="$RUST_PATH" cargo build -p sd-mobile-ios --target aarch64-apple-ios "$@"
+  lipo -create -output "$TARGET_DIRECTORY"/libsd_mobile_ios.a "${TARGET_DIRECTORY}/aarch64-apple-ios/${TARGET_CONFIG}/libsd_mobile_ios.a"
+  symlink_libs "${DEPS}/aarch64-apple-ios/lib" "$TARGET_DIRECTORY"
 fi
