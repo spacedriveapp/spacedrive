eb20e992fb90335963231f6a0ae0634f81b7ee66
Matheus Consoli <matheus.consoli7@gmail.com>
2024-09-30 20:02:45 -0300

    Add new core crate

---
+++ core/crates/file-track/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn add(left: u64, right: u64) -> u64 {
+	panic!("it works");
+	left + right
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn it_works() {
+		let result = add(2, 2);
+		assert_eq!(result, 4);
+	}
+}
