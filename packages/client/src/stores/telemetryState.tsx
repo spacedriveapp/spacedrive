3fb7ffecd133110fcc0c03f8caf442b6a522e6f0
lynx <141365347+iLynxcat@users.noreply.github.com>
2024-10-02 09:33:52 -0700

    add telemetry state migration

--- packages/client/src/stores/telemetryState.tsx
+++ packages/client/src/stores/telemetryState.tsx
@@ -11,27 +11,75 @@ import { createPersistedMutable, useSolidStore } from '../solid';
  */
 export type PlausiblePlatformType = 'web' | 'mobile' | 'desktop' | 'unknown';
 
-type TelemetryState = {
-	telemetryLevelPreference: TelemetryLevelPreference;
+type TelemetryStateV0 = {
+	shareFullTelemetry: boolean;
 	platform: PlausiblePlatformType;
 	buildInfo: BuildInfo | undefined;
 };
 
+type TelemetryStateV1 = {
+	telemetryLevelPreference: TelemetryLevelPreference;
+	platform: PlausiblePlatformType;
+	buildInfo: BuildInfo | undefined;
+	stateVersion: 1;
+};
+
+export type TelemetryLevelPreference = 'full' | 'minimal' | 'none';
 export const TELEMETRY_LEVEL_PREFERENCES = [
 	'full',
 	'minimal',
 	'none'
 ] satisfies TelemetryLevelPreference[];
-export type TelemetryLevelPreference = 'full' | 'minimal' | 'none';
 
-export const telemetryState = createPersistedMutable(
+const DEFAULT_TELEMETRY_STATE: TelemetryStateV1 = {
+	telemetryLevelPreference: 'none',
+	platform: 'unknown',
+	buildInfo: undefined,
+	stateVersion: 1
+};
+
+export const telemetryState = createPersistedMutable<TelemetryStateV1>(
 	'sd-explorer-layout',
-	createMutable<TelemetryState>({
-		telemetryLevelPreference: 'none', // no telemetry by default
-		platform: 'unknown',
-		buildInfo: undefined
-	})
+	createMutable<TelemetryStateV1>(DEFAULT_TELEMETRY_STATE),
+	{
+		onLoad(value: unknown): TelemetryStateV1 {
+			if (isV1State(value)) return value;
+			if (isV0State(value)) return migrateV0ToV1(value);
+
+			// If the value is neither v0 nor v1, return the default state
+			return DEFAULT_TELEMETRY_STATE;
+		}
+	}
 );
+
+function isV0State(value: unknown): value is TelemetryStateV0 {
+	return (
+		typeof value === 'object' &&
+		value !== null &&
+		'shareFullTelemetry' in value &&
+		'platform' in value &&
+		'buildInfo' in value
+	);
+}
+
+function isV1State(value: unknown): value is TelemetryStateV1 {
+	return (
+		typeof value === 'object' &&
+		value !== null &&
+		'telemetryLevelPreference' in value &&
+		'platform' in value &&
+		'buildInfo' in value &&
+		'stateVersion' in value &&
+		value.stateVersion === 1
+	);
+}
+
+function migrateV0ToV1(value: TelemetryStateV0): TelemetryStateV1 {
+	return {
+		...DEFAULT_TELEMETRY_STATE,
+		telemetryLevelPreference: value.shareFullTelemetry ? 'full' : 'minimal'
+	};
+}
 
 export function useTelemetryState() {
 	return useSolidStore(telemetryState);
