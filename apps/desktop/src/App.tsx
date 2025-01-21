ab39525f364c3780e9339877467894cce797bb79
Arnab Chakraborty <11457760+Rocky43007@users.noreply.github.com>
2025-01-20 21:33:02 -0500

    Remove auth

--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -59,21 +59,22 @@ declare global {
 	}
 }
 
-SuperTokens.init({
-	appInfo: {
-		apiDomain: AUTH_SERVER_URL,
-		apiBasePath: '/api/auth',
-		appName: 'Spacedrive Auth Service'
-	},
-	cookieHandler: getCookieHandler,
-	windowHandler: getWindowHandler,
-	recipeList: [
-		Session.init({ tokenTransferMethod: 'header' }),
-		EmailPassword.init(),
-		ThirdParty.init(),
-		Passwordless.init()
-	]
-});
+// Disabling until sync is ready.
+// SuperTokens.init({
+// 	appInfo: {
+// 		apiDomain: AUTH_SERVER_URL,
+// 		apiBasePath: '/api/auth',
+// 		appName: 'Spacedrive Auth Service'
+// 	},
+// 	cookieHandler: getCookieHandler,
+// 	windowHandler: getWindowHandler,
+// 	recipeList: [
+// 		Session.init({ tokenTransferMethod: 'header' }),
+// 		EmailPassword.init(),
+// 		ThirdParty.init(),
+// 		Passwordless.init()
+// 	]
+// });
 
 const startupError = (window as any).__SD_ERROR__ as string | undefined;
 
