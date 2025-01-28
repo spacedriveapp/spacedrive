68fbf842dbde5e99b1128ed4177160d8f038c9a2
Arnab Chakraborty <11457760+Rocky43007@users.noreply.github.com>
2025-01-28 00:48:30 -0500

    Fix migrations

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -144,7 +144,7 @@ impl Node {
 					"https://relay.spacedrive.com/".to_string(),
 					"https://irohdns.spacedrive.com".to_string(),
 					"https://irohdns.spacedrive.com/pkarr".to_string(),
-					"https://cloud.spacedrive.com".to_string(),
+					"cloud.spacedrive.com".to_string(),
 				)
 			}
 		};
