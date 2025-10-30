058f9e59ad4bd28cf7c30c5dd0422c699ff32f6b
Jamie Pine <ijamespine@me.com>
2025-10-30 16:25:36 -0700

    refactor: remove LivePhoto and LivePhotoQuery modules from media/mod.rs

--- core/src/ops/media/mod.rs
+++ core/src/ops/media/mod.rs
@@ -6,10 +6,6 @@
 //! - Audio metadata extraction
 //! - Image optimization
 
-pub mod live_photo;
-pub mod live_photo_query;
 pub mod thumbnail;
 
-pub use live_photo::{LivePhoto, LivePhotoDetector};
-pub use live_photo_query::{LivePhotoPair, LivePhotoQuery};
 pub use thumbnail::ThumbnailJob;
