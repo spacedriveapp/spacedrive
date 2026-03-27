e36ee3a625a3354d20cd2e5e90dcc0f9ae558e8b
James Pine <ijamespine@me.com>
2026-03-26 21:10:31 -0700

    delete old ui

--- packages/ui/src/Divider.tsx
+++
@@ -1,3 +0,0 @@
-import { tw } from './utils';
-
-export const Divider = tw.div`bg-app-line/60 my-1 h-[1px] w-full`;
--- packages/ui/src/Layout.tsx
+++
@@ -1,5 +0,0 @@
-import { tw } from './utils';
-
-export const Card = tw.div`flex px-4 py-2 text-sm border rounded-md shadow-sm border-app-line bg-app-box`;
-
-export const GridLayout = tw.div`grid grid-cols-2 gap-3 lg:grid-cols-3`;
